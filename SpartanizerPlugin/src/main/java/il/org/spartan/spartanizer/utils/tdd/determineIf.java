d45729782be3bf9583f8fba42639b8dd38c7f4a3
orimarco <marcovitch.ori@gmail.com>
2016-10-31 22:40:45 +0200

    determineIf

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/determineIf.java
@@ -0,0 +1,8 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+/** @author Ori Marcovitch
+ * @since Oct 31, 2016 */
+public enum determineIf {
+  ;
+  // For you to implement! Let's TDD and get it on!
+}
