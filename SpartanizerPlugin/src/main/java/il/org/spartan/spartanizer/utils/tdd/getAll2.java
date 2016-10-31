c84d419cf5118e1ed6a2f479c5aee59dcc3c3804
orimarco <marcovitch.ori@gmail.com>
2016-10-31 22:09:07 +0200

    getAll2

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll2.java
@@ -0,0 +1,8 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+/** @author Ori Marcovitch
+ * @since Oct 31, 2016 */
+public enum getAll2 {
+  ;
+  // For you to implement! Let's TDD and get it on!
+}
