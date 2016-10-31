6ca10059750311f658028e28c4692938331823ec
orimarco <marcovitch.ori@gmail.com>
2016-10-31 15:11:50 +0200

    tdd find

---
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/find.java
@@ -0,0 +1,7 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+/** @author Ori Marcovitch
+ * @since Oct 31, 2016 */
+public interface find {
+  // For you to implement! Let's TDD and get it on!
+}
