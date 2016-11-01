62be235eee218eeb7be3f1cb4c46f58f5e3a6c57
dormaayn <dor.d.ma@gmail.com>
2016-11-01 20:04:10 +0200

    File transfer + formating (Issue #691)

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
@@ -9,12 +9,10 @@ import org.eclipse.jdt.core.dom.*;
  * @since Oct 31, 2016 */
 public enum getAll {
   ;
-  /**
-   * Get all the methods invoked in m
+  /** Get all the methods invoked in m
    * @param m JD
-   * @return List of the names of the methods
-   */
-  public static Set<String> invocations(MethodDeclaration m){
+   * @return List of the names of the methods */
+  public static Set<String> invocations(MethodDeclaration m) {
     return null;
   }
   // For you to implement! Let's TDD and get it on!
