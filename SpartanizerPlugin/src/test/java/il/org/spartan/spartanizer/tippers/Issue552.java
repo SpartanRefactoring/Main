320e9064a37cad144e25794b58a4e2e0a1e55c33
orimarco <marcovitch.ori@gmail.com>
2016-11-01 09:04:21 +0200

    sync

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue552.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue552.java
@@ -1,5 +1,7 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static org.junit.Assert.*;
+
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import org.junit.runners.*;
@@ -11,51 +13,57 @@ import il.org.spartan.spartanizer.utils.tdd.*;
  * @author Ori Marcovitch
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
-@SuppressWarnings({ "javadoc" }) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue552 {
-  // @Test public void a() {
-  // auxInt(enumerate.expressions((ASTNode) null));
-  // }
-  // @Test public void b() {
-  // assertEquals(1, enumerate.expressions(ast("a")));
-  // }
-  //
-  // @Test public void c() {
-  // assertEquals(3, enumerate.expressions(ast("a + b")));
-  // }
-  //
-  // @Test public void d() {
-  // assertEquals(0, enumerate.expressions(null));
-  // }
-  //
-  // @Test public void e() {
-  // assertEquals(3, enumerate.expressions(ast("b + c")));
-  // }
-  //
-  // @Test public void f() {
-  // assertEquals(4, enumerate.expressions(ast("return a + b + c;")));
-  // }
-  //
-  // @Test public void g() {
-  // assertEquals(4, enumerate.expressions(ast("if(a == null) return null;")));
-  // }
-  //
-  // @Test public void h() {
-  // assertEquals(4, enumerate.expressions(ast("while(true) print(i);")));
-  // }
-  //
-  // @Test public void i() {
-  // assertEquals(1, enumerate.expressions(ast("true")));
-  // }
-  //
-  // @Test public void j() {
-  // assertEquals(3, enumerate.expressions(ast("1 + 2")));
-  // }
+  @Test public void a0() {
+    @SuppressWarnings("unused") int x = enumerate.expressions(null);
+  }
+
+  @Test public void a() {
+    auxInt(enumerate.expressions((ASTNode) null));
+  }
+
+  @Test public void b() {
+    assertEquals(1, enumerate.expressions(ast("a")));
+  }
+
+  @Test public void c() {
+    assertEquals(3, enumerate.expressions(ast("a + b")));
+  }
+
+  @Test public void d() {
+    assertEquals(0, enumerate.expressions(null));
+  }
+
+  @Test public void e() {
+    assertEquals(4, enumerate.expressions(ast("a + b + c")));
+  }
+
+  @Test public void f() {
+    assertEquals(4, enumerate.expressions(ast("return a + b + c;")));
+  }
+
+  @Test public void g() {
+    assertEquals(4, enumerate.expressions(ast("if(a == null) return null;")));
+  }
+
+  @Test public void h() {
+    assertEquals(4, enumerate.expressions(ast("while(true) print(i);")));
+  }
+
+  @Test public void i() {
+    assertEquals(1, enumerate.expressions(ast("true")));
+  }
+
+  @Test public void j() {
+    assertEquals(3, enumerate.expressions(ast("1 + 2")));
+  }
+
   static void auxInt(@SuppressWarnings("unused") final int __) {
     assert true;
   }
 
-  static ASTNode ast(final String ¢) {
+  static ASTNode ast(String ¢) {
     return wizard.ast(¢);
   }
 }
