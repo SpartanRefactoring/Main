0661467ef98c9a40693898db62cadc679da403fa
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-01 20:21:55 +0200

    added a dummy names function to getAll as requested in #687

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
@@ -6,6 +6,8 @@ import org.eclipse.jdt.core.dom.*;
 
 /** @author Ori Marcovitch
  * @author Dor Ma'ayan
+ * @author Raviv Rachmiel
+ * @author Kfir Marx
  * @since Oct 31, 2016 */
 public enum getAll {
   ;
@@ -15,6 +17,14 @@ public enum getAll {
    * @return List of the names of the methods
    */
   public static Set<String> invocations(MethodDeclaration m){
+    return null;
+  }
+  /**
+   * Get list of names in a Block
+   * @param b Block
+   * @return List of the names in the block
+   */
+  public static List<Name> names(Block b) {
     return null;
   }
   // For you to implement! Let's TDD and get it on!
