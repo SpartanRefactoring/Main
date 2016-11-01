18554e1ffff793d3be710a3027b2b1724ab3e106
orimarco <marcovitch.ori@gmail.com>
2016-11-01 09:04:21 +0200

    sync

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/enumerate.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/enumerate.java
@@ -2,14 +2,28 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.utils.*;
+
 /** @author Ori Marcovitch
  * @since Oct 28, 2016 */
-public interface enumerate {
+public enum enumerate {
+  ;
   /** @author Ori Marcovitch
    * @param n node
    * @since Oct 28, 2016 */
-  static int expressions(@SuppressWarnings("unused") final ASTNode __) {
-    return 1;
+  public static int expressions(final ASTNode n) {
+    if (n == null)
+      return 0;
+    final Int $ = new Int();
+    $.inner = 0;
+    n.accept(new ASTVisitor() {
+      @Override public void preVisit(final ASTNode ¢) {
+        if (iz.expression(¢))
+          ++$.inner;
+      }
+    });
+    return $.inner;
   }
   // For you to implement! Let's TDD and get it on!
 }
