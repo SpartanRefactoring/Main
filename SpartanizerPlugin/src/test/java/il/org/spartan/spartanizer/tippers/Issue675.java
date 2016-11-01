a34f600e2507ff2a7ab7a5baeb60638c0a25b98f
Aviad Cohen <aviadcohen7@gmail.com>
2016-11-01 09:36:00 -0700

    TDD/Issue: #675: 1st TDD commit - check for method existance
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue675.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue675.java
@@ -1,7 +1,19 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.utils.tdd.*;
+
 /** @author Aviad Cohen & Noam Yefet
  *  @since 16-11-1
  */
 public class Issue675 {
+  @Test public void statements_test0() {
+    enumerate.statements(null);
+    assert true;
+  }
 }
