6db75982cd203078f39c8f41283eb98dfd5e617b
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-02 19:30:40 +0200

    added an advanced block names test - checks a full block with imported functions and a few variables. all tests passed regarding issue #687

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue687.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue687.java
@@ -38,6 +38,7 @@ public class Issue687 {
   @Test public void testCheckActualName() {
     assertTrue("a".equals(getAll.names((Block) wizard.ast("{a=1+1;}")).get(0) + ""));
   }
+
   @Test public void testCheckTwoNamesWithMoreThenOneLiteral() {
     List<Name> names = getAll.names((Block) wizard.ast("{aba=1+1; ima = 787-9;}"));
     assertTrue("aba".equals(names.get(0) + "") && 
@@ -45,5 +46,19 @@ public class Issue687 {
   }
   
   
+
+  @Test public void testCheckNamesFineBlock() {
+    //assuming we need to get all names in the block, including repetitions
+    List<Name> n = getAll.names((Block) wizard.ast("{a=1+1;b=2+3;System.out.println(a);c=2;c*=a;}"));
+    assertTrue(n.get(0).toString().equals("a"));
+    assertTrue("b".equals(n.get(1).toString()));
+    assertTrue("System".equals(n.get(2).toString()));
+    assertTrue("out".equals(n.get(3).toString()));
+    assertTrue("println".equals(n.get(4).toString()));
+    assertTrue("a".equals(n.get(5).toString()));
+    assertTrue("c".equals(n.get(6).toString()));
+    assertTrue("c".equals(n.get(7).toString()));
+    assertTrue("a".equals(n.get(8).toString()));
+  }
   
 }
