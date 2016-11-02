fc96e83d683b17c417a00e7a676d99d486d659ce
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-11-02 07:24:01 -0700

    Check if 0 is returned when no methods are present #679
    A new test that makes sure 0 is returned when a compilation unit with no methods has been added. This test didn't require the change of the 'methods' function since it already returned 0, and all the tests passed.
    In addition, a helper function has been added to the test class to easily create compilation units from strings.

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue679.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue679.java
@@ -4,11 +4,18 @@ import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
 import il.org.spartan.spartanizer.utils.tdd.*;
+import il.org.spartan.spartanizer.ast.navigate.wizard;
 
 /** Test class for tdd.enumerate.methods (see issue #679)
  * @author Sharon Kuninin & Yarden Lev
  * @since 2016-11-2 */
 @SuppressWarnings("static-method") public class Issue679 {
+  public static CompilationUnit cu(String program) {
+    ASTParser parser = wizard.parser(ASTParser.K_COMPILATION_UNIT);
+    parser.setSource(program.toCharArray());
+    return (CompilationUnit) parser.createAST(null);
+  }
+
   @Test public void checkExistence() {
     enumerate.methods((CompilationUnit) null);
   }
@@ -19,5 +26,9 @@ import il.org.spartan.spartanizer.utils.tdd.*;
 
   @Test public void checkParameterType() {
     enumerate.methods((CompilationUnit) null);
+  }
+
+  @Test public void noMethodsInCompilationUnit() {
+    Assert.assertEquals(0, enumerate.methods(cu("1111")));
   }
 }
