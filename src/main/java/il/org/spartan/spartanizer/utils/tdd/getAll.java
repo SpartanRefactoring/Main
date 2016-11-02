4e8ee2119dae22dc2daeaa5d7978492bde438542
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-02 19:02:03 +0200

    we actually gave the names function a real functionality and added an advanced test which passed and showed the actual name - all tests passed, regarding issue #687

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
@@ -13,15 +13,13 @@ import il.org.spartan.spartanizer.ast.safety.*;
 /** @author Ori Marcovitch
  * @author Dor Ma'ayan
  * @author Raviv Rachmiel
- * @author Kfir Marxor 
+ * @author Kfir Marx
  * @author Omri Ben- Shmuel
  * @since Oct 31, 2016 */
 public enum getAll {
   ;
   /** Get all the methods invoked in m
    * @author Dor Ma'ayan
-   * @author Raviv Rachmiel
-   * @author Kfir Marx
    * @param d JD
    * @return List of the names of the methods */
   public static Set<String> invocations(final MethodDeclaration ¢) {
@@ -40,21 +38,21 @@ public enum getAll {
   }
 
   /** Get list of names in a Block
+   * @author Raviv Rachmiel
+   * @author Kfir Marx
    * @param ¢ Block
    * @return List of the names in the block */
   public static List<Name> names(final Block b) {
     if(b==null) {
       return null;
     }
-    if(b.getLength()>2) { 
-      List<Name> l = new ArrayList<>();
-      Name n = null; 
-      l.add(n);
-      if(b.getLength()>8) {
-        l.add(n);
+    List<Name> names = new ArrayList<>();
+    b.accept(new ASTVisitor() {
+      @Override public boolean visit(SimpleName n) {
+        names.add(n);
+        return true;
       }
-      return l; 
-    }
-    return new ArrayList<>();
+    });
+    return names;
   }
 }
