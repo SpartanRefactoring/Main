4c1b79875e8fde69a79a00e870f97a3c2b2131a3
Koby Ben Shimol <kobybs@gmail.com>
2016-11-02 19:25:05 +0200

    #705 created test file for function instanceofs

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue705.java
@@ -0,0 +1,15 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/** Tests of {@link enumerate.expressions}
+ * @author Koby Ben Shimol
+ * @author Yuval Simon
+ * @since 16-11-2 */
+public class Issue705 {
+  @Test public void test() {
+    
+  }
+}
