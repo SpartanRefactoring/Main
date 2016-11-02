e078b1c903c355fbd9e8d045bc0beb9bb3efffaa
kfir-marx <kfir.marx@gmail.com>
2016-11-02 15:33:35 +0200

    function return type List<Name>  #687

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue687.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue687.java
@@ -11,6 +11,9 @@ import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.utils.tdd.*;
 
 public class Issue687 {
+  private static void forceTypeAtCompileTime(List<Name> ss) {
+    assert true; 
+   }
   @Test public void a() {
     assertNotNull(getAll.names(null));
   }
@@ -20,4 +23,8 @@ public class Issue687 {
   @Test public void c() {
     assertTrue(getAll.names((Block) wizard.ast("{}")).isEmpty());
   }
+  @Test public void d() {
+    forceTypeAtCompileTime(getAll.names((Block) wizard.ast("{}")));
+  }
+  
 }
