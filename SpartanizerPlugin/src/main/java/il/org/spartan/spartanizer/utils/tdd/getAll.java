cd56d694d4e254053aee643c14f93df8a92631b4
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-02 17:55:47 +0200

    added the getName simple test and the solution - all tests passed with lazy solving #687

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
@@ -2,8 +2,12 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import java.util.*;
 
+
+
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.classfiles.reify.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
 /** @author Ori Marcovitch
@@ -33,8 +37,17 @@ public enum getAll {
   /** Get list of names in a Block
    * @param ¢ Block
    * @return List of the names in the block */
-  public static List<Name> names(final Block ¢) {
-    return ¢==null ? null: new ArrayList<>();
+  public static List<Name> names(final Block b) {
+    if(b==null) {
+      return null;
+    }
+    if(b.getLength()>2) { 
+      List<Name> l = new ArrayList<>();
+      Name n = null; 
+      l.add(n);
+      return l; 
+    }
+    return new ArrayList<>();
   }
   
   
