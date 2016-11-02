32e62f4a36f824baa09e53e0235276503ede8f2b
assaflu <assaf.ss3@gmail.com>
2016-11-02 19:06:35 +0200

    JUnit for Issue 714

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue714.java
@@ -0,0 +1,11 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class Issue714 {
+  @Test public void test() {
+    fail("Not yet implemented");
+  }
+}
