57b253a346f155319d7df02e55f8917ff3421ef2
danabramovich <sdan635z@campus.technion.ac.il>
2016-11-02 20:40:05 +0200

    added names and date #714

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue714.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue714.java
@@ -4,6 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+/**
+ * see issue #714 for more details
+ * @author Dan Abramovich
+ * @author Assaf Lustig
+ * @author Arthur Sapozhnikov
+ * @since 16-11-02
+ */
+
 public class Issue714 {
   @Test public void test() {
     fail("Not yet implemented");
