d15d58a4b2e23d25df48d624b47729104e92f36a
YuvalSimon <yuvaltechnion@gmail.com>
2016-11-02 19:40:27 +0200

    #705 added return type test

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll.java
@@ -61,7 +61,7 @@ public enum getAll {
    * @author Koby Ben Shimol
    * @author Yuval Simon
    * @since 16-11-01 */
-  public void instanceofs(MethodDeclaration m){
-    
+  public static void instanceofs(MethodDeclaration m){
+
   }
 }
