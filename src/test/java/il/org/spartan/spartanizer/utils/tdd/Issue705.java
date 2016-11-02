161253f3229bf14165f9e43dcd4aec2678a3a213
YuvalSimon <yuvaltechnion@gmail.com>
2016-11-02 19:40:27 +0200

    #705 added return type test

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue705.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue705.java
@@ -2,6 +2,9 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
 /** Tests of {@link enumerate.expressions}
@@ -9,7 +12,10 @@ import org.junit.*;
  * @author Yuval Simon
  * @since 16-11-2 */
 public class Issue705 {
-  @Test public void test() {
-    
+  @Test public void a() {
+    auxList(getAll.instanceofs((MethodDeclaration) null));
+  }
+  void auxList(@SuppressWarnings("unused") List<InstanceofExpression> __) {
+    assert true;
   }
 }
