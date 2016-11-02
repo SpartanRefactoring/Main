c5941906fe087e20188de29f1ada85a55aa925ea
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-11-02 03:56:29 -0700

    Create test class for issue #679

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue679.java
@@ -0,0 +1,9 @@
+package il.org.spartan.spartanizer.tippers;
+
+/** Test class for tdd.enumerate.methods (see issue #679)
+ * 
+ * @author Sharon Kuninin & Yarden Lev
+ * @since 2016-11-2 */
+public class Issue679 {
+  // Place holder for future unit tests for tdd.enumerate.methods
+}
