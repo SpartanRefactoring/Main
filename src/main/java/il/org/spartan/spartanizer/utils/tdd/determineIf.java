f53a56fc21c1ab139e9b8a6a8ddf37f53eac39b0
RonGatenio <ron.gatenio@gmail.com>
2016-11-02 07:52:53 -0700

    #716 created the hasManyStatements function in tdd.determineIf

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/determineIf.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/determineIf.java
@@ -1,8 +1,24 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
+import org.eclipse.jdt.core.dom.*;
+
 /** @author Ori Marcovitch
  * @since Oct 31, 2016 */
 public enum determineIf {
   ;
+  
+  /**
+   * see issue #716 for more details
+   * 
+   * @author Ron Gatenio
+   * @author Roy Shchory
+   * @since 16-11-02
+   * @param m
+   * @return true iff the method has at least 10 statements
+   */
+  public static boolean hasManyStatements(MethodDeclaration m) {
+    return true;
+  }
+  
   // For you to implement! Let's TDD and get it on!
 }
