c4d9066319a72f2b8e0efff5fefa5652667eb442
orimarco <marcovitch.ori@gmail.com>
2016-11-03 15:25:54 +0200

    analyze.type ready! #763

---
+++ src/test/java/il/org/spartan/spartanizer/ast/navigate/Issue763.java
@@ -0,0 +1,29 @@
+package il.org.spartan.spartanizer.ast.navigate;
+
+import static il.org.spartan.azzert.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+import org.junit.runners.*;
+
+/** Test for analyze.type
+ * @author Ori Marcovitch
+ * @since Nov 3, 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class Issue763 {
+  @Test public void a() {
+    assertEquals("Map", analyze.type(
+        searchDescendants.forClass(Name.class).suchThat(x -> "x".equals(x + "")).from(wizard.ast("class C{ Map x; void foo(){ print(x);}}")).get(0)));
+  }
+
+  @Test public void b() {
+    assertEquals("Map", analyze.type(
+        searchDescendants.forClass(Name.class).suchThat(x -> "x".equals(x + "")).from(wizard.ast("class C{  void foo(){Map x; print(x);}}")).get(0)));
+  }
+
+  @Test public void c() {
+    assertEquals("Map", analyze.type(
+        searchDescendants.forClass(Name.class).suchThat(x -> "x".equals(x + "")).from(wizard.ast("class C{  void foo(Map x){ print(x);}}")).get(0)));
+  }
+}
