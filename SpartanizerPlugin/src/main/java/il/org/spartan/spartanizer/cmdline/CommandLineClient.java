397edf0fc54ee2e9c2f37da926e39ff012e89980
M4tt30rru <matt.orru@gmail.com>
2016-11-03 11:21:48 +0200

    update command line args parser

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
@@ -1,8 +1,15 @@
 package il.org.spartan.spartanizer.cmdline;
 
+import java.util.*;
+
+import il.org.spartan.*;
+
 /** Simplified version of command line client that uses spartizer applicator
  * @author Matteo Orru' */
-public class CommandLineClient { // extends AbstractCommandLineSpartanizer{
+public class CommandLineClient { private static String outputDir;
+private static String folder;
+
+// extends AbstractCommandLineSpartanizer{
   // TODO Matteo: Add instruction to parse command line
   // TODO Matteo: Add prompt help
   public static void main(final String[] args) {
@@ -14,6 +21,23 @@ public class CommandLineClient { // extends AbstractCommandLineSpartanizer{
   private static void parseCommandLineArgs(String[] args) {
     if(args.length == 0)
       printPrompt();
+    String inputDir;
+    for (int ¢ = 0; ¢ < args.length;)
+      if ("-o".equals(args[¢])) {
+        outputDir = args[¢ + 1];
+        ¢ += 2;
+      } else if ("-i".equals(args[¢])) {
+        inputDir = args[¢ + 1];
+        ¢ += 2;
+      } else if ("-d".equals(args[¢])) {
+        inputDir = ".";
+        outputDir = folder;
+        ¢ += 1;
+      } else {
+        System.out.println(args[¢]);
+        System.out.println("[ERROR]: Something went wrong! Parameter or switch not allowed");
+        ++¢;
+      }
   }
 
   static void printPrompt() {
