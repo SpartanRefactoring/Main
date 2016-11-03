4d804386e02874f021fd9fdaa77b6ba092f49d85
Yossi Gil <yossi.gil@gmail.com>
2016-11-03 10:12:30 +0200

    Cleanup

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue679.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue679.java
@@ -3,15 +3,15 @@ package il.org.spartan.spartanizer.tippers;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
+import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.utils.tdd.*;
-import il.org.spartan.spartanizer.ast.navigate.wizard;
 
 /** Test class for tdd.enumerate.methods (see issue #679)
  * @author Sharon Kuninin & Yarden Lev
  * @since 2016-11-2 */
 @SuppressWarnings("static-method") public class Issue679 {
-  public static CompilationUnit cu(String program) {
-    ASTParser parser = wizard.parser(ASTParser.K_COMPILATION_UNIT);
+  public static CompilationUnit cu(final String program) {
+    final ASTParser parser = wizard.parser(ASTParser.K_COMPILATION_UNIT);
     parser.setSource(program.toCharArray());
     return (CompilationUnit) parser.createAST(null);
   }
@@ -21,7 +21,7 @@ import il.org.spartan.spartanizer.ast.navigate.wizard;
   }
 
   @Test public void checkReturnType() {
-    @SuppressWarnings("unused") int $ = enumerate.methods((CompilationUnit) null);
+    @SuppressWarnings("unused") final int $ = enumerate.methods((CompilationUnit) null);
   }
 
   @Test public void checkParameterType() {
--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue714.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue714.java
@@ -6,34 +6,29 @@ import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.utils.tdd.*;
 
-/**
- * see issue #714 for more details
+/** see issue #714 for more details
  * @author Dan Abramovich
  * @author Assaf Lustig
  * @author Arthur Sapozhnikov
- * @since 16-11-02
- */
+ * @since 16-11-02 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue714 {
-  
- @Test public void testRetTypeCompiles() {
+  @Test public void testRetTypeCompiles() {
     @SuppressWarnings("unused") final boolean b = determineIf.isImmutable(null);
   }
-  
-  @Test public void testNull(){
+
+  @Test public void testNull() {
     auxBool(determineIf.isImmutable((TypeDeclaration) null));
   }
-  
+
   @Test public void testSimpleTypeDecleration() {
     assertTrue(determineIf.isImmutable((TypeDeclaration) wizard.ast("final int x")));
   }
-  
-  
+
   static void auxBool(@SuppressWarnings("unused") final boolean __) {
     assert true;
   }
