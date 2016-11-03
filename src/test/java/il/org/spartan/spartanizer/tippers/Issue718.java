2aad077f68bbd9ebefe7f9649d1b130414481969
Amir Sagiv <amirsagiv15@gmail.com>
2016-11-03 19:21:15 +0200

    added Issue718 test start

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue718.java
@@ -0,0 +1,13 @@
+package il.org.spartan.spartanizer.tippers;
+
+import org.junit.*;
+
+/** see issue #718 for more details
+ * @author Oren Afek
+ * @author Amir Sagiv
+ * @since 16-11-03 */
+public class Issue718 {
+  @Test public void a() {
+    assert true;
+  }
+}
