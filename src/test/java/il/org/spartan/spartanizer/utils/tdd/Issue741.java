0be7854f0808e9b54fe84555c9471a645ebe3ce3
Idan Atias <Idan2k3@gmail.com>
2016-11-03 17:15:11 +0200

    Signed-off-by: Idan Atias <idan2k3@gmail.com>
    Added checking return value test #741

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue741.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue741.java
@@ -1,15 +1,20 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
+import static org.junit.Assert.*;
+
+import java.util.*;
+
 import org.junit.*;
 
 /** @author Shimon Azulay & Idan Atias & Lior Ben Ami
  * @since 16-11-3 */
-
-@SuppressWarnings({ "static-method", "javadoc" }) 
-public class Issue741 {
-  
+@SuppressWarnings({ "static-method", "javadoc" }) public class Issue741 {
   @Test public void publicFields_test0() {
     getAll2.publicFields(null);
     assert true;
-  } 
+  }
+
+  @SuppressWarnings("cast") @Test public void publicFields_test1() {
+    assertTrue(getAll2.publicFields(null) instanceof List<?>);
+  }
 }
