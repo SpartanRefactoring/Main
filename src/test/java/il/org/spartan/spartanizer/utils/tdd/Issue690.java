f0032da7d03ad0dca64a74d393b37e0796224693
Toma <tom.nof@gmail.com>
2016-11-04 13:39:14 +0200

    #690 adding the test issue file

---
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue690.java
@@ -0,0 +1,12 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Issue690 {
+  @Test public void a0() {
+    assertNull(getAll.casts(null));
+  }
+
+  
+}
