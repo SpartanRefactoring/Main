75bcfe44a92cd0314e5eb54f971248b1e15d79cc
yaelAmitay <yaelam75@gmail.com>
2016-11-04 03:50:57 -0700

    add file Issue719.java

---
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue719.java
@@ -0,0 +1,13 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+import org.junit.*;
+
+/** see issue #719 for more details
+ * @author koralchapnik
+ * @author yaelAmitay
+ * @since 16-11-04 */
+public class Issue719 {
+  @Test public static void test() {
+    assert true;
+  }
+}
