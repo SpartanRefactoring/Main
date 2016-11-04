9b6bc1f169438dc643bdbd9e4b19045c87839cfd
yaelAmitay <yaelam75@gmail.com>
2016-11-04 04:34:55 -0700

    add test checkZeroDefinitions

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue719.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue719.java
@@ -2,8 +2,10 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
+import il.org.spartan.spartanizer.ast.navigate.*;
 
 /** see issue #719 for more details
  * @author koralchapnik
@@ -13,8 +15,12 @@ public class Issue719 {
   @SuppressWarnings("static-method") @Test public void test() {
     assert true;
   }
-  
+
   @SuppressWarnings("static-method") @Test public void nullCheck() {
     assertFalse(determineIf.definesManyVariables(null, 0));
+  }
+
+  @SuppressWarnings("static-method") @Test public void checkZeroDefinition() {
+    assertTrue(determineIf.definesManyVariables(((MethodDeclaration) wizard.ast("public void methodZeroDefinition() {} ")), 0));
   }
 }
