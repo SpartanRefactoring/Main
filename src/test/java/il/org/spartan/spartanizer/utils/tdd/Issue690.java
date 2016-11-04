b1d6f7f0a16f09135cb003486d69ca17a9113507
ortroyaner <or.troyaner@gmail.com>
2016-11-04 14:36:00 +0200

    #690 input check

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue690.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue690.java
@@ -1,12 +1,20 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
+
+import org.eclipse.jdt.internal.compiler.ast.*;
 import org.junit.*;
+import java.util.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 
 public class Issue690 {
-  @Test public void a0() {
+  @Test public void test0() {
     assertNull(getAll.casts(null));
   }
-
+  @Test public void test1() {
+    assertEquals(0,getAll.casts(az.methodDeclaration(wizard.ast("static void foo() {}"))).size());
+  }
   
 }
