fe944f210e7399885d9abe6123496c9ec7d66548
Nikita Dizhurgit <blenderon@gmail.com>
2016-11-05 18:01:49 +0200

    added test to check that hasBigBlock returns True for big functions with random name and random amount of statements (>=5). #717

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue717.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue717.java
@@ -2,12 +2,12 @@ package il.org.spartan.spartanizer.tippers;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
-import static org.junit.Assert.*;
-
 import il.org.spartan.spartanizer.utils.tdd.*;
 
 /** see Issue #717 for more details
@@ -19,6 +19,9 @@ import il.org.spartan.spartanizer.utils.tdd.*;
   
   MethodDeclaration fiveStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; int b; int c; int d; int e;}");
 
+  private static final String CHAR_LIST =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  
   @Test public void isCompiled() {
     assert true;
   }
@@ -32,6 +35,40 @@ import il.org.spartan.spartanizer.utils.tdd.*;
     assertTrue(determineIf.hasBigBlock(fiveStatMethod));
   }
   
+  private String generateRandomString(int maxLen) {
+    StringBuffer randStr = new StringBuffer();
+    int len = 0;
+    Random randomGenerator = new Random();
+    len = randomGenerator.nextInt(maxLen);
+    if(len<=0)
+      len = 1;
+    for (int ¢ = 0; ¢ < len; ++¢)
+      randStr.append(CHAR_LIST.charAt(randomGenerator.nextInt(CHAR_LIST.length())));
+    return randStr + "";
+  }
+  
+  private final int MAX_NAME_SIZE = 100;
+  private final int MAX_STAT_AMOUNT = 100;
+  
+  @Test public void randomBigBlockReturnsTrue() {
+    String methodName = generateRandomString(MAX_NAME_SIZE);
+    String firstStat = "{int x;";
+    String nextStat = "x=4;";
+    
+    int statAmount = 0;
+    Random randomGenerator = new Random();
+    statAmount = randomGenerator.nextInt(MAX_STAT_AMOUNT);
+    if(statAmount<4)
+      statAmount = 4;
+    
+
+    String randomBigBlock = "public void " + methodName+"()"+firstStat;
+    for(int ¢=0; ¢<statAmount; ++¢)
+      randomBigBlock += nextStat;
+    randomBigBlock += "}";
+    
+    assertTrue(determineIf.hasBigBlock(((MethodDeclaration) wizard.ast(randomBigBlock))));
+  }
   
 
 }
