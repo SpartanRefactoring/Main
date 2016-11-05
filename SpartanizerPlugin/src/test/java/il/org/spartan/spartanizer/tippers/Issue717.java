fa320e437f5b1dd9135930191ac0c2059b78920d
Nikita Dizhurgit <blenderon@gmail.com>
2016-11-05 17:07:05 +0200

    hasBigBlock returns True if the method has 5 statements #717

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue717.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue717.java
@@ -1,6 +1,12 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static org.junit.Assert.*;
+
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.utils.tdd.*;
 
 /** see Issue #717 for more details
  * @author Lidia Piatigorski
@@ -8,7 +14,17 @@ import org.junit.*;
  * @author Alex V.
  * @since 16-11-05 */
 @SuppressWarnings("static-method") public class Issue717 {
+  
+  MethodDeclaration fiveStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; int b; int c; int d; int e;}");
+  
   @Test public void a() {
     assert true;
   }
+  
+  @Test public void containsBigBlock() {
+    assertTrue(determineIf.hasBigBlock(fiveStatMethod));
+  }
+  
+  
+  
 }
