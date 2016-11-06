801e3fea3af92a67fe8a5b401cbea17df938f8bf
inbalzukerman <inbal.zukerman@gmail.com>
2016-11-06 14:58:24 +0200

    added test to check if return value is list

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue713.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue713.java
@@ -1,6 +1,9 @@
 package il.org.spartan.spartanizer.tippers;
 
 import org.junit.*;
+import java.util.*;
+
+import il.org.spartan.spartanizer.utils.tdd.*;
 
 /**
  * see issue #713 for more details
@@ -12,5 +15,9 @@ import org.junit.*;
 public class issue713 {
   @SuppressWarnings("static-method") @Test public void doesCompile(){
     assert true;
+  }
+  
+  @SuppressWarnings("static-method") @Test public void returnsList(){
+    @SuppressWarnings("unused") List<String> lst = getAll.publicFields(null);
   }
 }
