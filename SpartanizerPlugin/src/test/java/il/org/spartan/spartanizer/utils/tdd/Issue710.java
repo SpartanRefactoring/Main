7f81ace9607b643aa97a8746c58facb6f774f939
davidcohen55 <dcohen484@gmail.com>
2016-11-06 05:27:43 -0800

    Adding test that returns true #710

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue710.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue710.java
@@ -10,13 +10,21 @@ import org.junit.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
-/** @author Zahi Mizrahi
- * @author Shahar Yair
+/** 
  * @author David Cohen 
+ * @author Shahar Yair
+ * @author Zahi Mizrahi
  * @since 16-11-6 */
 @SuppressWarnings("static-method") public class Issue710 { 
   
-@Test public void a() {
+@Test public void test01() {
   assertFalse(determineIf.returnsNull(null));
 }
+
+@Test public void test02() {
+  assertTrue(determineIf.returnsNull(az.methodDeclaration(wizard.ast("static int f() { return null;}"))));
+}
+
+
+
 }
