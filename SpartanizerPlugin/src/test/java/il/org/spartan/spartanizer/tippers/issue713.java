b803fdfc4b0ff9a9e0869d7bffd188ec703139f2
inbalzukerman <inbal.zukerman@gmail.com>
2016-11-06 14:19:47 +0200

    first test

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue713.java
@@ -0,0 +1,16 @@
+package il.org.spartan.spartanizer.tippers;
+
+import org.junit.*;
+
+/**
+ * see issue #713 for more details
+ * @author Inbal Zukerman
+ * @author Elia Traore
+ * @since 2016-11-06
+ */
+
+public class issue713 {
+  @SuppressWarnings("static-method") @Test public void doesCompile(){
+    assert true;
+  }
+}
