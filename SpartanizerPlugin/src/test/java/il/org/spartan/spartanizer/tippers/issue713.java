5d853b39dabda5da7a9090227f5f73a598a3d62d
inbalzukerman <inbal.zukerman@gmail.com>
2016-11-07 00:00:10 +0200

    adding test checking a class with no public fields

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue713.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue713.java
@@ -1,8 +1,13 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static org.junit.Assert.*;
+
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import java.util.*;
 
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.utils.tdd.*;
 
 /**
@@ -13,11 +18,23 @@ import il.org.spartan.spartanizer.utils.tdd.*;
  */
 
 public class issue713 {
+  
+  TypeDeclaration noPublic = createTypeDeclerationFromStr("public class noPublic {   } ");
+      
   @SuppressWarnings("static-method") @Test public void doesCompile(){
     assert true;
   }
   
   @SuppressWarnings("static-method") @Test public void returnsList(){
     @SuppressWarnings("unused") List<String> lst = getAll.publicFields(null);
+  }
+  
+  @Test public void returnsNoPublic(){
+    assertEquals(0, getAll.publicFields(noPublic).size());
+  }
+  
+  
+  private static TypeDeclaration createTypeDeclerationFromStr( String ¢) {
+    return az.typeDeclaration(wizard.ast(¢));
   }
 }
