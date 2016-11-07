fe6261700ccda1e127966ad815a8dea42a9b9da0
Doron Mehsulam <doronmmm@hotmail.com>
2016-11-07 22:28:09 +0200

    fix test1 #707

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
@@ -1,7 +1,7 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
 import org.junit.*;
-
+ 
 /**
  * @author Doron Meshulam
  * @author Tomer Dragucki
@@ -11,7 +11,7 @@ public class Issue707 {
   
   @Test
   public void test1() {
-    names(null);
+    getAll2.names(null);
     return;
   }
 }
