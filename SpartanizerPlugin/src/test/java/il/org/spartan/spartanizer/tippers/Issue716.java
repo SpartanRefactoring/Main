7e655c6c25b2137bde70e673740e8d4f367510b7
RonGatenio <ron.gatenio@gmail.com>
2016-11-07 15:17:59 -0800

    added a test with blocks, and fixed the function #716

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue716.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue716.java
@@ -41,5 +41,19 @@ import il.org.spartan.spartanizer.utils.tdd.*;
     assertFalse(determineIf.hasManyStatements(
         (MethodDeclaration) wizard.ast("public void nineStatements() {int a; int b; int c; int d; int e; int f; int g; int h; int i;} ")));
   }
+  
+  @Test public void checkWithIf() {
+    assertFalse(determineIf.hasManyStatements(
+        (MethodDeclaration) wizard.ast("public void withIf() {"
+            + "int a = 1;"
+            + "if (a == 2) {"
+            + "    ++a; ++a; ++a; ++a; ++a; ++a;} }")));
+    
+    assertTrue(determineIf.hasManyStatements(
+        (MethodDeclaration) wizard.ast("public void withIf() {"
+            + "int a = 1;"
+            + "if (a == 2) {"
+            + "    ++a; ++a; ++a; ++a; ++a; ++a; ++a;} }")));
+  }
 }
 
