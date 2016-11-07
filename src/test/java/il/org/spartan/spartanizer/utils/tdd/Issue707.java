7797021e58548428b4f3c6105a70d551fd4ca2ed
Doron Mehsulam <doronmmm@hotmail.com>
2016-11-08 00:06:34 +0200

    adding REAL test to #707

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
@@ -32,6 +32,17 @@ public class Issue707 {
   public void c() {
     assertEquals((getAll2.names(az.block(ast("{int i;}"))).get(0) + ""), "i");
   }
+  
+  @Test
+  public void d() {
+    assertEquals((getAll2.names(az.block(ast("{int i = x;}"))).get(0) + ""), "i");
+    assertEquals((getAll2.names(az.block(ast("{int i = x;}"))).get(1) + ""), "x");
+  }
+
+  @Test
+  public void e() {
+    assertEquals((getAll2.names(az.block(ast("{int x = i;}"))).get(0) + ""), "x");
+  }
   static ASTNode ast(final String ¢) {
     return wizard.ast(¢);
   }
