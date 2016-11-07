42800c58800a6831febaa5e6d4626a125cd13ddc
Yossi Gil <yossi.gil@gmail.com>
2016-11-07 12:17:37 +0200

    Fixing more warnings for Michal and Anna: #564 #565

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/find.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/tdd/find.java
@@ -2,9 +2,6 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.utils.*;
-
 /** @author AnnaBel7
  * @author michalcohen
  * @since Nov 4, 2016 */
@@ -16,7 +13,7 @@ public enum find {
    * @author  michalcohen
    * @since  Nov 4, 2016 
    */
-  public static TypeDeclaration ancestorType(ASTNode __) {
+  public static TypeDeclaration ancestorType(@SuppressWarnings("unused") ASTNode __) {
     return null;
   }
 }
