62c57f9b5c2401f1c9a65e2ee61b2f8d3170e49e
michalcohen <cohenmelany1@gmail.com>
2016-11-07 20:58:09 +0200

    test 3 #683

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/find.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/find.java
@@ -3,11 +3,10 @@ package il.org.spartan.spartanizer.utils.tdd;
 import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.ast.safety.*;
-
+/** @author Ori Marcovitch*/
 /** @author AnnaBel7
  * @author michalcohen
  * @since Nov 4, 2016 */
-// TODO: I get warnings on unused imports in your code. --yg
 /** @author Shay Segal
  * @author Sefi Albo
  * @author Daniel Shames */
@@ -16,9 +15,10 @@ public enum find {
   /** @author AnnaBel7
    * @author michalcohena
    * @since Nov 4, 2016 */
-  public static TypeDeclaration ancestorType(@SuppressWarnings("unused") final ASTNode __) {
-    return null;
+  public static TypeDeclaration ancestorType(final ASTNode ¢) {
+    return ¢ == null ? null : (TypeDeclaration) ¢.getParent().getParent().getParent().getParent();
   }
+
 
   public static MethodDeclaration ancestorMethod(ASTNode n) {
   ASTNode temp = n;
