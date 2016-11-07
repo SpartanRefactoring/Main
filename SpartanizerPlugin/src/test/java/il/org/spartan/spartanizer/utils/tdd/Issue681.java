449194072a7f81e06154cec1e2f028210cb6388f
sefialbo <sefialbo@gmail.com>
2016-11-07 16:50:49 +0200

    AncestorMethod Test2

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue681.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue681.java
@@ -2,7 +2,11 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 
 /** 
  * @author Shay Segal 
@@ -10,7 +14,12 @@ import org.junit.*;
  * @author Daniel Shames
  * @since 16-11-7 */
 @SuppressWarnings("static-method") public class Issue681 {
-  @Test public void simpleTest() {
+  @Test public void simpleTest1() {
     assertEquals(find.ancestorMethod(null),null);
+  }
+  @Test public void simpleTest2() {
+    ASTNode ast = wizard.ast("static void f() { return null;}");
+    MethodDeclaration method = az.methodDeclaration(ast);
+    assertEquals(find.ancestorMethod(ast), method);
   }
 }
