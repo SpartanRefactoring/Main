00bd03c582244efbd574503184e9b9ac3fb5304c
Tomer Dragucki <tomerdragucki@Tomers-MacBook-Pro-2.local>
2016-11-07 21:56:56 +0200

    Start writing tests #707

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
@@ -1,9 +1,16 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
+import org.junit.*;
+
 /**
  * @author Doron Meshulam
  * @author Tomer Dragucki
  */
 
 public class Issue707 {
+  
+  @Test
+  public void test1() {
+    return;
+  }
 }
