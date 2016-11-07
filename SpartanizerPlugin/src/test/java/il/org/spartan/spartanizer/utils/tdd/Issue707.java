5fa06b25e69ace4c58814d80c3fa7dd2a9d90c06
Tomer Dragucki <tomerdragucki@Tomers-MacBook-Pro-2.local>
2016-11-08 01:12:52 +0200

    added two tests #707

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue707.java
@@ -1,6 +1,6 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
-<<<<<<< Updated upstream
+
 import static org.junit.Assert.*;
 
 import java.util.*;
@@ -11,9 +11,8 @@ import org.junit.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
  
-=======
 import org.junit.*; 
->>>>>>> Stashed changes
+
 /**
  * @author Doron Meshulam
  * @author Tomer Dragucki
@@ -36,7 +35,7 @@ public class Issue707 {
   public void c() {
     assertEquals((getAll2.names(az.block(ast("{int i;}"))).get(0) + ""), "i");
   }
-  
+    
   @Test
   public void d() {
     assertEquals((getAll2.names(az.block(ast("{int i = x;}"))).get(0) + ""), "i");
@@ -46,6 +45,18 @@ public class Issue707 {
   @Test
   public void e() {
     assertEquals((getAll2.names(az.block(ast("{int x = i;}"))).get(0) + ""), "x");
+  }
+  
+  @Test
+  public void f() {
+    assertEquals((getAll2.names(az.block(ast("{int x = i; double y = 5;}"))).get(0) + ""), "x");
+    assertEquals((getAll2.names(az.block(ast("{int x = i; double y = 5;}"))).get(2) + ""), "y");
+  }
+  
+  @Test
+  public void g() {
+    assertEquals((getAll2.names(az.block(ast("{if(i > 0)  return z;}"))).get(0) + ""), "i");
+    assertEquals((getAll2.names(az.block(ast("{if(i > 0)  return z;}"))).get(1) + ""), "z");
   }
   
   static ASTNode ast(final String ¢) {
