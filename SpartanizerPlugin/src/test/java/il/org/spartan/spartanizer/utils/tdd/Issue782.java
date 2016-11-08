278a9fe92f6a9d78a868a5a2a184a473426fdcf3
rodedzats <rodedzats@gmail.com>
2016-11-08 20:17:42 +0200

    Added test number 3 #782

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue782.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue782.java
@@ -2,7 +2,11 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 
 /** Tests of methods according to issue 778
  * @author Yonatan Zarecki
@@ -14,8 +18,18 @@ public class Issue782 {
     assert true;
   }
   
-  @SuppressWarnings("static-method") @Test public void returnsList(){
+  @SuppressWarnings("static-method") @Test public void returnsNotNull(){
     assertNotNull(getAll.privateFields(null));
   }
+  
+  @SuppressWarnings("static-method") @Test public void emptyClassShouldReturnEmptyList(){
+    assertTrue(getAll.privateFields(((TypeDeclaration) az.compilationUnit(wizard.ast("public class empty{}")).types().get(0))).isEmpty());
+  }
+  
+  @SuppressWarnings("static-method") @Test public void onePrivateFieldClassShouldReturnSingleElementList(){
+    assertEquals(1,getAll.privateFields(((TypeDeclaration) az.compilationUnit(wizard.ast("public class OnePrivate{private f;}")).types().get(0))));
+  }
+  
+  
   
 }
