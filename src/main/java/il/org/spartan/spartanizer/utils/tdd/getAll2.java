e532523a1646734aa355d6fac6a30c8c954323ef
Yossi Gil <yossi.gil@gmail.com>
2016-11-08 15:35:34 +0200

    RM CLASS

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll2.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/getAll2.java
@@ -1,43 +1,48 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
-import org.eclipse.jdt.core.dom.*;
-import org.eclipse.jdt.core.dom.ASTVisitor;
+import java.util.*;
 
-import il.org.spartan.spartanizer.ast.navigate.*;
+import org.eclipse.jdt.core.dom.*;
+
 import il.org.spartan.spartanizer.ast.safety.*;
 
-import java.util.*;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 /** @author Ori Marcovitch
  * @author Moshe ELiasof
  * @author Netanel Felcher
  * @author Doron Meshulam
  * @author Tomer Dragucki
- * @since Nov 7, 2016 */
+ * @author Shimon Azulay
+ * @author Idan Atias
+ * @since Nov 8, 2016 */
 public enum getAll2 {
   ;
   // For you to implement! Let's TDD and get it on!
-  
-  /** takes a single parameter, which is a CompilationUnit
-   * returns a list of methods in cu.
+  /** takes a single parameter, which is a CompilationUnit returns a list of
+   * methods in cu.
    * @param u CompilationUnit
    * @author Moshe Eliasof
-   * @author Netanel Felcher
-    */
-  public static List<MethodDeclaration> methods(@SuppressWarnings("unused") CompilationUnit __)
-  {
-    List<MethodDeclaration> $= new ArrayList<>();
-    MethodDeclaration MD = az.methodDeclaration(wizard.ast("public void foo();"));
-    $.add(MD);
+   * @author Netanel Felcher */
+  public static List<MethodDeclaration> methods(CompilationUnit u) {
+    if (u == null)
+      return null;
+    List<MethodDeclaration> $ = new ArrayList<>();
+    u.accept(new ASTVisitor() {
+      @Override public boolean visit(MethodDeclaration ¢) {
+        $.add(¢);
+        return super.visit(¢);
+      }
+    });
     return $;
   }
-  /**
-   * Takes Block b and returns list of names in it
+  /** Takes Block b and returns list of names in it
    * @param b
    * @return List<Name> which is all names in b
    * @author Doron Meshulam
-   * @author Tomer Dragucki
-   */
+   * @author Tomer Dragucki */
   public static List<Name> names(Block b) {
     if (b == null)
       return null;
@@ -48,6 +53,27 @@ public enum getAll2 {
           $.add(az.name(an));
       }
     });
+    return $;
+  }
+  /** @author Shimon Azulay
+   * @author Idan Atias
+   * @since 16-11-3 */
+  /** Given a TypeDeclaration argument, this function returns a list of it's
+   * public fields names.
+   * @param d - the TypeDeclaration argument */
+  public static List<String> publicFields(TypeDeclaration d) {
+    LinkedList<String> $ = new LinkedList<>();
+    if (d == null)
+      return $;
+    for (FieldDeclaration fd : d.getFields())
+      for (Object mod : fd.modifiers())
+        if ("public".equals((mod + ""))) {
+          String[] field_splitted_to_words = (fd + "").trim().split(" ");
+          int field_name_idx = field_splitted_to_words.length - 1;
+          if (field_name_idx < 0)
+            continue;
+          $.add(field_splitted_to_words[field_name_idx].replace(";", "").trim());
+        }
     return $;
   }
 }
