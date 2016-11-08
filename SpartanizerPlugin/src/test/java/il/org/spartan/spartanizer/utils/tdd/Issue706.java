a74de83cb1e0c6833d825c6bd33f3f061f9d3a73
SapirBismot <Sapirbismot@gmail.com>
2016-11-08 19:42:59 +0200

    check if the function return list #706

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue706.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue706.java
@@ -1,11 +1,11 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+
+import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
-
-import il.org.spartan.spartanizer.ast.navigate.*;
 
 /** Unit tests for issue #706
  * @author Sapir Bismot
@@ -14,5 +14,8 @@ import il.org.spartan.spartanizer.ast.navigate.*;
 public class Issue706{
   @Test public void test00() {
     getAll2.stringVariables((MethodDeclaration) null);
+  }
+  @Test public void test01() {
+    List<VariableDeclaration> l = getAll2.stringVariables((MethodDeclaration) null);
   }
 }
