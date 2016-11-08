0da7290cbd09a990a3b78d31142defd9e74c5e79
leyaniv <leyaniv@gmail.com>
2016-11-08 20:04:22 +0200

    check if the function return empty list #706

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue706.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue706.java
@@ -1,11 +1,16 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
+import static org.junit.Assert.*;
+
 //import static org.junit.Assert.*;
 
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 
 /** Unit tests for issue #706
  * @author Sapir Bismot
@@ -17,5 +22,8 @@ public class Issue706{
   }
   @Test public void test01() {
     List<VariableDeclaration> l = getAll2.stringVariables((MethodDeclaration) null);
+  }
+  @Test public void test02() {
+    assertTrue(getAll2.stringVariables(az.methodDeclaration(wizard.ast("void f(int n);"))).isEmpty());
   }
 }
