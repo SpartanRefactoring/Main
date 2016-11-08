db55536a81cf0901b7d5da9bdcabae4b25b77dca
yonatan zarecki <yonilx@gmail.com>
2016-11-08 19:31:56 +0200

    Add new test file #782

---
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue782.java
@@ -0,0 +1,16 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/** Tests of methods according to issue 778
+ * @author Yonatan Zarecki
+ * @author Roded Zats
+ * @author Ziv Izhar
+ * @since Nov 8, 2016 */
+public class Issue782 {
+  @SuppressWarnings("static-method") @Test public void checkCompiles(){
+    assert true;
+  }
+}
