b033f9d163989f3dd82e73a8ba19c105570a98eb
Yossi Gil <yossi.gil@gmail.com>
2016-11-08 08:04:15 +0200

    Substandard: #633. Also tell Moshe. He has accumlated some bad Karma.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue778.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue778.java
@@ -10,28 +10,21 @@ import org.junit.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
-
 /** Tests of methods according to issue 778
  * @author Netanel Felcher
  * @author Moshe Eliasof
  * @since Nov 7, 2016 */
-
-
 public class Issue778 {
-  @SuppressWarnings("static-method") @Test public void test0(){
+  @SuppressWarnings("static-method") @Test public void test0() {
     getAll2.methods(null);
   }
-  @SuppressWarnings("static-method") @Test public void test1(){
-    List<MethodDeclaration> lst =getAll2.methods(null);
+  @SuppressWarnings("static-method") @Test public void test1() {
+    List<MethodDeclaration> lst = getAll2.methods(null);
   }
-
-@SuppressWarnings({ "static-method", "deprecation" }) @Test public void test2(){  
-  assertEquals(getAll2.methods(az.compilationUnit(wizard.ast("public class Dog {public  void foo() {} }"))).size(), 1);
-}
-
-
-@SuppressWarnings({ "static-method", "deprecation" }) @Test public void test3(){  
-  assertEquals(getAll2.methods(az.compilationUnit(wizard.ast("public class Dog {public void foo() {} }"))).get(0).getName().getIdentifier(), "foo");
-}
-
+  @SuppressWarnings({ "static-method", "deprecation" }) @Test public void test2() {
+    assertEquals(getAll2.methods(az.compilationUnit(wizard.ast("public class Dog {public  void foo() {} }"))).size(), 1);
+  }
+  @SuppressWarnings({ "static-method", "deprecation" }) @Test public void test3() {
+    assertEquals(getAll2.methods(az.compilationUnit(wizard.ast("public class Dog {public void foo() {} }"))).get(0).getName().getIdentifier(), "foo");
+  }
 }
