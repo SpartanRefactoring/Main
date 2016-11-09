6ae14242b334f54329f0cccbebc075d631809254
zahimizrahi <zahimizrahi1@gmail.com>
2016-11-09 16:19:23 +0200

    added testInt. now test coverage is 100%. it was so easy, so we are gonna cover another file. Issue #801 is closed. @zahimizrahi

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue801.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue801.java
@@ -1,18 +1,19 @@
 package il.org.spartan.spartanizer.utils.tdd;
 
+
+import static org.junit.Assert.*;
 import org.junit.*;
-
-//import static org.junit.Assert.*;
-
-//import il.org.spartan.spartanizer.ast.navigate.*;
-//import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.utils.Int;
 
 /** @author David Cohen
  * @author Shahar Yair
  * @author Zahi Mizrahi
  * @since 16-11-9 */
  public class Issue801{
-  @SuppressWarnings("static-method") @Test public void test01() {
-    return ; 
+  @SuppressWarnings({ "static-method", "static-access" }) @Test public void testInt() {
+    final Int x = new Int();
+    x.valueOf(5);
+    assertEquals(Integer.valueOf(5), x.inner());
   }
+
  }
