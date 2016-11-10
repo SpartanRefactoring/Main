d1531bde220ebf1ac2789c5864a157ad74037708
M4tt30rru <matt.orru@gmail.com>
2016-11-10 14:17:36 +0200

    reloaded yesterday work

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSelection.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSelection.java
@@ -20,15 +20,18 @@ public class CommandLineSelection extends AbstractSelection<CommandLineSelection
     inner = compilationUnits != null ? compilationUnits : new ArrayList<>();
     this.name = name;
   }
+
   public List<CompilationUnit> getCompilationUnits() {
     final List<CompilationUnit> $ = new ArrayList<>();
     for (final WrappedCompilationUnit ¢ : inner)
       $.add(¢.compilationUnit);
     return $;
   }
+
   public List<WrappedCompilationUnit> get() {
     return inner;
   }
+
   /** Factory method for empty selection
    * @return empty selection */
   public static CommandLineSelection empty() {
@@ -38,28 +41,49 @@ public class CommandLineSelection extends AbstractSelection<CommandLineSelection
   public static class Util {
     private static String presentSourcePath = "."; // default input path
 
+    public static String getPresentSourcePath() {
+      return presentSourcePath;
+    }
+
+    public static void setPresentSourcePath(String presentSourcePath) {
+      Util.presentSourcePath = presentSourcePath;
+    }
+
     /** @return CommandLineSelection */
     public static CommandLineSelection getAllCompilationUnits() {
       return getSelection();
     }
+
     /** @return CommandLineSelection */
     private static CommandLineSelection getSelection() {
       return null;
     }
+
     /** @return */
     public static AbstractSelection<CommandLineSelection> get() {
       return getFromPath(presentSourcePath);
     }
+
     /** @return */
     public static AbstractSelection<CommandLineSelection> get(final String from) {
       return getFromPath(from);
     }
+    
+    /** @param path
+     * @return
+     */
+
     public static AbstractSelection<CommandLineSelection> getFromPath(final String path) {
       final List<WrappedCompilationUnit> cuList = new ArrayList<>();
       for (final File ¢ : new FilesGenerator(".java").from(path))
         cuList.add(WrappedCompilationUnit.of((CompilationUnit) makeAST.COMPILATION_UNIT.from(¢)));
       return new CommandLineSelection(cuList, "selection");
     }
+    
+    /** @param from
+     * @return
+     */
+
     public static List<CompilationUnit> getAllCompilationUnit(final String from) {
       final List<CompilationUnit> $ = new ArrayList<>();
       for (final File ¢ : new FilesGenerator(".java").from(from)) {
@@ -91,11 +115,13 @@ public class CommandLineSelection extends AbstractSelection<CommandLineSelection
     inner = cuList;
     System.err.println("Loading selection: done!");
   }
+
   public CommandLineSelection buildAll() {
     for (final WrappedCompilationUnit ¢ : compilationUnits)
       ¢.build();
     return this;
   }
+
   public static AbstractSelection<?> of(final List<CompilationUnit> ¢) {
     return new CommandLineSelection(WrappedCompilationUnit.ov(¢), "cuList");
   }
