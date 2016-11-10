d2f1b88e94f4e6116ff53f359f91af2a951ac1c7
Yossi Gil <yossi.gil@gmail.com>
2016-11-10 14:33:21 +0200

    Cleanup

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
@@ -1,28 +1,13 @@
 package il.org.spartan.spartanizer.cmdline;
 
-//import static il.org.spartan.external.External.Introspector.*;
-
-import java.util.*;
-
-import il.org.spartan.*;
-//import il.org.spartan.external.*;
-
 /** Simplified version of command line client that uses spartanizer applicator
  * @author Matteo Orru' */
 public class CommandLineClient {
   // TODO Matteo: try to fix compilation errors - matteo
-//  @External(alias = "i", value = "name of the input directory")  
   static String inputDir = ".";
-//  @External(alias = "o", value = "name of the output directory")  
   private static String outputDir = "/tmp";
-//  @External(alias = "d", value = "default values for the directories")  
-  private static boolean devault;
-
-  public static void main(final String[] args) {
-//    for (final String ¢ : args.length != 0 ? args : as.array("."))
-//      new CommandLineSpartanizer(¢).apply();
+public static void main(final String[] args) {
     if(args.length == 0)
-//      usageErrorExit("name(s)", new CommandLineClient());
     processCommandLine(args);
   }
   
@@ -37,25 +22,6 @@ public class CommandLineClient {
 //    r.printExternals();
   }
   
-  private void printExternals() {
-//    System.out.println(usage(this));
-    System.out.println("Externals after processing command line arguments:");
-    System.out.println("==================================================");
-    System.out.println("outputDir: " + outputDir());
-    System.out.println("inputDir: " + inputDir());
-    System.out.println();
-} 
-
-  @SuppressWarnings({ "static-method" }) 
-  private String inputDir() {
-    return inputDir;
-  }
-
-  @SuppressWarnings({ "static-method" }) 
-  private String outputDir() {
-    return outputDir;
-  }
-
   @SuppressWarnings("unused") private static void parseCommandLineArgs(final String[] args) {
     if (args.length == 0)
       printPrompt();
