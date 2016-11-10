716bc876408495465b106bddab9c4fbe2480b70c
shaysegal <siglons@gmail.com>
2016-11-10 19:29:21 +0200

    add test to WriterTest #804 @shaysegal

--- src/test/java/il/org/spartan/spartanizer/utils/WriterTest.java
+++ src/test/java/il/org/spartan/spartanizer/utils/WriterTest.java
@@ -2,16 +2,21 @@ package il.org.spartan.spartanizer.utils;
 
 import org.junit.*;
 
-
 /** Tests of Writer
  * @author Sefi Albo
  * @author Shay Segal
- * @author Dani Shames
- */
+ * @author Dani Shames */
 @SuppressWarnings("static-method") public class WriterTest {
-  
   @Test public void writerTest1() {
     new LogWriter("lib").close();
   }
-  
+  @Test public void writerTest2() {
+    new WriterTestTemp().initializeWriter("\0");
+  }
+}
+
+class WriterTestTemp extends Writer {
+  public WriterTestTemp() {
+    outputPath = "lib";
+  }
 }
