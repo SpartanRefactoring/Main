b578afee9f6ea87b30d986b5e4cb0621b7502445
Yossi Gil <yossi.gil@gmail.com>
2016-11-10 18:16:31 +0200

    Cleanup

--- src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
@@ -10,9 +10,9 @@ import il.org.spartan.plugin.*;
  * @author Matteo Orru'
  * @since 2016 */
 public class CommandLineSpartanizer extends AbstractCommandLineProcessor {
-  private String name;
+  private final String name;
   private boolean selection;
-  private boolean CommandLine$Applicator = true;
+  private final boolean CommandLine$Applicator = true;
   private boolean Spartanizer$Applicator;
   private boolean DefaultApplicator;
 
@@ -26,44 +26,32 @@ public class CommandLineSpartanizer extends AbstractCommandLineProcessor {
   @Override public void apply() {
     System.out.println("presentsSourcePath:" + presentSourcePath);
     try {
-//      if (collectApplicator) {
-//        Reports.initializeReport(folder + name + ".tips.CSV", "tips");
-//        CollectApplicator.defaultApplicator().selection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(presentSourcePath)))
-//            .go();
-//        Reports.close("tips");
-//        System.err.println("CollectApplicator: " + "Done!");
-//      }
-      
+      // if (collectApplicator) {
+      // Reports.initializeReport(folder + name + ".tips.CSV", "tips");
+      // CollectApplicator.defaultApplicator().selection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(presentSourcePath)))
+      // .go();
+      // Reports.close("tips");
+      // System.err.println("CollectApplicator: " + "Done!");
+      // }
       System.out.println("Reports.getOutputFolder(): " + Reports.getOutputFolder());
       Reports.initializeFile(Reports.getOutputFolder() + "/" + name + ".before.java", "before");
       Reports.initializeFile(Reports.getOutputFolder() + "/" + name + ".after.java", "after");
       Reports.initializeReport(Reports.getOutputFolder() + "/" + name + ".CSV", "metrics");
       Reports.initializeReport(Reports.getOutputFolder() + "/" + name + ".spectrum.CSV", "spectrum");
-      
       if (DefaultApplicator)
-        CommandLineApplicator.defaultApplicator()
-                             .defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder()))
-                             .defaultListenerNoisy()
-                             .go();
-      if (Spartanizer$Applicator )
-        CommandLineApplicator.defaultApplicator()
-                             .defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder()))
-                             .defaultRunAction(new Spartanizer$Applicator())
-                             .defaultListenerNoisy()
-                             .go();
+        CommandLineApplicator.defaultApplicator().defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder())).defaultListenerNoisy()
+            .go();
+      if (Spartanizer$Applicator)
+        CommandLineApplicator.defaultApplicator().defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder()))
+            .defaultRunAction(new Spartanizer$Applicator()).defaultListenerNoisy().go();
       if (CommandLine$Applicator)
-        CommandLineApplicator.defaultApplicator()
-                             .defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder()))
-                             .defaultRunAction(new CommandLine$Applicator())
-                             .defaultListenerNoisy()
-                             .go();
-      
+        CommandLineApplicator.defaultApplicator().defaultSelection(CommandLineSelection.Util.get(Reports.getInputFolder()))
+            .defaultRunAction(new CommandLine$Applicator()).defaultListenerNoisy().go();
       Reports.close("metrics");
       Reports.close("spectrum");
       Reports.closeFile("before");
       Reports.closeFile("after");
       System.err.println("commandLineApplicator: " + "Done!");
-
       if (selection)
         CommandLineApplicator.defaultApplicator().defaultListenerNoisy()
             .defaultSelection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(presentSourcePath)))
