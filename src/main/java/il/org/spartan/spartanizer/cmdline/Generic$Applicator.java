4f1e0a60fd945874a369ac0597d0c5e873b7a13a
M4tt30rru <matt.orru@gmail.com>
2016-11-10 15:45:23 +0200

    added Generic$Applicator

---
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Generic$Applicator.java
@@ -0,0 +1,49 @@
+package il.org.spartan.spartanizer.cmdline;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.*;
+import il.org.spartan.spartanizer.dispatch.*;
+
+public class Generic$Applicator {
+
+  protected static List<Class<? extends ASTNode>> selectedNodeTypes = as.list(MethodDeclaration.class, 
+                InfixExpression.class, //
+                VariableDeclarationFragment.class, //
+                EnhancedForStatement.class, //
+                Modifier.class, //
+                VariableDeclarationExpression.class, //
+                ThrowStatement.class, //
+                CastExpression.class, //
+                ClassInstanceCreation.class, //
+                SuperConstructorInvocation.class, //
+                SingleVariableDeclaration.class, //
+                ForStatement.class, //
+                WhileStatement.class, //
+                Assignment.class, //
+                Block.class, //
+                PostfixExpression.class, //
+                InfixExpression.class, //
+                InstanceofExpression.class, //
+                MethodDeclaration.class, //
+                MethodInvocation.class, //
+                IfStatement.class, //
+                PrefixExpression.class, //
+                ConditionalExpression.class, //
+                TypeDeclaration.class, //
+                EnumDeclaration.class, //
+                FieldDeclaration.class, //
+                CastExpression.class, //
+                EnumConstantDeclaration.class, //
+                NormalAnnotation.class, //
+                Initializer.class, //
+                VariableDeclarationFragment.class //
+              );
+  public Toolbox toolbox;
+  public int tippersAppliedOnCurrentObject;
+  protected int done;
+  
+  
+}
