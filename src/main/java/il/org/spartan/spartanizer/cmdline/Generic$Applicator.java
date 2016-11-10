25e2869f4a31f9a2b58559261d6dc315f7fc6718
M4tt30rru <matt.orru@gmail.com>
2016-11-10 20:46:49 +0200

    listOfClass not working properly

--- src/main/java/il/org/spartan/spartanizer/cmdline/Generic$Applicator.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Generic$Applicator.java
@@ -15,75 +15,57 @@ public class Generic$Applicator {
   public Toolbox toolbox;
   public int tippersAppliedOnCurrentObject;
   protected int done;
-  protected static List<Class<? extends ASTNode>> selectedNodeTypes = listOfClass();
   
-//  protected static List<Class<? extends ASTNode>> selectedNodeTypes = as.list(MethodDeclaration.class, 
-//                InfixExpression.class, //
-//                VariableDeclarationFragment.class, //
-//                EnhancedForStatement.class, //
-//                Modifier.class, //
-//                VariableDeclarationExpression.class, //
-//                ThrowStatement.class, //
-//                CastExpression.class, //
-//                ClassInstanceCreation.class, //
-//                SuperConstructorInvocation.class, //
-//                SingleVariableDeclaration.class, //
-//                ForStatement.class, //
-//                WhileStatement.class, //
-//                Assignment.class, //
-//                Block.class, //
-//                PostfixExpression.class, //
-//                InfixExpression.class, //
-//                InstanceofExpression.class, //
-//                MethodDeclaration.class, //
-//                MethodInvocation.class, //
-//                IfStatement.class, //
-//                PrefixExpression.class, //
-//                ConditionalExpression.class, //
-//                TypeDeclaration.class, //
-//                EnumDeclaration.class, //
-//                FieldDeclaration.class, //
-//                CastExpression.class, //
-//                EnumConstantDeclaration.class, //
-//                NormalAnnotation.class, //
-//                Initializer.class, //
-//                VariableDeclarationFragment.class //
-//              );
+  protected static List<Class<? extends ASTNode>> selectedNodeTypes = as.list(MethodDeclaration.class, 
+                InfixExpression.class, //
+                VariableDeclarationFragment.class, //
+                EnhancedForStatement.class, //
+                Modifier.class, //
+                VariableDeclarationExpression.class, //
+                ThrowStatement.class, //
+                CastExpression.class, //
+                ClassInstanceCreation.class, //
+                SuperConstructorInvocation.class, //
+                SingleVariableDeclaration.class, //
+                ForStatement.class, //
+                WhileStatement.class, //
+                Assignment.class, //
+                Block.class, //
+                PostfixExpression.class, //
+                InfixExpression.class, //
+                InstanceofExpression.class, //
+                MethodDeclaration.class, //
+                MethodInvocation.class, //
+                IfStatement.class, //
+                PrefixExpression.class, //
+                ConditionalExpression.class, //
+                TypeDeclaration.class, //
+                EnumDeclaration.class, //
+                FieldDeclaration.class, //
+                CastExpression.class, //
+                EnumConstantDeclaration.class, //
+                NormalAnnotation.class, //
+                Initializer.class, //
+                VariableDeclarationFragment.class //
+              );
   
-  static List l = new ArrayList<>();
-  
-  @SuppressWarnings({ "static-access", "unused" }) public static void main(final String[] args){
-    listOfClass();
+  @SuppressWarnings({ "unused" }) public static void main(final String[] args){
+    List<Class<? extends ASTNode>> l = listOfClass();
   }
 
-  private static List<Class<? extends ASTNode>> listOfClass() {
+  @SuppressWarnings({ "unchecked", "rawtypes", "static-access", "unused" }) private static List<Class<? extends ASTNode>> listOfClass() {
+    List l = new ArrayList<>();
     Toolbox tb = new Toolbox().defaultInstance();
     int tipnum = tb.tippersCount();
-    System.out.println(tipnum);
-    System.out.println("hooks: " + tb.hooksCount());
-    int hooknum = 0;
     for (int i = 0; i <= tipnum; ++i){
       List<Tipper<? extends ASTNode>> b = tb.get(i);
       if(b.size()>0){
-        System.out.print("======\t" + ++hooknum + "\t");
-        int j = 0;
         for(Tipper<?> ¢: b){
-          System.out.println(¢.getClass().getSimpleName());
-//          System.out.println("" + ++j + " - " + ¢.getClass().getSuperclass());
-//          System.out.println("" + ++j + " - " + ¢.getClass().getGenericSuperclass().getTypeName());
-//          Type type = ¢.getClass().getGenericSuperclass();
-//          Class<?> clazz = (Class<?>) type.getType();
-          
           Class<? extends Tipper> class1 = ¢.getClass();
-          System.out.println(class1.getGenericSuperclass().getClass());
           ParameterizedType genericSuperclass = (ParameterizedType) class1.getGenericSuperclass();
           Type type = genericSuperclass.getActualTypeArguments()[0];
           if(!l.contains(type))
             l.add(type);
-          System.out.println(l.size());
-          System.out.println(type.getTypeName());
-//          System.out.println("" + ++j + " - " + ¢.getClass().getGenericSuperclass());
-//          System.out.println("" + ++j + " - " + ¢.getClass().getInterfaces());
         }
       }
     }
