b1453a39426c1371ff6cabf5bdf70ab666647906
orenafek <oren.afek@gmail.com>
2016-11-11 15:49:55 +0200

    added weirdo and class constants tests #815

--- src/test/java/il/org/spartan/spartanizer/engine/Issue815.java
+++ src/test/java/il/org/spartan/spartanizer/engine/Issue815.java
@@ -34,6 +34,25 @@ public class Issue815 {
     assertEquals(NameGuess.of("¢¢"), NameGuess.CENT);
     assertNotEquals(NameGuess.of(""), NameGuess.CENT);
   }
+  //_$¢
+  @SuppressWarnings("static-method") @Test public void weirdoCheckForOfMethod(){
+    assertEquals(NameGuess.of("_$¢"), NameGuess.WEIRDO);
+    assertEquals(NameGuess.of("_$¢_$¢"), NameGuess.WEIRDO);
+    assertEquals(NameGuess.of("$_¢"), NameGuess.WEIRDO);
+    assertEquals(NameGuess.of("$$__¢_¢_¢"), NameGuess.WEIRDO);
+    assertNotEquals(NameGuess.of("___"), NameGuess.WEIRDO);
+    assertEquals(NameGuess.of("$__$"), NameGuess.WEIRDO);
+    
+  }
   
+  @SuppressWarnings("static-method") @Test public void classConstCheckForOfMethod(){
+    assertEquals(NameGuess.of("A_ABC_CLASS_1"), NameGuess.CLASS_CONSTANT);
+    assertEquals(NameGuess.of("B99"), NameGuess.CLASS_CONSTANT);
+    assertEquals(NameGuess.of("A_35"), NameGuess.CLASS_CONSTANT);
+    assertEquals(NameGuess.of("A______4"), NameGuess.CLASS_CONSTANT);
+    assertNotEquals(NameGuess.of("a_35"), NameGuess.CLASS_CONSTANT);
+    assertNotEquals(NameGuess.of("_A_A"), NameGuess.CLASS_CONSTANT);
+    
+  }
   
 }
