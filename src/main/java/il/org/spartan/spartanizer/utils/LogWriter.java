cafc09bdbad64d80fbf062111950270bfe536b13
Yossi Gil <yossi.gil@gmail.com>
2016-11-11 20:06:39 +0200

    #200

--- src/main/java/il/org/spartan/spartanizer/utils/LogWriter.java
+++ src/main/java/il/org/spartan/spartanizer/utils/LogWriter.java
@@ -1,5 +1,29 @@
 package il.org.spartan.spartanizer.utils;
 
+import java.io.*;
+
+// TODO: Matteo Remove this one --yg
+class Writer {
+  protected PrintWriter writer;
+  protected String outputPath;
+
+  public void close() {
+    writer.close();
+  }
+  protected void initializeWriter(final String outputFileName) {
+    // TODO yossigil , call to this function with Writer and not with one of his
+    // descendant‏ cause nullPointerException maybe need to be abstract?
+    final File outputDir = new File(outputPath);
+    if (!outputDir.exists())
+      outputDir.mkdir();
+    try {
+      writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFileName)));
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
+// TODO Matteo: erase this class
 public final class LogWriter extends Writer {
   public LogWriter(final String outputPath) {
     this.outputPath = outputPath;
@@ -11,5 +35,23 @@ public final class LogWriter extends Writer {
   }
   private void initializeWriter() {
     initializeWriter(outputPath + "/tips.csv");
+  }
+  private static class Writer {
+    protected PrintWriter writer;
+    protected String outputPath;
+
+    public void close() {
+      writer.close();
+    }
+    protected void initializeWriter(final String outputFileName) {
+      final File outputDir = new File(outputPath);
+      if (!outputDir.exists())
+        outputDir.mkdir();
+      try {
+        writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFileName)));
+      } catch (final IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
