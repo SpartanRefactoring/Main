064d117728669adae4a0bf17da5b25e8b3863b9d
Yossi Gil <yossi.gil@gmail.com>
2016-11-11 16:17:20 +0200

    #816 You did not Source->Cleanup your code.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/cmdline/Issue816.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/cmdline/Issue816.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-
 /** Tests of {@link cmdline.TrimmerLog}
  * @author AnnaBel7
  * @author michalcohen
@@ -16,27 +15,22 @@ public class Issue816 {
   @Test public void a() {
     assertEquals(10, TrimmerLog.getMaxApplications());
   }
-  
   @Test public void b() {
     assertEquals(20, TrimmerLog.getMaxTips());
   }
-  
   @Test public void c() {
     assertEquals(30, TrimmerLog.getMaxVisitations());
   }
-  
   @Test public void d() {
     TrimmerLog.setMaxApplications(50);
     assertEquals(50, TrimmerLog.getMaxApplications());
   }
-  
   @Test public void e() {
     TrimmerLog.setMaxTips(50);
     assertEquals(50, TrimmerLog.getMaxTips());
   }
-  
   @Test public void f() {
     TrimmerLog.setMaxVisitations(50);
     assertEquals(50, TrimmerLog.getMaxVisitations());
-  }  
+  }
 }
