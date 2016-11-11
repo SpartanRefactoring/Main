89ece3c7d7129ff3e8834c9752446de309839c91
orimarco <marcovitch.ori@gmail.com>
2016-11-11 11:38:28 +0200

    just some refactoring

--- src/main/java/il/org/spartan/spartanizer/research/patterns/IfThrow.java
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/IfThrow.java
@@ -5,11 +5,11 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.research.*;
 
-/** if(X == null) throw Exception;
+/** if(X) throw Exception;
  * @author Ori Marcovitch
  * @year 2016 */
 public final class IfThrow extends NanoPatternTipper<IfStatement> {
-  private static final UserDefinedTipper<IfStatement> nullTipper = TipperFactory.tipper("if($X == null) throw $X2;", "", "");
+  private static final IfNullThrow nullTipper = new IfNullThrow();
   private static final UserDefinedTipper<IfStatement> tipper = TipperFactory.tipper("if($X) throw $X2;", "explodeWith($X2).when($X);",
       "IfThrow pattern. Go fluent!");
 
