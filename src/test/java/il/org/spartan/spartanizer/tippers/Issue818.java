bd02016ef7861515b86b5999a891f3caf698e435
RoeyMaor <maor.roey@gmail.com>
2016-11-11 21:04:52 +0200

    increased coverage to 92.1 percent. #818

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue818.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue818.java
@@ -10,11 +10,15 @@ import il.org.spartan.spartanizer.engine.*;
 
 /** @author RoeiMaor
  * @author RoeiRaz
- * @since 16-11-11 */
+ * @since 11-11-16 */
 public class Issue818 {
   static final InfixExpressionConcatentateCompileTime tipper = new InfixExpressionConcatentateCompileTime();
   static final InfixExpression ie01 = into.i("\"a\" + \"b\"");
   static final InfixExpression ie02 = into.i("\"a\" + runTimeValue");
+  static final InfixExpression ie03 = into.i("\"a\" * \"b\"");
+  static final InfixExpression ie04 = into.i("\"a\" + b + \"c\"");
+  static final InfixExpression ie05 = into.i("b + \"a\"");
+  static final InfixExpression ie06 = into.i("\"a\" + \"b\" + c");
 
   // This test performs a simple concatenation operation: "a" + "b" => "ab"
   @SuppressWarnings("static-method") @Test public void a() {
@@ -25,5 +29,23 @@ public class Issue818 {
   // Should fail.
   @SuppressWarnings("static-method") @Test public void b() {
     assertNull(az.stringLiteral(tipper.replacement(ie02)));
+  }
+  @SuppressWarnings("static-method") @Test public void c() {
+    assertNull(az.stringLiteral(tipper.replacement(ie03)));
+  }
+  @SuppressWarnings("static-method") @Test public void d() {
+    assert tipper.description() != null;
+  }
+  @SuppressWarnings("static-method") @Test public void e() {
+    assert tipper.description(null) != null;
+  }
+  @SuppressWarnings("static-method") @Test public void f() {
+    assertNull(az.stringLiteral(tipper.replacement(ie04)));
+  }
+  @SuppressWarnings("static-method") @Test public void g() {
+    assertNull(az.stringLiteral(tipper.replacement(ie05)));
+  }
+  @SuppressWarnings("static-method") @Test public void h() {
+    assert tipper.replacement(ie06) != null;
   }
 }
