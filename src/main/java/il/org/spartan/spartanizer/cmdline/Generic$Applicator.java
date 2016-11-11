9d39ef62153598185fb9896afe6732c194b7dec2
Yossi Gil <yossi.gil@gmail.com>
2016-11-11 19:03:38 +0200

    sync

--- src/main/java/il/org/spartan/spartanizer/cmdline/Generic$Applicator.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Generic$Applicator.java
@@ -17,41 +17,67 @@ public class Generic$Applicator {
   public Toolbox toolbox;
   public int tippersAppliedOnCurrentObject;
   protected int done;
-  protected static List<Class<? extends ASTNode>> selectedNodeTypes = as.list(MethodDeclaration.class, InfixExpression.class, //
-      VariableDeclarationFragment.class, //
-      EnhancedForStatement.class, //
-      Modifier.class, //
-      VariableDeclarationExpression.class, //
-      ThrowStatement.class, //
-      CastExpression.class, //
-      ClassInstanceCreation.class, //
-      SuperConstructorInvocation.class, //
-      SingleVariableDeclaration.class, //
-      ForStatement.class, //
-      WhileStatement.class, //
-      Assignment.class, //
-      Block.class, //
-      PostfixExpression.class, //
-      InfixExpression.class, //
-      InstanceofExpression.class, //
-      MethodDeclaration.class, //
-      MethodInvocation.class, //
-      IfStatement.class, //
-      PrefixExpression.class, //
-      ConditionalExpression.class, //
-      TypeDeclaration.class, //
-      EnumDeclaration.class, //
-      FieldDeclaration.class, //
-      CastExpression.class, //
-      EnumConstantDeclaration.class, //
-      NormalAnnotation.class, //
-      Initializer.class, //
-      VariableDeclarationFragment.class //
-  );
 
-  @SuppressWarnings({ "unused" }) public static void main(final String[] args) {
-    final List<Class<? extends ASTNode>> l = listOfClass();
+  private static String fqn_base = "org.eclipse.jdt.core.dom.";
+  
+  protected static List<Class<? extends ASTNode>> selectedNodeTypes = setAll();
+
+  @SuppressWarnings("unchecked") private static List<Class<? extends ASTNode>> setSelected(String ... ss){
+    List<Class<? extends ASTNode>> $ = new ArrayList<>();
+    try {
+      for(String ¢: ss) {
+//        Class<? extends ASTNode> c = (Class<? extends ASTNode>) Class.forName("org.eclipse.jdt.core.dom." + ¢);
+         $.add((Class<? extends ASTNode>) Class.forName(fqn_base + ¢));
+       }
+      } catch (ClassNotFoundException x) {
+        x.printStackTrace();
+      }
+    return as.list($); // useless?
   }
+  
+  private static List<Class<? extends ASTNode>> setAll() {
+    return as.list(MethodDeclaration.class, 
+                  InfixExpression.class, //
+                  VariableDeclarationFragment.class, //
+                  EnhancedForStatement.class, //
+                  Modifier.class, //
+                  VariableDeclarationExpression.class, //
+                  ThrowStatement.class, //
+                  CastExpression.class, //
+                  ClassInstanceCreation.class, //
+                  SuperConstructorInvocation.class, //
+                  SingleVariableDeclaration.class, //
+                  ForStatement.class, //
+                  WhileStatement.class, //
+                  Assignment.class, //
+                  Block.class, //
+                  PostfixExpression.class, //
+                  InfixExpression.class, //
+                  InstanceofExpression.class, //
+                  MethodDeclaration.class, //
+                  MethodInvocation.class, //
+                  IfStatement.class, //
+                  PrefixExpression.class, //
+                  ConditionalExpression.class, //
+                  TypeDeclaration.class, //
+                  EnumDeclaration.class, //
+                  FieldDeclaration.class, //
+                  CastExpression.class, //
+                  EnumConstantDeclaration.class, //
+                  NormalAnnotation.class, //
+                  Initializer.class, //
+                  VariableDeclarationFragment.class //
+                );
+  }
+  
+  @SuppressWarnings({ "unused" }) public static void main(final String[] args){
+//    List<Class<? extends ASTNode>> l = setAll();
+    List<Class<? extends ASTNode>> l = setSelected("MethodDeclaration", "VariableDeclarationFragment");
+    for(Class<? extends ASTNode> i: l){
+      System.out.println(i);
+    }
+  }
+
   @SuppressWarnings({ "unchecked", "rawtypes", "unused" }) private static List<Class<? extends ASTNode>> listOfClass() {
     final List l = new ArrayList<>();
     new Toolbox();
@@ -75,7 +101,7 @@ public class Generic$Applicator {
   /** Printing definition of events that occur during spartanization.
    * @author Ori Roth
    * @since 2.6 */
-  enum message {
+  @SuppressWarnings("unused") private enum message {
     run_start(1, inp -> "Spartanizing " + printableAt(inp, 0)), //
     run_pass(1, inp -> "Pass #" + printableAt(inp, 0)), //
     run_pass_finish(1, inp -> "Pass #" + printableAt(inp, 0) + " finished"), //
