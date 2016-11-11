5b80cbd62450587a40ca92096fedd3393281b0a5
Yossi Gil <yossi.gil@gmail.com>
2016-11-11 19:30:06 +0200

    Make class Str more interesting so that you can really test it

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/Issue819.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/Issue819.java
@@ -9,11 +9,11 @@ import org.junit.*;
  * @author Shimon Azulay
  * @author Idan Atias
  * @since 16-11-11 */
-@SuppressWarnings({ "static-method", "javadoc" }) public class Issue819 {
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class Issue819 {
+  // TODO: 
   @Test public void str_test0() {
-    final Str s = new Str();
-    s.inner = "coverage";
-    assertEquals("coverage", s.inner());
+    assertEquals("coverage", (new Str("coverage")).inner());
   }
   @Test public void str_test1() {
     assertEquals(null, new Str().inner());
