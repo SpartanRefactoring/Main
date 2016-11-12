b322d02ccd7dc91999cf405891054774d19e25d8
Yossi Gil <yossi.gil@gmail.com>
2016-11-12 17:22:28 +0200

    Cleanup

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
@@ -68,10 +68,6 @@ public class CommandLineSpartanizer extends AbstractCommandLineProcessor {
       x.printStackTrace();
     }
   }
-  @SuppressWarnings("unused") private Function<WrappedCompilationUnit, Integer> getSpartanizer() {
-    return u -> Integer.valueOf(
-        new CommandLine$Applicator().apply(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(presentSourcePath))) ? 1 : 0);
-  }
   public void inputDir(final String ¢) {
     presentSourcePath = ¢;
   }
