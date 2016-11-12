6d352b76fab34d6b41cbe6a25df5af824fa35900
Alexander Kaplan <alexkaplan.mail@gmail.com>
2016-11-12 03:10:51 -0800

    added test for count.java #825

---
+++ src/test/java/il/org/spartan/spartanizer/ast/navigate/countTest.java
@@ -0,0 +1,24 @@
+package il.org.spartan.spartanizer.ast.navigate;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+
+/**
+ * test for count.java
+ * @author KaplanAlexander
+ * @author Kolikant
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc", "unused" }) 
+public class countTest {
+  @Test public void a() {
+    assertNotEquals("a", "b");
+  }
+}
