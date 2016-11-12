482fedcb43f6216fc128f6152a138dfcf2ff7f17
sefialbo <sefialbo@gmail.com>
2016-11-12 20:26:45 +0200

    Essence add test #804 @sefialbo

--- src/test/java/il/org/spartan/spartanizer/cmdline/EssenceTest.java
+++ src/test/java/il/org/spartan/spartanizer/cmdline/EssenceTest.java
@@ -24,4 +24,13 @@ import org.junit.*;
     assertEquals("", Essence.of("\r\n"));
     assertEquals("", Essence.of("\n\r"));
   }
+  @Test public void simpleTest2() {
+    assertEquals("a b", Essence.of("a    b\r\n\n\r"));
+    assertEquals("a b c d", Essence.of("a  b\r\nc\n\r\td\r\n\r"));
+    assertEquals("", Essence.of("//comment\r\n\r\r\r\n"));
+    assertEquals("", Essence.of("/*multi_line_comment\n*\n*/"));
+    assertEquals("try_tabs b", Essence.of("try_tabs\t\t   b \t\t\t\r\n"));
+    assertEquals("10 3", Essence.of("1\t0 3"));
+    assertEquals("1078", Essence.of("1\t07\t\t\t8"));
+  }
 }
