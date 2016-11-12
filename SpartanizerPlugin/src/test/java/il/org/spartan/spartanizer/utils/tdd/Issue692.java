72494b55fdbf30e90d91f12d9c822e0fb47b25f3
Yossi Gil <yossi.gil@gmail.com>
2016-11-12 17:22:04 +0200

    #692: bad karma for #674 #648, on `@SuppressWarnings` is enough. Fixed it for you. added another todo.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue692.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue692.java
@@ -10,37 +10,35 @@ import org.junit.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
-// TODO: You have warnings. --yg
-// Viviansh: now there is no warnings 
-
 /** @author Vivian Shehadeh
  * @author Ward Mattar
  * @since 16-11-06 */
+@SuppressWarnings("static-method") //
 public class Issue692 {
-  @SuppressWarnings("static-method") @Test public void test0() {
+  @Test public void test0() {
     assertNull(getAll.invocations((MethodInvocation) null));
   }
-  @SuppressWarnings("static-method") @Test public void test1() {
+  @Test public void test1() {
     assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,2,3)"))).size(), 0);
   }
-  @SuppressWarnings("static-method") @Test public void test2() {
+  @Test public void test2() {
     assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,2,i)"))).size(), 1);
   }
-  @SuppressWarnings("static-method") @Test public void test3() {
+  @Test public void test3() {
+    // TODO: you may simplify the code by writing something such 
+    // as new TreeSet(az.list("a","b")) --yg
     final Set<String> tmp = new TreeSet<>();
     tmp.add("j");
     tmp.add("i");
     assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,foo(2,j),i)"))), tmp);
   }
-  @SuppressWarnings("static-method") @Test public void test4() {
+  @Test public void test4() {
     final Set<String> tmp = new TreeSet<>();
     tmp.add("j");
     tmp.add("i");
     assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,foo(2,m(j)),i)"))), tmp);
   }
-  @SuppressWarnings("static-method") @Test public void test5() {
-    assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,foo(2,m(1)),2)"))), (new TreeSet<>()));
+  @Test public void test5() {
+    assertEquals(getAll.invocations(az.methodInvocation(wizard.ast("example(1,foo(2,m(1)),2)"))), new TreeSet<>());
   }
-  
-  
 }
