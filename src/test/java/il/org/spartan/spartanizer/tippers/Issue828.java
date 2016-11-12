cc24e6c5b1241526e4d254248fab1a165965ad30
geniash <genia.shandalov@gmail.com>
2016-11-12 20:43:34 +0200

    test to prequisite method added #828

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue828.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue828.java
@@ -16,23 +16,24 @@ public class Issue828 {
       .statements().get(0);
   static ForStatement trueFor = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{for(int i=0;true;i++){x=7;}}")).getBody()
       .statements().get(0);
-  static ForStatement trueStatementFor = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{x=7; for(int i=0;x==7;i++){x=7;}}")).getBody()
-      .statements().get(1);
-  static ForStatement obviouseTrueStatement = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{for(int i=0;i==i;i++){x=7;}}")).getBody()
-      .statements().get(0);
-  static ForStatement numEqualTrueStatement = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{for(int i=0;5>3;i++){x=7;}}")).getBody()
-      .statements().get(0);
-  static ForStatement strEqualTrueStatement = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{for(int i=0;!\"h\".equals(\"828\");i++){x=7;}}")).getBody()
-      .statements().get(0);
-  static ForTrueConditionRemove s=new ForTrueConditionRemove();
-  
-  
+  static ForStatement falseFor = (ForStatement) ((MethodDeclaration) wizard.ast("public void foo(int x)" + "{x=7; for(int i=0;false;i++){x=7;}}"))
+      .getBody().statements().get(1);
+  static ForStatement obviouseTrueStatement = (ForStatement) ((MethodDeclaration) wizard
+      .ast("public void foo(int x)" + "{for(int i=0;i==i;i++){x=7;}}")).getBody().statements().get(0);
+  static ForTrueConditionRemove s = new ForTrueConditionRemove();
+
   @SuppressWarnings("static-method") @Test public void descriptionTest() {
     assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(simpleFor)));
     assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(trueFor)));
-    assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(trueStatementFor)));
+    assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(falseFor)));
     assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(obviouseTrueStatement)));
-    assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(numEqualTrueStatement)));
-    assertTrue("Convert loop: 'for(?;true;?)' to 'for(?;;?)'".equals(s.description(strEqualTrueStatement)));
+  }
+  
+  @SuppressWarnings("static-method") @Test public void prerequisiteTest() {
+    assertFalse(s.prerequisite(simpleFor));
+    assertTrue(s.prerequisite(trueFor));
+    assertFalse(s.prerequisite(falseFor));
+    assertFalse(s.prerequisite(obviouseTrueStatement));
+    assertFalse(s.prerequisite(null));
   }
 }
