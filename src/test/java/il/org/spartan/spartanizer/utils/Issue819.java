c03a8ce7f59764d87d1aca926e411e1f3d15be18
Yossi Gil <yossi.gil@gmail.com>
2016-11-13 19:40:40 +0200

    #819: you did not apply cleanup before committing...

--- src/test/java/il/org/spartan/spartanizer/utils/Issue819.java
+++ src/test/java/il/org/spartan/spartanizer/utils/Issue819.java
@@ -13,7 +13,7 @@ import org.junit.*;
 public class Issue819 {
   // TODO:
   @Test public void str_test0() {
-    assertEquals("coverage", (new Str("coverage")).inner());
+    assertEquals("coverage", new Str("coverage").inner());
   }
   @Test public void str_test1() {
     assertEquals(null, new Str().inner());
