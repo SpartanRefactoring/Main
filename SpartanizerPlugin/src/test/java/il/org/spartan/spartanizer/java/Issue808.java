e59231bb726fbd5bf2ab49e6c25fe10c68743b01
Ori Roth <ori.rothh@gmail.com>
2016-11-13 17:09:49 +0200

    use of duplicate in #808

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
@@ -1,12 +1,12 @@
 
 package il.org.spartan.spartanizer.java;
-/*import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
+import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-*/
 
 /** 
  * @author David Cohen
@@ -15,8 +15,8 @@ import il.org.spartan.spartanizer.ast.safety.*;
  * @since 16-11-9
  **/
  public class Issue808{
-  /* @Test public void testTerm01() {
+  @SuppressWarnings("static-method") @Test public void testTerm01() {
     Expression ex = az.simpleName(wizard.ast("x"));
-    assertNotEquals(ex, (new Term(true, ex)).asExpression());
-  }*/
+    assertNotEquals(ex, (new Term(true, duplicate.of(ex))).asExpression());
+  }
  }
