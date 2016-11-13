3587d6039622caef7b15419e6419dc9ca7972a9c
Yossi Gil <yossi.gil@gmail.com>
2016-11-13 19:17:10 +0200

    Cleanup

--- src/main/java/il/org/spartan/spartanizer/tippers/InfixMultiplicationEvaluate.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/InfixMultiplicationEvaluate.java
@@ -7,7 +7,6 @@ import java.util.*;
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.InfixExpression.*;
 
-import il.org.spartan.plugin.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.engine.type.Primitive.*;
