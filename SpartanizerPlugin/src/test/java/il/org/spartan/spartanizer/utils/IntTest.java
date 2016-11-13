78ee38a51a144601d100915d7d824b07421af1f0
Koby Ben Shimol <kobybs@gmail.com>
2016-11-13 21:14:26 +0200

    added coverage for Int.ValueOf

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/IntTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/IntTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class IntTest {
-  @Test public void test() {
-    
+  
+  
+@Test public void testValueOf() {
+    assertEquals(Int.valueOf(0).inner, 0);
   }
 }
