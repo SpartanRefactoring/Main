ef678242d72f466d2307a6374780bfe7c0cd7740
yonatan zarecki <yonilx@gmail.com>
2016-11-13 15:45:42 +0200

    add test file with stubs #820

---
+++ src/test/java/il/org/spartan/spartanizer/engine/LinguisticTest.java
@@ -0,0 +1,41 @@
+package il.org.spartan.spartanizer.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class LinguisticTest {
+  @Test public void testTrim() {
+    fail("Not yet implemented");
+  }
+  @Test public void testTrimAbsolute() {
+    fail("Not yet implemented");
+  }
+  @Test public void testTime() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralsStringInteger() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralesStringInteger() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralsStringAtomicInteger() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralesStringAtomicInteger() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralsStringInt() {
+    fail("Not yet implemented");
+  }
+  @Test public void testPluralesStringInt() {
+    fail("Not yet implemented");
+  }
+  @Test public void testUnknownIfNullX() {
+    fail("Not yet implemented");
+  }
+  @Test public void testUnknownIfNullXFunctionOfXQ() {
+    fail("Not yet implemented");
+  }
+}
