c5b80131173ee0477af96c6362aa09bb0f0cc828
rodedzats <rodedzats@gmail.com>
2016-11-13 15:59:34 +0200

    Added test for trimAbsolute #820

--- src/test/java/il/org/spartan/spartanizer/engine/LinguisticTest.java
+++ src/test/java/il/org/spartan/spartanizer/engine/LinguisticTest.java
@@ -5,37 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 @SuppressWarnings("static-method") public class LinguisticTest {
-  @Test public void testTrim() {
-    fail("Not yet implemented");
-  }
-  @Test public void testTrimAbsolute() {
-    fail("Not yet implemented");
-  }
-  @Test public void testTime() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralsStringInteger() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralesStringInteger() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralsStringAtomicInteger() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralesStringAtomicInteger() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralsStringInt() {
-    fail("Not yet implemented");
-  }
-  @Test public void testPluralesStringInt() {
-    fail("Not yet implemented");
-  }
-  @Test public void testUnknownIfNullX() {
-    fail("Not yet implemented");
-  }
-  @Test public void testUnknownIfNullXFunctionOfXQ() {
-    fail("Not yet implemented");
+  @Test public void testTrimAbsoluteReturnsSameStringForShortString() {
+    assertNull(Linguistic.trimAbsolute(null,Linguistic.TRIM_THRESHOLD,Linguistic.TRIM_SUFFIX));
+    assertEquals("short string", Linguistic.trimAbsolute("short string", Linguistic.TRIM_THRESHOLD, Linguistic.TRIM_SUFFIX));
   }
 }
