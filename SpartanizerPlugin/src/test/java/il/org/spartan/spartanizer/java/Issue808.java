40f0a56fd860ec00aea5b55e2f5d9af2bb0f9416
DavidCohen55 <dcohen484@gmail.com>
2016-11-13 07:02:36 -0800

    Added Issue808.java, @OriRoth #808

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
@@ -0,0 +1,22 @@
+
+package il.org.spartan.spartanizer.java;
+/*import static org.junit.Assert.*;
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+*/
+
+/** 
+ * @author David Cohen
+ * @author Shahar Yair
+ * @author Zahi Mizrahi
+ * @since 16-11-9
+ **/
+ public class Issue808{
+  /* @Test public void testTerm01() {
+    Expression ex = az.simpleName(wizard.ast("x"));
+    assertNotEquals(ex, (new Term(true, ex)).asExpression());
+  }*/
+ }
