ad6a7724287230c94239240745b7c60a5c828416
yonatan zarecki <yonilx@gmail.com>
2016-11-13 16:15:56 +0200

    add another test for AbsoluteTrim #820

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/engine/LinguisticTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/engine/LinguisticTest.java
@@ -9,4 +9,12 @@ import org.junit.*;
     assertNull(Linguistic.trimAbsolute(null,Linguistic.TRIM_THRESHOLD,Linguistic.TRIM_SUFFIX));
     assertEquals("short string", Linguistic.trimAbsolute("short string", Linguistic.TRIM_THRESHOLD, Linguistic.TRIM_SUFFIX));
   }
+  
+  @Test public void testTrimAbsoluteTrimsOnLongStrings() {
+    StringBuilder sb = new StringBuilder();
+    for (int ¢=0;¢<=Linguistic.TRIM_THRESHOLD;++¢)
+      sb.append("a");
+    assertEquals((sb + "").substring(0, (sb + "").length() - 4) + Linguistic.TRIM_SUFFIX,
+        Linguistic.trimAbsolute((sb + ""), Linguistic.TRIM_THRESHOLD, Linguistic.TRIM_SUFFIX));
+  }
 }
