b6058f427c4fc33a8b58bcf6cf73e0d7a3a0f775
orimarco <marcovitch.ori@gmail.com>
2016-11-13 19:32:57 +0200

    generalizer

--- src/test/java/il/org/spartan/spartanizer/leonidas/normalizeTest.java
+++ src/test/java/il/org/spartan/spartanizer/leonidas/normalizeTest.java
@@ -9,7 +9,7 @@ import il.org.spartan.spartanizer.research.util.*;
 /** @author Ori Marcovitch
  * @since 2016 */
 @SuppressWarnings("static-method") //
-public class NormalizeTest {
+public class normalizeTest {
   @Test public void testRenaming() {
     assertEquals("if(a == b) return c(a, d());", normalize.shortenIdentifiers("if(omg == val) return oomph(omg, dear());"));
   }
