cd7041407bd62a0087852648d08cc7fd6bbc1a87
YuvalSimon <yuvaltechnion@gmail.com>
2016-11-13 21:10:56 +0200

    tested Int.inner() method #832

--- src/test/java/il/org/spartan/spartanizer/utils/IntTest.java
+++ src/test/java/il/org/spartan/spartanizer/utils/IntTest.java
@@ -1,11 +1,13 @@
 package il.org.spartan.spartanizer.utils;
 
-import static org.junit.Assert.*;
-
 import org.junit.*;
 
-public class IntTest {
-  @Test public void test() {
-    
+@SuppressWarnings("static-method") public class IntTest {
+  @Test public void testInner() {
+    Int $ = new Int();
+    $.inner = 4;
+    assert $.inner() == Integer.valueOf(4);
+    $.inner += 3;
+    assert $.inner() == Integer.valueOf(7);
   }
 }
