2e12076f34bb7f89b89ca8e3bc06d552c8879506
Yossi Gil <yossi.gil@gmail.com>
2016-11-13 19:41:34 +0200

    #808: you did not apply cleanup before pushing.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
@@ -1,6 +1,7 @@
 package il.org.spartan.spartanizer.java;
 
 import static org.junit.Assert.*;
+
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
@@ -14,19 +15,19 @@ import il.org.spartan.spartanizer.ast.safety.*;
  * @since 16-11-9 **/
 public class Issue808 {
   @SuppressWarnings("static-method") @Test public void test01() {
-    Expression ex = az.numberLiteral(wizard.ast("5"));
-    assertNotEquals(ex, (new Term(true, duplicate.of(ex))).asExpression());
+    final Expression ex = az.numberLiteral(wizard.ast("5"));
+    assertNotEquals(ex, new Term(true, duplicate.of(ex)).asExpression());
   }
   @SuppressWarnings("static-method") @Test public void test02() {
-    Expression ex = az.simpleName(wizard.ast("shahar"));
-    assertEquals(ex, (new Term(false, ex)).asExpression());
+    final Expression ex = az.simpleName(wizard.ast("shahar"));
+    assertEquals(ex, new Term(false, ex).asExpression());
   }
   @SuppressWarnings("static-method") @Test public void test03() {
-    Expression ex1 = az.simpleName(wizard.ast("shahar"));
-    Expression ex2 = az.simpleName(wizard.ast("david"));
-    Expression ex3 = az.simpleName(wizard.ast("zahi"));
-    assertEquals(ex1, (new Term(false, ex1)).asExpression());
-    assertNotEquals(ex2, (new Term(true, duplicate.of(ex2))).asExpression());
-    assertNotEquals(ex3, (new Term(true, duplicate.of(ex3))).asExpression());
+    final Expression ex1 = az.simpleName(wizard.ast("shahar"));
+    final Expression ex2 = az.simpleName(wizard.ast("david"));
+    final Expression ex3 = az.simpleName(wizard.ast("zahi"));
+    assertEquals(ex1, new Term(false, ex1).asExpression());
+    assertNotEquals(ex2, new Term(true, duplicate.of(ex2)).asExpression());
+    assertNotEquals(ex3, new Term(true, duplicate.of(ex3)).asExpression());
   }
 }
