65df9a538857a26f8cd66ef941bc6de2ff99a3f5
Yossi Gil <yossi.gil@gmail.com>
2016-11-13 19:39:09 +0200

    put monitor in in a better package

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/Issue808.java
@@ -1,5 +1,5 @@
-
 package il.org.spartan.spartanizer.java;
+
 import static org.junit.Assert.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
@@ -8,13 +8,11 @@ import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
-/** 
- * @author David Cohen
+/** @author David Cohen
  * @author Shahar Yair
  * @author Zahi Mizrahi
- * @since 16-11-9
- **/
- public class Issue808{
+ * @since 16-11-9 **/
+public class Issue808 {
   @SuppressWarnings("static-method") @Test public void test01() {
     Expression ex = az.numberLiteral(wizard.ast("5"));
     assertNotEquals(ex, (new Term(true, duplicate.of(ex))).asExpression());
@@ -31,5 +29,4 @@ import il.org.spartan.spartanizer.ast.safety.*;
     assertNotEquals(ex2, (new Term(true, duplicate.of(ex2))).asExpression());
     assertNotEquals(ex3, (new Term(true, duplicate.of(ex3))).asExpression());
   }
-  
- }
+}
