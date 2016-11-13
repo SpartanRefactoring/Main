8a42ec6abcbca81f99863fae7d24d21158c3c197
Yarden Lev <ylev@campus.technion.ac.il>
2016-11-13 07:18:05 -0800

    Re-add our deleted test class for code.java
    Hopefully, the deletion was a mistake.

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/cmdline/codeTest.java
@@ -0,0 +1,18 @@
+package il.org.spartan.spartanizer.cmdline;
+
+import org.junit.*;
+
+/** @author Sharon Kuninin
+ * @author Yarden Lev
+ * @since Nov 12, 2016 */
+@SuppressWarnings("static-method") public class codeTest {
+  @Test public void noWords() {
+    Assert.assertEquals(0, code.wc(""));
+  }
+  @Test public void oneWord() {
+    Assert.assertEquals(1, code.wc("hello"));
+  }
+  @Test public void fiveWords() {
+    Assert.assertEquals(5, code.wc("hello world how are you?"));
+  }
+}
