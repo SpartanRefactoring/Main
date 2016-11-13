b44f41b961a306c86f1994b5f2d5c229c89b4345
Tomer Dragucki <tomerdragucki@Tomers-MacBook-Pro-2.local>
2016-11-13 23:34:50 +0200

    adding 1 test #835

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
@@ -2,11 +2,15 @@ package il.org.spartan.spartanizer.tippers;
 
 import org.junit.*;
 
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 // import il.org.spartan.spartanizer.ast.navigate.*;
 // import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 import static org.junit.Assert.*;
+
+import org.eclipse.jdt.core.dom.*;
 
 // wizard.ast("<some code>"); // creating an ASTNode
 // az.block(null); // casting an ASTNode to block
@@ -15,9 +19,13 @@ import static org.junit.Assert.*;
 // TrimmerTestsUtils.trimmingOf("<some code>").stays(); // checking no.
 // spartanization
 public class Issue835 {
-  Tipper<?> t = new CastBlockSingletonVariableDefinition();
+  Tipper<Block> t = new CastBlockSingletonVariableDefinition();
 
   @Test public void descriptionNotNull() {
     assertNotNull(t.description());
+  }
+  
+  @Test public void descriptionPrintBlockNotNull() {
+    assertNotNull(t.description(az.block(wizard.ast("{int x;}"))));
   }
 }
