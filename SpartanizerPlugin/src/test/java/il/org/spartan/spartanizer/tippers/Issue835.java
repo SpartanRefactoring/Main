5210b38872bab1a0d5d682484c89f570e98cc936
Tomer Dragucki <tomerdragucki@Tomers-MacBook-Pro-2.local>
2016-11-14 16:27:03 +0200

    adding some tests #835

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
@@ -4,20 +4,11 @@ import org.junit.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-// import il.org.spartan.spartanizer.ast.navigate.*;
-// import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.tipping.*;
-
 import static org.junit.Assert.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-// wizard.ast("<some code>"); // creating an ASTNode
-// az.block(null); // casting an ASTNode to block
-// TrimmerTestsUtils.trimmingOf("if (true) x = x+1;").gives("x += 1;"); //
-// checking spartanization
-// TrimmerTestsUtils.trimmingOf("<some code>").stays(); // checking no.
-// spartanization
 public class Issue835 {
   Tipper<Block> t = new CastBlockSingletonVariableDefinition();
 
@@ -35,5 +26,19 @@ public class Issue835 {
   }
   @SuppressWarnings("static-method") @Test public void emptyBlock3() {
     assertEquals(az.block(wizard.ast("\n{int a;}\n")).statements().size(), 1);
+  }
+  // use t.tip instead of trimmer cause tip is probably unused by the
+  // spartanizer at the moment
+  @Test public void returnNullOnEmptyBlock1() {
+    assertNull(t.tip(az.block(wizard.ast("{}"))));
+  }
+  @Test public void returnNullOnEmptyBlock2() {
+    assertNull(t.tip(az.block(wizard.ast("\n{}\n"))));
+  }
+  @Test public void returnNullIfBlockIfNotSingleVarDef() {
+    assertNull(t.tip(az.block(wizard.ast("{while(true){}}"))));
+  }
+  @Test public void returnNullIfBlockIfNotSingleVarDef2() {
+    assertNull(t.tip(az.block(wizard.ast("{return 1;}"))));
   }
 }
