6bac564f0f63a0386aa026a7f603c278ff08e7c4
Yossi Gil <yossi.gil@gmail.com>
2016-11-14 15:16:07 +0200

    #805 fewer supresswarnigns.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/associativityTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/associativityTest.java
@@ -10,33 +10,37 @@ import il.org.spartan.spartanizer.ast.safety.*;
 /** @author Dor Ma'yan
  * @author Sapir Bismot
  * @since 16-11-9 **/
-
+@SuppressWarnings("static-method")
 public class associativityTest {
-  @SuppressWarnings("static-method") @Test public void test0() {
+  @Test public void test0() {
     assertTrue(!associativity.isLeftToRight(az.infixExpression(wizard.ast("5+4")).getOperator()));
   }
 
-  @SuppressWarnings("static-method") @Test public void test1() {
+  @Test public void test1() {
     assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("(7-4)+2"))));
   }
 
-  @SuppressWarnings("static-method") @Test public void test2() {
+  @Test public void test2() {
     assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("7-(4+2)"))));
   }
 
-  @SuppressWarnings("static-method") @Test public void test3() {
+  @Test public void test3() {
     assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("!q()"))));
   }
-  @SuppressWarnings("static-method") @Test public void test4() {
+
+  @Test public void test4() {
     assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("x=y=z=17"))));
   }
-  @SuppressWarnings("static-method") @Test public void test5() {
+
+  @Test public void test5() {
     assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("++x"))));
   }
-  @SuppressWarnings("static-method") @Test public void test6() {
+
+  @Test public void test6() {
     assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("x++"))));
   }
-  @SuppressWarnings("static-method") @Test public void test7() {
+
+  @Test public void test7() {
     assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("x && y"))));
   }
 }
