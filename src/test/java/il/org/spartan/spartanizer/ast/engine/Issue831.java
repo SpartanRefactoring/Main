5ca62a5c5bb0105cdf6476f7e1cd7433b16435c1
Nikita Dizhur <blenderon@gmail.com>
2016-11-14 17:37:00 +0200

    added extension of MethodScanner #831

--- src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
+++ src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
@@ -1,5 +1,11 @@
 package il.org.spartan.spartanizer.ast.engine;
 
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.engine.*;
+
 /** see Issue #7831 for more details
  * @author Lidia Piatigorski
  * @author Nikita Dizhur
@@ -7,5 +13,18 @@ package il.org.spartan.spartanizer.ast.engine;
  * @since 16-11-14 */
 @SuppressWarnings({ "static-method", "javadoc" })
 public class Issue831 {
+  
+  protected class MethodScannerIExt extends MethodScanner {
+
+    public MethodScannerIExt(MethodDeclaration method) {
+      super(method);
+    }
+
+    @Override protected List<Statement> availableStatements() {
+      return statements;
+    }
+    
+  }
+  
   
 }
