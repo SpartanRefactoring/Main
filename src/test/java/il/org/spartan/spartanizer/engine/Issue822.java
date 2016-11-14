49eefb8789cdf628f761a1e115ca4fc5e12fadde
yosefraisman <yosefraisman@gmail.com>
2016-11-14 17:06:45 +0200

    Added Test for StringBuilder created from a file object #822

--- src/test/java/il/org/spartan/spartanizer/engine/Issue822.java
+++ src/test/java/il/org/spartan/spartanizer/engine/Issue822.java
@@ -14,17 +14,20 @@ import il.org.spartan.spartanizer.ast.navigate.*;
  * @author Yosef Raisman
  * @author Entony Lekhtman
  * @since 16-11-12 */
-@SuppressWarnings("static-method") public class Issue822 {
+@SuppressWarnings("static-method")
+public class Issue822 {
   @Test public void testFromFile() throws IOException {
     final Path p = Files.createTempFile("test_file", ".tmp");
     Files.write(p, Arrays.asList("a = a + b;"));
     assertEquals(wizard.ast("a = a + b;") + "", makeAST.STATEMENTS.from(p.toFile()) + "");
   }
+
   @Test public void returnsNullOnIOException() throws IOException {
     final File f = Files.createTempFile("test_file", ".tmp").toFile();
     f.setReadable(false);
     assertNull(makeAST.string(f));
   }
+
   @Test public void testBuilderException() {
     StringBuilder sb = null;
     try {
@@ -33,5 +36,11 @@ import il.org.spartan.spartanizer.ast.navigate.*;
       fail();
     }
     assertEquals(new StringBuilder() + "", sb + "");
+  }
+
+  @Test public void testBuilderFromFile() throws IOException {
+    final Path p = Files.createTempFile("test_file", ".tmp");
+    Files.write(p, Arrays.asList("a = a + b;"));
+    assertEquals("a = a + b;", makeAST.STATEMENTS.builder(p.toFile()) + "");
   }
 }
