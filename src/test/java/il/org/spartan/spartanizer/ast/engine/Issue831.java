1ced7c39d8ae4d24fb1794a4bb318a85a3da5571
Lidkin5 <lidkin5@gmail.com>
2016-11-14 18:56:34 +0200

    added one statement test, and fixed number of issue #831

--- src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
+++ src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
@@ -10,16 +10,14 @@ import org.junit.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.engine.*;
 
-/** see Issue #7831 for more details
+/** see Issue #831 for more details
  * @author Lidia Piatigorski
  * @author Nikita Dizhur
  * @author Alex V.
  * @since 16-11-14 */
-@SuppressWarnings({ "static-method", "javadoc" })
+@SuppressWarnings({ "javadoc" })
 public class Issue831 {
-  
   protected class MethodScannerIExt extends MethodScanner {
-
     public MethodScannerIExt(MethodDeclaration method) {
       super(method);
     }
@@ -27,15 +25,21 @@ public class Issue831 {
     @Override protected List<Statement> availableStatements() {
       return statements;
     }
-    
   }
-  
+
+  MethodDeclaration fiveStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; int b; int c; int d; int e;}");
+  MethodDeclaration oneStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; }");
+  MethodDeclaration fourStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; ; ; ; }");
+
   @Test public void statementsInScannerAreUndefinedWhenMethodDoesNotHaveBody() {
     assertTrue((new MethodScannerIExt((MethodDeclaration) wizard.ast("public int a(String a);"))).availableStatements() == null);
   }
-  
+
   @Test public void noStatementsInScannerWhenMethodHasEmptyBody() {
     assertTrue((new MethodScannerIExt((MethodDeclaration) wizard.ast("public int a(String a){}"))).availableStatements().isEmpty());
   }
-  
+
+  @Test public void oneStatementInScanner() {
+   assertTrue("int a;\n".equals((new MethodScannerIExt(oneStatMethod).availableStatements().get(0) + "")));
+  }
 }
