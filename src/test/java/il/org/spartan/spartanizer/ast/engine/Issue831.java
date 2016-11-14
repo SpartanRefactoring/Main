094b67a0fd920b3a26a4bdf9d048f4f04740844a
Nikita Dizhur <blenderon@gmail.com>
2016-11-14 17:25:31 +0200

    created class Issue831

---
+++ src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
@@ -0,0 +1,11 @@
+package il.org.spartan.spartanizer.ast.engine;
+
+/** see Issue #7831 for more details
+ * @author Lidia Piatigorski
+ * @author Nikita Dizhur
+ * @author Alex V.
+ * @since 16-11-14 */
+@SuppressWarnings({ "static-method", "javadoc" })
+public class Issue831 {
+  
+}
