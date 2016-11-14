c932feb1c07dc7e9b569c3ada84ff369aa7da48e
Lidkin5 <lidkin5@gmail.com>
2016-11-14 19:39:14 +0200

    added 4 statements test using the iterator #831

--- src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
+++ src/test/java/il/org/spartan/spartanizer/ast/engine/Issue831.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 
-import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
 import org.junit.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
@@ -21,14 +22,14 @@ public class Issue831 {
       super(method);
     }
 
-    @Override protected List<Statement> availableStatements() {
+   @Override protected List<Statement> availableStatements() {
       return statements;
     }
   }
 
   MethodDeclaration fiveStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; int b; int c; int d; int e;}");
   MethodDeclaration oneStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; }");
-  MethodDeclaration fourStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; ; ; ; }");
+  MethodDeclaration fourStatMethod = (MethodDeclaration) wizard.ast("public void foo() {int a; int b; int c; int d; }");
 
   @Test public void statementsInScannerAreUndefinedWhenMethodDoesNotHaveBody() {
     assertTrue((new MethodScannerIExt((MethodDeclaration) wizard.ast("public int a(String a);"))).availableStatements() == null);
@@ -40,5 +41,11 @@ public class Issue831 {
 
   @Test public void oneStatementInScanner() {
     assertTrue("int a;\n".equals((new MethodScannerIExt(oneStatMethod).availableStatements().get(0) + "")));
+  }
+  @Test public void fourStatementInScanner() {
+   String body="";
+    for ( Statement iter : (new MethodScannerIExt(fourStatMethod).statements()))
+      body += iter + "";
+    assertTrue("int a;\nint b;\nint c;\nint d;\n".equals(body));
   }
 }
