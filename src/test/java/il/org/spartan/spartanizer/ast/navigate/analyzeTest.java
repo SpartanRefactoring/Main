ee4fe24123c1ee6de185c9b1d69dd11a657692a7
Vivian Shehadeh <vivian_sh_12@hotmail.com>
2016-11-14 09:24:05 -0800

    Issue #827: added tests and fixed bug

--- src/test/java/il/org/spartan/spartanizer/ast/navigate/analyzeTest.java
+++ src/test/java/il/org/spartan/spartanizer/ast/navigate/analyzeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.*;
 import java.util.stream.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import org.junit.runners.*;
 
@@ -34,5 +35,15 @@ public final class analyzeTest {
     assertTrue(s.contains("b"));
     assertTrue(s.contains("a.b"));
     assertEquals(5, s.size());
+  }
+
+  @Test public void testType0() {
+    assertEquals(analyze.type(searchDescendants.forClass(VariableDeclaration.class).from(wizard.ast("public void m(){ int x; }")).get(0).getName()),
+        "int");
+  }
+
+  @Test public void testType1() {
+    assertEquals(analyze.type(searchDescendants.forClass(VariableDeclaration.class).from(wizard.ast(" public class A{ int x;} ")).get(0).getName()),
+        "int");
   }
 }
