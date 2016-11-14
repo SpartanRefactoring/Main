9ea81783463da66282ae958f5713314a5de168c3
M4tt30rru <matt.orru@gmail.com>
2016-11-14 14:41:26 +0200

    update

--- src/main/java/il/org/spartan/spartanizer/cmdline/TypeFeaturesCollector.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/TypeFeaturesCollector.java
@@ -50,6 +50,8 @@ public class TypeFeaturesCollector extends FilesASTVisitor {
         .put("method", NameGuess.of(¢.getName() + "") == NameGuess.METHOD_OR_VARIABLE) //
         .put("unknonwn", NameGuess.of(¢.getName() + "") == NameGuess.UNKNOWN) //
         .put("weirdo", NameGuess.of(¢.getName() + "") == NameGuess.WEIRDO) //
+        .put("Non whites", count.nonWhiteCharacters(¢)) //
+        .put("Condensed size", metrics.condensedSize(¢)) //
         .put("Dexterity", metrics.dexterity(¢)) //
         .put("Leaves", metrics.leaves(¢)) //
         .put("Nodes", metrics.nodes(¢)) //
