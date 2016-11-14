a41478dc9e3f5eabc1babb6b2a5207c4f9cd723d
Yossi Gil <yossi.gil@gmail.com>
2016-11-14 15:17:29 +0200

    #805: simplified assertions

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/associativityTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/associativityTest.java
@@ -1,7 +1,5 @@
 package il.org.spartan.spartanizer.java;
 
-import static org.junit.Assert.*;
-
 import org.junit.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
@@ -13,34 +11,34 @@ import il.org.spartan.spartanizer.ast.safety.*;
 @SuppressWarnings("static-method")
 public class associativityTest {
   @Test public void test0() {
-    assertTrue(!associativity.isLeftToRight(az.infixExpression(wizard.ast("5+4")).getOperator()));
+    assert !associativity.isLeftToRight(az.infixExpression(wizard.ast("5+4")).getOperator());
   }
 
   @Test public void test1() {
-    assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("(7-4)+2"))));
+    assert !associativity.isRightToLeft(az.expression(wizard.ast("(7-4)+2")));
   }
 
   @Test public void test2() {
-    assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("7-(4+2)"))));
+    assert !associativity.isRightToLeft(az.expression(wizard.ast("7-(4+2)")));
   }
 
   @Test public void test3() {
-    assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("!q()"))));
+    assert associativity.isRightToLeft(az.expression(wizard.ast("!q()")));
   }
 
   @Test public void test4() {
-    assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("x=y=z=17"))));
+    assert associativity.isRightToLeft(az.expression(wizard.ast("x=y=z=17")));
   }
 
   @Test public void test5() {
-    assertTrue(associativity.isRightToLeft(az.expression(wizard.ast("++x"))));
+    assert associativity.isRightToLeft(az.expression(wizard.ast("++x")));
   }
 
   @Test public void test6() {
-    assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("x++"))));
+    assert !associativity.isRightToLeft(az.expression(wizard.ast("x++")));
   }
 
   @Test public void test7() {
-    assertTrue(!associativity.isRightToLeft(az.expression(wizard.ast("x && y"))));
+    assert !associativity.isRightToLeft(az.expression(wizard.ast("x && y")));
   }
 }
