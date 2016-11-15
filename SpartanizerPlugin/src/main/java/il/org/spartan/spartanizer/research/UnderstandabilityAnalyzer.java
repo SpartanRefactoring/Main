632993c78d7748ac903f8cec35f4434adaa97f27
orimarco <marcovitch.ori@gmail.com>
2016-11-15 18:39:42 +0200

    understandability analysis #837

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/UnderstandabilityAnalyzer.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/UnderstandabilityAnalyzer.java
@@ -1,15 +1,29 @@
 package il.org.spartan.spartanizer.research;
 
+import java.util.*;
+
 import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.cmdline.*;
 
-/** Class to count statement inside a method before and after refactoring +
- * patterning
+/** Class to measure Understandability metrics in a method
  * @author Ori Marcovitch
  * @since Nov 3, 2016 */
 public class UnderstandabilityAnalyzer extends MetricalAnalyzer {
   @Override protected int metric(ASTNode ¢) {
-    return metrics.understandability(¢);
+    return metrics.subtreeUnderstandability(¢);
+  }
+
+  public static void main(final String args[]) {
+    try (Scanner reader = new Scanner(System.in)) {
+      String s = "";
+      while (reader.hasNext())
+        s += "\n" + reader.nextLine();
+      System.out.println("before: " + new UnderstandabilityAnalyzer().metric(wizard.ast(s)));
+      String spartanized = new InteractiveSpartanizer().fixedPoint(s);
+      System.out.println("after: " + new UnderstandabilityAnalyzer().metric(wizard.ast(spartanized)));
+      System.out.println(spartanized);
+    }
   }
 }
