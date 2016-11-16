5c316838d5451111e6f2a8416e7a070a71e714e9
orimarco <marcovitch.ori@gmail.com>
2016-11-16 19:55:56 +0200

    some analyzing architecture

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/MetricalAnalyzer.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/MetricalAnalyzer.java
@@ -4,31 +4,17 @@ import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.spartanizer.utils.*;
-
 /** Class to count statement inside a method before and after refactoring +
  * patterning
  * @author Ori Marcovitch
  * @since Nov 3, 2016 */
-public abstract class MetricalAnalyzer {
-  Map<Integer, Int> beforeHistogram = new HashMap<>();
-  Map<Integer, Int> afterHistogram = new HashMap<>();
+public abstract class MetricalAnalyzer<T> {
+  Map<Integer, T> beforeHistogram = new HashMap<>();
+  Map<Integer, T> afterHistogram = new HashMap<>();
 
   protected abstract int metric(ASTNode n);
 
-  public void logMethod(final MethodDeclaration before, final ASTNode after) {
-    ++getSafe(beforeHistogram, Integer(metric(before))).inner;
-    ++getSafe(afterHistogram, Integer(metric(after))).inner;
-  }
-
-  private static Int getSafe(final Map<Integer, Int> m, final Integer i) {
-    m.putIfAbsent(i, new Int());
-    return m.get(i);
-  }
-
-  private static Integer Integer(final int ¢) {
-    return Integer.valueOf(¢);
-  }
+  public abstract void logMethod(final MethodDeclaration before, final ASTNode after);
 
   public void print() {
     System.out.println("[before]");
@@ -45,8 +31,8 @@ public abstract class MetricalAnalyzer {
     System.out.println();
     for (int ¢ = 0; ¢ < max; ++¢)
       if (beforeHistogram.containsKey(¢) || afterHistogram.containsKey(¢))
-        System.out.println("[" + ¢ + "] " + (beforeHistogram.containsKey(¢) ? beforeHistogram.get(¢).inner : "0") + " -> "
-            + (afterHistogram.containsKey(¢) ? afterHistogram.get(¢).inner : "0"));
+        System.out.println("[" + ¢ + "] " + (beforeHistogram.containsKey(¢) ? enumElement(beforeHistogram.get(¢)) : "0") + " -> "
+            + (afterHistogram.containsKey(¢) ? enumElement(afterHistogram.get(¢)) : "0"));
   }
 
   /** [[SuppressWarningsSpartan]] */
@@ -54,21 +40,38 @@ public abstract class MetricalAnalyzer {
     final int max1 = getMax(beforeHistogram);
     final int max2 = getMax(afterHistogram);
     final int max = max1 > max2 ? max1 : max2;
-    int acc1 = 0;
-    int acc2 = 0;
+    double acc1 = 0;
+    double acc2 = 0;
     System.out.println();
     for (int ¢ = 0; ¢ < max; ++¢)
       if (beforeHistogram.containsKey(¢) || afterHistogram.containsKey(¢))
-        System.out.println("[" + ¢ + "] " + (acc1 += beforeHistogram.containsKey(¢) ? beforeHistogram.get(¢).inner : 0) + " -> "
-            + (acc2 += afterHistogram.containsKey(¢) ? afterHistogram.get(¢).inner : 0));
+        System.out.println("[" + ¢ + "] " + tidy(acc1 += beforeHistogram.containsKey(¢) ? enumElement(beforeHistogram.get(¢)) : 0) + " -> "
+            + tidy(acc2 += afterHistogram.containsKey(¢) ? enumElement(afterHistogram.get(¢)) : 0));
   }
 
-  private static int getMax(final Map<Integer, Int> i) {
+  private int getMax(final Map<Integer, T> i) {
     return i.keySet().stream().max((x, y) -> x.intValue() > y.intValue() ? 1 : -1).get().intValue();
   }
 
-  private static void printMap(final Map<Integer, Int> i) {
+  private void printMap(final Map<Integer, T> i) {
     for (final Integer k : i.keySet())
-      System.out.println(k.intValue() + " : " + i.get(k).inner);
+      System.out.println(k.intValue() + " : " + tidy(enumElement(i.get(k))));
   }
+
+  /** If double is integer, removes the .0
+   * @param ¢
+   * @return */
+  public static String tidy(double ¢) {
+    int a = 0;
+    if (¢ != Math.floor(¢))
+      return ¢ + "";
+    a = (int) ¢;
+    return a + "";
+  }
+
+  protected static Integer Integer(final int ¢) {
+    return Integer.valueOf(¢);
+  }
+
+  protected abstract double enumElement(T t);
 }
