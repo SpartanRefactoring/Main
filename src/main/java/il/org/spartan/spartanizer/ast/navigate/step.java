f55db73db98c2a9933b6723e0d72a9d601732875
orenafek <oren.afek@gmail.com>
2016-11-17 21:01:04 +0200

    Done. Added the new LambdaExpression visitor as @OriRoth suggested. not in toolbox. Added some tests. #455

--- src/main/java/il/org/spartan/spartanizer/ast/navigate/step.java
+++ src/main/java/il/org/spartan/spartanizer/ast/navigate/step.java
@@ -601,4 +601,10 @@ public enum step {
   public static SimpleName name(final MethodDeclaration ¢) {
     return ¢ == null ? null : ¢.getName();
   }
+  
+  /** @param ¢ JD
+   * @return */
+  public static Block body(final LambdaExpression ¢) {
+    return ¢ == null ? null : az.block(¢.getBody());
+  }
 }
