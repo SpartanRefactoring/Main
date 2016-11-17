3625fbd523308cf542d20d0144ab907fe409db68
Yossi Gil <yossi.gil@gmail.com>
2016-11-17 21:38:59 +0200

    #455 brushup

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue455.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue455.java
@@ -5,37 +5,35 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-import il.org.spartan.spartanizer.research.patterns.*;
-
 /** @author Oren Afek
- * @since 2016 
- * Testing {@link LambdaExpressionRemoveRedundantCurlyBraces } */
+ * @since 2016 Testing {@link LambdaExpressionRemoveRedundantCurlyBraces } */
 @SuppressWarnings("static-method")
 public class Issue455 {
-  
-  @Test public void singleReturnStatementAndSingleParameterd() {
-    trimmingOf("new ArrayList<Integer>().map(x -> {return x + 1;});")
-        .withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces()).gives("new ArrayList<Integer>().map(x -> x + 1);");
+  @Test public void emptyReturnStatement() {
+    trimmingOf("(x) -> {return;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())//
+        .gives("(x) -> {}");
+  }
+
+  @Test public void keepsParansOnParamsAsTheyWere() {
+    trimmingOf("(x) -> {return x;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())//
+        .gives("(x) -> x");
+    trimmingOf("x -> {return x;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())//
+        .gives("x -> x");
   }
 
   @Test public void simpleBiFunction() {
     trimmingOf("(x,y) -> {return x + y;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())
-        .gives("(x,y) -> x + y");
+        .gives("(x,y) -> x + y")//
+        .stays();
   }
 
   @Test public void simpleProducer() {
-    trimmingOf("() -> {return 42;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces()).gives("() -> 42");
+    trimmingOf("() -> {return 42;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())//
+        .gives("() -> 42");
   }
 
-  @Test public void emptyReturnStatement() {
-    trimmingOf("(x) -> {return;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces()).gives("(x) -> {}");
-  }
-  
-  @Test public void keepsParansOnParamsAsTheyWere(){
-    trimmingOf("(x) -> {return x;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())
-    .gives("(x) -> x");
-    
-    trimmingOf("x -> {return x;}").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())
-    .gives("x -> x");
+  @Test public void singleReturnStatementAndSingleParameterd() {
+    trimmingOf("new ArrayList<Integer>().map(x->{return x+1;});").withTipper(LambdaExpression.class, new LambdaExpressionRemoveRedundantCurlyBraces())//
+        .gives("new ArrayList<Integer>().map(x -> x+1);");
   }
 }
