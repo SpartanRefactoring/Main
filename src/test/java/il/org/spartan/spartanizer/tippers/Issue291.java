d2c94f7db3b73ee713c71f02e9d0242408c17aff
dormaayn <dor.d.ma@gmail.com>
2016-11-18 10:00:44 +0200

    Works for first case (with a little bug the I will solve soon) (Issue #291)

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue291.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue291.java
@@ -6,21 +6,24 @@ import org.junit.*;
 /** @author Dor Ma'ayan
  * @since 18-11-2016 */
 @SuppressWarnings("static-method")
-@Ignore
 public class Issue291 {
   @Test public void test0() {
-    trimmingOf("a+2==3").gives("a==1").stays();
+    trimmingOf("a+2==3").gives("a==3-2").gives("a==1").stays();
   }
 
   @Test public void test1() {
-    trimmingOf("3==a+2").gives("a+2==3").gives("a==1").stays();
+    trimmingOf("3==a+2").gives("a+2==3").gives("a==3-2").gives("a==1").stays();
   }
 
   @Test public void test2() {
-    trimmingOf("a+5==b+2").gives("a==b-3").stays();
+    trimmingOf("a+5==b+2").gives("a==b+2-5");
   }
 
   @Test public void test3() {
-    trimmingOf("a+2.2==3.89").gives("a==1.69").stays();
+    trimmingOf("a+2.2==3.89").gives("a==3.89-2.2").gives("a==1.69").stays();
+  }
+
+  @Test public void test4() {
+    trimmingOf("a+2.2==b").gives("a==b-2.2").stays();
   }
 }
