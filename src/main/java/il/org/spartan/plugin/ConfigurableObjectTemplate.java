98d1e6e60a1db1c96df555ec71c0d43e86bc75ca
Yossi Gil <yossi.gil@gmail.com>
2016-11-19 16:16:34 +0200

    Cleanup

--- src/main/java/il/org/spartan/plugin/ConfigurableObjectTemplate.java
+++ src/main/java/il/org/spartan/plugin/ConfigurableObjectTemplate.java
@@ -2,9 +2,8 @@ package il.org.spartan.plugin;
 
 // TODO: Matteo: check this out, its Yossi's example for a configurable object.
 // See what you can get out of it.
-/** Demo of recommended use of {@link Listener.S}
- * Copy the code, changing the name {@link ConfigurableObjectTemplate} to
- * whatever you need. provide.
+/** Demo of recommended use of {@link Listener.S} Copy the code, changing the
+ * name {@link ConfigurableObjectTemplate} to whatever you need. provide.
  * @author Yossi Gil
  * @year 2016 */
 public interface ConfigurableObjectTemplate {
