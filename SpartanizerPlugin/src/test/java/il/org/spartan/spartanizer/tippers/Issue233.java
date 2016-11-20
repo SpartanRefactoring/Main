5bc09cd638bffc8ea94083995ed62251c2266533
YuvalSimon <yuvaltechnion@gmail.com>
2016-11-20 19:15:56 +0200

    #233 created tipper class and test case class

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue233.java
@@ -0,0 +1,11 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class Issue233 {
+  @Ignore public void test() {
+    fail("Not yet implemented");
+  }
+}
