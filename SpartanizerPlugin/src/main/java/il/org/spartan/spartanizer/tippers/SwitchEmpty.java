dad9bf44c59e5f3212f5be2c4d8cf170f1183c4d
YuvalSimon <yuvaltechnion@gmail.com>
2016-11-20 20:14:55 +0200

    #233 started implementing overriden methods

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchEmpty.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchEmpty.java
@@ -2,19 +2,41 @@ package il.org.spartan.spartanizer.tippers;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
 
+/** convert
+*
+* <pre>
+* switch(x) {}
+* </pre>
+*
+* into
+*
+* <pre>
+* 
+* </pre>
+*
+* .
+* @author Yuval Simon
+* @since 2016-11-20 */
+
 public final class SwitchEmpty extends ReplaceCurrentNode<SwitchStatement> implements TipperCategory.Collapse {
 
-  @Override public ASTNode replacement(@SuppressWarnings("unused") SwitchStatement __) {
-    // TODO Auto-generated method stub
-    return null;
+  //TODO: yuval - check the below method and finish the other methods
+  @Override public Statement replacement(@SuppressWarnings("unused") final SwitchStatement __) {
+  //    return instanceOf(EmptyStatement.class, ¢);
+    return (Statement) wizard.ast(";");
+  }
+  
+  @Override public boolean prerequisite(final SwitchStatement s) {
+    return false;
   }
 
-  @Override public String description(@SuppressWarnings("unused") SwitchStatement __) {
-    // TODO Auto-generated method stub
-    return null;
+  @Override public String description(@SuppressWarnings("unused") final SwitchStatement __) {
+    return "Remove empty switch statement";
   }
 
 }
