c956a187d96cd5a5d56cb0032687c919da8e8064
dormaayn <dor.d.ma@gmail.com>
2016-11-22 19:01:36 +0200

    Issue Solved (#308)

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue308.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue308.java
@@ -6,7 +6,6 @@ import org.junit.*;
 /** @author Dor Ma'ayan
  * @since 22-11-2016 */
 @SuppressWarnings("static-method")
-@Ignore
 public class Issue308 {
   @Test public void test0() {
     trimmingOf("try{int a;int b; return a+b;}catch(Exception e){return e;}")//
@@ -21,5 +20,15 @@ public class Issue308 {
   @Test public void test2() {
     trimmingOf("try{int a;int b; return a+b;}catch(Exception e){return e;}finally{int c=0;}")//
         .gives("try{int a;int b; return a+b;}catch(Exception ¢){return ¢;}finally{int c=0;}");
+  }
+
+  @Test public void test3() {
+    trimmingOf("try{int a;int b; return a+b;}catch(Exception e){t.find();return e;}finally{int c=0;}")//
+        .gives("try{int a;int b; return a+b;}catch(Exception ¢){t.find();return ¢;}finally{int c=0;}");
+  }
+
+  @Test public void test4() {
+    trimmingOf("try{int a;int b; return a+b;}catch(Exception e){t.find();return e;}catch(Exceptional e){return e;}finally{int c=0;}")//
+        .gives("try{int a;int b; return a+b;}catch(Exception ¢){t.find();return ¢;}catch(Exceptional ¢){return ¢;}finally{int c=0;}");
   }
 }
