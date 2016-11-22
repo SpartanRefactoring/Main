cbde561266195ea0c6eeb512bf73421e6c48858d
SapirBismot <Sapirbismot@gmail.com>
2016-11-22 13:20:05 +0200

    test with finally (issue #234)

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue234.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue234.java
@@ -7,6 +7,7 @@ import org.junit.*;
 /** @author Sapir Bismot
  * @since 2016-11-21 */
 @SuppressWarnings("static-method")
+@Ignore
 public class Issue234 {
   @Test public void b$01() {
     trimmingOf("try { f(); } catch(Exception e) { return -1; }").stays();
@@ -18,5 +19,8 @@ public class Issue234 {
 
   @Test public void b$03() {
     trimmingOf("int a; try { } catch(Exception e) { return -1; }").gives("int a;");
+  }
+  @Test public void b$04() {
+    trimmingOf("int a=5; try { } catch(Exception e) { return -1; } finally { ++a; }").gives("int a=5; ++a;");
   }
 }
