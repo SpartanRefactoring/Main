97f3d513ab79b20868df8d580779574f6e332ee3
SapirBismot <Sapirbismot@gmail.com>
2016-11-22 12:40:44 +0200

    test case (issue #234)

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue234.java
@@ -0,0 +1,23 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/** @author Sapir Bismot
+ * @since 2016-11-21 */
+@SuppressWarnings("static-method")
+@Ignore
+public class Issue234 {
+  @Test public void b$01() {
+    trimmingOf("try { f(); } catch(Exception e) { return -1; }").stays();
+  }
+
+  @Test public void b$02() {
+    trimmingOf("try { } catch(Exception e) { return -1; }").gives("");
+  }
+
+  @Test public void b$03() {
+    trimmingOf("int a; try { } catch(Exception e) { return -1; }").gives("int a;");
+  }
+}
