fc2ca1e42d99f1be5c3625cd8cccf80a102dfa93
Ori Roth <ori.rothh@gmail.com>
2016-11-23 23:31:38 +0200

    get plugin jar path

--- src/main/java/il/org/spartan/libraries/LibrariesManagement.java
+++ src/main/java/il/org/spartan/libraries/LibrariesManagement.java
@@ -1,5 +1,7 @@
 package il.org.spartan.libraries;
 
+import java.io.*;
+import java.net.*;
 import java.util.*;
 import java.util.function.*;
 
@@ -8,9 +10,43 @@ import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.core.*;
 
 import il.org.spartan.plugin.*;
+import il.org.spartan.plugin.Plugin;
 import il.org.spartan.spartanizer.utils.*;
 
 public class LibrariesManagement {
+  public static IPath getPluginJarPath() throws IOException {
+    return new Path(FileLocator.resolve(FileLocator.find(Plugin.plugin().getBundle(), new Path(""), null)).getPath().replaceAll("!.*", "")
+        .replaceAll("^file..", ""));
+  }
+
+  public static URL getPluginJarPath(IPath ¢) throws IOException {
+    return FileLocator.resolve(FileLocator.find(Plugin.plugin().getBundle(), ¢, null));
+  }
+
+  /** [[SuppressWarningsSpartan]] --bug */
+  public static boolean addLibrary(final IPath path) {
+    return touchLibrary(path, (l, p) -> l.add(JavaCore.newLibraryEntry(p, null, null)));
+  }
+
+  public static boolean addLibrary(final String path) {
+    return addLibrary(new Path(path));
+  }
+
+  /** [[SuppressWarningsSpartan]] --bug */
+  public static boolean removeLibrary(final IPath path) {
+    return touchLibrary(path, (l, p) -> {
+      final List<IClasspathEntry> x = new LinkedList<>();
+      for (final IClasspathEntry e : l)
+        if (path.equals(e.getPath()))
+          x.add(e);
+      l.removeAll(x);
+    });
+  }
+
+  public static boolean removeLibrary(final String path) {
+    return removeLibrary(new Path(path));
+  }
+
   private static boolean touchLibrary(final IPath path, final BiConsumer<List<IClasspathEntry>, IPath> operation) {
     final IProject p = Selection.Util.project();
     if (p == null)
@@ -36,29 +72,5 @@ public class LibrariesManagement {
       return false;
     }
     return true;
-  }
-
-  /** [[SuppressWarningsSpartan]] --bug */
-  public static boolean addLibrary(final IPath path) {
-    return touchLibrary(path, (l, p) -> l.add(JavaCore.newLibraryEntry(p, null, null)));
-  }
-
-  public static boolean addLibrary(final String path) {
-    return addLibrary(new Path(path));
-  }
-
-  /** [[SuppressWarningsSpartan]] --bug */
-  public static boolean removeLibrary(final IPath path) {
-    return touchLibrary(path, (l, p) -> {
-      final List<IClasspathEntry> x = new LinkedList<>();
-      for (final IClasspathEntry e : l)
-        if (path.equals(e.getPath()))
-          x.add(e);
-      l.removeAll(x);
-    });
-  }
-
-  public static boolean removeLibrary(final String path) {
-    return removeLibrary(new Path(path));
   }
 }
