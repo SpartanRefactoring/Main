bc9f04120255b5bda7cdf6088bd19232200f8b0a
Ori Roth <ori.rothh@gmail.com>
2016-11-24 17:02:27 +0200

    Added Athens applicator + button

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/athenizer/AthensHandler.java
@@ -0,0 +1,12 @@
+package il.org.spartan.spartanizer.athenizer;
+
+import org.eclipse.core.commands.*;
+
+import il.org.spartan.plugin.*;
+
+public class AthensHandler extends AbstractHandler {
+  @Override public Object execute(ExecutionEvent event) {
+    Dialogs.message("ma ani sam ba intent?").open();
+    return null;
+  }
+}
