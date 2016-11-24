908b922b3f5c7b0c738a7bbfdc49aaf3bfa83967
Ori Roth <ori.rothh@gmail.com>
2016-11-25 00:59:32 +0200

    Athenizer + LibrariesManagement documentation

--- src/main/java/il/org/spartan/libraries/LibrariesManagement.java
+++ src/main/java/il/org/spartan/libraries/LibrariesManagement.java
@@ -1,24 +1,28 @@
 package il.org.spartan.libraries;
 
-import java.io.*;
-import java.net.*;
 import java.util.*;
-import java.util.function.*;
-
-import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.internal.core.*;
 
-import il.org.spartan.plugin.*;
-import il.org.spartan.plugin.Plugin;
 import il.org.spartan.spartanizer.utils.*;
 
+/** A utility class to manage libraries at the users side, in front of the
+ * ecilpse machine and specific code. Current implementation makes use of the
+ * Users Libraries feature.
+ * @author Ori Roth
+ * @since Nov 25, 2016 */
 public class LibrariesManagement {
+  /** Absolute path of the spartan feature. */
   public static final IPath FEATURE_PATH;
+  /** Eclipse's installation folder absolute path. */
   public static final IPath INSTALLATION_FOLDER;
+  /** Library name, as seen by the user. */
   public static final String LIBRARY_NAME = "Spartan Library";
+  /** The libraries qualified name, based upon folders hierarchy in the
+   * SpartanFeature project. */
   public static final String LIBRARY_QULIFIED_NAME = "spartan.libraries";
+  /** The class path container for our library. */
   public static final IClasspathContainer LIBRARY_PATH_CONTAINER;
   static {
     INSTALLATION_FOLDER = new Path(Platform.getInstallLocation().getURL().getPath());
@@ -43,11 +47,14 @@ public class LibrariesManagement {
     };
   }
 
+  /** @return true iff the spartan library exists within eclipse. */
   public static boolean libraryExists() {
     @SuppressWarnings("restriction") List<String> userLibrariesNames = Arrays.asList(new UserLibraryManager().getUserLibraryNames());
     return userLibrariesNames.contains(LIBRARY_NAME);
   }
 
+  /** @param p JD
+   * @return true iff the project uses the spartan library. */
   public static boolean hasLibrary(final IJavaProject p) {
     if (p == null)
       return false;
@@ -61,10 +68,9 @@ public class LibrariesManagement {
     return false;
   }
 
-  public static boolean checkLibrary(final IJavaProject ¢) {
-    return ¢ != null && (hasLibrary(¢) || addLibrary(¢));
-  }
-
+  /** Adding the spartan library to a project.
+   * @param p JD
+   * @return true upon success */
   public static boolean addLibrary(IJavaProject p) {
     if (p == null)
       return false;
@@ -88,72 +94,18 @@ public class LibrariesManagement {
     return true;
   }
 
-  /** [[SuppressWarningsSpartan]] --bug */
+  /** If the project does not make use of the spartan library, we try to add it.
+   * @param ¢ JD
+   * @return true iff the project uses the spartan library */
+  public static boolean checkLibrary(final IJavaProject ¢) {
+    return ¢ != null && (hasLibrary(¢) || addLibrary(¢));
+  }
+
+  /** Add the spartan library to eclipse's user libraries.
+   * @throws CoreException */
   public static void initializeUserLibraries() throws CoreException {
-    final ClasspathContainerInitializer initializer = JavaCore.getClasspathContainerInitializer(JavaCore.USER_LIBRARY_CONTAINER_ID);
-    if (libraryExists())
-      return;
-    final IPath containerPath = new Path(JavaCore.USER_LIBRARY_CONTAINER_ID);
-    initializer.requestClasspathContainerUpdate(containerPath.append(LIBRARY_NAME), null, LIBRARY_PATH_CONTAINER);
-  }
-
-  @Deprecated public static IPath getPluginJarPath() throws IOException {
-    return new Path(FileLocator.resolve(FileLocator.find(Plugin.plugin().getBundle(), new Path(""), null)).getPath().replaceAll("!.*", "")
-        .replaceAll("^file..", ""));
-  }
-
-  @Deprecated public static URL getPluginJarPath(final IPath ¢) throws IOException {
-    return FileLocator.resolve(FileLocator.find(Plugin.plugin().getBundle(), ¢, null));
-  }
-
-  /** [[SuppressWarningsSpartan]] --bug */
-  @Deprecated public static boolean addLibrary(final IPath path) {
-    return touchLibrary(path, (l, p) -> l.add(JavaCore.newLibraryEntry(p, null, null)));
-  }
-
-  @Deprecated public static boolean addLibrary(final String path) {
-    return addLibrary(new Path(path));
-  }
-
-  /** [[SuppressWarningsSpartan]] --bug */
-  @Deprecated public static boolean removeLibrary(final IPath path) {
-    return touchLibrary(path, (l, p) -> {
-      final List<IClasspathEntry> x = new LinkedList<>();
-      for (final IClasspathEntry e : l)
-        if (path.equals(e.getPath()))
-          x.add(e);
-      l.removeAll(x);
-    });
-  }
-
-  @Deprecated public static boolean removeLibrary(final String path) {
-    return removeLibrary(new Path(path));
-  }
-
-  @Deprecated private static boolean touchLibrary(final IPath path, final BiConsumer<List<IClasspathEntry>, IPath> operation) {
-    final IProject p = Selection.Util.project();
-    if (p == null)
-      return false;
-    final IJavaProject jp = JavaCore.create(p);
-    if (jp == null)
-      return false;
-    final List<IClasspathEntry> nes = new LinkedList<>();
-    final IClasspathEntry[] es;
-    try {
-      es = jp.getRawClasspath();
-    } catch (final JavaModelException ¢) {
-      monitor.log(¢);
-      return false;
-    }
-    if (es != null)
-      nes.addAll(Arrays.asList(es));
-    operation.accept(nes, path);
-    try {
-      jp.setRawClasspath(nes.toArray(new IClasspathEntry[nes.size()]), null);
-    } catch (final JavaModelException ¢) {
-      monitor.log(¢);
-      return false;
-    }
-    return true;
+    if (!libraryExists())
+      JavaCore.getClasspathContainerInitializer(JavaCore.USER_LIBRARY_CONTAINER_ID)
+          .requestClasspathContainerUpdate((new Path(JavaCore.USER_LIBRARY_CONTAINER_ID)).append(LIBRARY_NAME), null, LIBRARY_PATH_CONTAINER);
   }
 }
--- src/main/java/il/org/spartan/spartanizer/athenizer/AthensHandler.java
+++ src/main/java/il/org/spartan/spartanizer/athenizer/AthensHandler.java
@@ -2,6 +2,13 @@ package il.org.spartan.spartanizer.athenizer;
 
 import org.eclipse.core.commands.*;
 
+import il.org.spartan.plugin.*;
+
+/** Handler for the Athenizer project's main feature (global athenizer). Uses
+ * {@link AthensApplicator} as an {@link Applicator} and {@link Augmenter} as an
+ * {@link Application}.
+ * @author Ori Roth
+ * @since Nov 25, 2016 */
 public class AthensHandler extends AbstractHandler {
   @Override public Object execute(@SuppressWarnings("unused") final ExecutionEvent __) {
     applicator().defaultSelection().go();
