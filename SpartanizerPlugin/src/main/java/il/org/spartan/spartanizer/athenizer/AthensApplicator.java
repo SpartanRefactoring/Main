fd2bb5bf508f7f0fb0cfa558a2cf9d629cce0fa1
Ori Roth <ori.rothh@gmail.com>
2016-11-24 17:02:27 +0200

    Added Athens applicator + button

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/athenizer/AthensApplicator.java
@@ -0,0 +1,9 @@
+package il.org.spartan.spartanizer.athenizer;
+
+import il.org.spartan.plugin.*;
+
+public class AthensApplicator extends Applicator {
+  @Override public void go() {
+    //
+  }
+}
