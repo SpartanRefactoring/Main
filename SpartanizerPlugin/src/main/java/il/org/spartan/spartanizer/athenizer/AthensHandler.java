de7d9f4fcb02d06974ebe3675fbf189ee8e60b44
Ori Roth <ori.rothh@gmail.com>
2016-11-24 21:18:33 +0200

    Augmenter introduced

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/athenizer/AthensHandler.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/athenizer/AthensHandler.java
@@ -6,7 +6,19 @@ import il.org.spartan.plugin.*;
 
 public class AthensHandler extends AbstractHandler {
   @Override public Object execute(@SuppressWarnings("unused") final ExecutionEvent __) {
-    Dialogs.message("ma ani sam ba intent?").open();
+    applicator().defaultSelection().go();
     return null;
+  }
+
+  private static AthensApplicator applicator() {
+    final AthensApplicator $ = AthensApplicator.defaultApplicator();
+    final Augmenter a = new Augmenter();
+    $.setRunAction(¢ -> a.commitChanges(¢, $.selection()));
+    // TODO Roth: remove dialog
+    $.runContext(¢ -> {
+      Dialogs.message("Athenising " + $.selection() + " selection").open();
+      ¢.run();
+    });
+    return $;
   }
 }
