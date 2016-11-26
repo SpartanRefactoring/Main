1f308ff32c9651c63430ac46c8ac4816d4fab9ed
Yossi Gil <yossi.gil@gmail.com>
2016-11-26 16:20:06 +0200

    Rewrite #869

--- src/main/java/il/org/spartan/spartanizer/java/range.java
+++ src/main/java/il/org/spartan/spartanizer/java/range.java
@@ -1,133 +1,103 @@
 package il.org.spartan.spartanizer.java;
 
 import java.util.NoSuchElementException;
+
 import java.util.Iterator;
 
-/** A fluent api implementation for range
+/** A fluent API implementation for range
  * @author Dor Ma'ayan
  * @since 26-11-2016 */
-public class range implements Iterable<Integer> {
+public class range {
+  public static BeforeTo from(final int ¢) {
+    return makeFrom(¢).new BeforeTo();
+  }
+
+  public static AfterTo to(final int to) {
+    return makeTo(to).new AfterTo();
+  }
+
+  private static range makeFrom(final int ¢) {
+    return new range() {
+      {
+        from = ¢;
+      }
+    };
+  }
+
+  private static range makeTo(final int ¢) {
+    return new range() {
+      {
+        to = ¢;
+      }
+    };
+  }
+
   int from;
-  int to;
+  int to = -1;
   boolean inclusive;
   int step = 1;
 
-  void initialize() {
-    from = 0;
-    inclusive = false;
-    step = 1;
+  public class AfterTo extends Inner<AfterTo> {
+    public AfterTo from(final int ¢) {
+      to = ¢;
+      return this;
+    }
+
+    public AfterTo step(final int ¢) {
+      step = ¢;
+      return this;
+    }
+
+    @Override AfterTo self() {
+      return this;
+    }
   }
 
-  public AfterTo to(int ¢) {
-    to = ¢;
-    return new AfterTo();
-  }
+  public class BeforeTo extends Inner<BeforeTo> {
+    public AfterTo step(final int ¢) {
+      step = ¢;
+      return new AfterTo();
+    }
 
-  public BeforeTo from(int ¢) {
-    from = ¢;
-    return new BeforeTo();
-  }
-
-  @Override public Iterator<Integer> iterator() {
-    return new RangeIterator();
-  }
-
-  class BeforeTo {
-    public AfterTo to(int ¢) {
+    public AfterTo to(final int ¢) {
       to = ¢;
       return new AfterTo();
     }
 
-    public AfterTo step(int ¢) {
-      step = ¢;
-      return new AfterTo();
-    }
-  }
-
-  class RangeIterator implements Iterator<Integer>, Iterable<Integer> {
-    private int cursor;
-
-    public RangeIterator() {
-      this.cursor = from;
-    }
-
-    public RangeIterator inclusive() {
-      inclusive = true;
-      return this;
-    }
-
-    public RangeIterator notInclusive() {
-      inclusive = false;
-      return this;
-    }
-
-    @Override public boolean hasNext() {
-      boolean $ = inclusive ? this.cursor <= to : this.cursor < to;
-      if (!$)
-        initialize();
-      return $;
-    }
-
-    @SuppressWarnings("boxing") @Override public Integer next() {
-      if (!this.hasNext())
-        throw new NoSuchElementException();
-      int $ = cursor;
-      cursor += step;
-      return $;
-    }
-
-    @Override public void remove() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override public Iterator<Integer> iterator() {
+    @Override BeforeTo self() {
       return this;
     }
   }
 
-  class AfterTo implements Iterator<Integer>, Iterable<Integer> {
-    private int cursor;
-
-    public AfterTo() {
-      this.cursor = from;
-    }
-
-    public RangeIterator step(int ¢) {
-      step = ¢;
-      return new RangeIterator();
-    }
-
-    public RangeIterator inclusive() {
-      inclusive = true;
-      return new RangeIterator();
-    }
-
-    public RangeIterator notInclusive() {
+  abstract class Inner<Self extends Inner<Self>> implements Iterable<Integer> {
+    public final Self exclusive() {
       inclusive = false;
-      return new RangeIterator();
+      return self();
     }
 
-    @Override public boolean hasNext() {
-      boolean $ = inclusive ? this.cursor <= to : this.cursor < to;
-      if (!$)
-        initialize();
-      return $;
-    }
+    abstract Self self();
 
-    @SuppressWarnings("boxing") @Override public Integer next() {
-      if (!this.hasNext())
-        throw new NoSuchElementException();
-      int $ = cursor;
-      ++cursor;
-      return $;
-    }
-
-    @Override public void remove() {
-      throw new UnsupportedOperationException();
+    public final Self inclusive() {
+      inclusive = true;
+      return self();
     }
 
     @Override public Iterator<Integer> iterator() {
-      return this;
+      return new Iterator<Integer>() {
+        int next = from;
+
+        @Override public boolean hasNext() {
+          return inclusive ? next <= to : next < to;
+        }
+
+        @Override public Integer next() {
+          if (!hasNext())
+            throw new NoSuchElementException();
+          final int $ = next;
+          next += step;
+          return Integer.valueOf($);
+        }
+      };
     }
   }
 }
