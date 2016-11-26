da9d687c87a5f6b3e0648d2ac672d9eb02cceeb5
Yossi Gil <yossi.gil@gmail.com>
2016-11-26 16:20:06 +0200

    Rewrite #869

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/rangeTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/java/rangeTest.java
@@ -1,5 +1,6 @@
 package il.org.spartan.spartanizer.java;
-
+import static il.org.spartan.azzert.*;
+import il.org.spartan.*;
 import il.org.spartan.spartanizer.java.range;
 import org.junit.*;
 
@@ -8,101 +9,87 @@ import org.junit.*;
 @SuppressWarnings({ "boxing", "static-method" })
 public class rangeTest {
   @Test public void test0() {
-    range r = new range();
-    int counter = 0;
-    for (@SuppressWarnings("unused") Integer i : r.to(5))
-      ++counter;
-    assert counter == 5;
+    int s= 0;
+    for (@SuppressWarnings("unused") Integer i : range.to(5))
+      ++s;
+    assert s == 10;
   }
 
   @Test public void test1() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.from(3).to(5))
+    for (Integer ¢ : range.from(3).to(5))
       sum += ¢;
     assert sum == 7;
   }
 
   @Test public void test2() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.from(3).to(5).inclusive())
+    for (Integer ¢ : range.from(3).to(5).inclusive())
       sum += ¢;
     assert sum == 12;
   }
 
   @Test public void test3() {
-    range r = new range();
     int counter = 0;
-    for (@SuppressWarnings("unused") Integer i : r.to(5))
+    for (@SuppressWarnings("unused") Integer i : range.to(5))
       ++counter;
     assert counter == 5;
   }
 
   @Test public void test4() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.from(3).to(5).inclusive().notInclusive())
+    for (Integer ¢ : range.from(3).to(5).inclusive().exclusive())
       sum += ¢;
     assert sum == 7;
   }
 
   @Test public void test5() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.from(0).to(10).step(2).inclusive())
+    for (Integer ¢ : range.from(0).to(10).step(2).inclusive())
       sum += ¢;
     assert sum == 30;
   }
 
   @Test public void test6() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.to(5).inclusive())
+    for (Integer ¢ : range.to(5).exclusive())
       sum += ¢;
-    assert sum == 15;
+    azzert.that(sum,is(15));
   }
 
   @Test public void test7() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.to(5).notInclusive())
+    for (Integer ¢ : range.to(5).exclusive())
       sum += ¢;
     assert sum == 10;
   }
 
   @Test public void test8() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.to(5).step(3))
+    for (Integer ¢ : range.to(5).step(3))
       sum += ¢;
     assert sum == 3;
   }
 
   @Test public void test9() {
-    range r = new range();
     int sum = 0;
-    for (Integer ¢ : r.from(100).to(110).step(10))
+    for (Integer ¢ : range.from(100).to(110).step(10))
       sum += ¢;
     assert sum == 100;
   }
 
   @Test public void test10() {
-    range r1 = new range();
-    range r2 = new range();
     int sum = 0;
-    for (@SuppressWarnings("unused") Integer i1 : r1.to(10).step(2))
-      for (Integer i2 : r2.to(10).step(2))
+    for (@SuppressWarnings("unused") Integer i1 : range.to(10).step(2))
+      for (Integer i2 : range.to(10).step(2))
         sum += i2;
     assert sum == 100;
   }
 
   @Test public void test11() {
-    range r1 = new range();
-    range r2 = new range();
     int sum = 0;
-    for (@SuppressWarnings("unused") Integer i1 : r1.to(10).step(2))
-      for (Integer i2 : r2.to(10).step(2).inclusive())
+    for (@SuppressWarnings("unused") Integer i1 : range.to(10).step(2))
+      for (Integer i2 : range.to(10).step(2).inclusive())
         sum += i2;
     assert sum == 150;
   }
