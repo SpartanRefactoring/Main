8288a50327af07618fae16ee6410b1f6b55e45d8
dormaayn <dor.d.ma@gmail.com>
2016-11-26 10:47:41 +0200

    Almost Done + tests (Issue #869)

--- src/test/java/il/org/spartan/spartanizer/java/rangeTest.java
+++ src/test/java/il/org/spartan/spartanizer/java/rangeTest.java
@@ -14,4 +14,35 @@ public class rangeTest {
       ++counter;
     assert counter==5;
   }
+  
+  @SuppressWarnings({ "static-access", "boxing" })
+  @Test public void test1() {
+    int sum=0;
+    for(Integer ¢ : range.from(3).to(5))
+      sum += ¢;
+    assert sum==7;
+  }
+  
+  @SuppressWarnings({ "static-access", "boxing" })
+  @Test public void test2() {
+    int sum=0;
+    for(Integer ¢ : range.from(3).to(5).inclusive())
+      sum += ¢;
+    assert sum==12;
+  }
+  
+  @Test public void test3() {
+    int counter=0;
+    for(@SuppressWarnings("unused") Integer i : range.to(5))
+      ++counter;
+    assert counter==5;
+  }
+  
+  @SuppressWarnings({ "static-access", "boxing" })
+  @Test public void test4() {
+    int sum=0;
+    for(Integer ¢ : range.from(3).to(5).inclusive().notInclusive())
+      sum += ¢;
+    assert sum==7;
+  }
 }
