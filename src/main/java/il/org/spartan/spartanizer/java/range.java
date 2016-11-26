8a231a8b44c862809ffaf7f53de1a4383ca45430
dormaayn <dor.d.ma@gmail.com>
2016-11-26 10:47:41 +0200

    Almost Done + tests (Issue #869)

--- src/main/java/il/org/spartan/spartanizer/java/range.java
+++ src/main/java/il/org/spartan/spartanizer/java/range.java
@@ -6,47 +6,73 @@ import java.util.Iterator;
 public abstract class range implements Iterable<Integer> {
   static int from;
   static int to;
-  
-  public static RangeIterator to(int ¢){
-    to=¢; 
+  static boolean inclusive;
+
+  static void  initialize(){
+    from=0;
+    inclusive=false;
+  }
+  public static RangeIterator to(int ¢) {
+    to = ¢;
     return new RangeIterator();
+  }
+
+  public static BeforeTo from(int ¢) {
+    from = ¢;
+    return new BeforeTo();
+  }
+
+  @Override public Iterator<Integer> iterator() {
+    return new RangeIterator();
+  }
+
+  static class BeforeTo {
+    public static RangeIterator to(int ¢) {
+      to = ¢;
+      return new RangeIterator();
+    } 
   }
   
 
-  @Override
-  public Iterator<Integer> iterator() {
-    return new RangeIterator();
-  }
+  static class RangeIterator implements Iterator<Integer>, Iterable<Integer> {
+    private int cursor;
 
-  private static class RangeIterator implements
-                    Iterator<Integer>, Iterable<Integer> {
-        private int cursor;
-        public RangeIterator() {
-            this.cursor = from;
-        }
-        
-        @Override
-        public boolean hasNext() {
-            return this.cursor < to;
-        }
-
-        @SuppressWarnings("boxing")
-        @Override
-        public Integer next() {
-            if (!this.hasNext())
-              throw new NoSuchElementException();
-            int $ = cursor;
-            ++cursor;
-            return $;
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override public Iterator<Integer> iterator() {
-          return this;
-        }
+    public RangeIterator() {
+      this.cursor = from;
     }
+    
+    public RangeIterator inclusive(){
+      inclusive=true;
+      return this;
+    }
+    
+    public RangeIterator notInclusive(){
+      inclusive=false;
+      return this;
+    }
+
+    @Override public boolean hasNext() {
+      boolean $ = inclusive ? this.cursor <= to : this.cursor < to;
+      if(!$)
+        initialize();
+      return $;
+
+    }
+
+    @SuppressWarnings("boxing") @Override public Integer next() {
+      if (!this.hasNext())
+        throw new NoSuchElementException();
+      int $ = cursor;
+      ++cursor;
+      return $;
+    }
+
+    @Override public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override public Iterator<Integer> iterator() {
+      return this;
+    }
+  }
 }
