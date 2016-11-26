0d05892ada6c2c92e6ff3024ca80f19d53f0bf23
dormaayn <dor.d.ma@gmail.com>
2016-11-26 11:26:29 +0200

    Implementation done + more tests (Issue #869)

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/java/range.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/java/range.java
@@ -3,18 +3,26 @@ package il.org.spartan.spartanizer.java;
 import java.util.NoSuchElementException;
 import java.util.Iterator;
 
+/**
+ * A fluent api implementation for range 
+ * 
+ * @author Dor Ma'ayan
+ * @since 26-11-2016
+ */
 public abstract class range implements Iterable<Integer> {
   static int from;
   static int to;
   static boolean inclusive;
+  static int step=1;
 
   static void  initialize(){
     from=0;
     inclusive=false;
+    step=1;
   }
-  public static RangeIterator to(int ¢) {
+  public static AfterTo to(int ¢) {
     to = ¢;
-    return new RangeIterator();
+    return new AfterTo();
   }
 
   public static BeforeTo from(int ¢) {
@@ -27,10 +35,15 @@ public abstract class range implements Iterable<Integer> {
   }
 
   static class BeforeTo {
-    public static RangeIterator to(int ¢) {
+    public static AfterTo to(int ¢) {
       to = ¢;
-      return new RangeIterator();
+      return new AfterTo();
     } 
+    
+    public static AfterTo step(int ¢){
+      step=¢;
+      return new AfterTo();
+    }
   }
   
 
@@ -49,6 +62,53 @@ public abstract class range implements Iterable<Integer> {
     public RangeIterator notInclusive(){
       inclusive=false;
       return this;
+    }
+
+    @Override public boolean hasNext() {
+      boolean $ = inclusive ? this.cursor <= to : this.cursor < to;
+      if(!$)
+        initialize();
+      return $;
+
+    }
+
+    @SuppressWarnings("boxing") @Override public Integer next() {
+      if (!this.hasNext())
+        throw new NoSuchElementException();
+      int $ = cursor;
+      cursor+=step;
+      return $;
+    }
+
+    @Override public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override public Iterator<Integer> iterator() {
+      return this;
+    }
+  }
+  
+  static class AfterTo implements Iterator<Integer>, Iterable<Integer> {
+    private int cursor;
+
+    public AfterTo() {
+      this.cursor = from;
+    }
+    
+    public static RangeIterator step(int ¢){
+      step=¢;
+      return new RangeIterator();
+    }
+    
+    public static RangeIterator inclusive(){
+      inclusive=true;
+      return new RangeIterator();
+    }
+    
+    public static RangeIterator notInclusive(){
+      inclusive=false;
+      return new RangeIterator();
     }
 
     @Override public boolean hasNext() {
