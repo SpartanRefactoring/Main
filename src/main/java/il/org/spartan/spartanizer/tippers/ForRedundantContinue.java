438d5d003cca485a232d874e6a9b1d1e65664a07
kfir-marx <kfir.marx@gmail.com>
2016-11-26 22:09:45 +0200

    passes some basic tests (see Issue147.java). removes redundent continue in for loop. #147

---
+++ src/main/java/il/org/spartan/spartanizer/tippers/ForRedundantContinue.java
@@ -0,0 +1,58 @@
+package il.org.spartan.spartanizer.tippers;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.tipping.*;
+
+/** convert <code>if(!boolVar) 
+ *                  continue; 
+ *                VAR++;</code> to
+ * <code>if(boolVar) 
+ *          VAR++;</code>, etc.
+ * iff these are the last statements in their block, and if it's not a sequencer with expiration.
+ * @author Kfir Marx
+ * @since 2016-11-26 */
+public class ForRedundantContinue extends CarefulTipper<ForStatement> implements TipperCategory.SyntacticBaggage {
+
+  @Override public String description(ForStatement ¢) {
+    return "Eliminate redundant " + lastStatement(¢);
+  }
+  
+  @Override public String description() {
+    return "Eliminate redundant continue";
+  }
+  
+  static Statement lastStatement(final ForStatement ¢) {
+    return !iz.block(¢.getBody()) ? ¢.getBody()
+        : (Statement) (az.block(¢.getBody()).statements()).get((az.block(¢.getBody()).statements().size() - 1));
+  }
+  
+
+  @Override public Tip tip(final ForStatement ¢) {
+    return new Tip(description(¢), ¢, this.getClass()) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) { 
+        
+       remove(r,lastStatement(¢), g);
+      }
+    };
+  }
+
+  
+  @Override public boolean prerequisite(final ForStatement ¢) {
+    System.out.println("------------------------------- bug 0 -------------------------------------");
+   return ((lastStatement(¢)).getNodeType() == ASTNode.CONTINUE_STATEMENT);
+  }
+  
+  public static void remove(final ASTRewrite r, final Statement s, final TextEditGroup g) {
+    r.getListRewrite(parent(s), Block.STATEMENTS_PROPERTY).remove(s, g); 
+  }
+
+  
+}
