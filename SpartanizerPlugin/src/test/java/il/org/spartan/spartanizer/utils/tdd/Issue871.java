2ac1e9255c493514244c46ba8a9294daaac06479
Yossi Gil <yossi.gil@gmail.com>
2016-11-27 20:45:29 +0200

    Formatting

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue871.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue871.java
@@ -2,57 +2,50 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.*;
 
 @SuppressWarnings("static-method")
 public class Issue871 {
   @Test public void test0() {
-    assertEquals(0, (new ParameterInt(0)).intValue());
+    assertEquals(0, new ParameterInt(0).intValue());
   }
-  
+
   @Test public void test1() {
-    ParameterInt i = new ParameterInt();
+    final ParameterInt i = new ParameterInt();
     i.set(3);
     assertEquals(3, i.intValue());
   }
-  
-  @Test(expected=IllegalArgumentException.class) public void test2() {
-    ParameterInt i = new ParameterInt();
+
+  @Test(expected = IllegalArgumentException.class) public void test2() {
+    final ParameterInt i = new ParameterInt();
     i.set(3);
     assertEquals(3, i.intValue());
     i.set(4);
   }
-  
-  
-  @Test(expected=Exception.class) public void test3() {
-    (new ParameterInt()).intValue();
+
+  @Test(expected = Exception.class) public void test3() {
+    new ParameterInt().intValue();
   }
-  
-  @SuppressWarnings("boxing")
-  @Test public void test4() {
-    ParameterInt i = new ParameterInt();
+
+  @SuppressWarnings("boxing") @Test public void test4() {
+    final ParameterInt i = new ParameterInt();
     i.set(3);
     assertEquals(true, i.hasValue());
   }
-  
-  @SuppressWarnings("boxing")
-  @Test public void test5() {
-    assertEquals(false, (new ParameterInt()).hasValue());
+
+  @SuppressWarnings("boxing") @Test public void test5() {
+    assertEquals(false, new ParameterInt().hasValue());
   }
-  
-  @SuppressWarnings("boxing")
-  @Test public void test6() {
-    assertEquals(false, (new ParameterInt(2)).hasValue());
+
+  @SuppressWarnings("boxing") @Test public void test6() {
+    assertEquals(false, new ParameterInt(2).hasValue());
   }
-  
-  @SuppressWarnings("boxing")
-  @Test public void test7() {
-    assertEquals(false, (new ParameterInt()).hasDefault());
+
+  @SuppressWarnings("boxing") @Test public void test7() {
+    assertEquals(false, new ParameterInt().hasDefault());
   }
-  
-  @SuppressWarnings("boxing")
-  @Test public void test8() {
-    assertEquals(true, (new ParameterInt(4)).hasDefault());
+
+  @SuppressWarnings("boxing") @Test public void test8() {
+    assertEquals(true, new ParameterInt(4).hasDefault());
   }
 }
