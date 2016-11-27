5a79f0ff781c10a4df59918cf3f13c9ff1ab59a5
Koby Ben Shimol <kobybs@gmail.com>
2016-11-27 17:41:17 +0200

    ParamterInt TDD first test and solution Issue #871

---
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/ParameterInt.java
@@ -0,0 +1,15 @@
+package il.org.spartan.spartanizer.utils.tdd;
+
+public final class ParameterInt {
+  private int value;
+  
+  
+  public ParameterInt(int defaultValue){
+    value = defaultValue;
+  }
+  
+  public int intValue(){
+    return value;
+  }
+  
+}
