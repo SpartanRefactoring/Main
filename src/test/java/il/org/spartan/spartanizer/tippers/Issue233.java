54c84bb1908c2c5609041b1b351e2fc811c5853f
Yossi Gil <yossi.gil@gmail.com>
2016-11-27 06:36:23 +0200

    #233: Go over the code

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue233.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue233.java
@@ -14,11 +14,22 @@ public class Issue233 {
     trimmingOf("switch(x) {} switch(x) {}int x=5;").gives("int x=5;").stays();
   }
 
-  @Ignore public void c() {
-    trimmingOf("switch(x) { default: k=5; }").gives("{k=5;}");
-  }
-  
-  @Ignore public void d() {
-    trimmingOf("switch(x) { default: k=5; break; }").gives("{k=5;}");
+  @Ignore
+  public static class NotWorking {
+    @Test public void c() {
+      trimmingOf("switch(x) { default: k=5; }").gives("{k=5;}");
+    }
+
+    @Test public void d() {
+      trimmingOf("switch(x) { default: k=5; break; }").gives("{k=5;}");
+    }
+
+    @Test public void e() {
+      trimmingOf("switch(x) {} switch(x) { case a: }int x=5;").gives("int x=5;").stays();
+    }
+
+    @Test public void f() {
+      trimmingOf("switch(x) {} switch(x) { case a: }int x=5;").gives("int x=5;").stays();
+    }
   }
 }
