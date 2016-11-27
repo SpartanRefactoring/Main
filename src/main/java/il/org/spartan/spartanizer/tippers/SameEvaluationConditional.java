039683e971ae61c345d13249b66187f71b848adc
Yossi Gil <yossi.gil@gmail.com>
2016-11-27 20:45:29 +0200

    Formatting

--- src/main/java/il/org/spartan/spartanizer/tippers/SameEvaluationConditional.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/SameEvaluationConditional.java
@@ -10,32 +10,29 @@ import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** converts
-*
-* <pre>
-* x == y ? y : x
-* </pre>
-*
-* into
-*
-* <pre>
-* x
-* </pre>
-*
-* @author Dan Abramovich
-* @since 21-11-2016 */
-
-public class SameEvaluationConditional extends ReplaceCurrentNode<ConditionalExpression> implements TipperCategory.Collapse{
-
-  
-  @Override public ASTNode replacement(ConditionalExpression x) {
-    InfixExpression cond = az.infixExpression(x.getExpression());
-    return !iz.infixEquals(cond) || !wizard.same(x.getThenExpression(), cond.getLeftOperand()) && !wizard.same(x.getThenExpression(), cond.getRightOperand())
+ *
+ * <pre>
+ * x == y ? y : x
+ * </pre>
+ *
+ * into
+ *
+ * <pre>
+ * x
+ * </pre>
+ *
+ * @author Dan Abramovich
+ * @since 21-11-2016 */
+public class SameEvaluationConditional extends ReplaceCurrentNode<ConditionalExpression> implements TipperCategory.Collapse {
+  @Override public ASTNode replacement(final ConditionalExpression x) {
+    final InfixExpression cond = az.infixExpression(x.getExpression());
+    return !iz.infixEquals(cond)
+        || !wizard.same(x.getThenExpression(), cond.getLeftOperand()) && !wizard.same(x.getThenExpression(), cond.getRightOperand())
         || !wizard.same(x.getElseExpression(), cond.getLeftOperand()) && !wizard.same(x.getElseExpression(), cond.getRightOperand())
         || !sideEffects.free(cond.getLeftOperand()) || !sideEffects.free(cond.getRightOperand()) ? null : duplicate.of(x.getElseExpression());
   }
 
-  @Override public String description(@SuppressWarnings("unused") ConditionalExpression ¢) {
+  @Override public String description(@SuppressWarnings("unused") final ConditionalExpression ¢) {
     return "eliminate ternary expression that evaluates to the same value";
   }
-  
 }
