ad30d53ba3860216b420d4acc57f738e3db5c840
danabramovich <sdan635z@campus.technion.ac.il>
2016-11-28 14:07:30 +0200

    added tipper for issue #879

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue879.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue879.java
@@ -5,11 +5,10 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.junit.*;
 
 /** @author Dan Abramovich
- * @since 27-11-2016 */
+ * @since 28-11-2016 */
 @SuppressWarnings("static-method")
 public class Issue879 {
-  @Ignore
   @Test public void test0() {
-    trimmingOf("void f(){return;}").gives("int f(){}").stays();
+    trimmingOf("void f(){return;}").gives("void f(){}").stays();
   }
 }
