a21255fa245b7473192ba05c73c4c8c237c9c7f5
danabramovich <sdan635z@campus.technion.ac.il>
2016-11-30 23:43:11 +0200

    improved tipper + more tests #879

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue879.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue879.java
@@ -8,7 +8,22 @@ import org.junit.*;
  * @since 28-11-2016 */
 @SuppressWarnings("static-method")
 public class Issue879 {
+  @Ignore
   @Test public void test0() {
     trimmingOf("void f(){return;}").gives("void f(){}").stays();
+  }
+  @Ignore
+  @Test public void test1() {
+    trimmingOf("void f(){int x; int y;return;}").gives("void f(){int x; int y;}").stays();
+  }
+  @Test public void test2() {
+    trimmingOf("void f(){int x=4; if(x>3){x+=3;x+=4;}else{x+=5;x+=55;} return;}").gives("void f(){int x=4; if(x>3){x+=3;x+=4;}else{x+=5;x+=55;}}").stays();
+  }
+
+  @Test public void test3() {
+    trimmingOf("void f(){int x=9;int y=7; x+=4;y=x+x;return;}").gives("void f(){int x=9;int y=7; x+=4;y=x+x;}").stays();
+  }
+  @Test public void test4() {
+    trimmingOf("int f(){int x=9;int y=7; x+=4;y=x+x;return;}").stays();
   }
 }
