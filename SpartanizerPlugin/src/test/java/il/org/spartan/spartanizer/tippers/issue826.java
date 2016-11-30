2c54a3ff51e8d87f3810985a1c3e6e79e2243b6b
Koby Ben Shimol <kobybs@gmail.com>
2016-11-30 16:47:41 +0200

    tomerdragucki don't forget to spartnize your code #826

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue826.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/issue826.java
@@ -9,19 +9,12 @@ import org.junit.*;
 @SuppressWarnings({ "static-method", "javadoc" })
 public class issue826 {
   @Test public void a() {
-    trimmingOf("" + //
-        "A() ? 8 : 8"//
+    trimmingOf("A() ? 8 : 8"
     ).stays();
   }
 
   @Test public void b() {
-    trimmingOf("" + //
-        "public void b() {" + //
-        "    int i = 210;" + //
-        "    if (++i < 5)" + //
-        "      a(i);" + //
-        "    else" + //
-        "      a(i);"//
+    trimmingOf("public void b() {" + "    int i = 210;" + "    if (++i < 5)" + "      a(i);" + "    else" + "      a(i);"
     ).stays();
   }
 }
