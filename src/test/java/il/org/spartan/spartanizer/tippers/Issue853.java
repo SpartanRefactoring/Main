c5150b26c8a523a51d9df6bfbcd424d7208ecbbc
danabramovich <sdan635z@campus.technion.ac.il>
2016-12-02 01:40:34 +0200

    added Ignored tets + added code to the tipper #853

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue853.java
@@ -0,0 +1,16 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+
+/** @author Dan Abramovich
+ * @since 28-11-2016 */
+@SuppressWarnings("static-method")
+public class Issue853 {
+  @Ignore
+  @Test public void test0() {
+    trimmingOf("for(int ¢ = 0; ¢ < 10; ++¢){}").withTipper(ForStatement.class, new ReplaceForWithRange()).gives("for(Integer ¢ : range.from(0).to($10)){}");
+  }
+}
