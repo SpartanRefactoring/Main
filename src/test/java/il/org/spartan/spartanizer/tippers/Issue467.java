4d95e7c2dfcc21e003d23d1e77f70f736edbce83
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-01 20:11:51 +0200

    #467 all tests pass

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue467.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue467.java
@@ -4,11 +4,29 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
 
+@SuppressWarnings("static-method")
 public class Issue467 {
-  @Ignore
-  public static class NotWorking {
-    @Test public static void a() {
-      trimmingOf("switch(x--){case 1: y=3; case 2: y =4;}").stays();
-    }
+  @Test public void a() {
+    trimmingOf("switch(x--){case 1: y=3; case 2: y=4;}").stays();
+  }
+  
+  @Test public void b() {
+    trimmingOf("{switch(x++){case 1: y=3; case 2: y=4;}}").gives("switch(x++){case 1: y=3; case 2: y=4;}").stays();
+  }
+  
+  @Test public void c() {
+    trimmingOf("y=2; switch(y+x--){case 1: y=3; case 2: y=4;} y=6;").stays();
+  }
+  
+  @Test public void d() {
+    trimmingOf("y=2; switch(x-- + x--){case 1: y=3; case 2: y=4;} y=6;").stays();
+  }
+  
+  @Test public void e() {
+    trimmingOf("y=2; {switch(x--){case 1: y=3; case 2: y=4;}} y=6;").gives("y=2; switch(x--){case 1: y=3; case 2: y=4;} y=6;").stays();
+  }
+  
+  @Test public void f() {
+    trimmingOf("switch(x.z--){case 1: y=3; case 2: y=4;}").stays();
   }
 }
