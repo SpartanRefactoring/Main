8b7d29811df716f8d7264813a85c458994c3605e
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-01 03:14:36 +0200

    done testing #466, bug is dixed

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue466.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue466.java
@@ -1,15 +1,36 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
 import org.junit.*;
 import org.junit.runners.*;
 
 /** @author Raviv Rachmiel
  * @since 22-11-2016 */
-// TODO Raviv: complete or delete... yg
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue466 {
-  @Test @SuppressWarnings("unused") public void check__() {
-    new Object() {/* wha's going on */};
+  @Test public void TestDoubleUnderscore() {
+    trimmingOf("@SuppressWarnings(\"unused\") public void check__(Object... os) {  }")//
+    .gives("@SuppressWarnings(\"unused\") public void check__(Object... __) {  }")//
+    .stays();
   }
-}
+  
+  @Test public void TestDoubleUnderscoreInside() {
+    trimmingOf("public void check__(@SuppressWarnings(\"unused\")  Object... os) {  }")//
+    .gives("public void check__(@SuppressWarnings(\"unused\")  Object... __) {  }")//
+    .stays();
+  }
+  
+  @Test public void TestDoesUseShouldntChange() {
+    trimmingOf("@SuppressWarnings(\"unused\") public void check__(Object... ¢) { ¢.get(0);  }")//
+    .stays();
+  }
+  
+  @Test public void TestDoesUseShouldntChange2() {
+    trimmingOf("public void check__(@SuppressWarnings(\"unused\") Object... ¢) { ¢.get(0);  }")//
+    .stays();
+  }
+  
+} 
