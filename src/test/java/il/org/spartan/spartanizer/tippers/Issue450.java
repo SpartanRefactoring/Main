0539cf169affb7fbbdaf32580cecafe178cfcef9
SapirBismot <Sapirbismot@gmail.com>
2016-12-01 13:51:13 +0200

    #450 fix the bug + more tests

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue450.java
@@ -0,0 +1,29 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/** see Issue #450 for more details
+ * @author Sapir Bismot
+ * @since 16-11-30 */
+@SuppressWarnings("static-method")
+public class Issue450 {
+  @Test public void test0() {
+    trimmingOf("final Separator s = new Separator(\", \");" //
+        + "for (final String a : args)" //
+        + "System.out.print(s + a);").stays();
+  }
+  @Test public void test1() {
+    trimmingOf("int x = 7;" //
+        + "for (final String a : args)" //
+        + "System.out.print(x+a.length());").gives("for (final String a : args)" //
+            + "System.out.print(7+a.length());");
+  }
+  @Test public void test2() {
+    trimmingOf("final Separator s = \"hello\";" //
+        + "for (final String a : args)" //
+        + "System.out.print(s + a);").gives("for (final String a : args)" //
+        + "System.out.print(\"hello\" + a);");
+  }
+}
