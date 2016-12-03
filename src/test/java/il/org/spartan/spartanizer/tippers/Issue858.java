6d23cc616fb3430e9641a775d4a5d1c1932c9a95
Yossi Gil <yossi.gil@gmail.com>
2016-12-03 21:58:48 +0200

    Clean

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue858.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue858.java
@@ -4,16 +4,14 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
 
-@SuppressWarnings("static-method")
 @Ignore
+@SuppressWarnings("static-method")
 public class Issue858 {
   @Test public void a() {
-    trimmingOf("switch(x){" + "case a:" + "x=1;" + "break;" + "case b:" + "x=2;break;" + "default:" + "x=1;" + "break;" + "}")
-        .gives("switch(x){" + "case b:" + "x=2;break;" + "case a:" + "default:" + "x=1;" + "break;" + "}");
+    trimmingOf("switch(x){case a:x=1;break;case b:x=2;break;default:x=1;break;}").gives("switch(x){case b:x=2;break;case a:default:x=1;break;}");
   }
 
   @Test public void b() {
-    trimmingOf("switch(x){" + "case a:" + "x=1;" + "break;" + "case b:" + "x=1;break;" + "}")
-        .gives("switch(x){" + "case a:" + "case b:" + "x=1;" + "break;" + "}");
+    trimmingOf("switch(x){case a:x=1;break;case b:x=1;break;}").gives("switch(x){case a:case b:x=1;break;}");
   }
 }
