ab4db623550d81b9db4a69e9ef92f60b8036282a
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-03 16:28:44 +0200

    #858 coded most of the tipper (need to test and refactor yet)

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue858.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue858.java
@@ -4,14 +4,16 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
 
+
 @SuppressWarnings("static-method")
+@Ignore
 public class Issue858 {
-  @Ignore public void a() {
+  @Test public void a() {
     trimmingOf("switch(x){" + "case a:" + "x=1;" + "break;" + "case b:" + "x=2;break;" + "default:" + "x=1;" + "break;" + "}")
         .gives("switch(x){" + "case b:" + "x=2;break;" + "case a:" + "default:" + "x=1;" + "break;" + "}");
   }
 
-  @Ignore public void b() {
+  @Test public void b() {
     trimmingOf("switch(x){" + "case a:" + "x=1;" + "break;" + "case b:" + "x=1;break;" + "}")
         .gives("switch(x){" + "case a:" + "case b:" + "x=1;" + "break;" + "}");
   }
