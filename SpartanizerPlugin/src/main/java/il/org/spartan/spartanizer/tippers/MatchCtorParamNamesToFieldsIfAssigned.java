888a23b38e1ebcc7d657c6b0c30b8d23fd3970b4
Doron Meshulam <doronmmm@hotmail.com>
2016-12-05 00:25:19 +0200

    Starting to write our first Tipper #20

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/MatchCtorParamNamesToFieldsIfAssigned.java
@@ -0,0 +1,22 @@
+package il.org.spartan.spartanizer.tippers;
+
+import org.eclipse.jdt.core.dom.*;
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.tipping.*;
+
+public class MatchCtorParamNamesToFieldsIfAssigned extends CarefulTipper<MethodDeclaration> implements TipperCategory.Idiomatic {
+  @Override public String description(final MethodDeclaration ¢) {
+    return "Match constructor parameter names to fields";
+  }
+
+  @Override public Tip tip(final MethodDeclaration d) {
+    return null;
+  }
+//    final ExpressionStatement s = extract.expressionStatement(d);
+//    return true ? null : new Tip(description(d), d, this.getClass()) {
+//          @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+//          }
+//        };
+//  }
+}
