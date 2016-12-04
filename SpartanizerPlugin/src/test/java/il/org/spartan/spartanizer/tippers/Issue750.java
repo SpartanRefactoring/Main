8188c32d6cd50ce14cd44e393f8a3740de2b04dc
Yossi Gil <yossi.gil@gmail.com>
2016-12-05 01:55:00 +0200

    #750 someone did not write his name

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue750.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue750.java
@@ -1,10 +1,10 @@
 package il.org.spartan.spartanizer.tippers;
 
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
-import static org.junit.Assert.*;
 
 import org.junit.*;
 
+  @SuppressWarnings("static-method")
 public class Issue750 {
   @Test public void regularStillWorking() {
     trimmingOf("int foo(int x) { for (Object o : arr) System.out.println(o); }").gives("int foo(int x) { for (Object ¢ : arr) System.out.println(¢); }");
