82a5d35d3a6643ce83efce0d7c6194d84a0794e5
dormaayn <dor.d.ma@gmail.com>
2016-12-05 11:21:30 +0200

    Issue #892 Done

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue892.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue892.java
@@ -3,22 +3,19 @@ package il.org.spartan.spartanizer.tippers;
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
-/**
- * 
- * @author Dor Ma'ayan
- * @since 05-12-2016
- */
+
+/** @author Dor Ma'ayan
+ * @since 05-12-2016 */
 @SuppressWarnings("static-method")
-@Ignore
 public class Issue892 {
   @Test public void test0() {
     trimmingOf("b+-3").gives("b-3");
   }
-  
+
   @Test public void test1() {
     trimmingOf("x+-3.4").gives("x-3.4");
   }
-  
+
   @Test public void test2() {
     trimmingOf("x-+3.4").gives("x-3.4");
   }
