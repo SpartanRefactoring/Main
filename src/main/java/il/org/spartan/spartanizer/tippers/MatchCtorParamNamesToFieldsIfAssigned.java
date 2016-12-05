3bb2693cfc732a8a36b59a8c36d24b56d4c99df8
Yossi Gil <yossi.gil@gmail.com>
2016-12-05 12:14:51 +0200

    #709: TODO for Doron.

--- src/main/java/il/org/spartan/spartanizer/tippers/MatchCtorParamNamesToFieldsIfAssigned.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/MatchCtorParamNamesToFieldsIfAssigned.java
@@ -4,7 +4,7 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
-
+// TODO: Who is the author?
 public class MatchCtorParamNamesToFieldsIfAssigned extends CarefulTipper<MethodDeclaration> implements TipperCategory.Idiomatic {
   @Override public String description(final MethodDeclaration ¢) {
     return "Match constructor parameter names to fields";
