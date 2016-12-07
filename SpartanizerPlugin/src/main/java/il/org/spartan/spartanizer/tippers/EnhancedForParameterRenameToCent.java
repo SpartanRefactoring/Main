049ae09de26b287daee1123d752c8d19a2c5ec99
Yossi Gil <yossi.gil@gmail.com>
2016-12-07 12:16:50 +0200

    Cleanup

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/EnhancedForParameterRenameToCent.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/EnhancedForParameterRenameToCent.java
@@ -4,7 +4,6 @@ import static il.org.spartan.Utils.*;
 import static il.org.spartan.lisp.*;
 import static il.org.spartan.spartanizer.engine.JavaTypeNameParser.*;
 
-import java.lang.reflect.*;
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
@@ -28,22 +27,20 @@ public final class EnhancedForParameterRenameToCent extends EagerTipper<Enhanced
   }
 
   @Override public Tip tip(final EnhancedForStatement s, final ExclusionManager m) {
-    ASTNode p = s; 
-    while(!(p instanceof MethodDeclaration))
+    ASTNode p = s;
+    while (!(p instanceof MethodDeclaration))
       p = p.getParent();
-    
     if (p instanceof MethodDeclaration) {
       final MethodDeclaration pp = (MethodDeclaration) p;
-      List<SingleVariableDeclaration> l = parameters(pp);
+      final List<SingleVariableDeclaration> l = parameters(pp);
       if (l.size() == 1) {
-      final SingleVariableDeclaration parameter = onlyOne(l);
-      final SimpleName sn = parameter.getName();
-      assert sn != null;
-      if (in(sn.getIdentifier(), "¢"))
-        return null;
+        final SingleVariableDeclaration parameter = onlyOne(l);
+        final SimpleName sn = parameter.getName();
+        assert sn != null;
+        if (in(sn.getIdentifier(), "¢"))
+          return null;
       }
     }
-    
     final SingleVariableDeclaration d = s.getParameter();
     final SimpleName n = d.getName();
     if (in(n.getIdentifier(), "$", "¢", "__", "_") || !isJohnDoe(d))
