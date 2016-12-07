d541e064cfd16bab8c2e7f6fc4677bd84d26acab
Yossi Gil <yossi.gil@gmail.com>
2016-12-07 23:15:44 +0200

    Cleanup

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/AnnotationSort.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/AnnotationSort.java
@@ -12,39 +12,40 @@ import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** @author kobybs
  * @author Dan Abramovich
  * @since 20-11-2016 */
 public class AnnotationSort<N extends BodyDeclaration> extends EagerTipper<N> implements TipperCategory.Sorting {
-  private static final HashSet<String> rank0 = new HashSet<>(as.list("Deprecated"));
-  private static final HashSet<String> rank1 = new HashSet<>(as.list("Override"));
-  private static final HashSet<String> rank2 = new HashSet<>(
-      as.list("Documented", "FunctionalInterface", "Inherited", "Retention", "Repeatable", "SafeVarargs", "Target"));
-  private static final HashSet<String> rank3 = new HashSet<>(as.list("$USER_DEFINED_ANNOTATION$"));
-  private static final HashSet<String> rank4 = new HashSet<>(as.list("Action", "Addressing", "BindingType", "ConstructorProperties", "DescriptorKey",
-      "FaultAction", "Generated", "HandlerChain", "InitParam", "MTOM", "MXBean", "Oneway", "PostConstruct", "PreDestroy", "RequestWrapper",
-      "Resource", "Resources", "RespectBinding", "ResponseWrapper", "ServiceMode", "SOAPBinding", "SOAPMessageHandler", "SOAPMessageHandlers",
-      "SupportedAnnotationTypes", "SupportedOptions", "SupportedSourceVersion", "Transient", "WebEndpoint", "WebFault", "WebMethod", "WebParam",
-      "WebResult", "WebService", "WebServiceClient", "WebServiceFeatureAnnotation", "WebServiceProvider", "WebServiceRef", "WebServiceRefs",
-      "XmlAccessorOrder", "XmlAccessorType", "XmlAnyAttribute", "XmlAnyElement", "XmlAttachmentRef", "XmlAttribute", "XmlElement", "XmlElementDecl",
-      "XmlElementRef", "XmlElementRefs", "XmlElements", "XmlElementWrapper", "XmlEnum", "XmlEnumValue", "XmlID", "XmlIDREF", "XmlInlineBinaryData",
-      "XmlJavaTypeAdapter", "XmlJavaTypeAdapters", "XmlList", "XmlMimeType", "XmlMixed", "XmlNs", "XmlRegistry", "XmlRootElement", "XmlSchema",
-      "XmlSchemaType", "XmlSchemaTypes", "XmlSeeAlso", "XmlTransient", "XmlType", "XmlValue"));
-  private static final HashSet<String> rank5 = new HashSet<>(as.list("SuppressWarnings"));
-  private static final HashSet<String> rank6 = new HashSet<>(as.list("NonNull", "Nullable"));
-  private static final ArrayList<HashSet<String>> rankTable = new ArrayList<>(as.list(rank0, rank1, rank2, rank3, rank4, rank5, rank6));
+  private static final HashSet<String> rankTable[] = as.array(//
+      new HashSet<>(as.list("Deprecated")), //
+      new HashSet<>(as.list("Override")), //
+      new HashSet<>(as.list("Documented", "FunctionalInterface", "Inherited", "Retention", "Repeatable", "SafeVarargs", "Target")),
+      new HashSet<>(as.list("$USER_DEFINED_ANNOTATION$")),
+      new HashSet<>(as.list("Action", "Addressing", "BindingType", "ConstructorProperties", "DescriptorKey", "FaultAction", "Generated",
+          "HandlerChain", "InitParam", "MTOM", "MXBean", "Oneway", "PostConstruct", "PreDestroy", "RequestWrapper", "Resource", "Resources",
+          "RespectBinding", "ResponseWrapper", "ServiceMode", "SOAPBinding", "SOAPMessageHandler", "SOAPMessageHandlers", "SupportedAnnotationTypes",
+          "SupportedOptions", "SupportedSourceVersion", "Transient", "WebEndpoint", "WebFault", "WebMethod", "WebParam", "WebResult", "WebService",
+          "WebServiceClient", "WebServiceFeatureAnnotation", "WebServiceProvider", "WebServiceRef", "WebServiceRefs", "XmlAccessorOrder",
+          "XmlAccessorType", "XmlAnyAttribute", "XmlAnyElement", "XmlAttachmentRef", "XmlAttribute", "XmlElement", "XmlElementDecl", "XmlElementRef",
+          "XmlElementRefs", "XmlElements", "XmlElementWrapper", "XmlEnum", "XmlEnumValue", "XmlID", "XmlIDREF", "XmlInlineBinaryData",
+          "XmlJavaTypeAdapter", "XmlJavaTypeAdapters", "XmlList", "XmlMimeType", "XmlMixed", "XmlNs", "XmlRegistry", "XmlRootElement", "XmlSchema",
+          "XmlSchemaType", "XmlSchemaTypes", "XmlSeeAlso", "XmlTransient", "XmlType", "XmlValue")),
+      new HashSet<>(as.list("SuppressWarnings")), //
+      new HashSet<>(as.list("NonNull", "Nullable")));
 
   public static int rankAnnotation(final IExtendedModifier ¢) {
     return rankAnnotation(az.annotation(¢).getTypeName().getFullyQualifiedName());
   }
 
-  @SuppressWarnings("boxing") public static int rankAnnotation(final String annotationName) {
-    for (final Integer $ : range.from(0).to(rankTable.size()))
-      if (rankTable.get($).contains(annotationName))
-        return $;
+  public static int rankAnnotation(final String annotationName) {
+    int $ = 0;
+    for (final HashSet<String> ¢ : rankTable) {
+      ++$;
+      if (¢.contains(annotationName))
+       return $;
+    }
     return rankAnnotation("$USER_DEFINED_ANNOTATION$");
   }
 
@@ -58,14 +59,7 @@ public class AnnotationSort<N extends BodyDeclaration> extends EagerTipper<N> im
 
   private static List<? extends IExtendedModifier> sort(final List<? extends IExtendedModifier> ¢) {
     return ¢.stream().sorted(comp).collect(Collectors.toList());
-  }/* @SuppressWarnings("unchecked") void sortElements(final List<? extends
-    * IExtendedModifier> elements, ListRewrite r) { if (elements.isEmpty())
-    * return; final List myCopy = new ArrayList(); myCopy.addAll(elements);
-    * Collections.sort(myCopy, comp); for (int i = 0; i < elements.size(); ++i)
-    * { ASTNode oldNode = (ASTNode) elements.get(i); ASTNode newNode = (ASTNode)
-    * myCopy.get(i); if (oldNode != newNode) { r.replace(oldNode,
-    * $.createMoveTarget(newNode), g); } } */
-
+  }
   @Override public Tip tip(final N n) {
     if (n == null || az.bodyDeclaration(n) == null)
       return null;
@@ -88,11 +82,6 @@ public class AnnotationSort<N extends BodyDeclaration> extends EagerTipper<N> im
     };
   }
 
-  /* public ASTNode replacement(final N d) { final N $ = duplicate.of(d); final
-   * List<IExtendedModifier> as = new ArrayList<>(sort(extract.annotations($)));
-   * final List<IExtendedModifier> ms = new ArrayList<>(extract.modifiers($));
-   * extendedModifiers($).clear(); extendedModifiers($).addAll(as);
-   * extendedModifiers($).addAll(ms); return !wizard.same($, d) ? $ : null; } */
   @Override public String description(final N ¢) {
     return "Sort annotations of " + extract.category(¢) + " " + extract.name(¢) + " (" + extract.annotations(¢) + "->" + sort(extract.annotations(¢))
         + ")";
