570029c9032122ffe132eae90901372e62ce9702
orimarco <marcovitch.ori@gmail.com>
2016-12-07 15:36:46 +0200

    BitOperation pattern

---
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/TernaryBitOperation.java
@@ -0,0 +1,38 @@
+package il.org.spartan.spartanizer.research.patterns;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.research.*;
+
+/** @author Ori Marcovitch
+ * @since Dec 7, 2016 */
+public class TernaryBitOperation extends NanoPatternTipper<ConditionalExpression> {
+  private static Set<UserDefinedTipper<ConditionalExpression>> tippers = new HashSet<UserDefinedTipper<ConditionalExpression>>() {
+    static final long serialVersionUID = 1L;
+    {
+      add(TipperFactory.patternTipper("$X == 0 ? 0 : 1", "Bit.val($X)", ""));
+      add(TipperFactory.patternTipper("0 == $X ? 0 : 1", "Bit.val($X)", ""));
+      add(TipperFactory.patternTipper("$X == 0 ? 1 : 0", "Bit.not($X)", ""));
+      add(TipperFactory.patternTipper("0 == $X ? 1 : 0", "Bit.not($X)", ""));
+      add(TipperFactory.patternTipper("($X == 0) ? 0 : 1", "Bit.val($X)", ""));
+      add(TipperFactory.patternTipper("(0 == $X) ? 0 : 1", "Bit.val($X)", ""));
+      add(TipperFactory.patternTipper("($X == 0) ? 1 : 0", "Bit.not($X)", ""));
+      add(TipperFactory.patternTipper("(0 == $X) ? 1 : 0", "Bit.not($X)", ""));
+    }
+  };
+
+  @Override public boolean canTip(final ConditionalExpression ¢) {
+    return anyTips(tippers, ¢);
+  }
+
+  @Override public Tip tip(ConditionalExpression ¢) {
+    return firstThatTips(tippers, ¢).tip(¢);
+  }
+
+  @Override public String description(@SuppressWarnings("unused") ConditionalExpression __) {
+    return null;
+  }
+}
