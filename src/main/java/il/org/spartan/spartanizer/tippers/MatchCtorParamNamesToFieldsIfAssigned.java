8e805f76701e5e0b06e2395f9b03f1bcc5d87dee
Doron Mehsulam <doronmmm@hotmail.com>
2016-12-07 16:39:43 +0200

    Formatted, spartanized, used step lib, suppressed warnings #20

--- src/main/java/il/org/spartan/spartanizer/tippers/MatchCtorParamNamesToFieldsIfAssigned.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/MatchCtorParamNamesToFieldsIfAssigned.java
@@ -8,17 +8,16 @@ import org.eclipse.text.edits.*;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/**
- * @since 07-Dec-16
- * @author Doron Meshulam
- *
- */
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
-
+/** @since 07-Dec-16
+ * @author Doron Meshulam */
+@SuppressWarnings("unused") 
 public class MatchCtorParamNamesToFieldsIfAssigned extends CarefulTipper<MethodDeclaration> implements TipperCategory.Idiomatic {
   @Override public String description(final MethodDeclaration ¢) {
     return "Match constructor parameter names to fields";
@@ -26,43 +25,26 @@ public class MatchCtorParamNamesToFieldsIfAssigned extends CarefulTipper<MethodD
 
   @Override public Tip tip(final MethodDeclaration d) {
     if (!d.isConstructor())
-      return null; 
-     
+      return null;
     List<SingleVariableDeclaration> ctorParams = parameters(d);
-    List<Statement> bodyStatements = d.getBody().statements();
+    List<Statement> bodyStatements = statements(d);
     for (Statement s : bodyStatements) {
-      if (!(s instanceof ExpressionStatement))
+      if (!(iz.expressionStatement(s)))
         continue;
       Expression e = ((ExpressionStatement) s).getExpression();
-      if (!(e instanceof Assignment))
+      if (!(iz.assignment(e)))
         continue;
-      
-      Assignment a = (Assignment) e;
-      Expression leftAss = a.getLeftHandSide(); // LOL ^_^
-      Expression rightAss = a.getRightHandSide();
-      if(!(leftAss instanceof FieldAccess)) {
+      Assignment a = az.assignment(e);
+      if (!(iz.fieldAccess(left(a))) || !(((FieldAccess) left(a)).getExpression() instanceof ThisExpression))
         continue;
-      }
-      
-      if(!(((FieldAccess)leftAss).getExpression() instanceof ThisExpression)) {
+      SimpleName fieldName = ((FieldAccess) left(a)).getName();
+      if (!(iz.simpleName(right(a))))
         continue;
-      }
-      
-      SimpleName fieldName = ((FieldAccess)leftAss).getName();
-      if (!(rightAss instanceof SimpleName)) {
-        continue;
-      }
-      SimpleName paramName = (SimpleName)a.getRightHandSide();
-//    Check if right is parameter and also there wasn't a local variable in the name of the field4
-//        
-//      
-//    List<String> uses = Collect.usesOf("y").inside(d.getBody());
+      SimpleName paramName = az.simpleName(right(a));
     }
-    
-    
     return new Tip(description(d), d, this.getClass()) {
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
-        //TODO: Change of code here
+        // TODO: Change of code here
         System.out.println(r);
         System.out.println(g);
       }
