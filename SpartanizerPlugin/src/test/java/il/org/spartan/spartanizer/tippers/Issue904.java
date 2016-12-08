25c5917cf2d8892feac72be5684f0b49acca4e14
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-08 21:01:53 +0200

    added notes

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue904.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue904.java
@@ -3,9 +3,11 @@ package il.org.spartan.spartanizer.tippers;
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
-/** ?? TODO: Yuval Simon which tipper is tested?
- * @author Yuval Simon 
- * @year 2016-12-08 */
+
+/** This is a unit test for {@link BlockRemoveDeadVariables} of previously failed tests. 
+ * Related to Issue359. 
+ * @author Yuval Simon
+ * @since 2016-12-08 */
 @Ignore
 @SuppressWarnings("static-method")
 public class Issue904 {
