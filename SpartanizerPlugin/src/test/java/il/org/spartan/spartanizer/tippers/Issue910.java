fdbc7afc7fa695cb753ea0ee47184356f60a052d
Yossi Gil <yossi.gil@gmail.com>
2016-12-08 20:50:46 +0200

    Issue #587

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue910.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue910.java
@@ -4,6 +4,9 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
 
+/** ?? TODO: Yuval Simon which tipper is tested?
+ * @author Yuval Simon 
+ * @year 2016-12-08 */
 @Ignore
 @SuppressWarnings("static-method")
 public class Issue910 {
