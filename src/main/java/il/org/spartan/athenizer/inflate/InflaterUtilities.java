1caefe4a5ee7b94d6d094763eba93fc0d0d09366
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-08 19:53:36 +0200

    code fully organized, refactored, spartanized and documented - expander works well and passes tests - closing #883 the first expander is done

--- src/main/java/il/org/spartan/athenizer/inflate/InflaterUtilities.java
+++ src/main/java/il/org/spartan/athenizer/inflate/InflaterUtilities.java
@@ -14,16 +14,23 @@ import il.org.spartan.plugin.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.utils.*;
 
+/*
+ * @author Raviv Rachmiel
+ * @since 8-12-16
+ */
+
 public class InflaterUtilities {
   /** Main function of the application.
    * @param r JD
-   * @param statementsListList selection as list of lists of statements
+   * @param nl list of statements and expression in the selected code which for each we check if we can expand
    * @param g JD
    * @return true iff rewrite object should be applied For now - we only have a
    *         few expanders (1) so we will not infrastructure a full toolbox as
    *         in the spartanizer But we should definitely implement it one day
-   * @author Raviv Rachmiel
-   * @since 12-5-16 */
+   *
+   * more expanders should be added to the change ASTNode in a "for loop" for each expander.
+   * SHOULD BE ORGANIZED correctly in a toolbox infrastracture when we have more expanders
+   */
   static boolean rewrite(final ASTRewrite r, final List<ASTNode> nl, final TextEditGroup __) {
     boolean $ = false;
     if (nl.isEmpty())
@@ -38,6 +45,10 @@ public class InflaterUtilities {
     return $;
   }
 
+  /*
+   * @param u - the WrappedCompilationUnit which is athenized 
+   * @param ns - the list of statemend which were selected and might be changed
+   */
   static void commitChanges(final WrappedCompilationUnit u, final List<ASTNode> ns) {
     try {
       final TextFileChange textChange = new TextFileChange(u.descriptor.getElementName(), (IFile) u.descriptor.getResource());
@@ -53,6 +64,13 @@ public class InflaterUtilities {
     }
   }
 
+  /*
+   * @param u - the WrappedCompilationUnit
+   * @return - list of relevant statements for the expanders
+   * gets Statements (for now, maybe we should change the name to getASTNodes one day when we have a lot of statements)
+   * from compilationUnit - only kind of statements we might need for the athenizer
+   * SHOULD BE CHANGED when we add more expanders
+   */
   static List<ASTNode> getStatements(WrappedCompilationUnit u) {
     List<ASTNode> $ = new ArrayList<>();
     u.compilationUnit.accept(new ASTVisitor() {
@@ -83,6 +101,10 @@ public class InflaterUtilities {
         : startChar1 != startChar2 ? length2 + startChar2 > startChar1 : length1 > 0 && length2 > 0);
   }
 
+  /*
+   * @param ns ASTNodes in compilation unit which might be relevant
+   * @return list of selected ASTNodes
+   */
   static List<ASTNode> selectedStatements(List<ASTNode> ns) {
     List<ASTNode> $ = new ArrayList<>();
     for (ASTNode ¢ : ns)
