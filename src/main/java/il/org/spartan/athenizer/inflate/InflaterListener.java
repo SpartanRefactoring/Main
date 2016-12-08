0d81d4ad4c69b36585b9bc8bb7b656a84fa94015
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-08 19:44:27 +0200

    added tests for #833

--- src/main/java/il/org/spartan/athenizer/inflate/InflaterListener.java
+++ src/main/java/il/org/spartan/athenizer/inflate/InflaterListener.java
@@ -3,24 +3,19 @@ package il.org.spartan.athenizer.inflate;
 import java.util.*;
 import java.util.List;
 
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jdt.core.dom.*;
-import org.eclipse.jdt.core.dom.rewrite.*;
-import org.eclipse.ltk.core.refactoring.*;
+
 import org.eclipse.swt.*;
 import org.eclipse.swt.custom.*;
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.text.edits.*;
+
 import org.eclipse.ui.*;
 
-import il.org.spartan.athenizer.inflate.expanders.*;
+
 import il.org.spartan.plugin.*;
-import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.utils.*;
+
 
 public class InflaterListener implements MouseWheelListener, KeyListener {
   static final int CURSOR_IMAGE = SWT.CURSOR_CROSS;
@@ -44,90 +39,15 @@ public class InflaterListener implements MouseWheelListener, KeyListener {
     if (active)
       if (¢.count > 0)
         inflate();
-      else if (¢.count < 0)
+      else if (¢.count < 0)  
         deflate();
   }
 
-  /** Main function of the application.
-   * @param r JD
-   * @param statementsListList selection as list of lists of statements
-   * @param g JD
-   * @return true iff rewrite object should be applied For now - we only have a
-   *         few expanders (1) so we will not infrastructure a full toolbox as
-   *         in the spartanizer But we should definitely implement it one day
-   * @author Raviv Rachmiel
-   * @since 12-5-16 */
-  private static boolean rewrite(final ASTRewrite r, final List<ASTNode> nl, final TextEditGroup __) {
-    boolean $ = false;
-    if (nl.isEmpty())
-      return false;
-    for (final ASTNode statement : nl) {
-      final ASTNode change = new TernaryExpander().replacement(az.statement(statement));
-      if (change != null) {
-        r.replace(statement, change, __);
-        $ = true;
-      }
-    }
-    return $;
-  }
-
-  public static void commitChanges(final WrappedCompilationUnit u, final List<ASTNode> ns) {
-    try {
-      final TextFileChange textChange = new TextFileChange(u.descriptor.getElementName(), (IFile) u.descriptor.getResource());
-      textChange.setTextType("java");
-      final ASTRewrite r = ASTRewrite.create(u.compilationUnit.getAST());
-      if (rewrite(r, ns, null)) {
-        textChange.setEdit(r.rewriteAST());
-        if (textChange.getEdit().getLength() != 0)
-          textChange.perform(new NullProgressMonitor());
-      }
-    } catch (final CoreException ¢) {
-      monitor.log(¢);
-    }
-  }
-
-  private static List<ASTNode> getStatements(WrappedCompilationUnit u) {
-    List<ASTNode> $ = new ArrayList<>();
-    u.compilationUnit.accept(new ASTVisitor() {
-      @Override public boolean visit(ReturnStatement node) {
-        $.add(node);
-        return true;
-      }
-
-      @Override public boolean visit(ExpressionStatement node) {
-        if(az.assignment(node.getExpression())!=null)
-          $.add(node);
-        return true;
-      }
-    });
-    return $;
-  }
-
-  /* @param startChar1 - starting char of first interval
-   * 
-   * @param lenth1 - length of first interval
-   * 
-   * @param startChar2 - starting char of second interval
-   * 
-   * @param length2 - length of second interval SPARTANIZED - should use
-   * Athenizer one day to understand it */
-  private static boolean intervalsIntersect(int startChar1, int length1, int startChar2, int length2) {
-    return length1 != 0 && length2 != 0 && (startChar1 < startChar2 ? length1 + startChar1 > startChar2
-        : startChar1 != startChar2 ? length2 + startChar2 > startChar1 : length1 > 0 && length2 > 0);
-  }
-
-  private static List<ASTNode> selectedStatements(List<ASTNode> ns) {
-    List<ASTNode> $ = new ArrayList<>();
-    for (ASTNode ¢ : ns)
-      if (intervalsIntersect(¢.getStartPosition(), ¢.getLength(), Selection.Util.current().textSelection.getOffset(),
-          Selection.Util.current().textSelection.getLength()))
-        $.add(¢);
-    return $;
-  }
+  
 
   private static void inflate() {
     WrappedCompilationUnit wcu = Selection.Util.current().inner.get(0).build();
-    commitChanges(wcu, selectedStatements(getStatements(wcu)));
+    InflaterUtilities.commitChanges(wcu, InflaterUtilities.selectedStatements(InflaterUtilities.getStatements(wcu)));
   }
 
   private static void deflate() {
