19430cf392f7ec0ee10d97f0f8578edce96be461
Yossi Gil <yossi.gil@gmail.com>
2016-12-08 20:50:46 +0200

    Issue #587

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue907.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue907.java
@@ -3,7 +3,9 @@ package il.org.spartan.spartanizer.tippers;
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
-
+/** ?? TODO: Yuval Simon which tipper is tested?
+ * @author Yuval Simon 
+ * @year 2016-12-08 */
 @Ignore
 @SuppressWarnings("static-method")
 public class Issue907 {
