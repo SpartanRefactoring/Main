fb1c55c5d5a32e4f457fda0db75875e63ca1b79b
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-08 17:29:17 +0200

    #905

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue905.java
@@ -0,0 +1,11 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class Issue905 {
+  @Test public void test() {
+    fail("Not yet implemented");
+  }
+}
