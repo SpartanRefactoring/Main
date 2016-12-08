d5addb0ea09f06e05f75dfd132cd360e9dba478b
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-08 21:01:53 +0200

    added notes

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue903.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue903.java
@@ -3,9 +3,10 @@ package il.org.spartan.spartanizer.tippers;
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
-/** ?? TODO: Yuval Simon which tipper is tested?
- * @author Yuval Simon 
- * @year 2016-12-08 */
+
+/** This is a unit test for {@link ReturnToBreakFiniteFor} of previously failed tests. Related to Issue131. 
+ * @author Yuval Simon
+ * @since 2016-12-08 */
 @Ignore
 @SuppressWarnings("static-method")
 public class Issue903 {
