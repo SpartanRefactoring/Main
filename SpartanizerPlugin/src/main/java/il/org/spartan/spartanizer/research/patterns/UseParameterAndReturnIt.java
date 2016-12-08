f040cfa8ad00fb96e9b3d7f5a4f725362bc26ad5
orimarco <marcovitch.ori@gmail.com>
2016-12-08 10:26:24 +0200

    DoNothingReturnParam pattern

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/UseParameterAndReturnIt.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/UseParameterAndReturnIt.java
@@ -6,15 +6,13 @@ import org.eclipse.jdt.core.dom.*;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
-import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
 /** @author Ori Marcovitch
  * @since 2016 */
 public class UseParameterAndReturnIt extends JavadocMarkerNanoPattern<MethodDeclaration> {
   @Override protected boolean prerequisites(final MethodDeclaration ¢) {
-    return parameters(¢) != null && parameters(¢).size() == 1 && body(¢) != null && !iz.constructor(¢) && !iz.voidType(returnType(¢))
-        && searchDescendants.forClass(ReturnStatement.class).from(¢).stream().map(r -> expression(r) + "")
-            .allMatch(e -> e.equals(identifier(name(onlyOne(parameters(¢))))));
+    return oneParameter(¢) && body(¢) != null && !iz.constructor(¢) && !iz.voidType(returnType(¢))
+        && returnStatements(¢).stream().map(r -> expression(r) + "").allMatch(e -> e.equals(identifier(name(onlyOne(parameters(¢))))));
   }
 }
