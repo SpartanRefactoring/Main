21848dccd69df6ef19a96d6c436302d671dec174
dormaayn <dor.d.ma@gmail.com>
2016-12-09 13:13:58 +0200

    Added .givesEither(,,,) + tests (Issue #884)

---
+++ src/test/java/il/org/spartan/spartanizer/utils/AdvancedGivesTests.java
@@ -0,0 +1,27 @@
+package il.org.spartan.spartanizer.utils;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/** Test Class for Issue #884
+ * @author Dor Ma'ayan
+ * @since 09-12-2016 */
+@SuppressWarnings("static-method")
+public class AdvancedGivesTests {
+  @Test public void test0() {
+    trimmingOf("1+2")//
+        .givesEither("3", "").stays();
+  }
+
+  @Test public void test1() {
+    trimmingOf("1+2+a")//
+        .givesEither("a+3", "3+a");
+  }
+
+  @Test public void test2() {
+    trimmingOf("for (int ¢ = 0 ; ¢ < 5 ; ++¢) {++¢; continue;}")//
+        .givesEither("for (int ¢ = 0 ; ¢ < 5 ; ++¢) {++¢;}", //
+            "for (int ¢ = 0 ; ¢ < 5 ; ++¢) ++¢;");
+  }
+}
