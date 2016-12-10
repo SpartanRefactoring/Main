e69404b4308d2b768481f9c82e05fcc79986e762
orimarco <marcovitch.ori@gmail.com>
2016-12-10 23:37:49 +0200

    delegator, ConstantReturner

---
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/ConstantReturner.java
@@ -0,0 +1,22 @@
+package il.org.spartan.spartanizer.research.patterns;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.research.*;
+
+/** @author Ori Marcovitch
+ * @since 2016 */
+public class ConstantReturner extends JavadocMarkerNanoPattern<MethodDeclaration> {
+  private static Set<UserDefinedTipper<Statement>> tippers = new HashSet<UserDefinedTipper<Statement>>() {
+    static final long serialVersionUID = 1L;
+    {
+      add(TipperFactory.patternTipper("return $L;", "", ""));
+    }
+  };
+
+  @Override protected boolean prerequisites(final MethodDeclaration ¢) {
+    return anyTips(tippers, onlyStatement(¢));
+  }
+}
