6ce2441dcc96211aba67f5bc07d93b147cdb84ba
Yossi Gil <yossi.gil@gmail.com>
2016-12-11 19:11:20 +0200

    Closes #922

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue922.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue922.java
@@ -1,140 +1,66 @@
 package il.org.spartan.spartanizer.tippers;
 
-import static il.org.spartan.azzert.*;
 import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
-
-import java.util.*;
-
 import org.junit.*;
 import org.junit.runners.*;
 
-import il.org.spartan.*;
-
-/** Unit tests for {@link MethodInvocationAssertTrueFalse} 
+/** Unit tests for {@link ExpressionStatementAssertTrueFalse}
  * @author Yossi Gil // put your name here
  * @since 2016 // put the year/date here */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SuppressWarnings({ "static-method", "javadoc" })
 public class Issue922 {
-  private static Object object() {
-    return new Object();
+  @Test public void a$01() {
+    trimmingOf("assertTrue(true);")//
+        .gives("assert true;")//
+        .stays();
   }
 
-  private int $0() {
-    return 0;
+  @Test public void a$02() {
+    trimmingOf("assertFalse(true);")//
+        .gives("assert false;")//
+        .stays();
   }
 
-  private int $1() {
-    return 1;
+  @Test public void a$03() {
+    trimmingOf("assertTrue(T);")//
+        .gives("assert T;")//
+        .stays();
   }
 
-  private boolean $false() {
-    return false;
+  @Test public void a$04() {
+    trimmingOf("assertFalse(T);")//
+        .gives("assert !T;")//
+        .stays();
   }
 
-  private Object $null() {
-    return null;
+  @Test public void a$05() {
+    trimmingOf("assertTrue(M, true);")//
+        .gives("assert true:M;")//
+        .stays();
   }
 
-  private boolean $true() {
-    return true;
+  @Test public void a$06() {
+    trimmingOf("assertFalse(M, true);")//
+        .gives("assert false:M;")//
+        .stays();
   }
 
-  private Object sameSomeObject() {
-    return this;
+  @Test public void a$07() {
+    trimmingOf("assertTrue(M, T);")//
+        .gives("assert T:M;")//
+        .stays();
   }
 
-  /** if fails, suite did not compile... */
-  @Test public void Z$000() {
-    new Object().hashCode();
+  @Test public void a$08() {
+    trimmingOf("assertFalse(M, T);")//
+        .gives("assert !T:M;")//
+        .stays();
   }
 
-  /** if fails, assertions do not work */
-  @Test public void Z$010() {
-    assert null == null : "This assert must never fail";
-  }
-
-  /** if fails, enable assertions (flag '-va') to the JVM. --yg In Windows it
-   * worked for me by adding '-ea' flag in the run configurations -> VM
-   * arguments. --or */
-  @Test(expected = AssertionError.class) public void Z$020() {
-    assert null != null;
-  }
-
-  /** This is the incorrect way you should check for nulls, using {@link azzert}
-   * makes sure we get more informative messages */
-  @Test public void Z$030() {
-    azzert.isNull($null());
-  }
-
-  /** Correct way of checking for nulls. {@link azzert} cannot provide further
-   * information if the test fails, since failures give null which carries no
-   * information informative messages */
-  @Test public void Z$040() {
-    assert new Object() != null : "Weird... I (" + this + ") never knew that new can return null";
-  }
-
-  /** Correct way of checking for nulls. {@link azzert} cannot provide further
-   * information if the test fails, since failures give null which carries no
-   * information informative messages */
-  @Test public void Z$050() {
-    assert new Object() != null;
-  }
-
-  /** Correct way of checking for true value. {@link azzert} cannot provide
-   * further information if the test fails, since failures give nothing but
-   * boolean value. */
-  @Test public void Z$060() {
-    assert $true();
-    assert $true() : "Failure in " + object();
-  }
-
-  /** Correct way of checking for false value. {@link azzert} cannot provide
-   * further information if the test fails, since failures give nothing but
-   * boolean value. */
-  @Test public void Z$070() {
-    assert !$false();
-    assert !$false() : "Failure in " + object();
-  }
-
-  /** Correct way of checking types */
-  @Test public void Z$080() {
-    azzert.that(new ArrayList<>(), instanceOf(List.class));
-  }
-
-  /** Correct way of checking for inequality of values */
-  @Test public void Z$090() {
-    azzert.that(object(), not(object()));
-  }
-
-  /** Correct way of checking for equality of values */
-  @Test public void Z$100() {
-    azzert.that(sameSomeObject(), is(sameSomeObject()));
-  }
-
-  /** Correct way of checking for equality of numbers */
-  @Test public void Z$110() {
-    azzert.that($0(), is($0()));
-    azzert.that($0(), not(is($1())));
-  }
-
-  /** Correct ways of comparing numbers */
-  @Test public void Z$120() {
-    azzert.that($0(), greaterThanOrEqualTo($0()));
-    azzert.that($1(), greaterThanOrEqualTo($0()));
-    azzert.that($1(), greaterThan($0()));
-    azzert.that($0(), lessThanOrEqualTo($1()));
-    azzert.that($0(), lessThanOrEqualTo($0()));
-    azzert.that($0(), lessThan($1()));
-  }
-
-  /** Correct way of trimming does not change */
-  @Test public void Z$130() {
-    trimmingOf("a").stays();
-  }
-
-  /** Correct way of trimming does not change */
-  @Test public void Z$140() {
-    trimmingOf("a").stays();
+  @Test public void a$09() {
+    trimmingOf("aasdfa.asdf.assertFalse(M, T);")//
+        .gives("assert !T:M;")//
+        .stays();
   }
 }
