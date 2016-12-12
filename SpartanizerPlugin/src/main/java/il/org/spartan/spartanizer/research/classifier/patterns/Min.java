c19afab065121e31f642225488ce835ca6fb4bd6
orimarco <marcovitch.ori@gmail.com>
2016-12-12 09:07:45 +0200

    yet one more refactoring

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/classifier/patterns/Min.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/classifier/patterns/Min.java
@@ -1,4 +1,4 @@
-package il.org.spartan.spartanizer.research.patterns;
+package il.org.spartan.spartanizer.research.classifier.patterns;
 
 import java.util.*;
 
@@ -10,10 +10,12 @@ import il.org.spartan.spartanizer.research.patterns.common.*;
 
 /** @author Ori Marcovitch
  * @since 2016 */
-public class ForEachEnhanced extends NanoPatternTipper<ForStatement> {
+public class Min extends NanoPatternTipper<ForStatement> {
   Set<UserDefinedTipper<ForStatement>> tippers = new HashSet<UserDefinedTipper<ForStatement>>() {
     static final long serialVersionUID = 1L;
-    {}
+    {
+      add(TipperFactory.patternTipper("for (int $N0 = 1; $N0 < $N1.$N2; ++$N0)  if ($N1[$N0] < $N3)   $N3 = $N1[$N0];", "min();", "min"));
+    }
   };
 
   @Override public boolean canTip(final ForStatement ¢) {
@@ -25,6 +27,6 @@ public class ForEachEnhanced extends NanoPatternTipper<ForStatement> {
   }
 
   @Override public Tip pattern(final ForStatement ¢) {
-    return firstTipper(tippers, ¢).tip(¢);
+    return firstTip(tippers, ¢);
   }
 }
