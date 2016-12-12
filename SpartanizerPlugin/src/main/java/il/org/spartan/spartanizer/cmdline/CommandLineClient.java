32d3c0b83240217a48e9049d8be210112807110b
M4tt30rru <matt.orru@gmail.com>
2016-12-12 20:05:06 +0200

    clean up

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineClient.java
@@ -7,36 +7,13 @@ import il.org.spartan.spartanizer.cmdline.report.*;
 
 /** Simplified version of command line client that uses spartanizer applicator
  * @author Matteo Orru' */
-//<<<<<<< HEAD
-public final class CommandLineClient extends HeadlessSpartanizer {
-//=======
-//public final class CommandLineClient {
-//  @External(alias = "i", value = "name of the input directory") static String inputDir = ".";
-//  @External(alias = "o", value = "name of the output directory") private static String outputDir = "/tmp";
-//  @External(alias = "d", value = "default values for the directories") private static boolean devault;
-//  @External(alias = "cs", value = "class name on which apply the tippers") private static String[] clazzes;
-//  @External(alias = "tg", value = "tipper group to be applied to the clazzes") private static String[] tipperGroups;
-//  @External(alias = "etg", value = "exclude one or more tipper groups") private static String[] excludeTipperGroups;
-//  @External(alias = "np", value = "Nano Patterns") private static String[] NanoPatterns;
-//  @External(alias = "enp", value = "Exclude Selected Nano Patterns") private static String[] excludeNanoPatterns;
-//  @External(alias = "xallnp", value = "Exclude All Nano Patterns") private static boolean excludeAllNanoPatterns;
- private final MetricsReport metricsReport = new MetricsReport();
 
-//>>>>>>> matteo-experimental
+public final class CommandLineClient extends HeadlessSpartanizer {
+
   public static void main(final String[] args) {
     new CommandLineClient().go(args);
   }
 
-//<<<<<<< HEAD
-//  private void go(final String[] args) {
-//    if (External.Introspector.extract(args, this).isEmpty()) {
-//      System.err.println(usage(this, args, this));
-//      return;
-//    }
-//    MetricsReport.getSettings().setInputFolder(inputFolder);
-//    MetricsReport.getSettings().setOutputFolder(outputFolder);
-//    MetricsReport.initialize();
-//=======
   private void go(String[] args) {
     
     // TODO Yossi, the instruction
@@ -50,27 +27,18 @@ public final class CommandLineClient extends HeadlessSpartanizer {
     }
     
     extract(args, this);
- 
-//    MetricsReport.getSettings().setInputFolder(inputDir);
-//    MetricsReport.getSettings().setOutputFolder(outputDir);
-//    MetricsReport.initialize();
-//>>>>>>> matteo-experimental
+
     ReportGenerator.generate("metrics");
     ReportGenerator.setOutputFolder(outputDir);
     ReportGenerator.setInputFolder(inputDir);
     run();
-//    MetricsReport.generate();
   }
-//<<<<<<< HEAD
 
-//=======
   
   @SuppressWarnings("static-method")
-//>>>>>>> matteo-experimental
   private void run() {
     name(system.folder2File(inputDir));
     apply();
-    // r.printExternals();
   }
 
   static void printPrompt() {
