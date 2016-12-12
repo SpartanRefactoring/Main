4a72d5743256ec1993d3e61cea9d9896c7e30139
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-12 20:45:25 +0200

    #907

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue907.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue907.java
@@ -8,11 +8,10 @@ import org.junit.*;
  * Related to {@link Version250} and {@link Issue086}.
  * @author Yuval Simon
  * @since 2016-12-08 */
-@Ignore
+
 @SuppressWarnings("static-method")
-//TODO: unignore the test in {@link Version250} when fixed
 public class Issue907 {
   @Test public void issue086_1() {
-    trimmingOf("if(false)" + "c();\n" + "int a;").gives("if(false)" + "c();\n").gives("{}").gives("").stays();
+    trimmingOf("if(false)" + "c();\n" + "int a;").gives("{}int a;").gives("int a;").stays();
   }
 }
