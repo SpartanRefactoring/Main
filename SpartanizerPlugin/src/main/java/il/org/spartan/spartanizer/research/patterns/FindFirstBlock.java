3e09ea6669689a93b1e6f964e2f9ad6c9b850b79
orimarco <marcovitch.ori@gmail.com>
2016-12-12 08:58:34 +0200

    just renming

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/FindFirstBlock.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/FindFirstBlock.java
@@ -15,7 +15,7 @@ public final class FindFirstBlock extends NanoPatternTipper<Block> {
     static final long serialVersionUID = 1L;
     {
       add(TipperFactory.statementsPattern("for($T $N : $X1) if($X2) return $N;", "return $X1.stream().findFirst($N -> $X2).get();",
-          "Go Fluent : FindFirst", Option.LAST));
+          "Go Fluent : FindFirst", Option.LAST_IN_BLOCK));
       add(TipperFactory.statementsPattern("for($T $N : $X1) if($X2) return $N; throw $X3;",
           "if($X1.stream().anyMatch($N -> $X2)) return $X1.stream().findFirst($N -> $X2).get(); throw $X3;", "Go Fluent : FindFirst"));
       add(TipperFactory.statementsPattern("for($T $N : $X1) if($X2) {$N2 = $N; break;}", "$N2 = $X1.stream().findFirst($N -> $X2).get();",
