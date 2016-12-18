87fde9baefc59542270858767ef0e64c66ee291d
orimarco <marcovitch.ori@gmail.com>
2016-12-18 14:27:51 +0200

    some more steps

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue835.java
@@ -9,6 +9,7 @@ import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.tipping.*;
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 public class Issue835 {
   Tipper<Block> t = new CastBlockSingletonVariableDefinition();
@@ -22,15 +23,15 @@ public class Issue835 {
   }
 
   @Test @SuppressWarnings("static-method") public void emptyBlock1() {
-    azzert.that(0, is(az.block(wizard.ast("{}")).statements().size()));
+    azzert.that(0, is(statements(az.block(wizard.ast("{}"))).size()));
   }
 
   @Test @SuppressWarnings("static-method") public void emptyBlock2() {
-    azzert.that(0, is(az.block(wizard.ast("\n{\n}\n")).statements().size()));
+    azzert.that(0, is(statements(az.block(wizard.ast("\n{\n}\n"))).size()));
   }
 
   @Test @SuppressWarnings("static-method") public void emptyBlock3() {
-    azzert.that(1, is(az.block(wizard.ast("\n{int a;}\n")).statements().size()));
+    azzert.that(1, is(statements(az.block(wizard.ast("\n{int a;}\n"))).size()));
   }
 
   @Test public void returnNotNullNonEmptyBlock() {
