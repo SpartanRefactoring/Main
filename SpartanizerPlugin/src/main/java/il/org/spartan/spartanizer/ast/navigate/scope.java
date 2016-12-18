5ed8f0e6508b319cc10478b8c1c90faed4c6ec63
Yossi Gil <yossi.gil@gmail.com>
2016-12-18 14:10:10 +0200

    missing files

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/ast/navigate/scope.java
@@ -0,0 +1,27 @@
+package il.org.spartan.spartanizer.ast.navigate;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+/** @author Yossi Gil
+ * @since 2016-12 */
+public interface scope {
+  int a = 0, b = 0;
+
+  static List<ASTNode> of(final SingleVariableDeclaration x) {
+    final ArrayList<ASTNode> $ = new ArrayList<>();
+    $.add(x);
+    return $;
+  }
+
+  static List<ASTNode> of(final VariableDeclarationFragment ¢) {
+    final ArrayList<ASTNode> $ = new ArrayList<>();
+    $.add(¢.getInitializer());
+    return $;
+  }
+
+  static List<ASTNode> of(@SuppressWarnings("unused") final VariableDeclarationExpression ¢) {
+    return new ArrayList<>();
+  }
+}
