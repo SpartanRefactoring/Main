9feb80327ceee2c49ae5c90c9f448991e53ac4ad
orimarco <marcovitch.ori@gmail.com>
2016-12-18 10:18:34 +0200

    refactoring

--- src/main/java/il/org/spartan/spartanizer/research/patterns/methods/SelfCaster.java
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/methods/SelfCaster.java
@@ -14,6 +14,6 @@ public class SelfCaster extends JavadocMarkerNanoPattern {
     if (!hazOneStatement(¢) || hazParameters(¢))
       return false;
     final CastExpression $ = az.castExpression(expression(az.returnStatement(onlyStatement(¢))));
-    return $ != null && returnTypeSameAs(¢, type($)) && iz.thisExpression(expression($));
+    return returnTypeSameAs(¢, type($)) && iz.thisExpression(expression($));
   }
 }
