8047c60ed48e1c3bd3ed5b9602d61aa6fe9ee1ac
orimarco <marcovitch.ori@gmail.com>
2016-12-19 08:47:26 +0200

    some refactoring

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchWithOneCaseToIf.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchWithOneCaseToIf.java
@@ -1,6 +1,7 @@
 package il.org.spartan.spartanizer.tippers;
 
 import java.util.*;
+import static il.org.spartan.lisp.*;
 
 import org.eclipse.jdt.core.dom.*;
 
@@ -8,6 +9,7 @@ import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 /** convert
  *
@@ -37,28 +39,31 @@ public class SwitchWithOneCaseToIf extends ReplaceCurrentNode<SwitchStatement> i
   @Override public ASTNode replacement(SwitchStatement s) {
     if (s == null)
       return null;
-    List<SwitchCase> l = extract.switchCases(s);
-    List<Statement> ll = step.statements(s);
+    List<SwitchCase> $ = extract.switchCases(s);
+    List<Statement> ll = statements(s);
     int ind = firstBreakIndex(s);
     int sz = ll.size();
     String b1 = statementsToString(ll, 1, ind == -1 ? sz : ind);
-    if (l.size() == 1)
-      return wizard.ast("if(" + s.getExpression() + "==" + l.get(0).getExpression() + "){" + b1 + "}");
+    if ($.size() == 1)
+      return wizard.ast("if(" + expression(s) + "==" + expression(first($)) + "){" + b1 + "}");
     String b2 = statementsToString(ll, ind + 2, !iz.breakStatement(ll.get(sz - 1)) ? sz : sz - 1);
-    return wizard.ast("if(" + (l.get(0).isDefault() ? s.getExpression() + "==" + l.get(1).getExpression() + "){" + b2 + "} else{" + b1
-        : s.getExpression() + "==" + l.get(0).getExpression() + "){" + b1 + "} else{" + b2) + "}");
+    return wizard.ast("if(" + (first($).isDefault() ? expression(s) + "==" + expression($.get(1)) + "){" + b2 + "} else{" + b1
+        : expression(s) + "==" + expression(first($)) + "){" + b1 + "} else{" + b2) + "}");
   }
 
   @Override protected boolean prerequisite(SwitchStatement s) {
-    List<SwitchCase> l = extract.switchCases(s);
-    List<Statement> ll = step.statements(s);
+    List<SwitchCase> $ = extract.switchCases(s);
+    List<Statement> ll = statements(s);
     int ind = firstBreakIndex(s);
-//    return l.size() == 1 && !l.get(0).isDefault() && ll.size() > 1 && ind != 1
-//        || l.size() == 2 && ind > 1 && ind < ll.size() - 1 && !iz.switchCase(ll.get(ll.size() - 1)) && (l.get(0).isDefault() || l.get(1).isDefault());
-    return l.size() == 2 && ind > 1 && ind < ll.size() - 1 && !iz.switchCase(ll.get(ll.size() - 1)) && (l.get(0).isDefault() || l.get(1).isDefault());
+    // return l.size() == 1 && !l.get(0).isDefault() && ll.size() > 1 && ind !=
+    // 1
+    // || l.size() == 2 && ind > 1 && ind < ll.size() - 1 &&
+    // !iz.switchCase(ll.get(ll.size() - 1)) && (l.get(0).isDefault() ||
+    // l.get(1).isDefault());
+    return $.size() == 2 && ind > 1 && ind < ll.size() - 1 && !iz.switchCase(last(ll)) && (first($).isDefault() || $.get(1).isDefault());
   }
 
-  @SuppressWarnings("unused") @Override public String description(SwitchStatement __) {
+  @Override @SuppressWarnings("unused") public String description(SwitchStatement __) {
     return "Convert switch statement to if-else statement";
   }
 
@@ -69,7 +74,7 @@ public class SwitchWithOneCaseToIf extends ReplaceCurrentNode<SwitchStatement> i
         return $;
     return -1;
   }
-  
+
   static String statementsToString(final List<Statement> ss, int start, int end) {
     String $ = new String();
     for (int ¢ = start; ¢ < end; ++¢)
