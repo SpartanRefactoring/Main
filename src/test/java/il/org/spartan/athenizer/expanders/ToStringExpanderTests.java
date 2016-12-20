e514bcd94d2edad8d3baceafc1575b404bbfa84b
dormaayn <dor.d.ma@gmail.com>
2016-12-20 15:45:36 +0200

    Issue #965 Done

--- src/test/java/il/org/spartan/athenizer/expanders/ToStringExpanderTests.java
+++ src/test/java/il/org/spartan/athenizer/expanders/ToStringExpanderTests.java
@@ -18,8 +18,20 @@ public class ToStringExpanderTests {
   @Test public void test1() {
     expanderCheck("\"\"+t()", "t().toString()", new toStringExpander());
   }
-  
+
   @Test public void test2() {
     expanderCheckStays("\"abcd\"+t()", new toStringExpander());
+  }
+
+  @Test public void test3() {
+    expanderCheckStays("\"\"+3", new toStringExpander());
+  }
+
+  @Test public void test4() {
+    expanderCheckStays("true+\"\"", new toStringExpander());
+  }
+
+  @Test public void test5() {
+    expanderCheckStays("\"\"+\"hi\"", new toStringExpander());
   }
 }
