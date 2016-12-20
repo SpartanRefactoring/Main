df65bf8d6b965c19f22e69178de9edf69832e12b
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-20 22:09:14 +0200

    fixing ternary expander #986

--- src/main/java/il/org/spartan/athenizer/inflate/expanders/TernaryExpander.java
+++ src/main/java/il/org/spartan/athenizer/inflate/expanders/TernaryExpander.java
@@ -6,14 +6,14 @@ import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/* converts (a?b:c;) to (if(a) b; else c;) relevant for now to return <ternary>
+/** converts (a?b:c;) to (if(a) b; else c;) relevant for now to return <ternary>
  * or $ = <ternary> also relevant for return (<ternary>) or $ = (<ternary)
  *
  * @author Raviv Rachmiel
  *
  * @since 03-12-16 */
 public class TernaryExpander extends ReplaceCurrentNode<Statement> {
-  private static ASTNode innerReplacement(final Expression x, final Statement s) {
+  private static ASTNode innerReturnReplacement(final Expression x, final Statement s) {
     ConditionalExpression ¢;
     if (!(x instanceof ParenthesizedExpression))
       ¢ = az.conditionalExpression(x);
@@ -33,18 +33,41 @@ public class TernaryExpander extends ReplaceCurrentNode<Statement> {
     $.setElseStatement(duplicate.of(az.statement(elze)));
     return $;
   }
+  
+  private static ASTNode innerAssignReplacement(final Expression x, final Statement s,final Expression left) {
+    ConditionalExpression ¢;
+    if (!(x instanceof ParenthesizedExpression))
+      ¢ = az.conditionalExpression(x);
+    else {
+      final Expression unpar = az.parenthesizedExpression(x).getExpression();
+      if (!(unpar instanceof ConditionalExpression))
+        return null;
+      ¢ = az.conditionalExpression(unpar);
+    }
+    final IfStatement $ = s.getAST().newIfStatement();
+    $.setExpression(duplicate.of(¢.getExpression()));
+    final Assignment then = ¢.getAST().newAssignment();
+    then.setRightHandSide(duplicate.of(¢.getThenExpression()));
+    then.setLeftHandSide(left);
+    $.setThenStatement(duplicate.of(az.statement(then)));
+    final Assignment elze = ¢.getAST().newAssignment();
+    elze.setRightHandSide(duplicate.of(¢.getElseExpression()));
+    elze.setLeftHandSide(left);
+    $.setElseStatement(duplicate.of(az.statement(elze)));
+    return $;
+  }
 
   private static ASTNode replaceAssignment(final Statement ¢) {
     if (az.expressionStatement(¢) == null)
       return null;
     final Assignment $ = az.assignment(az.expressionStatement(¢).getExpression());
-    return $ == null ? null : innerReplacement($.getRightHandSide(), ¢);
+    return $ == null ? null : innerAssignReplacement($.getRightHandSide(), ¢,$.getLeftHandSide());
   }
 
   private static ASTNode replaceReturn(final Statement ¢) {
     final ReturnStatement $ = az.returnStatement(¢);
     return $ == null || !($.getExpression() instanceof ConditionalExpression) && !($.getExpression() instanceof ParenthesizedExpression) ? null
-        : innerReplacement($.getExpression(), ¢);
+        : innerReturnReplacement($.getExpression(), ¢);
   }
 
   @Override public ASTNode replacement(final Statement ¢) {
