1860f18b187ada54ac0bf772fda2c0059bc2bd82
orimarco <marcovitch.ori@gmail.com>
2016-12-21 15:04:10 +0200

    fluentSetter

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/methods/FluentSetterTest.java
@@ -0,0 +1,31 @@
+package il.org.spartan.spartanizer.research.methods;
+
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+
+import il.org.spartan.spartanizer.research.patterns.characteristics.*;
+
+/** @author Ori Marcovitch
+ * @since 2016 */
+@SuppressWarnings("static-method")
+public class FluentSetterTest extends JavadocerTest {
+  @BeforeClass public static void setUp() {
+    spartanizer.add(MethodDeclaration.class, JAVADOCER = new FluentSetter());
+  }
+
+  @Test public void a() {
+    assert is("boolean foo(Object o){this.c = o; return this;}");
+  }
+
+  @Test public void b() {
+    assert not("boolean foo(Object o){this.c = o;}");
+  }
+
+  @Test public void c() {
+    assert is("@Override public int set(Whatever o) {c = o; return this;}");
+  }
+
+  @Test public void d() {
+    assert is("@Override public int set(final Whatever o) {c = o; return this;}");
+  }
+}
