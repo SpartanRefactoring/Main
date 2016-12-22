670869a73bab2812a96756305f2ab372a524067a
Yossi Gil <yossi.gil@gmail.com>
2016-12-22 23:25:54 +0200

    Work on scope; resolve #995

--- src/main/java/il/org/spartan/spartanizer/dispatch/disabling.java
+++ src/main/java/il/org/spartan/spartanizer/dispatch/disabling.java
@@ -35,10 +35,6 @@ public interface disabling {
         return cautiousGo(¢);
       }
 
-      @Override public boolean visit(final EnumConstantDeclaration ¢) {
-        return cautiousGo(¢);
-      }
-
       @Override protected <N extends ASTNode> boolean go(final N ¢) {
         final BodyDeclaration ¢2 = az.bodyDeclaration(¢);
         if (!disabling.isDisabledByIdentifier(¢2))
@@ -67,9 +63,6 @@ public interface disabling {
         return cautiousGo(¢);
       }
 
-      @Override public boolean visit(final EnumConstantDeclaration ¢) {
-        return cautiousGo(¢);
-      }
 
       @Override protected <N extends ASTNode> boolean go(final N ¢) {
         if (¢ instanceof BodyDeclaration && disabling.isEnabledByIdentifier((BodyDeclaration) ¢)) {
