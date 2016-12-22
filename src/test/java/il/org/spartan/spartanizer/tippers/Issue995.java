510fb824a802e9307543313608ebca54056fa305
Yossi Gil <yossi.gil@gmail.com>
2016-12-22 23:25:54 +0200

    Work on scope; resolve #995

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue995.java
@@ -0,0 +1,18 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+import org.junit.*;
+import org.junit.runners.*;
+
+/** Unit tests for {@link $BodyDeclarationModifiersSort.ofEnumConstant}
+ * @author Yossi Gil // put your name here
+ * @since 2016 // put the year/date here */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SuppressWarnings({ "static-method", "javadoc" })
+public class Issue955 {
+  @Test public void a$01() {
+    trimmingOf("enum A{@c @b a}")//
+        .gives("enum A{@b @c a}")//
+        .stays();
+  }
+}
