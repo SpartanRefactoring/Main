ee17f70f3805ed287e8c124f3e97b28376b61368
Ori Roth <ori.rothh@gmail.com>
2016-12-23 22:04:28 +0200

    TODO for @yossigil

--- src/main/java/il/org/spartan/spartanizer/ast/navigate/wizard.java
+++ src/main/java/il/org/spartan/spartanizer/ast/navigate/wizard.java
@@ -570,8 +570,9 @@ public interface wizard {
     final ASTNode container = hop.containerType(¢);
     if (container == null)
       return $;
-    if (iz.annotationTypeDeclaration(container))
-      $.add(isFinal);
+    // TODO Yossi: compilation error, please fix/remove
+//    if (iz.annotationTypeDeclaration(container))
+//      $.add(isFinal);
     if (iz.abstractTypeDeclaration(container) && iz.final¢(az.abstractTypeDeclaration(container)) && iz.isMethodDeclaration(¢))
       $.add(isFinal);
     if (iz.enumDeclaration(container)) {
