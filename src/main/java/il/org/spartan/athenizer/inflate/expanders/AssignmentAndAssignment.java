4bb79f8fc46d38dbf8b7e806e4a2344c3d43f93a
Doron Meshulam <doronmmm@hotmail.com>
2016-12-24 00:44:36 +0200

    starting #999

---
+++ src/main/java/il/org/spartan/athenizer/inflate/expanders/AssignmentAndAssignment.java
@@ -0,0 +1,20 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.tipping.*;
+
+public class AssignmentAndAssignment extends ReplaceCurrentNode<ExpressionStatement> implements TipperCategory.InVain {
+
+  @Override public ASTNode replacement(ExpressionStatement n) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override public String description(ExpressionStatement n) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
