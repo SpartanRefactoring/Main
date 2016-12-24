037382d612797e551852d938fbccfbb83ea48ace
Ori Roth <ori.rothh@gmail.com>
2016-12-25 00:13:41 +0200

    testing CasesSplit

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/CasesSplitTest.java
@@ -0,0 +1,101 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import org.junit.*;
+
+import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
+
+/** Unit test for {@link CasesSplit}.
+ * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
+ * @since 2016-12-25 */
+@SuppressWarnings("static-method")
+public class CasesSplitTest {
+  @Test public void simpleSequencers() {
+    expandingOf("" //
+        + "switch (x) {\n" //
+        + "case 1:\n" //
+        + "  f(1);\n" //
+        + "case 2:\n" //
+        + "  f(2);\n" //
+        + "  break;\n" //
+        + "case 3:\n" //
+        + "  f(3);\n" //
+        + "case 4:\n" //
+        + "  throw new Exception();\n" //
+        + "default:\n" //
+        + "}")
+            .gives("" //
+                + "switch (x) {\n" //
+                + "case 1:\n" //
+                + "  f(1);\n" //
+                + "  f(2);\n" //
+                + "  break;\n" //
+                + "case 2:\n" //
+                + "  f(2);\n" //
+                + "  break;\n" //
+                + "case 3:\n" //
+                + "  f(3);\n" //
+                + "case 4:\n" //
+                + "  throw new Exception();\n" //
+                + "default:\n" //
+                + "}")
+            .gives("" //
+                + "switch (x) {\n" //
+                + "case 1:\n" //
+                + "  f(1);\n" //
+                + "  f(2);\n" //
+                + "  break;\n" //
+                + "case 2:\n" //
+                + "  f(2);\n" //
+                + "  break;\n" //
+                + "case 3:\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "case 4:\n" //
+                + "  throw new Exception();\n" //
+                + "default:\n" //
+                + "}")
+            .stays();
+  }
+
+  @Test public void complexMerging() {
+    expandingOf("" //
+        + "switch (x) {\n" //
+        + "case 1:\n" //
+        + "  f(1);\n" //
+        + "case 2:\n" //
+        + "  f(2);\n" //
+        + "default:\n" //
+        + "  f(3);\n" //
+        + "  throw new Exception();\n" //
+        + "}")
+            .gives("" //
+                + "switch (x) {\n" //
+                + "case 1:\n" //
+                + "  f(1);\n" //
+                + "  f(2);\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "case 2:\n" //
+                + "  f(2);\n" //
+                + "default:\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "}")
+            .gives("" //
+                + "switch (x) {\n" //
+                + "case 1:\n" //
+                + "  f(1);\n" //
+                + "  f(2);\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "case 2:\n" //
+                + "  f(2);\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "default:\n" //
+                + "  f(3);\n" //
+                + "  throw new Exception();\n" //
+                + "}")
+            .stays();
+  }
+}
