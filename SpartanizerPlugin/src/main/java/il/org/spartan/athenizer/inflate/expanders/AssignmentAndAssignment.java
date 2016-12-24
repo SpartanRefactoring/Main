b773f6e5192a359eb7e03ad9a50ff55a4002818b
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-24 15:27:20 +0200

    Doron, TODO and also spartanized the code

--- SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/AssignmentAndAssignment.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/AssignmentAndAssignment.java
@@ -1,6 +1,5 @@
 package il.org.spartan.athenizer.inflate.expanders;
 
-import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.rewrite.*;
@@ -13,6 +12,7 @@ import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
+//TODO: Doron, spartanize your code, add @author, @since, @description
 public class AssignmentAndAssignment extends CarefulTipper<ExpressionStatement> implements TipperCategory.InVain { 
 
   @Override public String description(@SuppressWarnings("unused") final ExpressionStatement __) {
@@ -20,31 +20,25 @@ public class AssignmentAndAssignment extends CarefulTipper<ExpressionStatement> 
   }
   
   @Override public Tip tip(final ExpressionStatement ¢) {
-//    System.out.println("@@@@@@@@Toolbox worked!");
     final Expression e = expression(¢);
     if (!iz.assignment(e))
       return null;
-    final Assignment a = az.assignment(e);
-    if (!iz.assignment(right(a)) || iz.assignment(right(a)) 
-        && iz.assignment(right(az.assignment(right(a)))))
-      return null;
-//    System.out.println("@@@@@@@@@@Identification worked!");
-    return new Tip(description(¢), ¢, this.getClass()) {
-      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
-        AST create = ¢.getAST();
-        Assignment newA = create.newAssignment();
-        newA.setLeftHandSide(left(a));
-        newA.setRightHandSide(right(az.assignment(right(a))));
-        
-        ExpressionStatement head = create.newExpressionStatement(newA);
-        ExpressionStatement tail = create.newExpressionStatement(right(a));    
-        
-        az.block(¢.getParent());
-        final ListRewrite l = r.getListRewrite(¢.getParent(), Block.STATEMENTS_PROPERTY);        
-        l.insertAfter(head, ¢, g);
-        l.insertAfter(tail, ¢, g);
-        l.remove(¢, g);
-      }
-    };
+    final Assignment $ = az.assignment(e);
+    return !iz.assignment(right($)) || iz.assignment(right($)) && iz.assignment(right(az.assignment(right($)))) ? null
+        : new Tip(description(¢), ¢, this.getClass()) {
+          @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+            AST create = ¢.getAST();
+            Assignment newA = create.newAssignment();
+            newA.setLeftHandSide(left($));
+            newA.setRightHandSide(right(az.assignment(right($))));
+            ExpressionStatement head = create.newExpressionStatement(newA);
+            ExpressionStatement tail = create.newExpressionStatement(right($));
+            az.block(¢.getParent());
+            final ListRewrite l = r.getListRewrite(¢.getParent(), Block.STATEMENTS_PROPERTY);
+            l.insertAfter(head, ¢, g);
+            l.insertAfter(tail, ¢, g);
+            l.remove(¢, g);
+          }
+        };
   }
 }
