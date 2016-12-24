bc69736ce1911cfc6a536efe79fc38a0906c5533
Doron Meshulam <doronmmm@hotmail.com>
2016-12-24 03:41:34 +0200

    finished creating replacement statements for #999. Now how to actually make a change?

--- src/main/java/il/org/spartan/athenizer/inflate/expanders/AssignmentAndAssignment.java
+++ src/main/java/il/org/spartan/athenizer/inflate/expanders/AssignmentAndAssignment.java
@@ -1,20 +1,57 @@
 package il.org.spartan.athenizer.inflate.expanders;
 
-import org.eclipse.jdt.core.dom.*;
+import java.util.*;
 
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-public class AssignmentAndAssignment extends ReplaceCurrentNode<ExpressionStatement> implements TipperCategory.InVain {
+public class AssignmentAndAssignment extends CarefulTipper<ExpressionStatement> implements TipperCategory.InVain { 
 
-  @Override public ASTNode replacement(ExpressionStatement n) {
-    // TODO Auto-generated method stub
-    return null;
+  @Override public String description(@SuppressWarnings("unused") final ExpressionStatement __) {
+    return "Split assignment statement";
+  }
+  
+  @Override public Tip tip(final ExpressionStatement ¢) {
+    @SuppressWarnings("unused") final List<Statement> $ = getAssignments(¢);
+    return new Tip(description(¢), ¢, this.getClass()) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+        r.getAST();
+        g.getClass();
+        // Waiting for help
+      }
+    };
   }
 
-  @Override public String description(ExpressionStatement n) {
-    // TODO Auto-generated method stub
-    return null;
+  private static List<Statement> getAssignments(ExpressionStatement s) {
+    final Expression e = expression(s);
+    if (!iz.assignment(e))
+      return null;
+    final Assignment a = az.assignment(e);
+    if (!iz.assignment(right(a)) || iz.assignment(right(a)) 
+        && iz.assignment(right(az.assignment(right(a)))))
+      return null;
+    AST create = s.getAST();
+    Assignment newA = create.newAssignment();
+    newA.setLeftHandSide(left(a));
+    newA.setRightHandSide(right(az.assignment(right(a))));
+    
+    ExpressionStatement head = create.newExpressionStatement(newA);
+    ExpressionStatement tail = create.newExpressionStatement(right(a));
+    
+    List<Statement> $ = new ArrayList<>();
+    $.add(head);
+    $.add(tail);
+    
+    return $;
   }
+
   
 }
