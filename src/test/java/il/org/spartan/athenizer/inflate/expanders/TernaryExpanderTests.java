a7a71ce3a41f79ffd59d0f06af27e448ed737021
dormaayn <dor.d.ma@gmail.com>
2016-12-24 23:59:09 +0200

    Rewrite Some tests + minor corrections #961

--- src/test/java/il/org/spartan/athenizer/inflate/expanders/TernaryExpanderTests.java
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/TernaryExpanderTests.java
@@ -1,89 +1,37 @@
 package il.org.spartan.athenizer.inflate.expanders;
 
-import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-import il.org.spartan.spartanizer.ast.navigate.*;
-import il.org.spartan.spartanizer.ast.safety.*;
+import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
 
-//TODO Raviv: add @link to tested expander class (also in the opposite direction if not exists) and change test class name to Issue#
+// TODO Raviv: add @link to tested expander class (also in the opposite
+// direction if not exists) and change test class name to Issue#
 /** @author Raviv Rachmiel
+ * @author Dor Ma'ayan
  * @since 8-12-2016 */
 @SuppressWarnings("static-method")
 public class TernaryExpanderTests {
-  @Test public void basicCanTip() {
-    wizard.ast("return a==0? 2:3;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ReturnStatement node) {
-        assert new ReturnTernaryExpander().canTip(node);
-        return true;
-      }
-    });
+  @Test public void test0() {
+    expandingOf("return a==0? 2:3;").gives("if(a==0)return 2;else return 3;").stays();
   }
 
-  @Test public void basicCanTip2() {
-    wizard.ast("a = a==0? 2:3;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ExpressionStatement node) {
-        assert new AssignmentTernaryExpander().canTip(node);
-        return true;
-      }
-    });
+  @Test public void test1() {
+    expandingOf("a = a==0? 2:3;").gives("if(a==0)a=2;else a=3;").stays();
   }
 
-
-  @Test public void nestedCanTip() {
-    wizard.ast("a = a==0? (b==2? 4: 5 ):3;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ExpressionStatement node) {
-        assert new AssignmentTernaryExpander().canTip(node);
-        return true;
-      }
-    });
+  @Test public void test2() {
+    expandingOf("a = a==0? (b==2? 4: 5 ):3;").gives("if(a==0)a=(b==2?4:5);else a=3;").gives("if(a==0)if(b==2)a=4;else a=5;else a=3;").stays();
   }
 
-  @Test public void appCanTip() {
-    wizard.ast("a = (a==0? (b==2? 4: 5 ):3);").accept(new ASTVisitor() {
-      @Override public boolean visit(final ExpressionStatement node) {
-        assert new AssignmentTernaryExpander().canTip(node);
-        return true;
-      }
-    });
+  @Test public void test3() {
+    expandingOf("a = (a==0? (b==2? 4: 5 ):3);").gives("if(a==0)a=(b==2?4:5);else a=3;").gives("if(a==0)if(b==2)a=4;else a=5;else a=3;").stays();
   }
 
-  @Test public void basicTest() {
-    wizard.ast("return a==0? 1:2;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ReturnStatement node) {
-        assert new ReturnTernaryExpander().replacement(node) instanceof IfStatement;
-        return true;
-      }
-    });
+  @Test public void test4() {
+    expandingOf("a = a==0? 1:2;").gives("if(a==0)a=1;else a=2;").stays();
   }
 
-  @Test public void basicTest2() {
-    wizard.ast("a = a==0? 1:2;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ExpressionStatement node) {
-        assert new AssignmentTernaryExpander().replacement(node) instanceof IfStatement;
-        return true;
-      }
-    });
-  }
-
-
-
-  @Test public void nestedTest() {
-    wizard.ast("a = b==0? (a==0? 1:2) : 4;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ExpressionStatement node) {
-        assert new AssignmentTernaryExpander().replacement(node) instanceof IfStatement;
-        return true;
-      }
-    });
-  }
-
-  @Test public void returnTest() {
-    wizard.ast("return a==0? 1:2;").accept(new ASTVisitor() {
-      @Override public boolean visit(final ReturnStatement node) {
-        assert new ReturnTernaryExpander().replacement(node) instanceof IfStatement;
-        assert "a == 0".equals(az.ifStatement(new ReturnTernaryExpander().replacement(node)).getExpression() + "");
-        return true;
-      }
-    });
+  @Test public void test5() {
+    expandingOf("a = b==0? (a==0? 1:2) : 4;").gives("if(b==0)a=(a==0?1:2);else a=4;");
   }
 }
