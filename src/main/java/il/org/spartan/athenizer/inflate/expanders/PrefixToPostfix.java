0b68877b0332be55f55adf5264d0329827fdf152
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-25 20:41:51 +0200

    #1005 done

--- src/main/java/il/org/spartan/athenizer/inflate/expanders/PrefixToPostfix.java
+++ src/main/java/il/org/spartan/athenizer/inflate/expanders/PrefixToPostfix.java
@@ -2,30 +2,39 @@ package il.org.spartan.athenizer.inflate.expanders;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
+import static il.org.spartan.spartanizer.ast.safety.iz.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** convert
  *
  * <pre>
- * ++i
- *
+ * ++i;
+ * --i;
  * <pre>
  * to
  *
  * <pre>
- * i++
- *
+ * i++;
+ * i--;
  * <pre>
+ * 
  * Test case is {@link Issue1005}
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
  * @since 2016-12-24 */
-public class PrefixToPostfix extends ReplaceCurrentNode<ExpressionStatement> implements TipperCategory.Idiomatic {
-  @Override @SuppressWarnings("unused") public ASTNode replacement(final ExpressionStatement __) {
-    return null;
+public class PrefixToPostfix extends ReplaceCurrentNode<PrefixExpression> implements TipperCategory.Idiomatic {
+  @Override public ASTNode replacement(final PrefixExpression ¢) {
+    return subject.operand(step.operand(¢)).to(prefixMinus(¢) ? PostfixExpression.Operator.DECREMENT : PostfixExpression.Operator.INCREMENT);
+  }
+  
+  @Override protected boolean prerequisite(PrefixExpression e) {
+    ASTNode parent = step.parent(e);
+    return expressionStatement(parent) || forStatement(parent);
   }
 
-  @Override @SuppressWarnings("unused") public String description(final ExpressionStatement __) {
+  @Override @SuppressWarnings("unused") public String description(final PrefixExpression __) {
     return null;
   }
 }
