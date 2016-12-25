92f4633f2aee4033ca232de073ef1508728241d7
dormaayn <dor.d.ma@gmail.com>
2016-12-25 07:54:14 +0200

    Test Cases (Issue #970)

---
+++ src/main/java/il/org/spartan/athenizer/inflate/expanders/MultiTypeCatchClause.java
@@ -0,0 +1,16 @@
+//package il.org.spartan.athenizer.inflate.expanders;
+//
+//import org.eclipse.jdt.core.dom.*;
+//
+//import il.org.spartan.spartanizer.dispatch.*;
+//import il.org.spartan.spartanizer.tipping.*;
+//
+///**
+// * 
+// * 
+// * @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
+// * @since 2016-12-25
+// */
+//public class MultiTypeCatchClause ReplaceCurrentNode<TryStatement> implements TipperCategory.InVain {
+//  
+//}
