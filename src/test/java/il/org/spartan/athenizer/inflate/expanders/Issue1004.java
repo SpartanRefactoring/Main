4072f4c1ec9a3b89cca1649e754defff7151ab8f
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-25 22:44:51 +0200

    #1004 created expander class and test case

---
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue1004.java
@@ -0,0 +1,30 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import org.junit.*;
+import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
+
+/**
+ * Test case for {@link OutlineArrayAccess}
+ * 
+ * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
+ * @since 2016-12-25
+ */
+@Ignore
+@SuppressWarnings("static-method")
+public class Issue1004 {
+  @Test public void t1() {
+    expandingOf("arr[i++] = 5;").gives("arr[i] = 5; ++i;");
+  }
+  @Test public void t2() {
+    expandingOf("arr[i++] = i;").stays();
+  }
+  @Test public void t3() {
+    expandingOf("arr[i++] = arr[i++] + i;").stays();
+  }
+  @Test public void t4() {
+    expandingOf("arr[++i] = i;").stays();
+  }
+  @Test public void t5() {
+    expandingOf("arr[++i] = x;").gives("++i; arr[i] = x;");
+  }
+}
