053e55cb5cd8d30133e2a8a004903a9c4fe17400
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-26 22:36:36 +0200

    closes #1011

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue107.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue107.java
@@ -11,35 +11,21 @@ import org.junit.*;
 @SuppressWarnings("static-method")
 public final class Issue107 {
   @Test public void b() {
-    trimmingOf("for(int c = 0; c <5; c-=1)\n" + "c*=2;").gives("for(int c = 0; c <5; c--)\n" + "c*=2;").gives("for(int c = 0; c <5; --c)\n" + "c*=2;")
-        .stays();
-  }
-
-  @Test public void c() {
-    trimmingOf("java_is_even_nice+=1+=1;").gives("java_is_even_nice+=1++;");
-  }
-
-  @Test public void d() {
-    trimmingOf("for(int a ; a<10 ; (--a)+=1){}").gives("for(int a ; a<10 ; (--a)++){}");
+    trimmingOf("for(int c = 0; c <5; c-=1)\n" + "c*=2;").gives("for(int c = 0; c <5; --c)\n" + "c*=2;").stays();
   }
 
   @Test public void i() {
-    trimmingOf("a-=1;").gives("a--;").gives("--a;").stays();
+    trimmingOf("a-=1;").gives("--a;").stays();
   }
 
   @Test public void j() {
-    trimmingOf("for(int a ; a<10 ; a-=1){}").gives("for(int a ; a<10 ; a--){}");
+    trimmingOf("for(int a ; a<10 ; a-=1){}").gives("for(int a ; a<10 ; --a){}");
   }
 
   @Test public void l() {
-    trimmingOf("while(x-=1){}").gives("while(x--){}");
+    trimmingOf("while((x-=1) > 0){}").gives("while((--x) > 0){}");
   }
 
-  @Test public void n() {
-    trimmingOf("for(;; (a = 3)+=1){}").gives("for(;; (a = 3)++){}");
-  }
-
-  // Not provably-not-string.
   @Test public void a() {
     trimmingOf("a+=1;").stays();
   }
@@ -66,5 +52,21 @@ public final class Issue107 {
 
   @Test public void o() {
     trimmingOf("for(int a ; a<3 ; a+=1){}").stays();
+  }
+  
+  @Test public void t1() {
+    trimmingOf("while((x-=1) >= 1) System.out.println(x);").gives("while((--x) >= 1) System.out.println(x);");
+  }
+  
+  @Test public void t2() {
+    trimmingOf("i = a += 1;").stays();
+  }
+  
+  @Test public void t3() {
+    trimmingOf("i += i + 1;").stays();
+  }
+  
+  @Test public void t4() {
+    trimmingOf("i -= i - 1").stays();
   }
 }
