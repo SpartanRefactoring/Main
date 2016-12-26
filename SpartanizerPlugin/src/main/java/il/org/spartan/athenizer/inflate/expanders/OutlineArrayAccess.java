8323084b6362f0992794ad440ae2c333dfe3d46d
YuvalSimon <yuvaltechnion@gmail.com>
2016-12-26 03:03:00 +0200

    #1004 spartanized

--- SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/OutlineArrayAccess.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/OutlineArrayAccess.java
@@ -31,26 +31,24 @@ import il.org.spartan.spartanizer.tipping.*;
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
  * @since 2016-12-25 */
 public class OutlineArrayAccess extends CarefulTipper<ArrayAccess> implements TipperCategory.InVain {
-  @SuppressWarnings("unused") @Override public String description(final ArrayAccess n) {
+  @Override @SuppressWarnings("unused") public String description(final ArrayAccess n) {
     return null;
   }
 
   @Override public Tip tip(final ArrayAccess a) {
-    final Expression e = duplicate.of(a.getIndex());
+    final Expression $ = duplicate.of(a.getIndex());
     final ASTNode b = extract.containingStatement(a);
     final AST t = b.getParent().getAST();
     return new Tip(description(a), a, this.getClass()) {
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final ListRewrite l = r.getListRewrite(b.getParent(), Block.STATEMENTS_PROPERTY);
         final ArrayAccess newa = duplicate.of(a);
-        if (iz.postfixExpression(e)) {
-          final SimpleName ee = a.getAST().newSimpleName(identifier(az.simpleName(operand(az.postfixExpression(e)))));
-          newa.setIndex(ee);
-          l.insertAfter(t.newExpressionStatement(e), b, g);
+        if (iz.postfixExpression($)) {
+          newa.setIndex(a.getAST().newSimpleName(identifier(az.simpleName(operand(az.postfixExpression($))))));
+          l.insertAfter(t.newExpressionStatement($), b, g);
         } else {
-          final SimpleName ee = a.getAST().newSimpleName(identifier(az.simpleName(operand(az.prefixExpression(e)))));
-          newa.setIndex(ee);
-          l.insertBefore(t.newExpressionStatement(e), b, g);
+          newa.setIndex(a.getAST().newSimpleName(identifier(az.simpleName(operand(az.prefixExpression($))))));
+          l.insertBefore(t.newExpressionStatement($), b, g);
         }
         r.replace(a, newa, g);
       }
@@ -59,30 +57,18 @@ public class OutlineArrayAccess extends CarefulTipper<ArrayAccess> implements Ti
 
   @Override protected boolean prerequisite(final ArrayAccess a) {
     final Expression e = a.getIndex();
-    SimpleName name;
+    SimpleName $;
     final ASTNode b = extract.containingStatement(a);
-    if (!iz.block(b.getParent()))
+    if (!iz.block(b.getParent()) || !iz.expressionStatement(b) || !iz.incrementOrDecrement(e) || iz.assignment(e))
       return false;
-    if (!iz.expressionStatement(b))
+    $ = iz.prefixExpression(e) ? az.simpleName(az.prefixExpression(e)) : az.simpleName(az.postfixExpression(e));
+    if ($ == null)
       return false;
-    if (!iz.incrementOrDecrement(e) || iz.assignment(e))
-      return false;
-    if (iz.prefixExpression(e))
-      name = az.simpleName(az.prefixExpression(e));
-    else
-      name = az.simpleName(az.postfixExpression(e));
-    if (name == null)
-      return false;
-    final ExpressionStatement ss = az.expressionStatement(b);
-    final Expression s = expression(ss);
-    if (!iz.assignment(s) || !left(az.assignment(s)).equals(a))
-      return false;
-    final Expression n = right(az.assignment(s));
-    return iz.plainAssignment(az.assignment(s)) && !containsName(name, n);
+    final Expression s = expression(az.expressionStatement(b));
+    return iz.assignment(s) && left(az.assignment(s)).equals(a) && iz.plainAssignment(az.assignment(s)) && !containsName($, right(az.assignment(s)));
   }
 
-  private static boolean containsName(final SimpleName n, final Expression e) {
-    final searchDescendants<SimpleName> f = searchDescendants.forClass(SimpleName.class);
-    return !f.suchThat(t -> identifier(t).equals(identifier(n))).inclusiveFrom(e).isEmpty();
+  private static boolean containsName(final SimpleName n, final Expression x) {
+    return !searchDescendants.forClass(SimpleName.class).suchThat(t -> identifier(t).equals(identifier(n))).inclusiveFrom(x).isEmpty();
   }
 }
