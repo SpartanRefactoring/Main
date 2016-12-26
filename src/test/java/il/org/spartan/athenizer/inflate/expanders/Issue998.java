c0114194c8268e6ca2dfca293687940b79649c19
Doron Meshulam <doronmmm@hotmail.com>
2016-12-26 18:16:26 +0200

    Tests for #998

---
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue998.java
@@ -0,0 +1,23 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/** @author Doron Meshulam <tt>doronmmm@hotmail.com</tt>
+ * @since 2016-12-26 */
+@SuppressWarnings("static-method")
+public class Issue998 {
+  @Test public void test0() {
+    expandingOf("throw a==0? 2:3;").gives("if(a==0)throw 2; else throw 3;").stays();
+  }
+
+  @Test public void test1() {
+    expandingOf("throw ((a==0)==42)? f(x):g(x);").gives("if((a==0)==42)throw f(x); else throw g(x);").stays();
+  }
+
+  @Test public void test2() {
+    expandingOf("throw a ? new A():new B();").gives("if(a)throw new A(); else throw new B();").stays();
+  }
+}
