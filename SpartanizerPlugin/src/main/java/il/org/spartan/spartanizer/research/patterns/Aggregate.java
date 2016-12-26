02d73e82e6df34b228e869beb2ec4a765f5b1754
orimarco <marcovitch.ori@gmail.com>
2016-12-26 17:03:59 +0200

    fixed Aggregate

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/Aggregate.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/Aggregate.java
@@ -13,13 +13,17 @@ import static il.org.spartan.spartanizer.research.TipperFactory.*;
 /** @author Ori Marcovitch
  * @year 2016 */
 public final class Aggregate extends NanoPatternTipper<EnhancedForStatement> {
-  private static Set<UserDefinedTipper<EnhancedForStatement>> tippers = new HashSet<UserDefinedTipper<EnhancedForStatement>>() {
+  private static List<UserDefinedTipper<EnhancedForStatement>> tippers = new ArrayList<UserDefinedTipper<EnhancedForStatement>>() {
     static final long serialVersionUID = 1L;
     {
-      add(patternTipper("for($T $N1 : $X1) $N2 = $X2;", " $N2 = $X1.stream().reduce(($N1,$N2) -> $X2).get();", "Go Fluent. Aggregate"));
-      add(patternTipper("for($T $N1 : $X1) if($X2) $N2 = $N1;", " $N2 = $X1.stream().reduce(($N1,$N2) -> $X2 ? $N1 : $N2).get();",
+      add(patternTipper("for($T $N1 : $N2) $N3 = $X;", " $N3 = $N2.stream().reduce(($N1,$N3) -> $X).get();", "Go Fluent. Aggregate"));
+      add(patternTipper("for($T $N1 : $N2) if($X) $N3 = $N1;", " $N3 = $N2.stream().reduce(($N1,$N3) -> $X ? $N1 : $N3).get();",
           "Go Fluent. Reduce"));
-      add(patternTipper("for($T $N1 : $X1) $N2 += $X2;", " $N2 = $X1.stream().map($N1 -> $X2).reduce((x,y) -> x + y).get();", "Go Fluent. Sum"));
+      add(patternTipper("for($T $N1 : $N2) $N3 += $X;", " $N3 = $N2.stream().map($N1 -> $X).reduce((x,y) -> x + y).get();", "Go Fluent. Sum"));
+      add(patternTipper("for($T $N1 : $X1) $N2 = $X2;", " $N2 = ($X1).stream().reduce(($N1,$N2) -> $X2).get();", "Go Fluent. Aggregate"));
+      add(patternTipper("for($T $N1 : $X1) if($X2) $N2 = $N1;", " $N2 = ($X1).stream().reduce(($N1,$N2) -> $X2 ? $N1 : $N2).get();",
+          "Go Fluent. Reduce"));
+      add(patternTipper("for($T $N1 : $X1) $N2 += $X2;", " $N2 = ($X1).stream().map($N1 -> $X2).reduce((x,y) -> x + y).get();", "Go Fluent. Sum"));
     }
   };
 
@@ -31,7 +35,7 @@ public final class Aggregate extends NanoPatternTipper<EnhancedForStatement> {
     return firstTip(tippers, x);
   }
 
-  @Override public String description(final EnhancedForStatement x) {
-    return firstTipper(tippers, x).description(x);
+  @Override public String description(@SuppressWarnings("unused") final EnhancedForStatement __) {
+    return "";
   }
 }
