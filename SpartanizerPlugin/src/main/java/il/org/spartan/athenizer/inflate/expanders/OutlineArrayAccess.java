227d8f5a2ebd712cd6c354039fc22497bc17e87e
Yossi Gil <yossi.gil@gmail.com>
2016-12-26 08:25:55 +0200

    Prepare for testing above definitions

--- SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/OutlineArrayAccess.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/inflate/expanders/OutlineArrayAccess.java
@@ -1,38 +1,74 @@
 package il.org.spartan.athenizer.inflate.expanders;
 
-import org.eclipse.jdt.core.dom.*;
+import java.util.function.*;
 
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/**
- * converts
- * 
+/** converts
+ *
  * <pre>
- * arr[i++] = y;
- * 
- * arr[++i] = z;
+ * arr[i++] = y; arr[++i] = z;
+ *
  * <pre>
- * 
  * to
- * 
+ *
  * <pre>
- * arr[i] = y;
- * ++i;
- * 
- * ++i;
- * arr[i] = z;
+ * arr[i] = y; ++i; ++i; arr[i] = z;
+ *
  * <pre>
- * 
- * does not expand if right hand side includes access index operand, such as in arr[i]=i
- * 
+ * does not expand if right hand side includes access index operand, such as in
+ * arr[i]=i
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
- * @since 2016-12-25
- */
-//TODO Yuval: expand also arr[i+=4], arr[(i=i+4)]
+ * @since 2016-12-25 */
 public class OutlineArrayAccess extends CarefulTipper<ArrayAccess> implements TipperCategory.InVain {
-  @Override
-  @SuppressWarnings("unused") public String description(ArrayAccess n) {
+  @Override @SuppressWarnings("unused") public String description(final ArrayAccess n) {
     return null;
-  } 
+  }
+
+  @Override public Tip tip(final ArrayAccess a) {
+    final Expression $ = duplicate.of(a.getIndex());
+    final ASTNode b = extract.containingStatement(a);
+    final AST t = b.getParent().getAST();
+    return new Tip(description(a), a, this.getClass()) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+        final ListRewrite l = r.getListRewrite(b.getParent(), Block.STATEMENTS_PROPERTY);
+        final ArrayAccess newa = duplicate.of(a);
+        if (iz.postfixExpression($)) {
+          newa.setIndex(a.getAST().newSimpleName(identifier(az.simpleName(operand(az.postfixExpression($))))));
+          l.insertAfter(t.newExpressionStatement($), b, g);
+        } else {
+          newa.setIndex(a.getAST().newSimpleName(identifier(az.simpleName(operand(az.prefixExpression($))))));
+          l.insertBefore(t.newExpressionStatement($), b, g);
+        }
+        r.replace(a, newa, g);
+      }
+    };
+  }
+
+  @Override protected boolean prerequisite(final ArrayAccess a) {
+    final Expression e = a.getIndex();
+    SimpleName $;
+    final ASTNode b = extract.containingStatement(a);
+    if (!iz.block(b.getParent()) || !iz.expressionStatement(b) || !iz.incrementOrDecrement(e) || iz.assignment(e))
+      return false;
+    $ = iz.prefixExpression(e) ? az.simpleName(az.prefixExpression(e)) : az.simpleName(az.postfixExpression(e));
+    if ($ == null)
+      return false;
+    final Expression s = expression(az.expressionStatement(b));
+    return iz.assignment(s) && left(az.assignment(s)).equals(a) && iz.plainAssignment(az.assignment(s)) && !containsName($, right(az.assignment(s)));
+  }
+
+  private static boolean containsName(final SimpleName n, final Expression x) {
+    return !searchDescendants.forClass(SimpleName.class).suchThat(t -> identifier(t).equals(identifier(n))).inclusiveFrom(x).isEmpty();
+  }
 }
--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue1005.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue1005.java
@@ -6,48 +6,60 @@ import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
 /** Test case for @link PrefixToPostfix}
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
  * @since 2016-12-24 */
-
 @SuppressWarnings("static-method")
 public class Issue1005 {
   @Test public void t1() {
     expandingOf("++i;").gives("i++;");
   }
+
   @Test public void t2() {
     expandingOf("while(++i < 0) { x=f(i); }").stays();
   }
+
   @Test public void t3() {
     expandingOf("if(++i + i++ > ++i + y) { x = f(); }").stays();
   }
+
   @Test public void t4() {
     expandingOf("f(i++,--j,++x)").stays();
   }
+
   @Test public void t5() {
     expandingOf("if(b) { ++i; }").gives("if(b) { i++; }");
   }
+
   @Test public void t6() {
     expandingOf("for(;x<5;++x);").gives("for(;x<5;x++);");
   }
+
   @Test public void t7() {
     expandingOf("for(++x;x<5;);").gives("for(x++;x<5;);");
   }
+
   @Test public void t8() {
     expandingOf("switch(++x){}").stays();
   }
+
   @Test public void t9() {
     expandingOf("for(;;) ++x;").gives("for(;;) x++;");
   }
+
   @Test public void t10() {
     expandingOf("x = ++y;").stays();
   }
+
   @Test public void t11() {
     expandingOf("int x = ++y;").gives("int x; x=++y;").stays();
   }
+
   @Test public void t12() {
     expandingOf("int x = ++y + 1;").gives("int x; x=++y + 1;").stays();
   }
+
   @Test public void t13() {
     expandingOf("for(;;) --x;").gives("for(;;) x--;");
   }
+
   @Test public void t14() {
     expandingOf("--x;").gives("x--;");
   }
