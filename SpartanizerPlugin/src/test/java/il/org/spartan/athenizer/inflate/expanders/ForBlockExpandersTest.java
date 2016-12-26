cbe1073cde644ad2a0003cf490d218b5e7c19124
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-12-26 22:25:07 +0200

    finished for tests #975

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/ForBlockExpandersTest.java
@@ -0,0 +1,27 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
+
+import org.junit.*;
+
+/** Test for the ForBlock expander, issue #975
+ * @author Raviv Rachmiel
+ * @since 26-12-16 */
+@SuppressWarnings("static-method")
+public class ForBlockExpandersTest {
+  @Test public void simpleBlockTest() {
+    expansionOf("for(int i=0;i<5;i++) a=5;").gives("for(int i=0;i<5;i++){a=5;}").stays();
+  }
+
+  @Test public void simpleShouldntAddTest() {
+    expansionOf("for(int i=0;i<5;i++){ a=5;}").stays();
+  }
+  
+  @Test public void notSimpleShouldntAddTest() {
+    expansionOf("for(int i=0;i<5;i++){ a=5;b=3;}").stays();
+  }
+
+  @Test public void notSimpleShouldAddTest() {
+    expansionOf("for(int i=0;i<5;i++) a=5; b=7;").gives("for(int i=0;i<5;i++){ a=5;}b=7;").stays();
+  }
+}
