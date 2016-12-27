7338bd093bdc13793b8814580824e840e8b80543
orimarco <marcovitch.ori@gmail.com>
2016-12-27 10:24:39 +0200

    Default methods close #1020

--- src/main/java/il/org/spartan/spartanizer/research/patterns/methods/Default.java
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/methods/Default.java
@@ -10,17 +10,20 @@ import static il.org.spartan.spartanizer.research.TipperFactory.patternTipper;
 
 /** @author Ori Marcovitch
  * @since 2016 */
-public class ConstantReturner extends JavadocMarkerNanoPattern {
-  private static final JavadocMarkerNanoPattern rival = new Default();
+public class Default extends JavadocMarkerNanoPattern {
   private static Set<UserDefinedTipper<Statement>> tippers = new HashSet<UserDefinedTipper<Statement>>() {
     static final long serialVersionUID = 1L;
     {
-      add(patternTipper("return $L;", "", ""));
-      add(patternTipper("return -$L;", "", ""));
+      add(patternTipper("return;", "", ""));
+      add(patternTipper("return 0;", "", ""));
+      add(patternTipper("return false;", "", ""));
+      add(patternTipper("return 0L;", "", ""));
+      add(patternTipper("return null;", "", ""));
+      add(patternTipper("return 0.;", "", ""));
     }
   };
 
   @Override protected boolean prerequisites(final MethodDeclaration ¢) {
-    return anyTips(tippers, onlyStatement(¢)) && !rival.canTip(¢);
+    return anyTips(tippers, onlyStatement(¢));
   }
 }
