6d594f8d874c7a72c2a2e2a7ece6c08f7eb456ce
orimarco <marcovitch.ori@gmail.com>
2016-12-27 17:55:19 +0200

    comment in

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/BlockInlineStatementIntoNext.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/BlockInlineStatementIntoNext.java
@@ -2,13 +2,14 @@ package il.org.spartan.spartanizer.tippers;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.research.*;
-import il.org.spartan.spartanizer.research.patterns.common.*;
+import il.org.spartan.spartanizer.tipping.*;
 
 /** @author Ori Marcovitch
  * @year 2016 */
-public final class BlockInlineStatementIntoNext extends NanoPatternTipper<Block> {
+public final class BlockInlineStatementIntoNext extends CarefulTipper<Block> implements TipperCategory.Inlining {
   private static final UserDefinedTipper<Block> tipper = TipperFactory.patternTipper("$X = $X.$N1($A1); $X = $X.$N2($A2);",
       "$X = $X.$N1($A1).$N2($A2);", "inline statement into next one");
 
@@ -16,11 +17,11 @@ public final class BlockInlineStatementIntoNext extends NanoPatternTipper<Block>
     return tipper.description();
   }
 
-  @Override public boolean canTip(final Block x) {
+  @Override public boolean prerequisite(final Block x) {
     return tipper.canTip(x);
   }
 
-  @Override public Tip pattern(final Block x) {
+  @Override public Tip tip(final Block x) {
     return tipper.tip(x);
   }
 }
