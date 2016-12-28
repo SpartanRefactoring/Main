d97152ba00c854059cb564425f3673a0e94177f6
dormaayn <dor.d.ma@gmail.com>
2016-12-28 21:07:25 +0200

    Fixed #970

--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue970.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue970.java
@@ -7,7 +7,6 @@ import org.junit.*;
 /** Tets class for issue #970
  * @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
  * @since 2016-12-25 */
-@Ignore
 @SuppressWarnings("static-method")
 public class Issue970 {
   @Test public void test0() {
@@ -43,14 +42,14 @@ public class Issue970 {
   @Test public void test4() {
     expansionOf("try" + "{" + "int a;" + " a=a+1;" + "}" + "catch(Type1 | Type2 e){" + "int z;" + "z=2;" + "return z;" + "}"
         + "catch (Exception e){f();g();}" + "finally {return t;}")
-            .gives("try" + "{" + "int a;" + " a=a+1;" + "}" + "catch (Exception e){f();g();}" + "catch(Type1 e){" + "int z;" + "z=2;" + "return z;"
-                + "}" + "catch(Type2 e){" + "int z;" + "z=2;" + "return z;" + "}" + "finally {return t;}")
+            .gives("try{int a;a=a+1;}catch(Type1 e){int z;z=2;return z;}catch(Type2 e)"
+                + "{int z;z=2;return z;}catch(Exception e){f();g();}finally{return t;}")
             .stays();
   }
 
   @Test public void test5() {
     expansionOf("try" + "{" + "int a;" + " a=a+1;" + "}" + "catch(Type1 | Type2 e){int z;}" + "catch(Type3 | Type4 e){int z;}")
-        .gives("try" + "{" + "int a;" + " a=a+1;" + "}" + "catch(Type3 | Type4 e){int z;}" + "catch(Type1 e){int z;}" + "catch(Type2 e){" + "int z;}")
+        .gives("try{int a;a=a+1;}catch(Type1 e){int z;}catch(Type2 e){int z;}catch(Type3|Type4 e){int z;}")
         .gives("try{int a;a=a+1;}catch(Type1 e){int z;}catch(Type2 e){int z;}catch(Type3 e){int z;}catch(Type4 e){int z;}").stays();
   }
 }
