8c932a96ea56a5d112fc8120e91be6788566cc97
Ori Roth <ori.rothh@gmail.com>
2016-12-28 11:42:21 +0200

    multiple selection inflation many passes see #1009

--- src/test/java/il/org/spartan/athenizer/inflate/expanders/InflateWholeProject.java
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/InflateWholeProject.java
@@ -7,11 +7,19 @@ import il.org.spartan.athenizer.inflate.*;
 import il.org.spartan.plugin.*;
 
 public class InflateWholeProject extends AbstractHandler {
+  private static final int MAX_PASSES = 20;
+
   @Override public Object execute(@SuppressWarnings("unused") ExecutionEvent __) {
     final Selection s = Selection.Util.current();
-    for (WrappedCompilationUnit ¢ : s.inner)
-      SingleFlater.commitChanges(SingleFlater.in(¢.build().compilationUnit).from(new InflaterProvider()),
-          ASTRewrite.create(¢.compilationUnit.getAST()), ¢, null);
+    for (WrappedCompilationUnit ¢ : s.inner) {
+      for (int i = 0; i < MAX_PASSES; ++i) {
+        if (!SingleFlater.commitChanges(SingleFlater.in(¢.build().compilationUnit).from(new InflaterProvider()),
+            ASTRewrite.create(¢.compilationUnit.getAST()), ¢, null))
+          break;
+        ¢.dispose();
+      }
+      ¢.dispose();
+    }
     return null;
   }
 }
