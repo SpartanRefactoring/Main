1137602226513b9ef79f4d59aad15de279daf28c
dormaayn <dor.d.ma@gmail.com>
2016-12-30 15:58:07 +0200

    First Attempt (Issue #1030)

--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue965.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue965.java
@@ -2,19 +2,27 @@ package il.org.spartan.athenizer.inflate.expanders;
 
 import org.junit.*;
 
+
 import static il.org.spartan.athenizer.inflate.expanders.ExpanderTestUtils.*;
+
 
 /** Test class for issue #965
  * @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
  * @since 2016-12-20 */
 @SuppressWarnings("static-method")
 public class Issue965 {
+ 
+
+  @Test public void testBinding0() {
+    //expansionOf((new Issue965Aux()).getCU()).givesWithBinding("");
+  }
+
   @Test public void test0() {
-    expansionOf("a+\"\"").stays(); //no binding for now...
+    expansionOf("a+\"\"").stays(); // no binding for now...
   }
 
   @Test public void test1() {
-    expansionOf("\"\"+t").stays(); //no binding for now...
+    expansionOf("\"\"+t").stays(); // no binding for now...
   }
 
   @Test public void test2() {
