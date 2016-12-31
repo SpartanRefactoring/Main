83ccf39db1bdde64aa2ca996ae46375cbc015cc2
dormaayn <dor.d.ma@gmail.com>
2016-12-31 14:13:14 +0200

    Binding Tests - Done closses #1035 #1033 #1030

--- src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue965Aux2.java
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue965Aux2.java
@@ -1,13 +1,10 @@
 package il.org.spartan.athenizer.inflate.expanders;
 
-import java.util.*;
-
 import il.org.spartan.spartanizer.ast.navigate.*;
 
 /** [[SuppressWarningsSpartan]] */
-public class Issue965Aux extends ReflectiveTester {
+public class Issue965Aux2 extends ReflectiveTester {
   @SuppressWarnings({ "static-method", "unused" }) public void check1() {
-    List<Integer> lst = new ArrayList<>();
-    String s = lst + "";
+    String s = 5 + "";
   }
 }
