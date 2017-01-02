ac03e56d894c07b6723e822080444015c3587ff4
orimarco <marcovitch.ori@gmail.com>
2017-01-02 23:21:30 +0200

    groups- almomst finished

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/nanos/analyses/Table_NanosByCategories.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/nanos/analyses/Table_NanosByCategories.java
@@ -1,21 +1,18 @@
 package il.org.spartan.spartanizer.cmdline.nanos.analyses;
 
-import static il.org.spartan.spartanizer.cmdline.Table_Tippers.*;
-
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.*;
 import il.org.spartan.spartanizer.cmdline.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.research.analyses.*;
 import il.org.spartan.spartanizer.research.patterns.common.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/** Generate a CSV file including all preliminary information we have on
- * patterns, i.e., without applying these.
- * @author Ori Marcovitch
- * @since Dec 4, 2016 */
+/** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-01-02 */
 public class Table_NanosByCategories {
   public static void main(final String[] args) {
     new Table_NanosByCategories().go();
@@ -23,21 +20,27 @@ public class Table_NanosByCategories {
 
   public void go() {
     final List<Tipper<? extends ASTNode>>[] implementation = Analyze.toolboxWithNanoPatterns().implementation;
+    final Map<String, List<String>> categories = new HashMap<>();
+    for (int i = 0; i < implementation.length; ++i)
+      if (implementation[i] != null)
+        for (final Tipper<?> ¢ : implementation[i])
+          if (¢ != null && ¢ instanceof NanoPatternTipper) {
+            final NanoPatternTipper<? extends ASTNode> np = ((NanoPatternTipper<? extends ASTNode>) ¢);
+            final String category = np.category() != null ? np.category() : Toolbox.intToClassName(i);
+            categories.putIfAbsent(category, new ArrayList<>());
+            categories.get(category).add(np.getClass().getSimpleName());
+          }
     try (Relation r = new Relation(this)) {
-      for (int i = 0; i < implementation.length; ++i)
-        if (implementation[i] != null)
-          for (final Tipper<?> ¢ : implementation[i])
-            if (¢ != null && ¢ instanceof NanoPatternTipper) 
-              r//
-                  .put("Tipper", name(¢.getClass()))//
-                  .put("Node Type Number", i) //
-                  .put("Node Class", Toolbox.intToClassName(i))//
-                  .put("Actual class", name(¢.myActualOperandsClass()))//
-                  .put("Abstract class", name(¢.myAbstractOperandsClass())) //
-                  .put("Description", ¢.description())//
-             .nl();
+      for (final String categoryName : categories.keySet())
+        r//
+            .put("Name", categoryName)//
+            .put("Count", categories.get(categoryName).size()) //
+            .put("Nanos", separate.these(categories.get(categoryName)).by(" "))//
+            // TODO: should be seperated by newline somehow but need to think on
+            // a format that will satisfy LaTeX
+            .nl();
       System.err.println(r.description());
     }
-  system.dumpOutput(system.bash("column -s \\& -t /tmp/nanos.tex"));
+    system.dumpOutput(system.bash("column -s \\& -t /tmp/nanos.tex"));
   }
 }
