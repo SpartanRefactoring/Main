a709573195a257770ab15c6782a2fb3017b5eb08
orimarco <marcovitch.ori@gmail.com>
2017-01-02 12:41:41 +0200

    removeExtraParenthesis - tipper I think we had to have long ago

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/ParenthesizedRemoveExtraParenthesisTest.java
@@ -0,0 +1,22 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/** A test clas for {@link ParenthesizedRemoveExtraParenthesis}
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-01-02 */
+@SuppressWarnings("static-method")
+public class ParenthesizedRemoveExtraParenthesisTest {
+  @Test public void a() {
+    trimmingOf("((a + b))")//
+        .gives("(a+b)")//
+        .stays();
+  }
+
+  @Test public void b() {
+    trimmingOf("((T)b)")//
+        .stays();
+  }
+}
