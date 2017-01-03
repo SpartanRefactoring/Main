1659d0d0289aefc1ec21e9d31eef76e6aa93880e
orimarco <marcovitch.ori@gmail.com>
2017-01-03 14:59:10 +0200

    safeInvocation #990

--- src/main/java/il/org/spartan/spartanizer/research/analyses/SpartAnalyzer.java
+++ src/main/java/il/org/spartan/spartanizer/research/analyses/SpartAnalyzer.java
@@ -45,6 +45,7 @@ public class SpartAnalyzer extends InteractiveSpartanizer {
             new DefaultsTo(), //
             new GeneralizedSwitch<ConditionalExpression>(), //
             new Unless(), //
+            new SafeInvocation(), //
             new SafeReference(), //
             new TakeDefaultTo(), //
             null) //
