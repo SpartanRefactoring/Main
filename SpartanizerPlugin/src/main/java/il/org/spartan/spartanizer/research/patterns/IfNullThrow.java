6550ab848f5b36d2cb3ac27a409ff7358480afe3
orimarco <marcovitch.ori@gmail.com>
2017-01-03 10:37:59 +0200

    some more work on categories

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/IfNullThrow.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/patterns/IfNullThrow.java
@@ -5,13 +5,14 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.patterns.common.*;
+import static il.org.spartan.spartanizer.research.TipperFactory.patternTipper;
 
 /** if(X == null) throw Exception;
  * @author Ori Marcovitch
  * @year 2016 */
 public final class IfNullThrow extends NanoPatternTipper<IfStatement> {
-  private static final UserDefinedTipper<IfStatement> tipper = TipperFactory.patternTipper("if($X == null) throw $X2;",
-      "If.Null($X).throwz(() -> $X2);", "If null throw pattern");
+  private static final UserDefinedTipper<IfStatement> tipper = patternTipper("if($X == null) throw $X2;", "If.Null($X).throwz(() -> $X2);",
+      "If null throw pattern");
 
   @Override public boolean canTip(final IfStatement ¢) {
     return tipper.canTip(¢);
@@ -19,5 +20,9 @@ public final class IfNullThrow extends NanoPatternTipper<IfStatement> {
 
   @Override public Tip pattern(final IfStatement ¢) {
     return tipper.tip(¢);
+  }
+
+  @Override public String category() {
+    return Category.Throw + "";
   }
 }
