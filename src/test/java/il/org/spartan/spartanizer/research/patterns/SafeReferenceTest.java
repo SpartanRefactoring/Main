fee0f14feaa4a19b73f7e67cba91af06c9bfc1b8
orimarco <marcovitch.ori@gmail.com>
2017-01-04 09:59:10 +0200

    SafeReference for default values too! #990

--- src/test/java/il/org/spartan/spartanizer/research/patterns/SafeReferenceTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/patterns/SafeReferenceTest.java
@@ -30,6 +30,27 @@ public class SafeReferenceTest {
         .stays();
   }
 
+  @Test public void field4() {
+    trimmingOf("return x != null && x.y;")//
+        .withTipper(InfixExpression.class, new Infix.SafeReference())//
+        .gives("return safe(x).get(()->x.y);")//
+        .stays();
+  }
+
+  @Test public void field5() {
+    trimmingOf("return x != null && x.y.z.w;")//
+        .withTipper(InfixExpression.class, new Infix.SafeReference())//
+        .gives("return safe(x).get(()->x.y.z.w);")//
+        .stays();
+  }
+
+  @Test public void field6() {
+    trimmingOf("return x == null ? 0 : x.y.z;")//
+        .withTipper(ConditionalExpression.class, new SafeReference())//
+        .gives("return safe(x).get(()->x.y.z);")//
+        .stays();
+  }
+
   @Test public void respect() {
     trimmingOf("return x ==null ? null : x.field;")//
         .withTippers(ConditionalExpression.class, //
@@ -54,6 +75,34 @@ public class SafeReferenceTest {
   @Test public void method2() {
     trimmingOf("return x == null ? null : x.y.z();")//
         .withTipper(ConditionalExpression.class, new SafeReference())//
+        .gives("return safe(x).invoke(()->x.y.z());")//
+        .stays();
+  }
+
+  @Test public void method3() {
+    trimmingOf("return x == null ? 0 : x.z();")//
+        .withTipper(ConditionalExpression.class, new SafeReference())//
+        .gives("return safe(x).invoke(()->x.z());")//
+        .stays();
+  }
+
+  @Test public void method4() {
+    trimmingOf("return x == null ? 0 : x.y.z();")//
+        .withTipper(ConditionalExpression.class, new SafeReference())//
+        .gives("return safe(x).invoke(()->x.y.z());")//
+        .stays();
+  }
+
+  @Test public void method5() {
+    trimmingOf("return x != null && x.z();")//
+        .withTipper(InfixExpression.class, new Infix.SafeReference())//
+        .gives("return safe(x).invoke(()->x.z());")//
+        .stays();
+  }
+
+  @Test public void method6() {
+    trimmingOf("return x != null && x.y.z();")//
+        .withTipper(InfixExpression.class, new Infix.SafeReference())//
         .gives("return safe(x).invoke(()->x.y.z());")//
         .stays();
   }
