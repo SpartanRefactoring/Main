239f8d5947433adc97c369666347da639fbc4127
Yossi Gil <yossi.gil@gmail.com>
2017-01-06 13:43:24 +0200

    Generalize #1042 to interfaces

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/RedundantModifierInAbstractMethod.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/RedundantModifierInAbstractMethod.java
@@ -10,6 +10,7 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
+import il.org.spartan.tables.*;
 
 /** convert <code><b>abstract</b> <b>interface</b>a{}</code> to
  * <code><b>interface</b> a{}</code>, etc.
@@ -25,18 +26,23 @@ public final class RedundantModifierInAbstractMethod extends CarefulTipper<Modif
   }
 
   @Override public Tip tip(final Modifier $) {
-    System.out.println($);
+    try (final Table r = new Table(this)) {
+     r.hashCode(); 
+    }
     if (!$.isFinal())
       return null;
     SingleVariableDeclaration singleVariableDeclaration = az.singleVariableDeclaration(parent($));
-    System.out.println(singleVariableDeclaration);
     if (singleVariableDeclaration == null)
       return null;
-    System.out.println(singleVariableDeclaration.getName());
-    System.out.println(parent(singleVariableDeclaration));
     MethodDeclaration methodDeclaration = az.methodDeclaration(parent(singleVariableDeclaration));
-    System.out.println(methodDeclaration);
-    return !Modifier.isAbstract(methodDeclaration.getModifiers()) ? null : new Tip(description($), $, this.getClass()) {
+    TypeDeclaration typeDeclaration = az.typeDeclaration(parent(methodDeclaration));
+    System.out.println(typeDeclaration);
+    return !Modifier.isAbstract(methodDeclaration.getModifiers())  && !typeDeclaration.isInterface()? null : new Tip(description($), $, this.getClass()) {
+      {
+       System.out.println($); 
+       System.out.println(parent($)); 
+       System.out.println(parent(parent($))); 
+      }
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         r.remove($, g);
       }
