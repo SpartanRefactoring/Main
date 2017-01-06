ad8365dd9679bad5ff89ba94bdf6a370ea1bc55d
Yossi Gil <yossi.gil@gmail.com>
2017-01-06 19:39:54 +0200

    Renaming

--- src/test/java/il/org/spartan/spartanizer/research/patterns/ReturnsHoldsForSomeTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/patterns/ReturnsHoldsForSomeTest.java
@@ -8,81 +8,81 @@ import org.junit.*;
 /** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-05 */
 @SuppressWarnings("static-method")
-public class HoldsForSomeTest {
+public class ReturnsHoldsForSomeTest {
   @Test public void _a() {
     trimmingOf(" for (final UserDefinedTipper<Statement> ¢ : tippers) if (¢.canTip(s)) return true; return false;")
-        .withTipper(Block.class, new HoldsForSome()).gives("return tippers.stream().anyMatch(¢ -> ¢.canTip(s));");
+        .withTipper(Block.class, new ReturnHoldsForSome()).gives("return tippers.stream().anyMatch(¢ -> ¢.canTip(s));");
   }
 
   @Test public void a() {
     trimmingOf("for (final Object ¢ : os)  if (¢.equals(best))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return os.stream().anyMatch(¢->¢.equals(best));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void b() {
     trimmingOf("something1(); something2(); for (final Object ¢ : os)  if (¢.equals(best))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("something1();something2();return os.stream().anyMatch(¢->¢.equals(best));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void c() {
     trimmingOf("for (Object ¢ : os)  if (best.equals(¢))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return os.stream().anyMatch(¢->best.equals(¢));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void d() {
     trimmingOf("for (Object ¢ : (B)bs)  if (best.equals(¢))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return(B)bs.stream().anyMatch(¢->best.equals(¢));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void e() {
     trimmingOf("for (Object ¢ : col.le(c,(tio)n))  if (best.equals(¢))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return col.le(c,(tio)n).stream().anyMatch(¢->best.equals(¢));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void f() {
     trimmingOf("for (Object ¢ : omg ? yes : no)  if (best.equals(¢))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return omg?yes:no.stream().anyMatch(¢->best.equals(¢));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void g() {
     trimmingOf("for (final Object ¢ : os)  if (¢ == (best))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return os.stream().anyMatch(¢->¢==(best));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void h() {
     trimmingOf("for (final Object ¢ : os)  if (¢ == a + b)   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return os.stream().anyMatch(¢->¢==a+b);")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 
   @Test public void i() {
     trimmingOf("for (final Object ¢ : os)  if (¢ == (omg ? yes : no))   return true; return false;")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .gives("return os.stream().anyMatch(¢->¢==(omg?yes:no));")//
-        .withTipper(Block.class, new HoldsForSome())//
+        .withTipper(Block.class, new ReturnHoldsForSome())//
         .stays();
   }
 }
