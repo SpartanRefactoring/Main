3a0c3e0b185f3cdb859943b4b787277153cc9955
Yossi Gil <yossi.gil@gmail.com>
2017-01-06 14:32:32 +0200

    Resolve #1043

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue1042.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue1042.java
@@ -4,19 +4,21 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
+/** Unit test for {@link ModifierFinalAbstractMethodRedundant}
+ * @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
  * @since 2016-12-23 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SuppressWarnings({ "static-method", "javadoc" })
 public class Issue1042 {
   @Test public void test() {
     trimmingOf("abstract int f(final int a, final int b);")//
-    .gives("abstract int f(int a, int b);")//
+        .gives("abstract int f(int a, int b);")//
         .stays();
   }
+
   @Test public void test1() {
     trimmingOf("interface a { int f(final int a, final int b);}")//
-    .gives("interface a { int f(int a, int b);}")//
+        .gives("interface a { int f(int a, int b);}")//
         .stays();
   }
 }
