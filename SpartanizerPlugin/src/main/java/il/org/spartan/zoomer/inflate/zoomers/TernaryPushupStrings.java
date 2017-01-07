78cd5edf4c3d7d658bd38ad36714b193aa33e2c0
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-08 01:31:58 +0200

    closes #1003

--- SpartanizerPlugin/src/main/java/il/org/spartan/zoomer/inflate/zoomers/TernaryPushupStrings.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/zoomer/inflate/zoomers/TernaryPushupStrings.java
@@ -12,10 +12,12 @@ import il.org.spartan.spartanizer.tipping.*;
  * for example
  * <pre>
  * "abc" + (condition ? "123" : "456")
+ * (condition ? "abc" : "def") + 123"
  * <pre>
  * to
  * <pre>
  * condition ? "abc123" : "abc456"
+ * condition ? "abc123" : "def123"
  * <pre>
  * 
  * Test case is {@link Issue1003}
@@ -27,25 +29,45 @@ public class TernaryPushupStrings extends ReplaceCurrentNode<InfixExpression> im
   @Override public ASTNode replacement(InfixExpression x) {
     AST ast = x.getAST();
     InfixExpression nn = duplicate.of(x);
-    StringLiteral l = az.stringLiteral(step.left(nn));
-    ConditionalExpression r = az.conditionalExpression(step.expression(step.right(nn)));
+    StringLiteral l;
+    ConditionalExpression r;
+    if(iz.stringLiteral(step.left(nn))) {
+      l = az.stringLiteral(step.left(nn));
+      r = az.conditionalExpression(step.expression(step.right(nn)));
+    }
+    else {
+      l = az.stringLiteral(step.right(nn));
+      r = az.conditionalExpression(step.expression(step.left(nn)));
+    }
     ConditionalExpression $ = ast.newConditionalExpression();
     $.setExpression(duplicate.of(r.getExpression()));
     StringLiteral l1 = az.stringLiteral(step.then(r));
     StringLiteral l2 = az.stringLiteral(step.elze(r));
-    l1.setLiteralValue(l.getLiteralValue() + l1.getLiteralValue());
-    l2.setLiteralValue(l.getLiteralValue() + l2.getLiteralValue());
-    $.setThenExpression(duplicate.of(l1));
-    $.setElseExpression(duplicate.of(l2));
+    StringLiteral n1 = duplicate.of(l1);
+    StringLiteral n2 = duplicate.of(l2);
+    if(iz.stringLiteral(step.left(nn))) {
+      n1.setLiteralValue(l.getLiteralValue() + l1.getLiteralValue());
+      n2.setLiteralValue(l.getLiteralValue() + l2.getLiteralValue());
+    }
+    else {
+      n1.setLiteralValue(l1.getLiteralValue() + l.getLiteralValue());
+      n2.setLiteralValue(l2.getLiteralValue() + l.getLiteralValue());
+    }
+    $.setThenExpression(duplicate.of(n1));
+    $.setElseExpression(duplicate.of(n2));
     return $;
   }
   
-  @Override protected boolean prerequisite(InfixExpression x) {
-    Expression l = step.left(x);
-    if (!iz.parenthesizedExpression(step.right(x)))
+  @Override protected boolean prerequisite(InfixExpression ¢) {
+    return step.operator(¢) == InfixExpression.Operator.PLUS && (iz.stringLiteral(step.left(¢)) && isStringTernaryWithParenth(step.right(¢))
+        || iz.stringLiteral(step.right(¢)) && isStringTernaryWithParenth(step.left(¢)));
+  }
+  
+  private static boolean isStringTernaryWithParenth(Expression x) {
+    if (!iz.parenthesizedExpression(x))
       return false;
-    Expression r = step.expression(step.right(x));
-    if (!iz.stringLiteral(l) || step.operator(x) != InfixExpression.Operator.PLUS || !iz.conditionalExpression(r))
+    Expression r = step.expression(x);
+    if (!iz.conditionalExpression(r))
       return false;
     ConditionalExpression $ = az.conditionalExpression(r);
     return iz.stringLiteral(step.then($)) && iz.stringLiteral(step.elze($));
