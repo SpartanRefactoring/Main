8e2cec56261021b591ae1c972e28197899cf0cb3
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-07 19:24:37 +0200

    #1048 test class

---
+++ src/test/java/il/org/spartan/athenizer/inflate/expanders/Issue1048.java
@@ -0,0 +1,22 @@
+package il.org.spartan.athenizer.inflate.expanders;
+
+import static il.org.spartan.zoomer.inflate.expanders.ExpanderTestUtils.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/** Test case for bug in {@link AssignmentTernaryExpander}
+ * .
+ * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
+ * @since 2017-01-07
+ */
+@Ignore
+@SuppressWarnings("static-method")
+public class Issue1048 { 
+  @Test public void test() {
+    expansionOf("public static InDeclaration instance() {"
+        + "instance = instance != null ? instance : new InDeclaration();"
+        + "return instance;"
+        + "}").stays();
+  }
+}
