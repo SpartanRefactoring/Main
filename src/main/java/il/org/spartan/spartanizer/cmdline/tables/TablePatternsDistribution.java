ebffc359cf1db26f24f9ee9d9c08c146a6266a81
Yossi Gil <yossi.gil@gmail.com>
2017-01-07 22:02:32 +0200

    Reorganize packages

--- src/main/java/il/org/spartan/spartanizer/cmdline/tables/TablePatternsDistribution.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/tables/TablePatternsDistribution.java
@@ -1,4 +1,4 @@
-package il.org.spartan.spartanizer.cmdline.nanos.analyses;
+package il.org.spartan.spartanizer.cmdline.tables;
 
 import java.lang.reflect.*;
 import java.util.*;
--- src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nanos.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nanos.java
@@ -1,4 +1,4 @@
-package il.org.spartan.spartanizer.cmdline.nanos.analyses;
+package il.org.spartan.spartanizer.cmdline.tables;
 
 import static il.org.spartan.spartanizer.cmdline.tables.Table_Tippers.*;
 
