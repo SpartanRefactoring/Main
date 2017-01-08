e7cffb5c649f85e4778f13298c6450969089a61b
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-08 19:11:20 +0200

    #1047

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue1047.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue1047.java
@@ -22,4 +22,25 @@ public class Issue1047 {
         .givesEither("for (int ¢ = 0 ; ¢ < 5 ; ++¢) {++¢;}", //
             "for (int ¢ = 0 ; ¢ < 5 ; ++¢) ++¢;");
   }
+  @Test public void t3() {
+    trimmingOf("for (int i = 0; i < length; ++i) {"
+        + "char c1 = s1.charAt(i);"
+        + "char c2 = s2.charAt(i);"
+        + "if (c1 == c2)"
+        + "  continue;"
+        + "int alphaIndex = getAlphaIndex(c1);"
+        + "if (alphaIndex >= 26 || alphaIndex != getAlphaIndex(c2))"
+        + "  return false;"
+        + "continue;"
+        + "}")
+    .gives("for (int i = 0; i < length; ++i) {"
+        + "char c1 = s1.charAt(i);"
+        + "char c2 = s2.charAt(i);"
+        + "if (c1 == c2)"
+        + "  continue;"
+        + "int alphaIndex = getAlphaIndex(c1);"
+        + "if (alphaIndex >= 26 || alphaIndex != getAlphaIndex(c2))"
+        + "  return false;"
+        + "}");
+  }
 }
