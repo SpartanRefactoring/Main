ade67eec38dcd1d8289f5f6a1ef187001b3d7a44
orimarco <marcovitch.ori@gmail.com>
2017-01-08 19:50:55 +0200

    technicalNames, ForEachFiltered

--- src/main/java/il/org/spartan/spartanizer/research/analyses/SpartAnalyzer.java
+++ src/main/java/il/org/spartan/spartanizer/research/analyses/SpartAnalyzer.java
@@ -55,6 +55,7 @@ public class SpartAnalyzer extends InteractiveSpartanizer {
         .add(EnhancedForStatement.class, //
             new Aggregate(), //
             new ForEach(), //
+            new ForEachFiltered(), //
             new Select(), //
             null) //
         .add(IfStatement.class, //
