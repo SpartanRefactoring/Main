db2deae31f5565438e697a0db73eaa49d320a25c
Yossi Gil <yossi.gil@gmail.com>
2017-01-08 22:01:28 +0200

    One fewer laconization

--- src/main/java/il/org/spartan/spartanizer/cmdline/Application.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Application.java
@@ -115,7 +115,7 @@ public final class Application implements IApplication {
         u = openCompilationUnit(f);
         final FileStats s = new FileStats(f);
         for (int i = 0; i < optRounds; ++i) {
-          final int n = new LaconizeProject().countTips();
+          final int n = new SpartanizeProject().countTips();
           if (n == 0)
             break;
           s.addRoundStat(n);
