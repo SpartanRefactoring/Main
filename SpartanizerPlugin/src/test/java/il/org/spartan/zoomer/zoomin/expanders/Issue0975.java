e9b73cc22f7c528aa7112f5fccbe33753e163a61
Yossi Gil <yossi.gil@gmail.com>
2017-01-08 20:54:32 +0200

    Move packages

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0975.java
@@ -0,0 +1,46 @@
+package il.org.spartan.zoomer.zoomin.expanders;
+
+import static il.org.spartan.zoomer.inflate.zoomers.ExpanderTestUtils.*;
+
+import org.junit.*;
+
+import il.org.spartan.zoomer.inflate.zoomers.*;
+
+/** Unit Test for the ForBlock expander {@link ForBlockExpander}, issue #975
+ * Also, Unit Test for the WhileBlock expander {@link WhileBlockExpander}
+ * @author Raviv Rachmiel
+ * @since 26-12-16 */
+@SuppressWarnings("static-method")
+public class Issue0975 {
+  @Test public void simpleBlockTest() {
+    expansionOf("for(int i=0;i<5;i++) a=5;").gives("for(int i=0;i<5;i++){a=5;}").stays();
+  }
+
+  @Test public void simpleShouldntAddTest() {
+    expansionOf("for(int i=0;i<5;i++){ a=5;}").stays();
+  }
+
+  @Test public void notSimpleShouldntAddTest() {
+    expansionOf("for(int i=0;i<5;i++){ a=5;b=3;}").stays();
+  }
+
+  @Test public void notSimpleShouldAddTest() {
+    expansionOf("for(int i=0;i<5;i++) a=5; b=7;").gives("for(int i=0;i<5;i++){ a=5;}b=7;").stays();
+  }
+
+  @Ignore @Test public void simpleBlockTestWhile() {
+    expansionOf("while(i<5) a=5;").gives("while(i<5){a=5;}").stays();
+  }
+
+  @Test public void simpleShouldntAddTestWhile() {
+    expansionOf("while(i<5){ a=5;}").stays();
+  }
+
+  @Test public void notSimpleShouldntAddTestWhile() {
+    expansionOf("while(i<5){ a=5;b=3;}").stays();
+  }
+
+  @Ignore @Test public void notSimpleShouldAddTestWhile() {
+    expansionOf("while(i<5) a=5; b=7;").gives("while(i<5){ a=5;}b=7;").stays();
+  }
+}
