d8cd3f0054e991a282a3a838640a18432ac58d2f
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-10 00:32:15 +0200

    working on #860

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue0860.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue0860.java
@@ -41,4 +41,68 @@ public class Issue0860 {
         + "x = 3;"
         + "}");
   }
+  @Test public void t2() {
+    trimmingOf("switch(x) {"
+        + "case 2:"
+        + "case 1:"
+        + "case 3:"
+        + "case 10:"
+        + "x = 2;"
+        + "break;"
+        + "}")
+    .gives("switch(x) {"
+        + "case 1:"
+        + "case 2:"
+        + "case 3:"
+        + "case 10:"
+        + "x = 2;"
+        + "break;"
+        + "}");
+  }
+  @Test public void t3() {
+    trimmingOf("switch(x) {"
+        + "case 'b':"
+        + "case 'c':"
+        + "case 'a':"
+        + "case 'd':"
+        + "x = 2;"
+        + "break;"
+        + "}")
+    .gives("switch(x) {"
+        + "case 'b':"
+        + "case 'a':"
+        + "case 'c':"
+        + "case 'd':"
+        + "x = 2;"
+        + "break;"
+        + "}")
+    .gives("switch(x) {"
+        + "case 'a':"
+        + "case 'b':"
+        + "case 'c':"
+        + "case 'd':"
+        + "x = 2;"
+        + "break;"
+        + "}");
+  }
+  @Test public void t4() {
+    trimmingOf("switch(x) {"
+        + "case C5:"
+        + "case C2:"
+        + "case C9:"
+        + "case C1:"
+        + "case B:"
+        + "x = 2;"
+        + "break;"
+        + "}")
+    .gives("switch(x) {"
+        + "case B:"
+        + "case C1:"
+        + "case C2:"
+        + "case C5:"
+        + "case C9:"
+        + "x = 2;"
+        + "break;"
+        + "}");
+  }
 }
