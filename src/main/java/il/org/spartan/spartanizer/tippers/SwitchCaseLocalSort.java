c143063aaf833946d6654ca9802882f57a11ffdf
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-09 22:37:02 +0200

    spartanized #860

--- src/main/java/il/org/spartan/spartanizer/tippers/SwitchCaseLocalSort.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/SwitchCaseLocalSort.java
@@ -21,24 +21,19 @@ import il.org.spartan.spartanizer.ast.factory.*;
  */
 public class SwitchCaseLocalSort extends CarefulTipper<SwitchCase> implements TipperCategory.Sorting {
   @Override public Tip tip(SwitchCase n) {
-    SwitchCase next = az.switchCase(extract.nextStatement(n));
-    if(next == null || next.isDefault() || n.isDefault())
-      return null;
-    if(!iz.intType(expression(n)) && expression(n).toString().compareTo(expression(next).toString()) <= 0)
-      return null;
-    else if(Integer.parseInt(expression(n).toString()) <= Integer.parseInt(expression(next).toString()))
-      return null;
-    return new Tip(description(n), n, getClass()) {
-      @Override public void go(ASTRewrite r, TextEditGroup g) {
-        final ListRewrite l = r.getListRewrite(n.getParent(), SwitchStatement.STATEMENTS_PROPERTY);
-        SwitchCase tmp = copy.of(n);
-        l.replace(n, copy.of(next), g);
-        l.replace(next, tmp, g);
-      }
-    };
+    SwitchCase $ = az.switchCase(extract.nextStatement(n));
+    return $ == null || $.isDefault() || n.isDefault() || !iz.intType(expression(n)) && (expression(n) + "").compareTo((expression($) + "")) <= 0
+        || Integer.parseInt((expression(n) + "")) <= Integer.parseInt((expression($) + "")) ? null : new Tip(description(n), n, getClass()) {
+          @Override public void go(ASTRewrite r, TextEditGroup g) {
+            final ListRewrite l = r.getListRewrite(n.getParent(), SwitchStatement.STATEMENTS_PROPERTY);
+            SwitchCase tmp = copy.of(n);
+            l.replace(n, copy.of($), g);
+            l.replace($, tmp, g);
+          }
+        };
   }
-  @SuppressWarnings("unused")
-  @Override public String description(SwitchCase n) {
+  @Override
+  @SuppressWarnings("unused") public String description(SwitchCase n) {
     return "sort cases with same flow control";
   }
 }
