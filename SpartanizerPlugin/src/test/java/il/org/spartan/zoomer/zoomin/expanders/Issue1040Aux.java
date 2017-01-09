030cc8ab302f3ef004040f64cc47d3e633b272df
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-09 23:36:19 +0200

    #1053 reverted changes made to the example

--- SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1040Aux.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1040Aux.java
@@ -2,15 +2,20 @@ package il.org.spartan.zoomer.zoomin.expanders;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
 
-/** @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
+/** Example for testing with binding
+ * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
  * @since 2017-01-07 [[SuppressWarningsSpartan]] */
-@SuppressWarnings({ "static-method" })
+@SuppressWarnings({ "static-method", "unused" })
 public class Issue1040Aux extends ReflectiveTester {
   int a() {
+    int i = 0;
+    ++i;
     return 0;
   }
-
+  
   int b() {
+    int i = 0;
+    ++i;
     return 0;
   }
 }
