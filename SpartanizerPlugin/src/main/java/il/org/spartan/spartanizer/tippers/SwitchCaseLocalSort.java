01b12ebd59397733141cd1789001f11898c41228
orimarco <marcovitch.ori@gmail.com>
2017-01-10 11:04:13 +0200

    warnings and spartanizing @yuvalsimon @dormaayan

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchCaseLocalSort.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchCaseLocalSort.java
@@ -6,46 +6,39 @@ import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.rewrite.*;
 import org.eclipse.text.edits.*;
 
-import il.org.spartan.spartanizer.ast.navigate.*;
-import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.tipping.*;
-import il.org.spartan.spartanizer.ast.safety.*;
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.tipping.*;
 
 /** sorts cases of a local branch
+ * 
  * <pre>
- * switch(x) {
- * case 2:
- * case 1:
- * break;
- * }
+ * switch(x) { case 2: case 1: break; }
+ * 
  * <pre>
- * to
- * switch(x) {
- * case 1:
- * case 2:
- * break;
- * }
+ * to switch(x) { case 1: case 2: break; }
+ * 
  * <pre>
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
- * @since 2017-01-09
- */
+ * @since 2017-01-09 */
 public class SwitchCaseLocalSort extends CarefulTipper<SwitchStatement> implements TipperCategory.Sorting {
   SwitchCase c1;
   SwitchCase c2;
+
   @Override public Tip tip(SwitchStatement $) {
     List<Statement> l = statements($);
     boolean found = false;
-    for (int i = 0; i < l.size() - 1; ++i)
-      if (iz.switchCase(l.get(i)) && iz.switchCase(l.get(i + 1))) {
-        c1 = az.switchCase(l.get(i));
-        c2 = az.switchCase(l.get(i + 1));
+    for (int ¢ = 0; ¢ < l.size() - 1; ++¢)
+      if (iz.switchCase(l.get(¢)) && iz.switchCase(l.get(¢ + 1))) {
+        c1 = az.switchCase(l.get(¢));
+        c2 = az.switchCase(l.get(¢ + 1));
         if (c1.isDefault() || c2.isDefault())
           continue;
-        Expression aaa = expression(c1);
+//        Expression aaa = exspression(c1);
         if (iz.intType(expression(c1)) && Integer.parseInt((expression(c1) + "")) > Integer.parseInt(expression(c2) + "")
             || !iz.intType(expression(c1)) && (expression(c1) + "").compareTo((expression(c2) + "")) > 0) {
           found = true;
@@ -60,8 +53,7 @@ public class SwitchCaseLocalSort extends CarefulTipper<SwitchStatement> implemen
     };
   }
 
-  @Override
-  @SuppressWarnings("unused") public String description(SwitchStatement __) {
+  @Override @SuppressWarnings("unused") public String description(SwitchStatement __) {
     return "sort cases with same flow control";
   }
 }
