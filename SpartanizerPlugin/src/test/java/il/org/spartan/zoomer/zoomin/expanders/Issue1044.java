6790768a792689f4d66ec4441d37d6f4d6150ccc
Doron Meshulam <doronmmm@hotmail.com>
2017-01-10 17:30:08 +0200

    Fixed warnings, added docs #1044

--- SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1044.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1044.java
@@ -1,12 +1,16 @@
 package il.org.spartan.zoomer.zoomin.expanders;
 
-import static org.junit.Assert.*;
-
+// import static org.junit.Assert.*;
 import org.junit.*;
 
 import il.org.spartan.spartanizer.java.namespace.*;
 
+/** Test class for name generation from Namespace (Environments) 
+ * @author Doron Meshulam <tt>doronmmm@hotmail.com</tt>
+ * @since 2017-01-10 */
+@SuppressWarnings("static-method")
 public class Issue1044 {
   @Test public void test() {
+    (new Namespace(null, null)).generateName(new Binding());
   }
 }
