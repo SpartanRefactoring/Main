537d6255949e5b0b61a8ad077c759cfcba711bdc
tomerdragucki <tomerd@campus.technion.ac.il>
2017-01-11 17:07:06 +0200

    Added #976 expander + tests

---
+++ src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0976.java
@@ -0,0 +1,30 @@
+package il.org.spartan.zoomer.zoomin.expanders;
+
+import static il.org.spartan.zoomer.inflate.zoomers.ExpanderTestUtils.*;
+
+import org.junit.*;
+
+import il.org.spartan.zoomer.inflate.zoomers.*;
+
+/** Unit tests for {@link LongIfExpander} Issue #976
+ * @author tomerdragucki <tt>tomerd@campus.technion.ac.il</tt>
+ * @since 2017-01-09 */
+@SuppressWarnings("static-method")
+public class Issue0976 {
+  @Test public void a() {
+    zoomingInto("if(a==b && c==d) { t=5; }").gives("if(a==b) if(c==d) { t=5; }");
+  }
+
+  @Test public void a1() {
+    zoomingInto("if(a==b && c==d) t=5;").gives("if(a==b && c==d) { t=5; }").gives("if(a==b) if(c==d) { t=5; }");
+  }
+
+  @Test public void b() {
+    zoomingInto("if(a && b && c) { t=5; }").gives("if(a) if(b && c) { t=5; }").gives("if(a) { if(b && c) { t=5; } }")
+        .gives("if(a) { if(b) if(c) { t=5; } }");
+  }
+
+  @Test public void c() {
+    zoomingInto("if(a && b) { f(); } else { g(); }").gives("if(a) if(b) { f(); } else { g(); } else { g(); } ");
+  }
+}
