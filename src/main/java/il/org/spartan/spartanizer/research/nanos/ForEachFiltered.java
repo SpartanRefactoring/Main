62039624850b7874aea404d0423797c7e3fe93b0
orimarco <marcovitch.ori@gmail.com>
2017-01-11 16:19:03 +0200

    extended ForEachFiltered

--- src/main/java/il/org/spartan/spartanizer/research/nanos/ForEachFiltered.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/ForEachFiltered.java
@@ -12,13 +12,16 @@ import il.org.spartan.spartanizer.research.*;
 /** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-08 */
 public class ForEachFiltered extends ForEach {
+  private static final String description = "ForEachThat pattern: conevrt to fluent API";
   private static final List<UserDefinedTipper<EnhancedForStatement>> tippers = new ArrayList<UserDefinedTipper<EnhancedForStatement>>() {
     static final long serialVersionUID = 1L;
     {
-      add(patternTipper("for($N1 $N2 : $N3) if($X1) $X2;", "$N3.stream().filter($N2 -> $X1).forEach($N2 -> $X2);",
-          "ForEachThat pattern: conevrt to fluent API"));
-      add(patternTipper("for($N1 $N2 : $X1) if($X2) $X3;", "($X1).stream().filter($N2 -> $X2).forEach($N2 -> $X3);",
-          "ForEachThat pattern: conevrt to fluent API"));
+      add(patternTipper("for($T $N1 : $N2) if($X1) $X2;", "$N2.stream().filter($N1 -> $X1).forEach($N1 -> $X2);", description));
+      add(patternTipper("for($T $N1 : $X1) if($X2) $X3;", "($X1).stream().filter($N1 -> $X2).forEach($N1 -> $X3);", description));
+      add(patternTipper("for($T $N1 : $N2) if($X1) try{ $X2; } catch($T2 $N3) $B",
+          "$N2.stream().filter($N1 -> $X1).forEach($N1 -> { try{$X2;} catch($T2 $N3) $B});", description));
+      add(patternTipper("for($T $N1 : $X1) if($X2) try{ $X3; } catch($T2 $N3) $B ",
+          "($X1).stream().filter($N1 -> $X2).forEach($N1 -> {try{ $X3; } catch($T2 $N3) $B});", description));
     }
   };
 
