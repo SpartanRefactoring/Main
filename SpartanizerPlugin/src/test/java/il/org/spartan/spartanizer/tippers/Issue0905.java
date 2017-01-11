d6d77ce211f34dfce70bb7600b9f934ae67addfc
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-11 21:34:00 +0200

    closes #905 closes #1059

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0905.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0905.java
@@ -6,56 +6,55 @@ import org.junit.*;
 
 /** This is a unit test for {@link RemoveRedundantIf},
  * {@link RemoveRedundantFor}, {@link RemoveRedundantWhile} of previously failed
- * tests. Related to {@link Issue251}.
+ * tests. Related to {@link Issue0251}.
  * @author Yuval Simon
  * @since 2016-12-08 */
-// TODO: Raviv R --yg
-@Ignore
 @SuppressWarnings("static-method")
 public class Issue0905 {
   @Test public void t06() {
     trimmingOf("if(b()){int i;}")//
-        .gives("if(b()){}");
+        .stays();
   }
 
   @Test public void t12() {
     trimmingOf("if(b==true){int i=5,q=g();}")//
-        .gives("if(b){int q=g();}");
+        .gives("if(b){int i=5,q=g();}")//
+        .stays() //
+    ;
   }
 
   @Test public void t15() {
     trimmingOf("if(b==q()){int i;}")//
-        .gives("if(b==q()){}")//
-    ;
+        .stays();
   }
 
   @Test public void t17() {
     trimmingOf("while(b==q){if(tipper==q()){int i;}}")//
         .gives("while(b==q)if(tipper==q()){int i;}")//
-        .gives("while(b==q)if(tipper==q()){}");//
+        .stays();
   }
 
   @Test public void t21() {
     trimmingOf("for(i=1;b==q;++i){if(tipper==q()){int i;}}")//
         .gives("for(i=1;b==q;++i)if(tipper==q()){int i;}")//
-        .gives("for(i=1;b==q;++i)if(tipper==q()){}");//
+        .stays();
   }
 
   @Test public void t23() {
     trimmingOf("for(i=1;b==q();++i){if(tipper==q()){int i;}}")//
         .gives("for(i=1;b==q();++i)if(tipper==q()){int i;}")//
-        .gives("for(i=1;b==q();++i)if(tipper==q()){}");//
+        .stays();
   }
 
   @Test public void t24() {
     trimmingOf("for(i=tipper();b==q;++i){if(tipper==q()){int i;}}")//
         .gives("for(i=tipper();b==q;++i)if(tipper==q()){int i;}")//
-        .gives("for(i=tipper();b==q;++i)if(tipper==q()){}");//
+        .stays();
   }
 
   @Test public void t25() {
     trimmingOf("for(i=4;b==q;f=i()){if(tipper==q()){int i;}}")//
         .gives("for(i=4;b==q;f=i())if(tipper==q()){int i;}")//
-        .gives("for(i=4;b==q;f=i())if(tipper==q()){}");//
+        .stays();
   }
 }
