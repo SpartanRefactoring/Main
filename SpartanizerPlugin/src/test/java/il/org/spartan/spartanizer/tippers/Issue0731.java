0e595ce34556f046a007e374964d1ac2d265c032
tomerdragucki <tomerd@campus.technion.ac.il>
2017-01-12 23:20:06 +0200

    #731 bug solved

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0731.java
@@ -0,0 +1,20 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/** @author tomerdragucki <tt>tomerd@campus.technion.ac.il</tt>
+ * @since 2017-01-12 */
+@SuppressWarnings("static-method")
+public class Issue0731 {
+  @Test public void a() {
+    trimmingOf("Integer i = 0; i.toString();").stays();
+//    Integer i = 0;
+//    i.toString();
+  }
+  
+  @Test public void b() {
+    trimmingOf("Integer i = 0; (i).toString();").stays();
+  }
+}
