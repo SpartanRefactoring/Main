baccad3a4908adcfd85b95100cf1cf642790e5f3
Yossi Gil <yossi.gil@gmail.com>
2017-01-15 14:36:52 +0200

    Renamings

--- src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0974.java
+++ src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0974.java
@@ -1,10 +1,10 @@
 package il.org.spartan.zoomer.zoomin.expanders;
 
-import static il.org.spartan.bloater.inflate.zoomers.BoatingTestUtilities.*;
+import static il.org.spartan.bloater.bloaters.BoatingTestUtilities.*;
 
 import org.junit.*;
 
-import il.org.spartan.bloater.inflate.zoomers.*;
+import il.org.spartan.bloater.bloaters.*;
 
 /** Test class for {@link PostFixToInfixExpander}
  * @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
--- src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1004.java
+++ src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1004.java
@@ -1,10 +1,10 @@
 package il.org.spartan.zoomer.zoomin.expanders;
 
-import static il.org.spartan.bloater.inflate.zoomers.BoatingTestUtilities.*;
+import static il.org.spartan.bloater.bloaters.BoatingTestUtilities.*;
 
 import org.junit.*;
 
-import il.org.spartan.bloater.inflate.zoomers.*;
+import il.org.spartan.bloater.bloaters.*;
 
 /** Test case for {@link OutlineArrayAccess}
  * @author YuvalSimon <tt>yuvaltechnion@gmail.com</tt>
