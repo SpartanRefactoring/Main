adae1bff99ddb74a549821544f1bd1e34dab9ee7
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-15 21:31:35 +0200

    fixed warnings

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchBranchSort.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/SwitchBranchSort.java
@@ -4,7 +4,6 @@ import java.util.*;
 import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** Sorts switch branches according to the metrics: 1. Depth - height of ast 2.
