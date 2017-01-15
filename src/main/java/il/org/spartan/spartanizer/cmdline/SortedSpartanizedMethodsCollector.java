ef2df271855ab9ea97b9974c2ef99b3847aadee4
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-01-15 13:46:29 +0200

    update

--- src/main/java/il/org/spartan/spartanizer/cmdline/SortedSpartanizedMethodsCollector.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/SortedSpartanizedMethodsCollector.java
@@ -1,4 +1,4 @@
-package il.org.spartan.spartanizer.cmdline.collector;
+package il.org.spartan.spartanizer.cmdline;
 
 import static il.org.spartan.spartanizer.research.analyses.util.Files.*;
 
@@ -13,8 +13,6 @@ import static il.org.spartan.spartanizer.ast.navigate.step.*;
 import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.cmdline.*;
-import il.org.spartan.spartanizer.cmdline.applications.*;
 import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.analyses.*;
 import il.org.spartan.spartanizer.research.analyses.util.*;
