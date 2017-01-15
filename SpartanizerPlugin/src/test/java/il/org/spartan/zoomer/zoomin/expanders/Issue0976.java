01c4a6990840d2ee1d8fa536da7f8893cecdce23
tomerdragucki <tomerd@campus.technion.ac.il>
2017-01-16 00:22:53 +0200

    some TODOs

--- SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0976.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue0976.java
@@ -13,23 +13,23 @@ import il.org.spartan.bloater.bloaters.*;
 public class Issue0976 {
   @Test public void a() {
     bloatingOf("if(a==b && c==d) { t=5; }")//
-        .gives("if(a==b) if(c==d) { t=5; }");
+        .gives("if(a==b) { if(c==d) t=5; }");
   }
 
   @Test public void a1() {
     bloatingOf("if(a==b && c==d) t=5;")//
         .gives("if(a==b && c==d) { t=5; }")//
-        .gives("if(a==b) if(c==d) { t=5; }");
+        .gives("if(a==b) { if(c==d) t=5; }");
   }
 
   @Test public void b() {
     bloatingOf("if(a && b && c) { t=5; }")//
-        .gives("if(a) if(b && c) { t=5; }")//
-        .gives("if(a) { if(b && c) { t=5; } }").gives("if(a) { if(b) if(c) { t=5; } }");
+        .gives("if(a) { if(b && c) t=5; }")//
+        .gives("if(a) { if(b && c) { t=5; } }").gives("if(a) { if(b) { if(c) t=5; } }");
   }
 
   @Test public void c() {
     bloatingOf("if(a && b) { f(); } else { g(); }")//
-        .gives("if(a) if(b) { f(); } else { g(); } else { g(); } ");
+        .gives("if(a) if(b) f(); else { g(); } else { g(); }");
   }
 }
