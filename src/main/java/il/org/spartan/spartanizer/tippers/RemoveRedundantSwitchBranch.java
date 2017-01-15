ac977177a0afb7b618c6f97c8ec401e02cac25fc
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-15 04:16:00 +0200

    closes #1064

--- src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundantSwitchBranch.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundantSwitchBranch.java
@@ -3,12 +3,8 @@ package il.org.spartan.spartanizer.tippers;
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
-
-import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
-import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 
@@ -29,69 +25,23 @@ import il.org.spartan.spartanizer.tipping.*;
 * case b: x=2; break;
  * </pre>
  *
- * Tested in {@link Issue858}
+ * Tested in {@link Issue0858}
  * @author Yuval Simon
  * @since 2016-11-26 */
 public class RemoveRedundantSwitchBranch extends ReplaceCurrentNode<SwitchStatement> implements TipperCategory.Collapse {
-  @Override @SuppressWarnings("boxing") public ASTNode replacement(final SwitchStatement s) {
-    if (s == null)
-      return null;
-    final List<Statement> ss = step.statements(s);
-    final List<Integer> ll = new ArrayList<>();
-    getNotContainedCasesIndexes(ss, ll);
-    final StringBuilder $ = new StringBuilder("switch(" + s.getExpression() + "){");
-    boolean isChanged = false;
-    for (int i = 0; i < ll.size(); ++i) {
-      $.append(ss.get(ll.get(i))).append("");
-      if (i < ll.size() - 1 && ll.get(i + 1) == ll.get(i) + 1)
-        continue;
-      for (int j = i + 1; j < ll.size(); ++j)
-        if (sameCommands(ss, ll.get(i) + 1, ll.get(j) + 1)) {
-          $.append(ss.get(ll.get(j)));
-          ll.remove(j);
-          --j;
-          isChanged = true;
+  @Override public ASTNode replacement(SwitchStatement s) {
+    List<SwitchBranch> $ = SwitchBranch.intoBranches(s);
+    for(int i = 0; i < $.size(); ++i)
+      for(int j = i + 1; j < $.size(); ++j)
+        if($.get(i).hasSameCode($.get(j))) {
+          $.get(i).cases().addAll($.get(j).cases());
+          $.remove(j);
+          return SwitchBranch.makeSwitchStatement($, step.expression(s), s.getAST());
         }
-      for (int k = ll.get(i) + 1; k < ss.size(); ++k) {
-        $.append(ss.get(k));
-        if (iz.breakStatement(ss.get(k)))
-          break;
-      }
-    }
-    $.append("}");
-    return !isChanged ? null : subject.statement(into.s($ + "")).toOneStatementOrNull();
-  }
-
-  private static void getNotContainedCasesIndexes(final List<Statement> ss, final List<Integer> ll) {
-    boolean startFromBreak = true;
-    for (int ¢ = 0; ¢ < ss.size(); ++¢)
-      if (startFromBreak && iz.switchCase(ss.get(¢)))
-        ll.add(Integer.valueOf(¢));
-      else
-        startFromBreak = iz.breakStatement(ss.get(¢));
-  }
-
-  @SuppressWarnings("boxing") private static boolean sameCommands(final List<Statement> ss, final int t1, final int t2) {
-    int $ = t1 < t2 ? t1 : t2;
-    int p2 = t2 > t1 ? t2 : t1;
-    for (; $ < ss.size() && iz.switchCase(ss.get($));)
-      ++$;
-    for (; p2 < ss.size() && iz.switchCase(ss.get(p2));)
-      ++p2;
-    if ($ == ss.size())
-      return false;
-    for (final Integer ¢ : range.from(0).to(ss.size())) {
-      if (p2 + ¢ == ss.size())
-        return iz.breakStatement(ss.get($ + ¢));
-      if (iz.breakStatement(ss.get($ + ¢)) && iz.breakStatement(ss.get(p2 + ¢)))
-        return true;
-      if (!(ss.get($ + ¢) + "").equals(ss.get(p2 + ¢) + ""))
-        return false;
-    }
-    return true;
+    return null;
   }
 
   @Override public String description(@SuppressWarnings("unused") final SwitchStatement __) {
-    return "Merging cases with identical code";
+    return "Merge branches with same code";
   }
 }
--- src/main/java/il/org/spartan/spartanizer/tippers/SwitchBranchSort.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/SwitchBranchSort.java
@@ -1,15 +1,11 @@
 package il.org.spartan.spartanizer.tippers;
 
 import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
-
-import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
-import il.org.spartan.lisp;
 
 /** Sorts switch branches according to the metrics: 1. Depth - height of ast 2.
  * Length measured in statements 3. Length measured in nodes 4. Sequencer level
@@ -19,157 +15,18 @@ import il.org.spartan.lisp;
  * @since 2017-01-11 */
 public class SwitchBranchSort extends ReplaceCurrentNode<SwitchStatement> implements TipperCategory.Sorting {
   @Override public ASTNode replacement(final SwitchStatement s) {
-    final List<SwitchBranch> b = SwitchBranch.intoBranches(s);
-    int ind = -1;
-    for (int ¢ = 0; ¢ < b.size() - 1; ++¢)
-      if (!b.get(¢).compareTo(b.get(¢ + 1))) {
-        ind = ¢;
-        break;
+    final List<SwitchBranch> $ = SwitchBranch.intoBranches(s);
+    for (int ¢ = 0; ¢ < $.size() - 1; ++¢)
+      if (!$.get(¢).compareTo($.get(¢ + 1))) {
+        SwitchBranch tmp = $.get(¢ + 1);
+        $.set(¢ + 1, $.get(¢));
+        $.set(¢, tmp);
+        return SwitchBranch.makeSwitchStatement($, step.expression(s), s.getAST());
       }
-    if (ind < 0)
-      return null;
-    final SwitchStatement $ = s.getAST().newSwitchStatement();
-    $.setExpression(copy.of(step.expression(s)));
-    final List<Statement> l = step.statements($);
-    for (int ¢ = 0; ¢ < b.size(); ++¢)
-      if (¢ != ind)
-        b.get(¢).addAll(l);
-      else {
-        b.get(¢ + 1).addAll(l);
-        b.get(¢++).addAll(l);
-      }
-    return $;
+    return null;
   }
 
   @Override public String description(@SuppressWarnings("unused") final SwitchStatement __) {
     return "Sort switch branches";
-  }
-}
-
-class SwitchBranch {
-  List<SwitchCase> cases;
-  List<Statement> statements;
-  int hasDefault;
-  int numOfStatements;
-  int numOfNodes;
-  int depth;
-  int sequencerLevel;
-
-  public SwitchBranch(final List<SwitchCase> cases, final List<Statement> statements) {
-    this.cases = cases;
-    this.statements = statements;
-    hasDefault = numOfNodes = numOfStatements = depth = sequencerLevel = -1;
-  }
-  
-  boolean hasDefault() {
-    if(hasDefault == -1) {
-      hasDefault = 0;
-      for(SwitchCase ¢ : cases)
-        if(¢.isDefault()) {
-          hasDefault = 1;
-          break;
-        }
-    }
-    return hasDefault == 1;
-  }
-  
-  int depth() {
-    if (depth < 0)
-      depth = metrics.height(statements, 0);
-    return depth;
-  }
-
-  int statementsNum() {
-    if (numOfStatements < 0)
-      numOfStatements = metrics.countStatements(statements);
-    return numOfStatements;
-  }
-
-  int nodesNum() {
-    if (numOfNodes < 0)
-      numOfNodes = metrics.nodes(statements);
-    return numOfNodes;
-  }
-
-  int casesNum() {
-    return cases.size();
-  }
-
-  int sequencerLevel() {
-    if(sequencerLevel < 0) {
-      int th = metrics.countStatementsOfType(statements, ASTNode.THROW_STATEMENT);
-      int re = metrics.countStatementsOfType(statements, ASTNode.RETURN_STATEMENT);
-      int br = metrics.countStatementsOfType(statements, ASTNode.BREAK_STATEMENT);
-      int co = metrics.countStatementsOfType(statements, ASTNode.CONTINUE_STATEMENT);
-      int sum = th+re+br+co;
-      assert sum > 0;
-      sequencerLevel = sum > th && sum > re && sum > br && sum > co ? 0 : th > 0 ? 1 : re > 0 ? 2 : br > 0 ? 3 : 4;
-    }
-    return sequencerLevel;
-  }
-
-  /** @param ¢
-   * @return returns 1 if _this_ has better metrics than b (i.e should come before b in the switch), -1 otherwise
-   */
-  boolean compare(final SwitchBranch ¢) {
-    if (hasDefault())
-      return false;
-    if (¢.hasDefault())
-      return true;
-    if (sequencerLevel() > 0 && ¢.sequencerLevel() > 0) {
-      if (sequencerLevel() > ¢.sequencerLevel())
-        return false;
-      if (sequencerLevel() < ¢.sequencerLevel())
-        return true;
-    }
-    return depth() < ¢.depth() || statementsNum() < ¢.statementsNum() || nodesNum() < ¢.nodesNum() || casesNum() < ¢.casesNum();
-  }
-  
-  boolean compareTo(final SwitchBranch ¢) {
-    return compare(¢) || (!¢.compare(this) && this.hashCode() < ¢.hashCode());
-  }
-
-  void addAll(final List<Statement> ss) {
-    for (final SwitchCase ¢ : cases)
-      ss.add(copy.of(¢));
-    for (final Statement ¢ : statements)
-      ss.add(copy.of(¢));
-  }
-  
-  @SuppressWarnings("null")
-  static List<SwitchBranch> intoBranches(final SwitchStatement n) {
-    List<SwitchCase> c = null;
-    List<Statement> s = null;
-    final List<SwitchBranch> $ = new ArrayList<>();
-    List<Statement> l = step.statements(n);
-    boolean nextBranch = true;
-    assert iz.switchCase(lisp.first(l));
-    for(int ¢ = 0; ¢ < l.size()-1; ++¢) {
-      if(nextBranch) {
-        c = new ArrayList<>();
-        s = new ArrayList<>();
-        $.add(new SwitchBranch(c, s));
-        nextBranch = false;
-        while(iz.switchCase(l.get(¢)) && ¢ < l.size()-1)
-          c.add(az.switchCase(l.get(¢++)));
-        if(¢ >= l.size()-1)
-          break;
-      }
-      if(iz.switchCase(l.get(¢+1)) && iz.sequencerComplex(l.get(¢)))
-        nextBranch = true;
-      s.add(l.get(¢));
-    }
-    if (!iz.switchCase(lisp.last(l)))
-      s.add(lisp.last(l));
-    else {
-      if (!s.isEmpty()) {
-        c = new ArrayList<>();
-        s = new ArrayList<>();
-        $.add(new SwitchBranch(c, s));
-      }
-      c.add(az.switchCase(lisp.last(l)));
-      s.add(n.getAST().newBreakStatement());
-    }
-    return $;
   }
 }
