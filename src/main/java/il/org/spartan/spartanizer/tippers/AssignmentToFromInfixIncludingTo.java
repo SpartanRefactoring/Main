425b6764bbcf5d435e57ace3f7574e0b38525740
dormaayn <dor.d.ma@gmail.com>
2017-01-16 13:06:08 +0200

    Fixed #1013

--- src/main/java/il/org/spartan/spartanizer/tippers/AssignmentToFromInfixIncludingTo.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/AssignmentToFromInfixIncludingTo.java
@@ -45,6 +45,9 @@ public final class AssignmentToFromInfixIncludingTo extends ReplaceCurrentNode<A
   }
 
   private static ASTNode replacement(final Expression to, final InfixExpression from) {
+    if(iz.arrayAccess(to)){
+      return null;
+    }
     if (!sideEffects.free(to))
       return null;
     final Expression $ = reduce(from, to);
