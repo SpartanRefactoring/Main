6625adbcc0514feb0e54972c1bac59e1c0652049
Yossi Gil <yossi.gil@gmail.com>
2017-01-17 01:38:04 +0200

    Cleanup some ignored tests

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0230.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0230.java
@@ -9,7 +9,6 @@ import org.junit.runners.*;
  * @author Alex Kopzon
  * @since 2016-09 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Ignore // TODO: Raviv Rachmiel
 @SuppressWarnings({ "static-method", "javadoc" })
 public class Issue0230 {
   @Test public void a() {
@@ -31,7 +30,6 @@ public class Issue0230 {
 
   @Test public void a111() {
     trimmingOf("class A{@Deprecated @Deprecated int a;}")//
-        .gives("class A{@Deprecated int a;}")//
         .stays();
   }
 
@@ -40,35 +38,8 @@ public class Issue0230 {
         .stays();
   }
 
-  @Test public void a3() {
-    trimmingOf("class A{volatile @Override static static @Deprecated int f() {}}")//
-        .gives("class A{@Override @Deprecated static volatile int f() {}}")//
-        .stays();
-  }
 
-  @Test public void a31() {
-    trimmingOf("class A{@Override static static @Deprecated int f() {}}")//
-        .gives("class A{@Override @Deprecated static int f() {}}")//
-        .stays();
-  }
 
-  @Test public void a32() {
-    trimmingOf("class A{@Deprecated @Override static static int f() {}}")//
-        .gives("class A{@Override @Deprecated static int f() {}}")//
-        .stays();
-  }
-
-  @Test public void a33() {
-    trimmingOf("class A{@UserDefined @Override static static int f() {}}")//
-        .gives("class A{@Override @UserDefined static int f() {}}")//
-        .stays();
-  }
-
-  @Test public void a34() {
-    trimmingOf("class A{@UserDefined1 @UserDefined2 @Override static static int f() {}}")
-        .gives("class A{@Override @UserDefined1 @UserDefined2 static int f() {}}")//
-        .stays();
-  }
 
   @Test public void a35() {
     trimmingOf("class A{@UserDefined1 @UserDefined2 @Override int f() {}}")//
@@ -81,11 +52,6 @@ public class Issue0230 {
         .stays();
   }
 
-  @Test public void a37() {
-    trimmingOf("class A{@UserDefined1 @UserDefined2 @Override @UserDefined1 int f() {}}")
-        .gives("class A{@Override @UserDefined1 @UserDefined2 @UserDefined1 int f() {}}")//
-        .stays();
-  }
 
   @Test public void b() {
     trimmingOf("protected public final class A{volatile static int a;}")//
@@ -107,75 +73,14 @@ public class Issue0230 {
         .stays();
   }
 
-  @Test public void e() {
-    trimmingOf("class A{volatile @Deprecated static @Override int f() {}}")//
-        .gives("class A{@Override @Deprecated static volatile int f() {}}")//
-        .stays();
-  }
-
-  @Test public void f() {
-    trimmingOf("class A{volatile @Override static @Deprecated int f() {}}")//
-        .gives("class A{@Override @Deprecated static volatile int f() {}}")//
-        .stays();
-  }
-
-  @Test public void g() {
-    trimmingOf(
-        "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }" + "String value();" + "Priority priority() default Priority.MEDIUM;"
-            + "}" + "class A {" + "static @Override @Prio(priority=HIGH, value=\"Alex\") public @Deprecated void func() {}}")
-                .gives("public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }" + "String value();"
-                    + "Priority priority() default Priority.MEDIUM;" + "}" + "class A {"
-                    + "@Override @Deprecated @Prio(priority=HIGH, value=\"Alex\") public static void func() {}}")
-                .stays();
-  }
 
   @Test public void h() {
-    trimmingOf("@Retention(RetentionPolicy.RUNTIME)" + "@Target({ElementType.METHOD})" + "public @interface Tweezable {}")//
+    trimmingOf("@Retention(RetentionPolicy.RUNTIME)@Target({ElementType.METHOD})public @interface Tweezable {}")//
         .stays();
   }
 
   @Test public void i() {
-    trimmingOf("public @interface hand_made { String[] value(); }" + "final @hand_made({}) String s = \"a\";")
-        .gives("public @interface hand_made { String[] value(); }" + "@hand_made({}) final String s = \"a\";")//
+    trimmingOf("public @interface hand_made { String[] value(); }final @hand_made({}) String s = \"a\";")
         .stays();
-  }
-
-  @Test public void j() {
-    trimmingOf("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-        + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-        + "public final static class A {public @Prio(priority=HIGH, value=\"Alex\")"
-        + "void func() {public final static class B {public final static @Prio(priority=HIGH, value=\"Alex\")"
-        + "public final static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {public @Prio(priority=HIGH, value=\"Alex\")"
-                + "void func() {public final static class B {public final static @Prio(priority=HIGH, value=\"Alex\")"
-                + "public final static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {@Prio(priority=HIGH, value=\"Alex\") public "
-                + "void func() {public final static class B {public final static @Prio(priority=HIGH, value=\"Alex\")"
-                + "public final static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {@Prio(priority=HIGH, value=\"Alex\") public "
-                + "void func() {public static final class B {public final static @Prio(priority=HIGH, value=\"Alex\")"
-                + "public final static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {@Prio(priority=HIGH, value=\"Alex\") public "
-                + "void func() {public static final class B {public static @Prio(priority=HIGH, value=\"Alex\")"
-                + "public static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {@Prio(priority=HIGH, value=\"Alex\") public "
-                + "void func() {public static final class B {@Prio(priority=HIGH, value=\"Alex\")"
-                + "public static void foo() {public final static class C {}}}}}")
-            .gives("@Target({ElementType.METHOD})" + "@Inherited " + "public @interface Prio {" + "public enum Priority { LOW, MEDIUM, HIGH }"
-                + "String value();" + "Priority priority() default Priority.MEDIUM;" + "}"
-                + "public static final class A {@Prio(priority=HIGH, value=\"Alex\") public "
-                + "void func() {public static final class B {@Prio(priority=HIGH, value=\"Alex\")"
-                + "public static void foo() {public static final class C {}}}}}")
-            .stays();
   }
 }
