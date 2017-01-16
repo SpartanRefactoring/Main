ec8222cd2164bc170a6ccbb61620241f67db02c4
Yossi Gil <yossi.gil@gmail.com>
2017-01-17 01:52:45 +0200

    More intelij

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/patterns/GeneralizedSwitchTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/patterns/GeneralizedSwitchTest.java
@@ -13,7 +13,7 @@ import il.org.spartan.spartanizer.research.nanos.*;
 public class GeneralizedSwitchTest {
   @Test public void iff() {
     trimmingOf("if(x == 0) d1(); else if(x == 1) d2(); else d3();")//
-        .using(IfStatement.class, new GeneralizedSwitch<IfStatement>())//
+        .using(IfStatement.class, new GeneralizedSwitch<>())//
         .gives("{holds(λ->x==λ).on(0,()->d1()).on(1,()->d2()).elze(()->d3());}")//
         .gives("holds(λ->x==λ).on(0,()->d1()).on(1,()->d2()).elze(()->d3());")//
         .stays();
@@ -21,14 +21,14 @@ public class GeneralizedSwitchTest {
 
   @Test public void ternary() {
     trimmingOf("d = x == 0 ? d1() : x == 1 ? d2() : d3();")//
-        .using(ConditionalExpression.class, new GeneralizedSwitch<ConditionalExpression>())//
+        .using(ConditionalExpression.class, new GeneralizedSwitch<>())//
         .gives("d = holds(λ->x==λ).on(0,()->d1()).on(1,()->d2()).elze(()->d3());")//
         .stays();
   }
 
   @Test public void ternary2() {
     trimmingOf("d = x(y+19) > a(b) ? d1() : z(w+17) > a(b) ? d2() : d3();")//
-        .using(ConditionalExpression.class, new GeneralizedSwitch<ConditionalExpression>())//
+        .using(ConditionalExpression.class, new GeneralizedSwitch<>())//
         .gives("d = holds(λ-> λ > a(b)).on(()->x(y+19),()->d1()).on(()->z(w+17),()->d2()).elze(()->d3());")//
         .stays();
   }
