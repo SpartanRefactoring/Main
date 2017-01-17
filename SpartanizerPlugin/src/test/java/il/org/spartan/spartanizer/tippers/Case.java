7ebb1042b2c9b392d8c5a439468709cfa42a7ca1
Yossi Gil <yossi.gil@gmail.com>
2017-01-17 21:55:29 +0200

    Cleanup

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Case.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Case.java
@@ -2,15 +2,11 @@ package il.org.spartan.spartanizer.tippers;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
 
-/**
- * Represents a test case, in conjunction with {@link ReflectiveTester}
+/** Represents a test case, in conjunction with {@link ReflectiveTester}
  * @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
- * @since 2017-01-17
- */
+ * @since 2017-01-17 */
 public interface Case {
-
   void after();
 
   void before();
-  
 }
