cd12aece9ae5bea655967822327d33ce7c7c6984
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-17 20:48:41 +0200

    no else for returnTernaryExpander

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/ReturnTernaryExpander.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/ReturnTernaryExpander.java
@@ -1,52 +1,53 @@
 package il.org.spartan.bloater.bloaters;
 
 import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** converts (a?b:c;) to (if(a) b; else c;) relevant to return <ternary> also
- * relevant for return (<ternary>) Issue #883 {@link Issue883}
+ * relevant for return (<ternary>) Issue #883 {@link Issue0883}
  * @author Raviv Rachmiel
+ * @author Yuval Simon
  * @since 03-12-16 */
-public class ReturnTernaryExpander extends ReplaceCurrentNode<ReturnStatement> implements TipperCategory.Expander {
-  private static ASTNode innerReturnReplacement(final Expression x, final Statement s) {
-    ConditionalExpression ¢;
-    if (!(x instanceof ParenthesizedExpression))
-      ¢ = az.conditionalExpression(x);
-    else {
-      final Expression unpar = expression(az.parenthesizedExpression(x));
-      if (!(unpar instanceof ConditionalExpression))
-        return null;
-      ¢ = az.conditionalExpression(unpar);
-      if (¢ == null)
-        return null;
-    }
-    final IfStatement $ = s.getAST().newIfStatement();
-    $.setExpression(copy.of(expression(¢)));
-    final ReturnStatement then = ¢.getAST().newReturnStatement();
-    then.setExpression(copy.of(¢.getThenExpression()));
-    $.setThenStatement(copy.of(az.statement(then)));
-    final ReturnStatement elze = ¢.getAST().newReturnStatement();
-    elze.setExpression(copy.of(¢.getElseExpression()));
-    $.setElseStatement(copy.of(az.statement(elze)));
-    return $;
+public class ReturnTernaryExpander extends CarefulTipper<ReturnStatement> implements TipperCategory.Expander {
+  @Override public Tip tip(ReturnStatement x) {
+    return new Tip(description(x), x, getClass()) {
+      @Override public void go(ASTRewrite r, TextEditGroup g) {
+        Expression ee = expression(x);
+        ConditionalExpression e = az.conditionalExpression(!iz.parenthesizedExpression(ee) ? ee : expression(ee));
+        Expression cond = expression(e);
+        AST a = x.getAST();
+        ReturnStatement whenTrue = a.newReturnStatement();
+        ReturnStatement whenFalse = a.newReturnStatement();
+        whenTrue.setExpression(copy.of(then(e)));
+        whenFalse.setExpression(copy.of(elze(e)));
+        IfStatement f = a.newIfStatement();
+        f.setExpression(copy.of(cond));
+        f.setThenStatement(copy.of(whenTrue));
+        ListRewrite l = r.getListRewrite(x.getParent(), iz.block(x.getParent()) ? Block.STATEMENTS_PROPERTY : SwitchStatement.STATEMENTS_PROPERTY);
+        l.insertAfter(whenFalse, x, g);
+        l.replace(x, f, g);
+      }
+    };
   }
 
-  private static ASTNode replaceReturn(final Statement ¢) {
-    final ReturnStatement $ = az.returnStatement(¢);
-    return $ == null || !($.getExpression() instanceof ConditionalExpression) && !(expression($) instanceof ParenthesizedExpression) ? null
-        : innerReturnReplacement(expression($), ¢);
-  }
 
-  @Override public ASTNode replacement(final ReturnStatement ¢) {
-    return replaceReturn(¢);
+  @Override protected boolean prerequisite(ReturnStatement $) {
+    if($ == null)
+      return false;
+    Expression e = expression($);
+    return (iz.block($.getParent()) || iz.switchStatement($.getParent())) && (iz.conditionalExpression(e)
+        || (iz.parenthesizedExpression(e) && iz.conditionalExpression(expression(az.parenthesizedExpression(e)))));
   }
-
+  
   @Override public String description(@SuppressWarnings("unused") final ReturnStatement __) {
     return "expanding a ternary operator to a full if-else statement";
   }
