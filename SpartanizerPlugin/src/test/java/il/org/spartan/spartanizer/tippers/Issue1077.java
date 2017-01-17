8aa4b15b8cb556837008ec4f4fd56b27fef6ccc6
Yossi Gil <yossi.gil@gmail.com>
2017-01-17 14:36:09 +0200

    Cleanup

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1077.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1077.java
@@ -7,28 +7,33 @@ import org.junit.*;
 @SuppressWarnings("static-method")
 public class Issue1077 {
   @Test public void t1() {
-    trimmingOf("@SuppressWarnings(\"unchecked\") static boolean sequencerComplex(final ASTNode ¢, int type) {"
-        + "if (¢ == null)"
-        + "return false;"
-        + "switch (¢.getNodeType()) {"
-        + "case ASTNode.IF_STATEMENT:"
-        + "final IfStatement $ = (IfStatement) ¢;"
-        + "return sequencerComplex($.getThenStatement(),type) && sequencerComplex($.getElseStatement(),type);"
-        + "case ASTNode.BLOCK:"
-        + "for (final Statement s : (List<Statement>) ((Block) ¢).statements())"
-        + "if (sequencerComplex(s,type))"
-        + "return true;"
-        + "return false;"
-        + "default:"
-        + "return sequencer(¢,type);"
-        + "}"
-        + "}").stays();
+    trimmingOf(//
+        "@SuppressWarnings(\"unchecked\") static boolean sequencerComplex(final ASTNode ¢, int type) {"//
+            + "if (¢ == null)"//
+            + "return false;"//
+            + "switch (¢.getNodeType()) {" //
+            + "case ASTNode.IF_STATEMENT:" //
+            + "final IfStatement $ = (IfStatement) ¢;" //
+            + "return sequencerComplex($.getThenStatement(),type) && sequencerComplex($.getElseStatement(),type);" //
+            + "case ASTNode.BLOCK:" //
+            + "for (final Statement s : (List<Statement>) ((Block) ¢).statements())" //
+            + "if (sequencerComplex(s,type))" //
+            + "return true;" //
+            + "return false;" //
+            + "default:" //
+            + "return sequencer(¢,type);" //
+            + "}" //
+            + "}")//
+    .stays(); //
   }
+
   @Test public void t2() {
-    trimmingOf("@SuppressWarnings(\"unchecked\") static boolean sequencerComplex(final ASTNode ¢, int type) {"
-        + "for (final Statement s : (List<Statement>) ((Block) ¢).statements())"
-        + "if (sequencerComplex(s))"
-        + "return true;"
-        + "}").stays();
+    trimmingOf(//
+        "@SuppressWarnings(\"unchecked\") static boolean sequencerComplex(final ASTNode ¢, int type) {" //
+            + "for (final Statement s : (List<Statement>) ((Block) ¢).statements())" //
+            + "if (sequencerComplex(s))" //
+            + "return true;" //
+            + "}")//
+    .stays();
   }
 }
