c5f2989b752eb05662494f090dbc3977488cfe7c
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-19 01:57:07 +0200

    #1013 bug fix

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/InliningPrefix.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/InliningPrefix.java
@@ -7,6 +7,8 @@ import org.eclipse.jdt.core.dom.PostfixExpression.*;
 import org.eclipse.jdt.core.dom.rewrite.*;
 import org.eclipse.text.edits.*;
 
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
@@ -35,7 +37,7 @@ public final class InliningPrefix extends EagerTipper<ArrayAccess> implements Ti
   }
 
   @Override public Tip tip(final ArrayAccess a) {
-    return checkInput(a) ? null : new Tip(description(a), a, getClass()) {
+    return checkInput(a) || !prerequisite(a) ? null : new Tip(description(a), a, getClass()) {
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final PostfixExpression newpost = a.getAST().newPostfixExpression();
         newpost.setOperand(copy.of(a.getIndex()));
@@ -63,5 +65,14 @@ public final class InliningPrefix extends EagerTipper<ArrayAccess> implements Ti
       if (iz.arrayAccess(¢))
         ++$;
     return $ != 1;
+  }
+  
+  protected static boolean prerequisite(final ArrayAccess a) {
+    if (a == null)
+      return false;
+    final SimpleName $ = az.simpleName(a.getIndex());
+    Expression bb = az.expression(a.getParent());
+    return bb != null && $ != null && (iz.assignment(bb) && (!left(az.assignment(bb)).equals(a) || !iz.containsName($, right(az.assignment(bb))))
+        || iz.infixExpression(bb) && (!left(az.infixExpression(bb)).equals(a) || !iz.containsName($, right(az.infixExpression(bb)))));
   }
 }
--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1013.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1013.java
@@ -9,7 +9,11 @@ import org.junit.*;
  * @since 2017-01-16 */
 @SuppressWarnings("static-method")
 public class Issue1013 {
-  @Test public void test0() {
+  @Test public void t1() {
     trimmingOf("a[x] = a[x] + x++;++x;").stays();
+  }
+  
+  @Test public void t2() {
+    trimmingOf("a[x] = x;++x;").stays();
   }
 }
