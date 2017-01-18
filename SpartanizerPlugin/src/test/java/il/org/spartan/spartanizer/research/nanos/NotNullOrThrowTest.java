ec030576b9563b84f7282121efaf9543b5e11ce2
orimarco <marcovitch.ori@gmail.com>
2017-01-18 18:50:38 +0200

    rename package

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/NotNullOrThrowTest.java
@@ -0,0 +1,25 @@
+package il.org.spartan.spartanizer.research.nanos;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+
+/** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-01-04 */
+@SuppressWarnings("static-method")
+public class NotNullOrThrowTest {
+  @Test public void a() {
+    trimmingOf("if(x == null) throw new Watever();")//
+        .using(IfStatement.class, new NotNullOrThrow())//
+        .gives("notNull(x).orThrow(()->new Watever());")//
+        .stays();
+  }
+
+  @Test public void b() {
+    trimmingOf("if(x == null) throw new Watever(with(This, and, zis()));")//
+        .using(IfStatement.class, new NotNullOrThrow())//
+        .gives("notNull(x).orThrow(()->new Watever(with(This,and,zis())));")//
+        .stays();
+  }
+}
