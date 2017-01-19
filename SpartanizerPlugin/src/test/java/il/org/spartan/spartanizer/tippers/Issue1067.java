1f7da05f0be398986caf4e196b6045885b2856f2
YuvalSimon <yuvaltechnion@gmail.com>
2017-01-19 19:44:39 +0200

    partial fix #1067

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1067.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue1067.java
@@ -20,4 +20,42 @@ public class Issue1067 {
   @Test public void t2() {
     trimmingOf("int zero = 1, result = 8;" + "++result;").stays();
   }
+  
+  static class Issue0856 {
+    @Test public void e() {
+      trimmingOf("  final InflaterListener il = (InflaterListener) ((TypedListener) l).getEventListener();" + //
+          "il.finalize(); " + //
+          "return 0;")//
+              .gives(//
+                  "((InflaterListener) ((TypedListener) l).getEventListener()).finalize();" + //
+                      "return 0;" //
+              )//
+              .stays();
+    }
+  }
+  
+  static class Issue0364 {
+    @Test public void notTerminating() {
+      trimmingOf("void f() {\n" + "  String[] x = {\"\"};" + "  g(x);" + "  h();" + "}")
+          .gives("void f() {\n" + "  g(new String[] {\"\"});" + "  h();" + "}");
+    }
+  }
+  
+  static class Version230 {
+    @Test public void inlineArrayInitialization1() {
+      trimmingOf("public void multiDimensionalIntArraysAreEqual() {\n" //
+          + "  int[][] int1 = {{1, 2, 3}, {4, 5, 6}};\n" //
+          + "  int[][] int2 = {{1, 2, 3}, {4, 5, 6}};\n" //
+          + "  assertArrayEquals(int1, int2);\n" //
+          + "}")
+              .gives("public void multiDimensionalIntArraysAreEqual() {\n" //
+                  + "  int[][] int1 = {{1, 2, 3}, {4, 5, 6}}" //
+                  + "  , int2 = {{1, 2, 3}, {4, 5, 6}};\n" //
+                  + "  assertArrayEquals(int1, int2);\n" //
+                  + "}")
+              .gives("public void multiDimensionalIntArraysAreEqual() {\n" //
+                  + "  assertArrayEquals(new int[][]{{1,2,3},{4,5,6}},new int[][]{{1,2,3},{4,5,6}});\n" //
+                  + "}");
+    }
+  }
 }
