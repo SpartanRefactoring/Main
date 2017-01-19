ca811e117184aa3688f0e4b1058451c6e64bbb4e
Yossi Gil <yossi.gil@gmail.com>
2017-01-19 22:46:16 +0200

    Remove empty javadoc

--- src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitializerStatementTerminatingScope.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitializerStatementTerminatingScope.java
@@ -80,7 +80,6 @@ public final class DeclarationInitializerStatementTerminatingScope extends $Vari
     return $;
   }
 
-  /**  */
   static boolean isNotAllowedOpOnPrimitive(final VariableDeclarationFragment f, final Statement nextStatement) {
     if (!iz.literal(f.getInitializer()) || !iz.expressionStatement(nextStatement))
       return false;
