e837ef088720454e5923ba47172485e5bfe10a2f
orimarco <marcovitch.ori@gmail.com>
2017-01-19 18:29:42 +0200

    Select is private case of Collect

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/AggregateTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/AggregateTest.java
@@ -5,6 +5,8 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
+import il.org.spartan.spartanizer.research.nanos.deprecated.*;
+
 /** @author Ori Marcovitch
  * @since 2016 */
 @SuppressWarnings("static-method")
