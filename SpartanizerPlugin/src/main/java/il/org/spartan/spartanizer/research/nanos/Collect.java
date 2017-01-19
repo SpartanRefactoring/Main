ef968d667661f4d5be19fbd8cc350b1114fc7323
orimarco <marcovitch.ori@gmail.com>
2017-01-19 11:29:03 +0200

    Collect++

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/Collect.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/Collect.java
@@ -28,36 +28,50 @@ public class Collect extends NanoPatternTipper<EnhancedForStatement> {
       add(statementsPattern("$T1 $N1 = new $T2(); for($T3 $N2 : $X1) $N1.add($N2);", //
           "$T1 $N1 = ($X1).stream().collect(Collectors.toList());", //
           "Go Fluent: filter pattern"));
-      add(statementsPattern("$T1 $N1 = new $T2(); for($T3 $N2 : $X1) $N1.add($X2);", //
-          "$T1 $N1 = ($X1).stream().map($N2 -> $X2).collect(Collectors.toList());", //
+      add(statementsPattern("$T1 $N1 = new $T2(); for($T3 $N2 : $X1) $N1.add($X3);", //
+          "$T1 $N1 = ($X1).stream().map($N2 -> $X3).collect(Collectors.toList());", //
+          "Go Fluent: filter pattern"));
+    }
+  };
+  static final List<UserDefinedTipper<EnhancedForStatement>> tippers2 = new ArrayList<UserDefinedTipper<EnhancedForStatement>>() {
+    static final long serialVersionUID = 1L;
+    {
+      add(patternTipper("for($T1 $N2 : $X1) if($X2) $N1.add($N2);", //
+          "$N1.addAll(($X1).stream().filter($N2 -> $X2).collect(Collectors.toList()));", //
+          "Go Fluent: filter pattern"));
+      add(patternTipper("for($T1 $N2 : $X1) if($X2) $N1.add($X3);", //
+          "$N1.addAll(($X1).stream().filter($N2 -> $X2).map($N2->$X3).collect(Collectors.toList()));", //
+          "Go Fluent: filter pattern"));
+      add(patternTipper("for($T1 $N2 : $X1) $N1.add($N2);", //
+          "$N1.addAll(($X1).stream().collect(Collectors.toList()));", //
+          "Go Fluent: filter pattern"));
+      add(patternTipper("for($T1 $N2 : $X1) $N1.add($X3);", //
+          "$N1.addAll(($X1).stream().map($N2->$X3).collect(Collectors.toList()));", //
           "Go Fluent: filter pattern"));
     }
   };
 
   public static class defender extends NanoPatternTipper<EnhancedForStatement> {
-    private static final List<UserDefinedTipper<EnhancedForStatement>> patterns = new ArrayList<UserDefinedTipper<EnhancedForStatement>>() {
-      static final long serialVersionUID = 1L;
-      {
-        add(patternTipper("for($T $N1 : $X1) if($X2) $N2.add($X3);", "", ""));
-        add(patternTipper("for($T $N1 : $X1) $N2.add($X2);", "", ""));
-      }
-    };
-
     @Override protected Tip pattern(final EnhancedForStatement ¢) {
-      return firstTip(patterns, ¢);
+      return firstTip(tippers2, ¢);
     }
 
     @Override public boolean canTip(final EnhancedForStatement ¢) {
-      return anyTips(patterns, ¢);
+      return anyTips(tippers2, ¢);
     }
   }
 
   @Override public boolean canTip(final EnhancedForStatement ¢) {
-    return anyTips(tippers, az.block(parent(¢)));
+    return anyTips(tippers, az.block(parent(¢)))//
+        || anyTips(tippers2, ¢);
   }
 
-  @Override public Tip pattern(final EnhancedForStatement ¢) {
-    return firstTip(tippers, az.block(parent(¢)));
+  @Override public Tip pattern(final EnhancedForStatement $) {
+    try {
+      return firstTip(tippers, az.block(parent($)));
+    } catch (@SuppressWarnings("unused") NoSuchElementException __) {
+      return firstTip(tippers2, $);
+    }
   }
 
   @Override public Category category() {
