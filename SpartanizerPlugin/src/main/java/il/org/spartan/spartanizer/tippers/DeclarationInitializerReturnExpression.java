93f72e113dd06bf9c68cedd88534e4a537143a5c
Yossi Gil <yossi.gil@gmail.com>
2017-01-20 02:03:48 +0200

    Resolves #1099

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitializerReturnExpression.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitializerReturnExpression.java
@@ -9,7 +9,6 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.engine.Inliner.*;
-import il.org.spartan.spartanizer.java.*;
 
 /** convert <code>int a = 3;return a;</code> into <code>return a;</code>
  * @author Yossi Gil
@@ -18,24 +17,24 @@ public final class DeclarationInitializerReturnExpression extends $VariableDecla
   @Override public String description(final VariableDeclarationFragment ¢) {
     return "Eliminate local " + ¢.getName() + " and inline its value into the expression of the subsequent return statement";
   }
-
+  
   @Override protected ASTRewrite go(final ASTRewrite $, final VariableDeclarationFragment f, final SimpleName n, final Expression initializer,
       final Statement nextStatement, final TextEditGroup g) {
-    if (initializer == null || haz.annotation(f))
-      return null;
-    final ReturnStatement s = az.returnStatement(nextStatement);
-    if (s == null)
-      return null;
-    final Expression newReturnValue = s.getExpression();
-    if (newReturnValue == null)
-      return null;
-    final InlinerWithValue i = new Inliner(n, $, g).byValue(initializer);
-    if (wizard.same(n, newReturnValue) || !i.canSafelyInlineinto(newReturnValue)
-        || i.replacedSize(newReturnValue) - eliminationSaving(f) - metrics.size(newReturnValue) > 0)
-      return null;
-    $.replace(s.getExpression(), newReturnValue, g);
-    i.inlineInto(newReturnValue);
-    eliminate(f, $, g);
-    return $;
-  }
+        if (forbidden(f, initializer) || usedInSubsequentInitializers(f, n))
+          return null;
+        final ReturnStatement s = az.returnStatement(nextStatement);
+        if (s == null)
+          return null;
+        final Expression newReturnValue = s.getExpression();
+        if (newReturnValue == null)
+          return null;
+        final InlinerWithValue i = new Inliner(n, $, g).byValue(initializer);
+        if (wizard.same(n, newReturnValue) || !i.canSafelyInlineinto(newReturnValue)
+            || i.replacedSize(newReturnValue) - eliminationSaving(f) - metrics.size(newReturnValue) > 0)
+          return null;
+        $.replace(s.getExpression(), newReturnValue, g);
+        i.inlineInto(newReturnValue);
+        eliminate(f, $, g);
+        return $;
+      }
 }
