84f1619303b789f9e721723001eab0e49bd83d9a
Ori Roth <ori.rothh@gmail.com>
2017-01-20 04:17:37 +0200

    test for issue 954

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue0954.java
@@ -0,0 +1,24 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+import org.junit.*;
+
+/** A test case for {@link DeclarationInlineIntoNext}. See issue #954.
+ * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
+ * @since 2017-01-20 [[SuppressWarningsSpartan]] */
+@SuppressWarnings("static-method")
+public class Issue0954 {
+  @Test public void a() {
+    trimmingOf("" //
+        + "void f() {\n" //
+        + "Object[] objectArray = {value};\n" //
+        + "String arrayString = Arrays.deepToString(objectArray);\n" //
+        + "anotherStatement();\n" //
+        + "}")
+            .gives("" //
+                + "void f() {\n" //
+                + "String arrayString = Arrays.deepToString(new Object[] {value});\n" //
+                + "anotherStatement();\n" //
+                + "}");
+  }
+}
