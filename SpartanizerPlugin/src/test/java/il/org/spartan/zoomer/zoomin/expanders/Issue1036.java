168328608fe2e63d84833d44cf44f0af25dd4cf8
dormaayn <dor.d.ma@gmail.com>
2017-01-20 23:54:18 +0200

    Fixed #1080

--- SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1036.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/Issue1036.java
@@ -11,12 +11,12 @@ import il.org.spartan.spartanizer.meta.*;
 /** Test class for issue #1036
  * @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
  * @since 2016-01-12 */
-@Ignore
 public class Issue1036 {
   @Test public void test0() {
     bloatingOf(new TestClass()) //
-        .givesWithBinding("public String check1(){" + "for(int i=1;i<children1.size();i++){" + "final String diff;diff=null;if($!=\"\"||diff==null)"
-            + "$=$;else $=diff;if(!$.equals(diff)&&!\"\".equals(diff))return null;}return \"\";}", "check1");
+        .givesWithBinding(
+            "@SuppressWarnings(\"null\")public String check1(){for(int i=1;i<children1.size();i++){final String diff=null;$=!Objects.equals($,\"\")||diff==null?$:diff;if(!$.equals(diff)&&!\"\".equals(diff))return null;}return \"\";}",
+            "check1");
   }
 
   class TestClass extends MetaFixture {
