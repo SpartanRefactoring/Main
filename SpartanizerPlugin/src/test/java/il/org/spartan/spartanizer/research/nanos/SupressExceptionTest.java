d7b236385413aa13175abffe98854a3872bf5d90
orimarco <marcovitch.ori@gmail.com>
2017-01-22 00:52:45 +0200

    Rename

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/SupressExceptionTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/SupressExceptionTest.java
@@ -8,14 +8,14 @@ import org.junit.*;
 /** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-05 */
 @SuppressWarnings("static-method")
-public class IgnoreExceptionTest {
+public class SupressExceptionTest {
   @Test public void a() {
     trimmingOf(//
         "try {" + //
             "    A.a(b).c().d(e -> f[g++]=h(e));" + //
             "  }" + //
             " catch (  B i) {}"//
-    ).using(CatchClause.class, new IgnoreException())//
+    ).using(CatchClause.class, new SupressException())//
         .gives("{try{{A.a(b).c().d(e->f[g++]=h(e));}}catch(B i){ignore();};}")//
         .gives("try{{A.a(b).c().d(e->f[g++]=h(e));}}catch(B i){ignore();}")//
         .gives("try{A.a(b).c().d(e->f[g++]=h(e));}catch(B i){ignore();}")//
@@ -26,7 +26,7 @@ public class IgnoreExceptionTest {
   @Test public void b() {
     trimmingOf("try{ thing(); } catch(A a){}catch(B b){}")//
         .gives("try{thing();}catch(B|A a){}")//
-        .using(CatchClause.class, new IgnoreException())//
+        .using(CatchClause.class, new SupressException())//
         .gives("{try{{thing();}}catch(B|A a){ignore();};}")//
         .gives("try{{thing();}}catch(B|A a){ignore();}")//
         .gives("try{thing();}catch(B|A a){ignore();}")//
