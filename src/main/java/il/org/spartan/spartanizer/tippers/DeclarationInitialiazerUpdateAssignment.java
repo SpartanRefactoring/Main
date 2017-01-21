8c4cb2c49a5e72cadda67dc5e013d94a1b58a58f
Yossi Gil <yossi.gil@gmail.com>
2017-01-21 12:54:23 +0200

    Recategorizing tippers

--- src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitialiazerUpdateAssignment.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/DeclarationInitialiazerUpdateAssignment.java
@@ -30,7 +30,7 @@ import il.org.spartan.spartanizer.engine.Inliner.*;
  *
  * @author Yossi Gil
  * @since 2015-08-07 */
-public final class DeclarationInitialiazerUpdateAssignment extends $VariableDeclarationFragementAndStatement implements TipperCategory.Collapse {
+public final class DeclarationInitialiazerUpdateAssignment extends $VariableDeclarationFragementAndStatement implements TipperCategory.Inlining {
   @Override public String description(final VariableDeclarationFragment ¢) {
     return "Consolidate declaration of " + ¢.getName() + " with its subsequent initialization";
   }
