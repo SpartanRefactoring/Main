e9b058eeea46bd9a803025e4c5da0925809be6fb
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-22 19:42:23 +0200

    fixing todos

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue0687.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue0687.java
@@ -1,5 +1,6 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static il.org.spartan.azzert.*;
 import static il.org.spartan.lisp.*;
 
 import java.util.*;
@@ -9,7 +10,6 @@ import org.junit.*;
 
 import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
-
 import il.org.spartan.spartanizer.utils.tdd.*;
 
 /** Tests for issue 687
@@ -29,16 +29,15 @@ public class Issue0687 {
   @Test public void testCheckNamesFineBlock() {
     // assuming we need to get all names in the block, including repetitions
     final List<Name> n = getAll.names((Block) wizard.ast("{a=1+1;b=2+3;System.out.println(a);c=2;c*=a;}"));
-    // TODO: Raviv Rachmiel use azzer.that(x, iz(y))
-    assert "a".equals(first(n) + "");
-    assert "b".equals(n.get(1) + "");
-    assert "System".equals(n.get(2) + "");
-    assert "out".equals(n.get(3) + "");
-    assert "println".equals(n.get(4) + "");
-    assert "a".equals(n.get(5) + "");
-    assert "c".equals(n.get(6) + "");
-    assert "c".equals(n.get(7) + "");
-    assert "a".equals(n.get(8) + "");
+    azzert.that(first(n), iz("a"));
+    azzert.that(n.get(1), iz("b"));
+    azzert.that(n.get(2), iz("System"));
+    azzert.that(n.get(3), iz("out"));
+    azzert.that(n.get(4), iz("println"));
+    azzert.that(n.get(5), iz("a"));
+    azzert.that(n.get(6), iz("c"));
+    azzert.that(n.get(7), iz("c"));
+    azzert.that(n.get(8), iz("a"));
   }
 
   @Test public void testCheckTwoNamesWithMoreThenOneLiteral() {
