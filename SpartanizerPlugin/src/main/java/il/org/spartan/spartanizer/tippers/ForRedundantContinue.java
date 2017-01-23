97d98449d93444d7867e2435e2f0fb065741f882
Doron Mehsulam <doronmmm@hotmail.com>
2017-01-23 15:22:24 +0200

    Todos for Doron #709

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/ForRedundantContinue.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/ForRedundantContinue.java
@@ -16,43 +16,37 @@ import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** removes continue in for loop if it's last statement in the loop.
+ * link {Issue0147}
  * @author Doron Meshulam
  * @since 2016-11-26 */
 public class ForRedundantContinue extends CarefulTipper<ForStatement>//
     implements TipperCategory.Shortcircuit {
   @Override public String description(final ForStatement ¢) {
-    return "Prune redundant " + lastStatement(¢);
+    return "Prune redundant " + wizard.lastStatement(¢);
   }
 
   @Override public String description() {
     return "Prune redundant continue";
   }
 
-  // TODO: Doron Meshulam: please use lisp.last --yg
-  // Also, move to class wizard
-  static Statement lastStatement(final ForStatement ¢) {
-    return !iz.block(body(¢)) ? body(¢) : last(statements(az.block(body(¢))));
-  }
-
   @Override public Tip tip(final ForStatement ¢) {
     return new Tip(description(¢), ¢, getClass()) {
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final Block b = az.block(step.body(¢));
-        if (b == null)
-          r.replace(lastStatement(¢), make.emptyStatement(¢), g);
-        else {
-          // TODO: Doron Meshulam: use list rewrite (search for code that does
-          // that) --yg
-          step.statements(b).remove(lastStatement(¢));
-          // TODO: Doron Meshulam: seems like a noop --yg
-          r.replace(b, copy.of(b), g);
+        final ListRewrite l = r.getListRewrite(¢.getParent(), Block.STATEMENTS_PROPERTY);
+        if (b == null) {
+          l.replace(wizard.lastStatement(¢), make.emptyStatement(¢), g);
+//          r.replace(wizard.lastStatement(¢), make.emptyStatement(¢), g);
+        } else {
+//          step.statements(b).remove(wizard.lastStatement(¢));
+          l.remove(wizard.lastStatement(¢), g);
         }
       }
     };
   }
 
   @Override public boolean prerequisite(final ForStatement ¢) {
-    final Statement s = lastStatement(¢);
+    final Statement s = wizard.lastStatement(¢);
     if (iz.continueStatement(s)) {
       final SimpleName n = label(az.continueStatement(s));
       if (n == null || iz.labeledStatement(parent(¢)) && n.getIdentifier().equals(((LabeledStatement) ¢.getParent()).getLabel().getIdentifier()))
