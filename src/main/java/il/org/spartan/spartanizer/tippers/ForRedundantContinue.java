1901cfa3271aefc0fb358961eb1f3f2a43a5204c
Doron Mehsulam <doronmmm@hotmail.com>
2017-01-23 16:18:02 +0200

    Spartanizing #709

--- src/main/java/il/org/spartan/spartanizer/tippers/ForRedundantContinue.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/ForRedundantContinue.java
@@ -13,8 +13,8 @@ import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/** removes continue in for loop if it's last statement in the loop.
- * link {Issue0147}
+/** removes continue in for loop if it's last statement in the loop. link
+ * {Issue0147}
  * @author Doron Meshulam
  * @since 2016-11-26 */
 public class ForRedundantContinue extends CarefulTipper<ForStatement>//
@@ -32,13 +32,10 @@ public class ForRedundantContinue extends CarefulTipper<ForStatement>//
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final Block b = az.block(step.body(¢));
         final ListRewrite l = r.getListRewrite(¢.getParent(), Block.STATEMENTS_PROPERTY);
-        if (b == null) {
-          l.replace(wizard.lastStatement(¢), make.emptyStatement(¢), g);
-//          r.replace(wizard.lastStatement(¢), make.emptyStatement(¢), g);
-        } else {
-//          step.statements(b).remove(wizard.lastStatement(¢));
+        if (b != null)
           l.remove(wizard.lastStatement(¢), g);
-        }
+        else
+          l.replace(wizard.lastStatement(¢), make.emptyStatement(¢), g);
       }
     };
   }
