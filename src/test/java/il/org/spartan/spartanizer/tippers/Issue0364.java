2970227587e9b83caacae9b78b59f1821b576590
Yossi Gil <yossi.gil@gmail.com>
2017-01-24 01:05:57 +0200

    Sync

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue0364.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue0364.java
@@ -5,43 +5,41 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-/** Unit test for
- * {@link FragmentInitializerStatementTerminatingScope}Inlining of
- * {@link ArrayInitializer}
+/** Unit test for {@link FragmentInitializerStatementTerminatingScope}Inlining
+ * of {@link ArrayInitializer}
  * @author Ori Roth
  * @since 2016 */
 @SuppressWarnings("static-method")
 public class Issue0364 {
   @Test public void emptyInitializer() {
-    trimmingOf("Object[] os = {};\n" + "System.out.println(os);").givesEither("System.out.println((new Object[] {}));",
-        "System.out.println(new Object[] {});");
+    trimmingOf("Object[] os = {};\nS.out.println(os);").givesEither("S.out.println((new Object[] {}));",
+        "S.out.println(new Object[] {});");
   }
 
   @Test public void realLifeExample() {
-    trimmingOf("if (opterr) {\n" + "  final Object[] msgArgs = { progname, Character.valueOf((char) c) + \"\" };\n"
-        + "  System.err.println(MessageFormat.format(_messages.getString(\"getopt.requires2\"), msgArgs));\n" + "}\n" + "X();")
+    trimmingOf("if (opterr) {\n  final Object[] msgArgs = { progname, Character.valueOf((char) c) + \"\" };\n"
+        + "  S.err.println(MessageFormat.format(_messages.getString(\"getopt.requires2\"), msgArgs));\n}\nX();")
             .gives("if (opterr) {\n"
-                + "  System.err.println(MessageFormat.format(_messages.getString(\"getopt.requires2\"), (new Object[] { progname, Character.valueOf((char) c) + \"\" })));\n"
-                + "}\n" + "X();");
+                + "  S.err.println(MessageFormat.format(_messages.getString(\"getopt.requires2\"), (new Object[] { progname, Character.valueOf((char) c) + \"\" })));\n"
+                + "}\nX();");
   }
 
   @Test public void notTerminating() {
-    trimmingOf("void f() {\n" + "  String[] x = {\"\"};" + "  g(x);" + "  h();" + "}")
-        .gives("void f() {\n" + "  g(new String[] {\"\"});" + "  h();" + "}");
+    trimmingOf("void f() {\n  String[] x = {\"\"};  g(x);  h();}").gives("void f() {\n  g(new String[] {\"\"});  h();}");
   }
 
   @Test public void notTerminatingRealWorld() {
-    trimmingOf("@Test public void failureCausesExitCodeOf1() throws Exception {\n"
-        + "  String[] cmd = {System.getProperty(\"java.home\") + File.separator + \"bin\"\n"
+    trimmingOf("@Test public void fz() throws Exception {\n"
+        + "  String[] cmd = {S.p(\"java.home\") + File.separator + \"bin\"\n"
         + "    + File.separator + \"java\", \"-cp\", getClass().getClassLoader().getResource(\".\").getFile()\n"
-        + "    + File.pathSeparator + System.getProperty(\"java.class.path\"), getClass().getName() + \"$Exit\"};\n"
-        + "  Process process = Runtime.getRuntime().exec(cmd);\n" + "  for (InputStream ¢ = process.getInputStream(); ¢.read() != -1;)\n" + "    ;\n"
-        + "  wizard.assertEquals(EXIT_CODE, process.waitFor());\n" + "}")
-            .gives("@Test public void failureCausesExitCodeOf1() throws Exception {\n"
-                + "  Process process = Runtime.getRuntime().exec(new String[] {System.getProperty(\"java.home\") + File.separator + \"bin\"\n"
+        + "    + File.pathSeparator + S.p(\"java.class.path\"), getClass().getName() + \"$Exit\"};\n"
+        + "  Process o = R.getR().exec(cmd);\n  for (I ¢ = o.getI(); ¢.read() != -1;)\n    ;\n"
+        + "  wizard.assertEquals(EXIT_CODE, o.waitFor());\n}")
+            .gives("@Test public void fz() throws Exception {\n"
+                + "  Process o = R.getR().exec(new String[] {S.p(\"java.home\") + File.separator + \"bin\"\n"
                 + "    + File.separator + \"java\", \"-cp\", getClass().getClassLoader().getResource(\".\").getFile()\n"
-                + "    + File.pathSeparator + System.getProperty(\"java.class.path\"), getClass().getName() + \"$Exit\"});\n"
-                + "  for (InputStream ¢ = process.getInputStream(); ¢.read() != -1;)\n" + "    ;\n"
-                + "  wizard.assertEquals(EXIT_CODE, process.waitFor());\n" + "}");
+                + "    + File.pathSeparator + S.p(\"java.class.path\"), getClass().getName() + \"$Exit\"});\n"
+                + "  for (I ¢ = o.getI(); ¢.read() != -1;)\n    ;\n"
+                + "  wizard.assertEquals(EXIT_CODE, o.waitFor());\n}");
   }
 }
