f562a7e1bcd40e658489b56f26049dd598f3a343
orimarco <marcovitch.ori@gmail.com>
2017-01-23 23:48:10 +0200

    TODOs

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/HoldsForAny.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/HoldsForAny.java
@@ -16,11 +16,14 @@ import il.org.spartan.spartanizer.research.nanos.common.*;
 /** @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-05 */
 public final class HoldsForAny extends NanoPatternTipper<EnhancedForStatement> {
+  private static final String description = "Any matches pattern. Consolidate into one statement";
   private static final List<UserDefinedTipper<Block>> tippers = new ArrayList<UserDefinedTipper<Block>>() {
     static final long serialVersionUID = 1L;
     {
-      add(statementsPattern("for($T $N : $X1) if($X2) return true; return false;", "return $X1.stream().anyMatch($N -> $X2);",
-          "Any matches pattern. Consolidate into one statement"));
+      add(statementsPattern("for($T $N1 : $X1) if($X2) return true; return false;", //
+          "return $X1.stream().anyMatch($N1 -> $X2);", description));
+      add(statementsPattern("for($T $N1 : $X1) if($X2) $N2 = true;", //
+          "$N2 = $X1.stream().anyMatch($N1 -> $X2);", description));
     }
   };
 
