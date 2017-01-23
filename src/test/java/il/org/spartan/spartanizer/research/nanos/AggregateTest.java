d3410cda61bee33ec5f694e4277615a7d775d2e8
Yossi Gil <yossi.gil@gmail.com>
2017-01-23 19:53:37 +0200

    7 failing tests

--- src/test/java/il/org/spartan/spartanizer/research/nanos/AggregateTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/AggregateTest.java
@@ -12,12 +12,12 @@ import il.org.spartan.spartanizer.research.nanos.deprecated.*;
 @SuppressWarnings("static-method")
 public class AggregateTest {
   @Ignore @Test public void a() {
-    trimmingOf("for (int ¢ = 1; ¢ < arr.length; ++¢)  if (arr[¢] < min)   min = arr[¢];")
+    trimmingOf("for (int ¢ = 1; ¢ < arr.length; ++¢) if (arr[¢] < min) min = arr[¢];")
         .gives("StatsAccumulator $=Create.a(StatsAccumulator.class).from(values);");
   }
 
   @Test public void b() {
-    trimmingOf("for (final Object ¢ : os)  if (¢.better(best))   best = ¢;")//
+    trimmingOf("for (final Object ¢ : os) if (¢.better(best)) best = ¢;")//
         .using(EnhancedForStatement.class, new Aggregate())//
         .gives("best=os.stream().reduce((¢,best)->¢.better(best)?¢:best).get();");
   }
@@ -28,6 +28,7 @@ public class AggregateTest {
         .gives("int $ = 0; $+=bs.stream().map(d->f()).reduce((x,y)->x+y).get();")//
         .gives("int $ = 0 + bs.stream().map(d->f()).reduce((x,y)->x+y).get();")//
         .gives("int $ = bs.stream().map(d->f()).reduce((x,y)->x+y).get();")//
+        .gives("int $ = bs.stream().map(¢->f()).reduce((x,y)->x+y).get();")//
         .stays();
   }
 
@@ -40,23 +41,21 @@ public class AggregateTest {
   }
 
   @Test public void e() {
-    trimmingOf("for(B d : omg ? yes : no) $ += f();"//
-    )//
+    trimmingOf("for(B d : omg ? yes : no) $ += f();")//
         .using(EnhancedForStatement.class, new Aggregate())//
-        .gives("$+=(omg ? yes : no).stream().map(d->f()).reduce((x,y)->x+y).get();")//
         .gives("$+=(omg ? yes : no).stream().map(d->f()).reduce((x,y)->x+y).get();")//
         .gives("$+=(omg ? yes : no).stream().map(¢->f()).reduce((x,y)->x+y).get();")//
         .stays();
   }
 
   @Test public void respect() {
-    trimmingOf("for (final Object ¢ : os)  if (¢.better(best))   best = ¢;")//
+    trimmingOf("for (final Object ¢ : os) if (¢.better(best)) best = ¢;")//
         .withTippers(EnhancedForStatement.class, new ForEach(), new Select(), new Aggregate())//
         .gives("best=os.stream().reduce((¢,best)->¢.better(best)?¢:best).get();");
   }
 
   @Test public void f() {
-    trimmingOf("for (final List<?> ¢ : implementation)    if (¢ != null)  $ += ¢.size();"//
+    trimmingOf("for (final List<?> ¢ : implementation) if (¢ != null) $ += ¢.size();"//
     )//
         .using(EnhancedForStatement.class, new Aggregate())//
         .gives("$+=implementation.stream().filter(¢->¢!=null).map(¢->¢.size()).reduce((x,y)->x+y).get();")//
@@ -64,7 +63,7 @@ public class AggregateTest {
   }
 
   @Test public void g() {
-    trimmingOf("int $ = init; for (final Statement ¢ : ss)    $ += base + horizontalComplexity(¢);"//
+    trimmingOf("int $ = init; for (final Statement ¢ : ss) $ += base + horizontalComplexity(¢);"//
     )//
         .using(EnhancedForStatement.class, new Aggregate())//
         .gives("int $=init;$+=ss.stream().map(¢->base+horizontalComplexity(¢)).reduce((x,y)->x+y).get();")//
