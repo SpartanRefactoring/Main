9767882b2ea5159a0c343b91f7d0e57d002a052a
Doron Mehsulam <doronmmm@hotmail.com>
2017-01-23 15:26:38 +0200

    Forgotten Todo #709

--- src/main/java/il/org/spartan/bloater/bloaters/ExtractExpressionFromReturn.java
+++ src/main/java/il/org/spartan/bloater/bloaters/ExtractExpressionFromReturn.java
@@ -12,7 +12,7 @@ import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/** {@link Issue #1000}
+/** link {@Issue1000}
  * @author Doron Meshulam <tt>doronmmm@hotmail.com</tt>
  * @since 2017-01-07 */
 public class ExtractExpressionFromReturn extends CarefulTipper<ReturnStatement>//
@@ -27,11 +27,8 @@ public class ExtractExpressionFromReturn extends CarefulTipper<ReturnStatement>/
         final Assignment a = az.assignment(expression(s));
         final AST create = r.getAST();
         final ExpressionStatement exp = create.newExpressionStatement(copy.of(expression(s)));
-        final ReturnStatement retNoExp = create.newReturnStatement();
-        // TODO: Doron Meshulam -- please use class subject --yg
+        final ReturnStatement retNoExp = subject.operand(expression(exp)).toReturn();
         retNoExp.setExpression(copy.of(left(a)));
-        // TODO: Doron Meshulam -- next statement does nothing --yg
-        az.block(s.getParent());
         final ListRewrite l = r.getListRewrite(s.getParent(), Block.STATEMENTS_PROPERTY);
         l.insertAfter(retNoExp, s, g);
         l.insertAfter(exp, s, g);
