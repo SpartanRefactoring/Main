42fdc8e6f4760da1fb344ccbe35387d3fb237d62
Yossi Gil <yossi.gil@gmail.com>
2017-01-24 08:06:10 +0200

    One fewer failing test

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0222.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/tippers/Issue0222.java
@@ -6,40 +6,35 @@ import org.junit.*;
 import org.junit.runners.*;
 
 /** Unit tests for centification of a single parameter to a function even if it
- * defines a "$" variable
+ * defines a "$"variable
  * @author Yossi Gil
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@SuppressWarnings({ "static-method", "javadoc" })
-public final class Issue0222 {
-  @Test public void chocolate1() {
-    trimmingOf("static List<Expression> operands(final InfixExpression x) {\n" + "  if (x == null)\n" + "    return null;\n" + "  int y = x; ++y;\n"
-        + "  final List<Expression> $ = new ArrayList<>();\n" + "  $.add(left(x));\n" + "  $.add(right(x));\n" + "  if (x.hasExtendedOperands())\n"
-        + "    $.addAll(step.extendedOperands(x));\n" + "  return $;\n" + "}\n")//
-            .stays();
-  }
+@SuppressWarnings({"static-method", "javadoc"})
+public class Issue0222 {
+ @Test public void chocolate1(){
+  trimmingOf("L<E> os(I x){if(x==null)return null;int y=x;++y;"
+    + " L<E> $=new A<>();$.a(l(x));$.a(r(x));if(x.h())"
+    + " $.d(s.es(x));return $;}")//
+      .stays();
+ }
 
-  @Test public void chocolate2() {
-    trimmingOf("private boolean continue¢(final List<VariableDeclarationFragment> fs) {\n" + "for (final VariableDeclarationFragment $ : fs){\n"
-        + "int b = f($);\n" + "f($,b);\n" + "return g($,b,f());\n" + "}\n" + "return true;\n" + "}")//
-            .stays();
-  }
+ @Test public void chocolate2(){
+  trimmingOf("tc¢(L<F> ifs){for(F $ : fs){int b=f($);f($,b);return g($,b,f());}return true;}")//
+      .stays();
+ }
 
-  @Test public void chocolate3() {
-    trimmingOf("int f(int i) {\n" + "for (int b: fs)\n" + "return 0;\n" + "return 1;\n" + "}")
-        .gives("int f(int __) {\n" + "for (int b: fs)\n" + "return 0;\n" + "return 1;\n" + "}")//
-        .stays();
-  }
+ @Test public void chocolate3(){
+  trimmingOf("int f(int i){for(int b: fs)return 0;return 1;}")
+    .gives("int f(int __){for(int b: fs)return 0;return 1;}")//
+    .stays();
+ }
 
-  @Ignore
-  // TODO: Yossi Gil, I guess #1115 breaks this some how
-  @Test public void vanilla() {
-    trimmingOf("static List<Expression> operands(final InfixExpression x) {\n" + "  if (x == null)\n" + "    return null;\n"
-        + "  final List<Expression> $ = new ArrayList<>();\n" + "  $.add(left(x));\n" + "  $.add(right(x));\n" + "  if (x.hasExtendedOperands())\n"
-        + "    $.addAll(step.extendedOperands(x));\n" + "  return $;\n" + "}\n")
-            .gives("static List<Expression> operands(final InfixExpression ¢) {\n" + "  if (¢ == null)\n" + "    return null;\n"
-                + "  final List<Expression> $ = new ArrayList<>();\n" + "  $.add(left(¢));\n" + "  $.add(right(¢));\n"
-                + "  if (¢.hasExtendedOperands())\n" + "    $.addAll(step.extendedOperands(¢));\n" + "  return $;\n" + "}\n")
-            .stays();
-  }
+ @Ignore
+ // TODO: Yossi Gil, I guess #1115 breaks this some how
+ @Test public void vanilla(){
+  trimmingOf("L<E> os(I x){if(x==null)return null;L<E> $=new A<>();$.a(l(x));$.a(r(x));if(x.h())$.d(s.es(x));return $;}")
+      .gives("L<E> os(I ¢){if(¢==null)return null;L<E> $=new A<>();$.a(l(¢));$.a(r(¢));if(¢.h())$.d(s.es(¢));return $;}")
+      .stays();
+ }
 }
