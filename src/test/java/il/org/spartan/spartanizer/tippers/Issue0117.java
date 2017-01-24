7baed01bf8b89610147bfaa4f3a1506182c7e389
Yossi Gil <yossi.gil@gmail.com>
2017-01-24 08:08:59 +0200

    One fewer TODO

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue0117.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue0117.java
@@ -12,10 +12,9 @@ import org.junit.runners.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SuppressWarnings("static-method")
 public class Issue0117 {
-  @Ignore // TODO Yossi Gil
   @Test public void issue54ForPlainUseInCondition() {
-    trimmingOf("int a  = f(); for (int ¢ = 0; a <100;  ++¢) b[¢] = 3;")//
-        .gives("for (int a  = f(), ¢ = 0; a <100;  ++¢) b[¢] = 3;")//
+    trimmingOf("int a  = f(); for (int ¢ = 0; a <100;  ++¢) b[¢] *= 3 << a;")//
+        .gives("for (int a  = f(), ¢ = 0; a <100;  ++¢) b[¢] *= 3 << a;")//
         .stays();
   }
 
