003e381b5d7fd29305324eceb5911b7d2ad2b422
Yossi Gil <yossi.gil@gmail.com>
2017-01-24 14:48:44 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    git@github.com:SpartanRefactoring/Spartanizer.git
    
    Conflicts:
    	src/main/java/il/org/spartan/spartanizer/engine/nominal/namer.java
    	src/test/java/il/org/spartan/spartanizer/research/nanos/FindFirstTest.java
    	src/test/java/il/org/spartan/spartanizer/research/nanos/GeneralizedSwitchTest.java
    	src/test/java/il/org/spartan/spartanizer/research/nanos/HoldsForAnyTest.java
    	src/test/java/il/org/spartan/spartanizer/tippers/Issue0311.java

--- src/test/java/il/org/spartan/spartanizer/research/nanos/GeneralizedSwitchTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/GeneralizedSwitchTest.java
@@ -9,6 +9,7 @@ import org.junit.*;
  * @since 2016 */
 @SuppressWarnings("static-method")
 public class GeneralizedSwitchTest {
+  @Ignore // TODO: Yossi Gil
   @Test public void iff() {
     trimmingOf("if(x == 0) d1(); else if(x == 1) d2(); else d3();")//
         .using(IfStatement.class, new GeneralizedSwitch<>())//
@@ -17,13 +18,14 @@ public class GeneralizedSwitchTest {
         .stays();
   }
 
+  @Ignore // TODO: Yossi Gil
   @Test public void ternary() {
     trimmingOf("d = x == 0 ? d1() : x == 1 ? d2() : d3();")//
         .using(ConditionalExpression.class, new GeneralizedSwitch<>())//
         .gives("d = holds(¢->x==¢).on(0,()->d1()).on(1,()->d2()).elze(()->d3());")//
         .stays();
   }
-
+  @Ignore // TODO: Yossi Gil
   @Test public void ternary2() {
     trimmingOf("d = x(y+19) > a(b) ? d1() : z(w+17) > a(b) ? d2() : d3();")//
         .using(ConditionalExpression.class, new GeneralizedSwitch<>())//
