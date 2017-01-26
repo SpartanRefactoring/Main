3c1548fc213a6a8eaadad58233153f1e713a541f
Yossi Gil <yossi.gil@gmail.com>
2017-01-26 14:11:27 +0200

    Fix methods order in tests

--- src/test/java/il/org/spartan/spartanizer/research/nanos/BetweenTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/BetweenTest.java
@@ -4,7 +4,8 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
-
+import org.junit.runners.*;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 /** TODO: Ori Marcovitch please add a description
  * @author Ori Marcovitch
  * @since 2016 */
