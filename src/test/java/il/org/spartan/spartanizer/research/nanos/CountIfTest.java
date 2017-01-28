02e17827c5bbfbad428a781c1e4a1bacc8901106
orimarco <marcovitch.ori@gmail.com>
2017-01-29 00:19:05 +0200

    while countIf

--- src/test/java/il/org/spartan/spartanizer/research/nanos/CountIfTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/CountIfTest.java
@@ -4,27 +4,31 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
-import org.junit.runners.*;
 
-/** Tests {@link CountIf}
+/** Tests {@link CountIf} and {@link While.CountIf}
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-08 */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SuppressWarnings("static-method")
 public class CountIfTest {
   @Test public void a() {
-    trimmingOf("for (M ¢ : AA) if (!¢.isCtr()) ++a;")//
+    trimmingOf("for (M λ : AA) if (!λ.isCtr()) ++a;")//
         .using(EnhancedForStatement.class, new CountIf(), new ForEachSuchThat())//
-        .gives("a += AA.stream().filter(¢->!¢.isCtr()).count();")//
-        .gives("a+=AA.stream().filter(λ->!λ.isCtr()).count();")//
+        .gives("a += (AA).stream().filter(λ->!λ.isCtr()).count();")//
+        .gives("a += AA.stream().filter(λ->!λ.isCtr()).count();")//
         .stays();
   }
 
   @Test public void b() {
-    trimmingOf("for (M ¢ : AA.a() ? b : c) if (!¢.isCtr()) ++a;")//
+    trimmingOf("for (M λ : AA.a() ? b : c) if (!λ.isCtr()) ++a;")//
         .using(EnhancedForStatement.class, new CountIf(), new ForEachSuchThat())//
-        .gives("a += (AA.a() ? b : c).stream().filter(¢->!¢.isCtr()).count();")//
-        .gives("a+=(AA.a() ? b : c).stream().filter(λ->!λ.isCtr()).count();")//
+        .gives("a += (AA.a() ? b : c).stream().filter(λ->!λ.isCtr()).count();")//
+        .stays();
+  }
+
+  @Test public void c() {
+    trimmingOf("while (BlockFalling.canFall(w,i,j - 1,k) && j > 0) --j;")//
+        .using(WhileStatement.class, new While.CountIf())//
+        .gives("j+=countWhile(()->BlockFalling.canFall(w,i,j-1,k)&&j>0);")//
         .stays();
   }
 }
