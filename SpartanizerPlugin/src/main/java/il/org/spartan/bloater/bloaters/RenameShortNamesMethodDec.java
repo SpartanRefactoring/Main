3e7f18ad3e2f2187037defe8903a5de317923450
Yossi Gil <yossi.gil@gmail.com>
2017-01-30 17:57:12 +0200

    sync

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/RenameShortNamesMethodDec.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/RenameShortNamesMethodDec.java
@@ -14,7 +14,6 @@ import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.engine.nominal.*;
 import il.org.spartan.spartanizer.java.namespace.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.zoomer.zoomin.expanders.*;
