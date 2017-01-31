134071ece1cb10f74c10fe9b1940f6a5986848d5
orimarco <marcovitch.ori@gmail.com>
2017-02-01 00:01:09 +0200

    doc

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/ForEachInRangeTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/ForEachInRangeTest.java
@@ -5,7 +5,7 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-/** TODO: orimarco <tt>marcovitch.ori@gmail.com</tt> please add a description
+/** Tests {@link ForEachInRange}
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-12 */
 @SuppressWarnings("static-method")
