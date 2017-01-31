fa8f414274bb6372997850679b2c397838b10551
Yossi Gil <yossi.gil@gmail.com>
2017-01-31 15:24:29 +0200

    Closes #1055

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue1055.java
@@ -0,0 +1,19 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+/** Unit test for Issue 1055 
+ * @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
+ * @since 2016-12-23 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SuppressWarnings({ "static-method", "javadoc" })
+public class Issue1055 {
+  @Test public void vanilla() {
+    trimmingOf("switch(x) { case 1: { y=2; } } ") //
+        .gives("switch(x) { case 1: y = 2; }") //
+        ;
+  }
+}
