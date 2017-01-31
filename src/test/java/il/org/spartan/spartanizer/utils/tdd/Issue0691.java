0900e2243b7d6f51d7e523e507799b4de17d7ca4
Yossi Gil <yossi.gil@gmail.com>
2017-01-31 14:48:10 +0200

    Fewer suppress warnings

--- src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue0691.java
+++ src/test/java/il/org/spartan/spartanizer/utils/tdd/Issue0691.java
@@ -15,45 +15,44 @@ import il.org.spartan.spartanizer.ast.safety.*;
  * @author Dor Ma'ayan
  * @author Omri Ben- Shmuel
  * @since 01-11-2016 */
-
   @SuppressWarnings("static-method")
 public class Issue0691 {
   @Test public void test0() {
     azzert.isNull(getAll.invocations((MethodDeclaration) null));
   }
 
-  @Test  public void test1() {
+  @Test public void test1() {
     azzert.that(0, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {}"))).size()));
   }
 
-  @Test  public void test2() {
+  @Test public void test2() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {int a;int b;y.t(a,b);}")))));
   }
 
-  @Test  public void test3() {
+  @Test public void test3() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("g");
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {t(); g();}")))));
   }
 
-  @Test  public void test4() {
+  @Test public void test4() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("q");
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {t(); q();}")))));
   }
 
-  @Test  public void test5() {
+  @Test public void test5() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("q");
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {int a = t(); q();}")))));
   }
 
-  @Test  public void test6() {
+  @Test public void test6() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("x");
@@ -61,7 +60,7 @@ public class Issue0691 {
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {int a = t(x(),z); q();}")))));
   }
 
-  @Test  public void test7() {
+  @Test public void test7() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("x");
@@ -70,7 +69,7 @@ public class Issue0691 {
     azzert.that(res, is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {int a = t(x(),z); q();int v = 5 +add();}")))));
   }
 
-  @Test  public void test8() {
+  @Test public void test8() {
     final Set<String> res = new TreeSet<>();
     res.add("t");
     res.add("x");
@@ -80,7 +79,7 @@ public class Issue0691 {
         is(getAll.invocations(az.methodDeclaration(wizard.ast("static void test() {int a = t(x(),z); q();int v = 5 +add(); int tmp = (int)5.5;}")))));
   }
 
-  @Test  public void test9() {
+  @Test public void test9() {
     final Set<String> res = new TreeSet<>();
     res.add("body");
     res.add("statements");
@@ -92,7 +91,6 @@ public class Issue0691 {
     azzert.that(res,
         is(getAll.invocations(az.methodDeclaration(wizard.ast("public static Set<String> invocations(final MethodDeclaration ¢) {if(¢ == null)"
             + "return null;Set<String> $ = new TreeSet<>();if(statements(body(¢)).isEmpty())return  $;¢.accept(new ASTVisitor() {"
-            + "@Override public boolean visit (MethodInvocation m) {$.add(m.getName().toString());return true;}});return $;"
-            + "}")))));
+            + "@Override public boolean visit (MethodInvocation m) {$.add(m.getName().toString());return true;}});return $;" + "}")))));
   }
 }
