3d1bd77e8993bddd34d62e4507b02664c04dd69f
Yossi Gil <yossi.gil@gmail.com>
2017-02-03 10:18:18 +0200

    Move method to wizard

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/IfCommandsSequencerNoElseSingletonSequencer.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/IfCommandsSequencerNoElseSingletonSequencer.java
@@ -29,7 +29,7 @@ public final class IfCommandsSequencerNoElseSingletonSequencer extends ReplaceTo
 
   @Override protected ASTRewrite go(@NotNull final ASTRewrite $, @NotNull final IfStatement s, @NotNull final Statement nextStatement,
       final TextEditGroup g) {
-    if (!iz.vacuousElse(s) || !iz.sequencer(nextStatement) || !endsWithSequencer(then(s)))
+    if (!iz.vacuousElse(s) || !iz.sequencer(nextStatement) || !wizard.endsWithSequencer(then(s)))
       return null;
     final IfStatement asVirtualIf = subject.pair(then(s), nextStatement).toIf(s.getExpression());
     if (wizard.same(then(asVirtualIf), elze(asVirtualIf))) {
