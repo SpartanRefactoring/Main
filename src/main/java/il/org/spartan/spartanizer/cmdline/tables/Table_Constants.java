828d4c35626eb9a5d793517419a7e421d6a1ad26
orimarco <marcovitch.ori@gmail.com>
2017-02-08 10:51:26 +0200

    constants table and more

---
+++ src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Constants.java
@@ -0,0 +1,59 @@
+package il.org.spartan.spartanizer.cmdline.tables;
+
+import java.lang.reflect.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.cmdline.*;
+import il.org.spartan.tables.*;
+
+/** Generates a table, counting constants in repositories
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-02-08 */
+public class Table_Constants extends FolderASTVisitor {
+  private static Table pWriter;
+  public static int ints;
+  public static int strings;
+  static {
+    clazz = Table_Constants.class;
+  }
+
+  private static void initializeWriter() {
+    pWriter = new Table(Table_Constants.class);
+  }
+
+  public static void main(final String[] args)
+      throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    FolderASTVisitor.main(args);
+    pWriter.close();
+  }
+
+  @Override protected void done(final String path) {
+    summarizeNPStatistics(path);
+    System.err.println(" " + path + " Done");
+  }
+
+  @Override public boolean visit(final FieldDeclaration ¢) {
+    if (iz.constant(¢))//
+      if (iz.intType(type(¢)))
+        ++ints;
+      else if (iz.stringType(type(¢)))
+        ++strings;
+    return super.visit(¢);
+  }
+
+  public static void summarizeNPStatistics(final String path) {
+    if (pWriter == null)
+      initializeWriter();
+    pWriter.col("Project", path);
+    pWriter//
+        .col("Ints", ints)//
+        .col("Strings", strings)//
+        .col("Total", ints + strings);
+    pWriter.nl();
+    ints = strings = 0;
+  }
+}
