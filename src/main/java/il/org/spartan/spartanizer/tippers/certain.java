e9199dd8e9e2cb349915766c6d2227576b9aa0b8
Yossi Gil <yossi.gil@gmail.com>
2017-02-08 22:41:01 +0200

    Cleanup and spartanization

---
+++ src/main/java/il/org/spartan/spartanizer/tippers/certain.java
@@ -0,0 +1,26 @@
+package il.org.spartan.spartanizer.tippers;
+
+import java.util.stream.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.engine.*;
+
+/** Fluent API
+ * @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
+ * @since 2017-02-08 */
+public interface certain {
+  static boolean string(final ASTNode... ¢) {
+    return Stream.of(¢).allMatch(certain::string);
+  }
+
+  static boolean string(final ASTNode ¢) {
+    return string(az.expression(¢));
+  }
+
+  static boolean string(final Expression ¢) {
+    return (¢) == null ? false : iz.stringLiteral(¢) || type.isString(¢) || iz.name(¢) && wizard.isString(analyze.type(az.simpleName(¢)));
+  }
+}
