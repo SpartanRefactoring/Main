8ab7fad2782f3cec876ab49cc157c0eb5451942e
Yossi Gil <yossi.gil@gmail.com>
2017-02-08 20:08:24 +0200

    Renaming

--- src/test/java/il/org/spartan/spartanizer/research/nanos/ReductionTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/ReductionTest.java
@@ -5,35 +5,35 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-/** Tests {@link Reduction}
+/** Tests {@link SelfReduction}
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-02-01 */
 @SuppressWarnings("static-method")
 public class ReductionTest {
   @Test public void a() {
     trimmingOf("void foo(){ foo(a);}")//
-        .using(MethodInvocation.class, new Reduction())//
+        .using(MethodInvocation.class, new SelfReduction())//
         .gives("void foo(){self(a);}")//
         .stays();
   }
 
   @Test public void b() {
     trimmingOf("void foo(){ return foo(a) + foo(a,b);}")//
-        .using(MethodInvocation.class, new Reduction())//
+        .using(MethodInvocation.class, new SelfReduction())//
         .gives("void foo(){ return self(a) + self(a,b);}")//
         .stays();
   }
 
   @Test public void c() {
     trimmingOf("void foo(){ return foo(a) + foo(a,b) == 7 ? foo() : bar();}")//
-        .using(MethodInvocation.class, new Reduction())//
+        .using(MethodInvocation.class, new SelfReduction())//
         .gives("void foo(){ return self(a) + self(a,b) == 7 ? foo() : bar();}")//
         .stays();
   }
 
   @Test public void d() {
     trimmingOf("void foo(int a){ return foo(a) + foo(a,b) == 7 ? foo() : barar();}")//
-        .using(MethodInvocation.class, new Reduction())//
+        .using(MethodInvocation.class, new SelfReduction())//
         .gives("void foo(int a){ return foo(a) + self(a,b) == 7 ? self() : barar();}")//
         .stays();
   }
