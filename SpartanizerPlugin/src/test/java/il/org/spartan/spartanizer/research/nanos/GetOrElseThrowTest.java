c08d8a83d40782f6cfab3974618254099ed8f693
orimarco <marcovitch.ori@gmail.com>
2017-02-09 18:41:32 +0200

    GetOrElseThrow

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/GetOrElseThrowTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/GetOrElseThrowTest.java
@@ -10,40 +10,19 @@ import org.junit.runners.*;
 /** Tests @link GetOrElseThrow
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-29 */
-@Ignore
 @SuppressWarnings("static-method")
 public class GetOrElseThrowTest {
   @Test public void a() {
     trimmingOf("if(x == null) throw new Error(); return x;")//
         .using(IfStatement.class, new GetOrElseThrow())//
-        .gives("statement(); azzert.notNull(x); use(); use();")//
+        .gives("notNull(x).get(x);")//
         .stays();
   }
 
   @Test public void a2() {
-    trimmingOf("statement(); if(x == null || y == null) return; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
-        .gives("statement(); azzert.notNull(x,y); use(); use();")//
-        .stays();
-  }
-
-  @Test public void b() {
-    trimmingOf("statement(); if(x == null) return null; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
-        .gives("statement(); azzert.notNull(x); use(); use();")//
-        .stays();
-  }
-
-  @Test public void b2() {
-    trimmingOf("statement(); if(x == null || y == null) return null; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
-        .gives("statement(); azzert.notNull(x,y); use(); use();")//
-        .stays();
-  }
-
-  @Test public void respect() {
-    trimmingOf("void m(){if(x == null) return; use(); use();}")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+    trimmingOf("s1(); if(x == null) throw new Error(); return x; s2();")//
+        .using(IfStatement.class, new GetOrElseThrow())//
+        .gives("s1(); notNull(x).get(x); s2();")//
         .stays();
   }
 }
