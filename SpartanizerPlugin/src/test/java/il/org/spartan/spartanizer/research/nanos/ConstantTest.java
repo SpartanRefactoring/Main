11c17616c7f78c930632d1ee453a1a5ed7da541b
orimarco <marcovitch.ori@gmail.com>
2017-02-09 13:05:11 +0200

    some more tests

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/ConstantTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/ConstantTest.java
@@ -10,10 +10,40 @@ import org.junit.*;
  * @since 2017-02-09 */
 @SuppressWarnings("static-method")
 public class ConstantTest {
-  @Test public void basic() {
+  @Test public void a() {
     trimmingOf("class C{public static final int a = 7;}")//
         .using(FieldDeclaration.class, new Constant())//
         .gives("class C{}")//
         .stays();
+  }
+
+  @Test public void b() {
+    trimmingOf("class C{public static final Boolean a = 7;}")//
+        .using(FieldDeclaration.class, new Constant())//
+        .gives("class C{}")//
+        .stays();
+  }
+
+  @Test public void c() {
+    trimmingOf("class C{public static final Boolean a = true;"//
+        + "public static final Integer b = 7;"//
+        + "public static final char b = 7;"//
+        + "public static final String b = \"fff\";"//
+        + "}")//
+            .using(FieldDeclaration.class, new Constant())//
+            .gives("class C{}")//
+            .stays();
+  }
+
+  @Test public void d() {
+    trimmingOf("class C{public static final Boolean a = true;"//
+        + "public static final Character b = 7;"//
+        + "public static final Int b = 7;"//
+        + "public static final char b = 7;"//
+        + "public static final String b = \"fff\";"//
+        + "}")//
+            .using(FieldDeclaration.class, new Constant())//
+            .gives("class C{public static final Int b = 7;}")//
+            .stays();
   }
 }
