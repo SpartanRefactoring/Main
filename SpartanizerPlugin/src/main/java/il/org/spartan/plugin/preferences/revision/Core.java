9c2aa1d6bb59a079e6368527f0f71e222881ec30
Ori Roth <ori.rothh@gmail.com>
2017-02-10 02:17:31 +0200

    Preferences revision POC see #1121

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/Core.java
@@ -0,0 +1,10 @@
+package il.org.spartan.plugin.preferences.revision;
+
+/**
+ * TODO Ori Roth: document class {@link }
+ * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
+ * @since 2017-02-01
+ */
+public interface Core {
+  //
+}
