fdc0c382cbfd8958e06da11c1e7ccae3ec269d1f
orimarco <marcovitch.ori@gmail.com>
2017-02-12 10:38:36 +0200

    Min/max nanos

--- src/main/java/il/org/spartan/spartanizer/research/nanos/Max.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/Max.java
@@ -10,22 +10,18 @@ import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
-/** Just another form of {@link SafeReference}
+/** Max between two expressions
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
- * @since 2017-01-04 */
+ * @since 2017-02-12 */
 public final class Max {
   public static class SafeReference extends NanoPatternTipper<InfixExpression> {
     private static final List<UserDefinedTipper<InfixExpression>> tippers = new ArrayList<UserDefinedTipper<InfixExpression>>() {
       static final long serialVersionUID = 1L;
       {
-        add(patternTipper("$N1 != null && $N1.$N2", "safe($N1).get(()->$N1.$N2)", "safe reference"));
-        add(patternTipper("null != $N1 && $N1.$N2", "safe($N1).get(()->$N1.$N2)", "safe reference"));
-        //
-        add(patternTipper("$SN1 != null && $SN1.$SN2.$SN3()", "safe($SN1).invoke(()->$SN1.$SN2.$SN3())", "safe reference"));
-        add(patternTipper("null != $SN1 && $SN1.$SN2.$SN3()", "safe($SN1).invoke(()->$SN1.$SN2.$SN3())", "safe reference"));
-        //
-        add(patternTipper("$N1 != null && $N1.$SN2()", "safe($N1).invoke(()->$N1.$SN2())", "safe reference"));
-        add(patternTipper("null != $N1 && $N1.$SN2()", "safe($N1).invoke(()->$N1.$SN2())", "safe reference"));
+        add(patternTipper("$X1 > $X2 ? $X1 : $X2", "max($X1,$X2)", "max"));
+        add(patternTipper("$X2 < $X1 ? $X1 : $X2", "max($X1,$X2)", "max"));
+        add(patternTipper("$X1 >= $X2 ? $X1 : $X2", "max($X1,$X2)", "max"));
+        add(patternTipper("$X2 <= $X1 ? $X1 : $X2", "max($X1,$X2)", "max"));
       }
     };
 
@@ -42,15 +38,7 @@ public final class Max {
     }
 
     @Override public String description() {
-      return "A field access or an invocation where the callee is checked to be non-null and if is, evaluates to a default value";
-    }
-
-    @Override public String example() {
-      return firstPattern(tippers);
-    }
-
-    @Override public String symbolycReplacement() {
-      return firstReplacement(tippers);
+      return "Max between two expressions";
     }
   }
 }
