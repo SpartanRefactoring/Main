c1a76a660e177640e4123937b5781e2a543d53fc
orimarco <marcovitch.ori@gmail.com>
2017-02-13 10:02:07 +0200

    HoldsForAny

--- src/main/java/il/org/spartan/spartanizer/research/nanos/HoldsForAny.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/HoldsForAny.java
@@ -1,16 +1,11 @@
 package il.org.spartan.spartanizer.research.nanos;
 
-import static il.org.spartan.spartanizer.research.TipperFactory.*;
-
-import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
 /** {@link HoldsForAny} Nano Pattern - holds P(c) for any c in C
@@ -18,22 +13,22 @@ import il.org.spartan.spartanizer.research.nanos.common.*;
  * @since 2017-01-05 */
 public final class HoldsForAny extends NanoPatternTipper<EnhancedForStatement> {
   private static final String description = "Any matches pattern. Consolidate into one statement";
-  private static final List<UserDefinedTipper<Block>> tippers = new ArrayList<UserDefinedTipper<Block>>() {
+  private static final BlockNanoPatternContainer tippers = new BlockNanoPatternContainer() {
     static final long serialVersionUID = 1L;
     {
-      add(statementsPattern("for($T $N1 : $X1) if($X2) return true; return false;", //
-          "return $X1.stream().anyMatch($N1 -> $X2);", description));
-      add(statementsPattern("for($T $N1 : $X1) if($X2) $N2 = true;", //
-          "$N2 = $X1.stream().anyMatch($N1 -> $X2);", description));
+      statementsPattern("for($T $N1 : $X1) if($X2) return true; return false;", //
+          "return $X1.stream().anyMatch($N1 -> $X2);", description);
+      statementsPattern("for($T $N1 : $X1) if($X2) $N2 = true;", //
+          "$N2 = $X1.stream().anyMatch($N1 -> $X2);", description);
     }
   };
 
   @Override public boolean canTip(final EnhancedForStatement x) {
-    return anyTips(tippers, az.block(parent(x)));
+    return tippers.anyTips(az.block(parent(x)));
   }
 
   @Override public Tip pattern(final EnhancedForStatement $) {
-    return firstTip(tippers, az.block(parent($)));
+    return tippers.firstTip(az.block(parent($)));
   }
 
   @Override public Category category() {
@@ -45,10 +40,10 @@ public final class HoldsForAny extends NanoPatternTipper<EnhancedForStatement> {
   }
 
   @Override public String example() {
-    return firstPattern(tippers);
+    return tippers.firstPattern();
   }
 
   @Override public String symbolycReplacement() {
-    return firstReplacement(tippers);
+    return tippers.firstReplacement();
   }
 }
