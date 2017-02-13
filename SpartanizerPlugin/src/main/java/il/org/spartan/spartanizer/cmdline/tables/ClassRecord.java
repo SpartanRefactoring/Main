aa07f63f22b3bf56e0d998e78597e473c049bafb
Matteo Orru <matteo.orru@cs.technion.ac.il>
2017-02-13 21:49:05 +0200

    compute table for the paper

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/ClassRecord.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package il.org.spartan.spartanizer.cmdline.tables;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.research.util.*;
+
+/**
+ * TODO Matteo Orru': document class {@link }
+ * @author Matteo Orru' <tt>matteo.orru@cs.technion.ac.il</tt>
+ * @since 2017-02-10
+ */
+public class ClassRecord {
+  
+  public static TypeDeclaration before;
+  public static String className;
+  public static int numStatements;
+  public static int numExpressions;
+  
+  public ClassRecord(final TypeDeclaration t){
+    before = t;
+    className = t.getName() + "";
+    numStatements = measure.statements(t);
+    numExpressions = measure.expressions(t);
+  }
+    
+}
