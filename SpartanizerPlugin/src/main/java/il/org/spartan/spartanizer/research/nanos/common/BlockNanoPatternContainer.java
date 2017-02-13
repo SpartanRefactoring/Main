29dfabb059375dbf43da879a305340126eee30cb
orimarco <marcovitch.ori@gmail.com>
2017-02-13 09:55:24 +0200

    HoldsForAll

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/common/BlockNanoPatternContainer.java
@@ -0,0 +1,17 @@
+package il.org.spartan.spartanizer.research.nanos.common;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.research.*;
+
+/** List that can contain {@link NanoPatternTipper<Block>}
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-02-13 */
+public class BlockNanoPatternContainer extends NanoPatternContainer<Block> {
+  private static final long serialVersionUID = 1L;
+
+  public BlockNanoPatternContainer statementsPattern(final String pattern, final String replacement, final String description) {
+    add(TipperFactory.statementsPattern(pattern, replacement, description));
+    return this;
+  }
+}
