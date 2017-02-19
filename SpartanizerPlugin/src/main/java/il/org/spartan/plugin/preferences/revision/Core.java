7808004ab6831fe84b17be4cbee595beada9948a
Ori Roth <ori.rothh@gmail.com>
2017-02-19 19:41:13 +0200

    new preferences mechanism see #1121

--- SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/Core.java
+++
@@ -1,8 +0,0 @@
-package il.org.spartan.plugin.preferences.revision;
-
-/** TODO Ori Roth: document class {@link }
- * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
- * @since 2017-02-01 */
-public interface Core {
-  //
-}
