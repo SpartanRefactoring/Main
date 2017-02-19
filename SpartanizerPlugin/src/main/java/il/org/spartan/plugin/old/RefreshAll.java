239f8ee6910e4774102ada99afcbc50dbe5e2cca
Ori Roth <ori.rothh@gmail.com>
2017-02-19 19:41:13 +0200

    new preferences mechanism see #1121

--- SpartanizerPlugin/src/main/java/il/org/spartan/plugin/old/RefreshAll.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/old/RefreshAll.java
@@ -12,14 +12,18 @@ import il.org.spartan.spartanizer.utils.*;
  * @since Oct 16, 2016 */
 public final class RefreshAll extends BaseHandler {
   public static void go() {
-    final IProgressMonitor npm = new NullProgressMonitor();
     for (final IProject p : ResourcesPlugin.getWorkspace().getRoot().getProjects())
-      try {
-        if (p.isOpen() && p.getNature(Nature.NATURE_ID) != null)
-          p.build(IncrementalProjectBuilder.FULL_BUILD, npm);
-      } catch (final CoreException ¢) {
-        monitor.logEvaluationError(new RefreshAll(), ¢);
-      }
+      go(p);
+  }
+
+  public static void go(final IProject p) {
+    final IProgressMonitor npm = new NullProgressMonitor();
+    try {
+      if (p.isOpen() && p.getNature(Nature.NATURE_ID) != null)
+        p.build(IncrementalProjectBuilder.FULL_BUILD, npm);
+    } catch (final CoreException ¢) {
+      monitor.logEvaluationError(new RefreshAll(), ¢);
+    }
   }
 
   @Override public Void execute(@SuppressWarnings("unused") final ExecutionEvent __) {
