24422dbd743001d2f9594f5bddb214f38af5dfe8
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-02-21 19:12:59 +0200

    add Table_TestClasses

---
+++ src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_TestClasses.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package il.org.spartan.spartanizer.cmdline.tables;
+
+import java.lang.reflect.*;
+
+import il.org.spartan.spartanizer.cmdline.*;
+import il.org.spartan.tables.*;
+
+/**
+ * TODO Matteo Orru': document class {@link }
+ * @author Matteo Orru' <tt>matteo.orru@cs.technion.ac.il</tt>
+ * @since 2017-02-21
+ */
+public class Table_TestClasses extends Table_SummaryForPaper {
+  
+  private static Table testClassesWriter;
+  
+  static {
+    clazz = Table_TestClasses.class;
+  }
+  
+  public static void main(final String[] args)
+      throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    FolderASTVisitor.main(args);
+    testClassesWriter.close();
+    System.err.println("Your output is in: " + Table.temporariesFolder);
+  }
+  
+  private void writeTestClasses(String path) {
+    for(final CompilationUnitRecord ¢: CompilationUnitRecords)
+      if(¢.testCount() > 0)
+        testClassesWriter//
+        .col("Project", path)//
+        .col("AbsolutePath", ¢.getPath())//
+        .col("RelativePath", ¢.getRelativePath())//
+//        .col("#LOC", countNoTestLOC())//
+//        .col("#Classes", countNoTestClasses() - countTestClasses())//
+//        //.col("#TestClasses", countTestClasses())//
+//        .col("#Methods", countNoTestMethods())//
+        .nl();   
+   }
+  
+  @Override protected void done(final String path) {
+    if (testClassesWriter == null)
+      initializeWriter();
+    writeTestClasses(path);
+    System.err.println("Your output is in: " + outputFolder);
+  }
+  
+  private static void initializeWriter() {
+    testClassesWriter = new Table(clazz);
+  }
+}
