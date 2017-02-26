02230a030f3fe38697afb3ac2484052ca7ba3936
orimarco <marcovitch.ori@gmail.com>
2017-02-26 13:21:14 +0200

    bug

--- src/test/java/il/org/spartan/spartanizer/research/nanos/CopyCollectionTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/CopyCollectionTest.java
@@ -7,20 +7,20 @@ import org.junit.*;
 import org.junit.runners.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-/** TODO: orimarco <tt>marcovitch.ori@gmail.com</tt> please add a description
+/** Tests {@link CopyCollection}
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-01-03 */
 @SuppressWarnings("static-method")
 public class CopyCollectionTest {
   @Test public void a() {
     trimmingOf("StatsAccumulator $=new StatsAccumulator();  $.addAll(values);")//
-        .using(Block.class, new CopyCollection())//
+        .using(MethodInvocation.class, new CopyCollection())//
         .gives("StatsAccumulator $=Create.from(values);");
   }
 
   @Test public void b() {
     trimmingOf("StatsAccumulator<N> $=new StatsAccumulator<>();  $.addAll(values);")//
-        .using(Block.class, new CopyCollection())//
+        .using(MethodInvocation.class, new CopyCollection())//
         .gives("StatsAccumulator<N> $=Create.from(values);");
   }
 }
