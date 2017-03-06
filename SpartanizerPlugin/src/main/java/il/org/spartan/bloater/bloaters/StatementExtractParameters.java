46ff31449eb322bc41d2230458a5f4d48f2e9f4b
Ori Roth <ori.rothh@gmail.com>
2017-03-07 00:01:58 +0200

    cleanup

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/StatementExtractParameters.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/StatementExtractParameters.java
@@ -81,13 +81,12 @@ public class StatementExtractParameters<S extends Statement> extends CarefulTipp
           goBlockParent((Block) s.getParent(), v, ns, r, g);
       }
 
-      void goNonBlockParent(final ASTNode p, final VariableDeclarationStatement x, final Statement s, final ASTRewrite r, final TextEditGroup g) {
+      void goNonBlockParent(final ASTNode p, final VariableDeclarationStatement x,
+          final Statement pleaseDoNotChangeThisVariableNameToSItCausesAHidingBug, final ASTRewrite r, final TextEditGroup g) {
         // TODO Ori Roth: Use subject to block.
         final Block b = p.getAST().newBlock();
         statements(b).add(x);
-        statements(b).add(s);
-        // TODO Ori Roth: Why twice?
-        r.replace(s, b, g);
+        statements(b).add(pleaseDoNotChangeThisVariableNameToSItCausesAHidingBug);
         r.replace(s, b, g);
       }
     };
