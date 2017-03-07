4258deb43ace44f0f7b41fac2735f2f39b09c89f
orimarco <marcovitch.ori@gmail.com>
2017-03-07 19:34:03 +0200

    lower bound

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nodes_Coverage_Lower_Bound.java
@@ -0,0 +1,12 @@
+package il.org.spartan.spartanizer.cmdline.tables;
+
+import org.eclipse.jdt.core.dom.*;
+
+/** Generates table presenting lower bound on {@link ASTNode}s coverage
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-03-07 */
+public class Table_Nodes_Coverage_Lower_Bound extends Table_Nodes_Coverage {
+  @Override protected String analyze(final String from) {
+    return spartanalyzer.once(from);
+  }
+}
