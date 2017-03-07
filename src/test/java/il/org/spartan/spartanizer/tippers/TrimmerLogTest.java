d41db9a11c031f9551b33e273c9a7c3471b71418
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-08 01:06:28 +0200

    New model for examples

--- src/test/java/il/org/spartan/spartanizer/tippers/TrimmerLogTest.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/TrimmerLogTest.java
@@ -18,7 +18,6 @@ import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.cmdline.*;
 import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import il.org.spartan.spartanizer.utils.*;
 
 @SuppressWarnings("static-method") //
