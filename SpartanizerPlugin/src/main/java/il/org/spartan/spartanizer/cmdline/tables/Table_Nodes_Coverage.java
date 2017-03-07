ca5c4b764f338a3a22d4792dba5c31470d6098c2
orimarco <marcovitch.ori@gmail.com>
2017-03-07 17:27:53 +0200

    behold: AgileSpartanizer

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nodes_Coverage.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nodes_Coverage.java
@@ -32,8 +32,6 @@ public class Table_Nodes_Coverage extends Table_ReusabilityIndices {
   }
 
   @Override public boolean visit(final CompilationUnit ¢) {
-    System.out.println("----------------------------------------------------");
-    System.out.println(¢);
     try {
       statistics.logCompilationUnit(¢);
       final String spartanzied = spartanizer.fixedPoint(¢);
