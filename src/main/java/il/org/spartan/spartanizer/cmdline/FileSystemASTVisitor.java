2f2caa54be480dda6d0823f8b5ff7ba707a8f81c
orimarco <marcovitch.ori@gmail.com>
2017-03-09 13:31:20 +0200

    yet another endless loop

--- src/main/java/il/org/spartan/spartanizer/cmdline/FileSystemASTVisitor.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/FileSystemASTVisitor.java
@@ -105,6 +105,7 @@ public class FileSystemASTVisitor {
   }
 
   void visit(final File f) {
+    monitor.debug("Visiting: " + f.getName());
     if (!silent)
       dotter.click();
     if (!system.isTestFile(f) && !containsTestAnnotation(f))
