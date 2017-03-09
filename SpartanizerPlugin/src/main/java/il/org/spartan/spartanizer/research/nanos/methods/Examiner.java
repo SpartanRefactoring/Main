800fb00c6f71ae5219eb5c4ab392eba56aee190e
orimarco <marcovitch.ori@gmail.com>
2017-03-10 00:50:19 +0200

    Examiner

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
@@ -1,30 +1,25 @@
 package il.org.spartan.spartanizer.research.nanos.methods;
 
-import static il.org.spartan.spartanizer.research.TipperFactory.*;
-
-import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.java.*;
-import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
 /** One statement method returning boolean expressio
  * @author Ori Marcovitch */
 public class Examiner extends JavadocMarkerNanoPattern {
   private static final long serialVersionUID = 7361477859663262247L;
-  private static final Collection<UserDefinedTipper<Statement>> tippers = new HashSet<UserDefinedTipper<Statement>>() {
+  private static final NanoPatternContainer<Statement> tippers = new NanoPatternContainer<Statement>() {
     @SuppressWarnings("hiding") static final long serialVersionUID = 1L;
     {
-      add(patternTipper("return $X;", "", ""));
-      add(patternTipper("synchronized ($X1) { return $X2;}", "", ""));
+      patternTipper("return $X;", "", "");
+      patternTipper("synchronized ($X1) { return $X2;}", "", "");
     }
   };
 
   @Override protected boolean prerequisites(final MethodDeclaration ¢) {
     return haz.booleanReturnType(¢)//
-        && anyTips(tippers, onlyStatement(¢))//
+        && tippers.canTip(onlyStatement(¢))//
     ;
   }
 }
