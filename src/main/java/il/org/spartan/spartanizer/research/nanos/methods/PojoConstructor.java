c897978c3e7cf24af6f22d5ca66ee37f286710be
orimarco <marcovitch.ori@gmail.com>
2017-03-09 20:28:13 +0200

    with files now

---
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/methods/PojoConstructor.java
@@ -0,0 +1,18 @@
+package il.org.spartan.spartanizer.research.nanos.methods;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.research.nanos.common.*;
+
+/** Constructor just assigning fields
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2017-03-09 */
+public class PojoConstructor extends JavadocMarkerNanoPattern {
+  private static final long serialVersionUID = 4172021544627831813L;
+
+  @Override public boolean prerequisites(final MethodDeclaration ¢) {
+    return iz.constructor(¢)//
+        && setter(¢);
+  }
+}
