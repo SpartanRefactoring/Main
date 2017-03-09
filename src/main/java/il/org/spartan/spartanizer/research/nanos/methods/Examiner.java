a0cd553104e3427c99924772cf588549ae44f34c
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-10 00:46:36 +0200

    Simplify initializiation

--- src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
@@ -6,21 +6,19 @@ import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.*;
 import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
-/** One statement method returning boolean expressio
+/** One statement method returning boolean expression
  * @author Ori Marcovitch */
 public class Examiner extends JavadocMarkerNanoPattern {
   private static final long serialVersionUID = 7361477859663262247L;
-  private static final Collection<UserDefinedTipper<Statement>> tippers = new HashSet<UserDefinedTipper<Statement>>() {
-    @SuppressWarnings("hiding") static final long serialVersionUID = 1L;
-    {
-      add(patternTipper("return $X;", "", ""));
-      add(patternTipper("synchronized ($X1) { return $X2;}", "", ""));
-    }
-  };
+  private static final Collection<UserDefinedTipper<Statement>> tippers = new HashSet<>(as.list(//
+      patternTipper("return $X;", "", ""), //
+      patternTipper("synchronized ($X1) { return $X2;}", "", "")//
+  ));
 
   @Override protected boolean prerequisites(final MethodDeclaration ¢) {
     return haz.booleanReturnType(¢)//
