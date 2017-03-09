938a36323c048af61aff37d8672e1f3d01348d91
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-10 01:20:22 +0200

    document class lazy

--- SpartanizerPlugin/src/main/java/il/org/spartan/utils/lazy.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/utils/lazy.java
@@ -2,6 +2,12 @@ package il.org.spartan.utils;
 
 import java.util.function.*;
 
+/**
+ * neat class for lazy initialization  
+ * @param <T>
+ * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
+ * @since 2017-03-10
+ */
 public interface lazy<T> extends Supplier<T> {
   static <T> lazy<T> get(final Supplier<T> ¢) {
     return new lazy<T>() {
