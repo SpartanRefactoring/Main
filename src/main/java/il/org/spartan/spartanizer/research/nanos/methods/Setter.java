3bf93ff4350fdefce36f65070ed058ffc6853b47
orimarco <marcovitch.ori@gmail.com>
2017-03-09 20:14:44 +0200

    clean

--- src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Setter.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Setter.java
@@ -1,15 +1,7 @@
 package il.org.spartan.spartanizer.research.nanos.methods;
 
-import static il.org.spartan.spartanizer.research.TipperFactory.*;
-
-import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
-import static il.org.spartan.spartanizer.ast.navigate.step.*;
-
-import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 import il.org.spartan.utils.*;
 
@@ -18,25 +10,11 @@ import il.org.spartan.utils.*;
  * @since 2016-10-22 */
 public class Setter extends JavadocMarkerNanoPattern {
   private static final long serialVersionUID = -4815724471383478205L;
-  private static final Collection<UserDefinedTipper<Expression>> tippers = new HashSet<UserDefinedTipper<Expression>>() {
-    @SuppressWarnings("hiding") static final long serialVersionUID = 1L;
-    {
-      add(patternTipper("this.$N1 = $N2", "", ""));
-      add(patternTipper("this.$N1 = $L", "", ""));
-      add(patternTipper("$N1 = $N2", "", ""));
-      add(patternTipper("$N1 = $L", "", ""));
-    }
-  };
 
   @Override public boolean prerequisites(final MethodDeclaration ¢) {
-    final List<String> $ = parametersNames(¢);
     ___.nothing();
     return notConstructor(¢)//
-        && notEmpty(¢)
-        && statements(¢).stream().allMatch(λ -> anyTips(tippers, expression(λ)) && isRightSideOK(right(az.assignment(expression(λ))), $));
-  }
-
-  private static boolean isRightSideOK(final Expression $, final Collection<String> paramNames) {
-    return iz.literal($) || paramNames.contains(identifier(az.name($)));
+        && notEmpty(¢) //
+        && setter(¢);
   }
 }
