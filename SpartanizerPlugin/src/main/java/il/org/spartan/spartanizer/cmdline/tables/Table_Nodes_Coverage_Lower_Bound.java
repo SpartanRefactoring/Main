083110863773c4cc1899bbc5e05ff1a685ffc0d7
orimarco <marcovitch.ori@gmail.com>
2017-03-09 12:31:57 +0200

    to new API

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nodes_Coverage_Lower_Bound.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Nodes_Coverage_Lower_Bound.java
@@ -1,23 +1,12 @@
 package il.org.spartan.spartanizer.cmdline.tables;
 
-import java.lang.reflect.*;
-
 import org.eclipse.jdt.core.dom.*;
 
 /** Generates table presenting lower bound on {@link ASTNode}s coverage
  * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
  * @since 2017-03-07 */
 public class Table_Nodes_Coverage_Lower_Bound extends Table_Nodes_Coverage {
-  static {
-    clazz = Table_Nodes_Coverage_Lower_Bound.class;
-  }
-
-  public static void main(final String[] args)
-      throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-    Table_Nodes_Coverage.main(args);
-  }
-
-  @Override protected String analyze(final String from) {
-    return spartanalyzer.once(from);
+  {
+    analyze = λ -> spartanalyzer.once(λ);
   }
 }
