1fd6cfba1cdd0e0a6446faaa8d7da73d98b0d9c0
orimarco <marcovitch.ori@gmail.com>
2017-03-09 23:21:16 +0200

    reafctor, another NPE

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Getter.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Getter.java
@@ -1,30 +1,17 @@
 package il.org.spartan.spartanizer.research.nanos.methods;
 
-import static il.org.spartan.spartanizer.research.TipperFactory.*;
-
-import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
-/** TODO: Ori Marcovitch please add a description
- * @author Ori Marcovitch
- * @since 2016 */
+/** Matches getter methods
+ * @author Ori Marcovitch */
 public class Getter extends JavadocMarkerNanoPattern {
   private static final long serialVersionUID = -6493535033790617317L;
-  private static final Collection<UserDefinedTipper<Statement>> tippers = new HashSet<UserDefinedTipper<Statement>>() {
-    @SuppressWarnings("hiding") static final long serialVersionUID = 1L;
-    {
-      add(patternTipper("return $N;", "", ""));
-      add(patternTipper("return this.$N;", "", ""));
-      add(patternTipper("return ($T)$N;", "", ""));
-      add(patternTipper("return ($T)this.$N;", "", ""));
-    }
-  };
 
   @Override protected boolean prerequisites(final MethodDeclaration ¢) {
-    return hazOneStatement(¢) && hazNoParameters(¢) && anyTips(tippers, onlyStatement(¢));
+    return hazOneStatement(¢)//
+        && hazNoParameters(¢)//
+        && getter(¢);
   }
 }
