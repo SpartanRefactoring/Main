a02a8fd25af36f7baffac26ead53c91663875b57
orimarco <marcovitch.ori@gmail.com>
2017-03-10 11:10:41 +0200

    new syntax

--- src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/methods/Examiner.java
@@ -2,20 +2,16 @@ package il.org.spartan.spartanizer.research.nanos.methods;
 
 import static il.org.spartan.spartanizer.research.TipperFactory.*;
 
-import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.*;
 import il.org.spartan.spartanizer.java.*;
-import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.research.nanos.common.*;
 
 /** One statement method returning boolean expression
  * @author Ori Marcovitch */
 public class Examiner extends JavadocMarkerNanoPattern {
   private static final long serialVersionUID = 7361477859663262247L;
-  private static final Collection<UserDefinedTipper<Statement>> tippers = as.list(//
+  private static final NanoPatternContainer<Statement> tippers = new NanoPatternContainer<>(//
       patternTipper("return $X;", "", ""), //
       patternTipper("synchronized ($X1) { return $X2;}", "", "")//
   );
