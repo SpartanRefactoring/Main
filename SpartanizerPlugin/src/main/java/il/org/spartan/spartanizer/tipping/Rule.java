fd370aeb23fd4bb9fbaf46003b8a524a64b9fb77
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-12 07:35:04 +0200

    sync

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tipping/Rule.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tipping/Rule.java
@@ -1,7 +1,12 @@
 package il.org.spartan.spartanizer.tipping;
 
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.tipping.Tipper.*;
+import il.org.spartan.utils.*;
 
 /** An abstract interface defining tippers, bloaters, and light weight pattern
  * search, logging, computing stats, etc.
@@ -9,18 +14,18 @@ import il.org.spartan.spartanizer.tipping.Tipper.*;
  * @param <T>
  * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
  * @since 2017-03-10 */
-public interface Rule<N, T> {
+public interface Rule<N, T> extends Multiplexor<Rule<N, T>> {
+  /** Should be overridden */
+  default String[] akas() {
+    return new String[] { technicalName() };
+  }
+
   /** Determine whether the parameter is "eligible" for application of this
    * instance. Should be overridden
    * @param n JD
    * @return <code><b>true</b></code> <i>iff</i> the argument is eligible for
    *         the simplification offered by this object. */
   boolean check(N n);
-
-  /** Should be overridden */
-  default String[] akas() {
-    return new String[] { technicalName() };
-  }
 
   /** Should be overridden */
   default String describe(final N ¢) {
@@ -43,6 +48,11 @@ public interface Rule<N, T> {
   }
 
   /** Should not be overridden */
+  @Override default Stream<Rule<N, T>> stream() {
+    return Stream.of(this);
+  }
+
+  /** Should not be overridden */
   default String technicalName() {
     return getClass().getSimpleName();
   }
@@ -57,64 +67,119 @@ public interface Rule<N, T> {
     return String.format("apply '%s' to '%%s'", technicalName());
   }
 
-  abstract class Stateful<R, T> implements Rule<R, T> {
-    private R current;
-
-    
-    @Override public final boolean check(R n) {
-      return ok(n); 
+  abstract class CountingDelegator<R, T> extends Delegator<R, T> {
+    public CountingDelegator(final Rule<R, T> inner) {
+      super(inner);
     }
 
-    abstract boolean ok(R n);
+    @Override public Void before(final String key, final Object... arguments) {
+      count.putIfAbsent(key, Integer.valueOf(0));
+      count.put(key, Box.it(count.get(key).intValue() + 1));
+      return super.before(key, arguments);
+    }
+
+    Map<String, Integer> count = new LinkedHashMap<>();
+  }
+
+  @SuppressWarnings("static-method")
+  abstract class Delegator<N, T> extends Stateful<N, T> implements Listener<N, T> {
+    public Delegator(final Rule<N, T> inner) {
+      this.inner = inner;
+    }
+
+    @Override public final String[] akas() {
+      before("akas");
+      return listenAkas(() -> inner.akas());
+    }
+
+    public Void before(@SuppressWarnings("unused") final String key, @SuppressWarnings("unused") final Object... arguments) {
+      return null;
+    }
+
+    @Override public final String describe(final N n) {
+      return listenDescribe((final N x) -> inner.describe(x), n);
+    }
+
+    @Override public final String description() {
+      return listenDescription(() -> inner.description());
+    }
+
+    @Override public final String description(final N ¢) {
+      return listenDescription(() -> inner.description(¢));
+    }
+
+    @Override public final Example[] examples() {
+      return listenExamples(() -> inner.examples());
+    }
+
+    @Override public boolean ok(final N ¢) {
+      before("ok");
+      return listenOk(() -> inner.check(¢));
+    }
+
+    @Override public final String technicalName() {
+      return listenTechnicalName(() -> inner.technicalName());
+    }
+
+    @Override public final T tip(final N ¢) {
+      before("tip");
+      return listenTip(λ -> inner.tip(λ), ¢);
+    }
+
+    @Override public final String verb() {
+      before("verb");
+      return listenVerb(() -> inner.verb());
+    }
+
+    public final Rule<N, T> inner;
+  }
+
+  interface Listener<N, T> {
+    default String[] listenAkas(final Supplier<String[]> $) {
+      return $.get();
+    }
+
+    /** [[SuppressWarningsSpartan]] */
+    default String listenDescribe(final Function<N, String> f, final N n) {
+      return f.apply(n);
+    }
+
+    default String listenDescription(final Supplier<String> $) {
+      return $.get();
+    }
+
+    default Example[] listenExamples(final Supplier<Example[]> $) {
+      return $.get();
+    }
+
+    default boolean listenOk(final BooleanSupplier ¢) {
+      return ¢.getAsBoolean();
+    }
+
+    default String listenTechnicalName(final Supplier<String> $) {
+      return $.get();
+    }
+
+    default T listenTip(final Function<N, T> f, final N n) {
+      return f.apply(n);
+    }
+
+    default String listenVerb(final Supplier<String> $) {
+      return $.get();
+    }
+  }
+
+  abstract class Stateful<R, T> implements Rule<R, T> {
+    @Override public final boolean check(final R n) {
+      return ok(n);
+    }
 
     public R current() {
       return current;
     }
 
+    abstract boolean ok(R n);
 
-  }
-
-  abstract class Delegator<N, T> extends Stateful<N, T> {
-    public final Rule<N, T> inner;
-
-    @Override public boolean ok(final N ¢) {
-      return inner.check(¢);
-    }
-
-    @Override public String[] akas() {
-      return inner.akas();
-    }
-
-    @Override public String describe(final N ¢) {
-      return inner.describe(¢);
-    }
-
-    @Override public String description() {
-      return inner.description();
-    }
-
-    @Override public String description(final N ¢) {
-      return inner.description(¢);
-    }
-
-    @Override public Example[] examples() {
-      return inner.examples();
-    }
-
-    @Override public String technicalName() {
-      return inner.technicalName();
-    }
-
-    @Override public T tip(final N ¢) {
-      return inner.tip(¢);
-    }
-
-    @Override public String verb() {
-      return inner.verb();
-    }
-
-    public Delegator(final Rule<N, T> inner) {
-      this.inner = inner;
-    }
+    private R current;
   }
 }
