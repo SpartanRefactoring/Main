1b77fd8e599a471e99ebaf492eddd16b354360c9
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-13 20:52:40 +0200

    Assignment for Dor/Oran.

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/Recursive.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/utils/Recursive.java
@@ -1,11 +1,39 @@
 package il.org.spartan.spartanizer.utils;
+
 import java.util.stream.*;
 
+import il.org.spartan.spartanizer.java.namespace.*;
 import il.org.spartan.utils.*;
+import junit.framework.*;
 
-/** Organizes object of the type parameter in a tree, supplying a
- * {@link #descendants()} of all inner instances
- * @param <T>
+/** Organizes objects of the type parameter in a tree, supplying a
+ * {@link #descendants()} of all objects contained in a sub-tree. Object of the
+ * type parameter may present on all nodes on of this tree, including inner
+ * nodes.
+ * <p>
+ * TODO Dor Maayan & Oran Gilboa: first assignment
+ * <ol>
+ * <li>Apply self judgment, don't come running to me with every little dilemma
+ * you have.
+ * <li>Implement interface {@link Nested} for the same concept, except that
+ * stream return ancestors.
+ * <li>Factor commonalities between the new interface and this interface. What
+ * should the new interface be called?
+ * <li>Create a new interface that unifies {@link Nested} and {@link Recursive},
+ * to be called what?
+ * <li>Re-implement class {@link Environment} using the diamond hierarchy that
+ * you are
+ * <li>Revise material on design patterns. Do you think interface names should
+ * be changed? How? Write your answer as JavaDOC: don't bother with formatting.
+ * <li>Otherwise, don't touch any documentation, before you attend the JavaDOC
+ * lecture. Do add {@code @author} and {@code @since}, but nothing more. You
+ * will have to document this interface, and all methods, but only after that.
+ * <li>If there are {@code }{@link Test} methods, make sure they run. Don't add
+ * any tests, even if you think you know what you are doing, before attending
+ * the TDD lecture.
+ * </ol>
+ * @see Compound
+ * @see Atomic
  * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since 2017-03-11 */
 public interface Recursive<@JD T> {
@@ -26,16 +54,16 @@ public interface Recursive<@JD T> {
    * @since 2017-03-13 */
   interface Atomic<@JD T> extends Recursive<T> {
     @Override default Stream<T> descendants() {
-      return streamSelf(); 
-    } 
+      return streamSelf();
+    }
   }
 
-
-  /** A compound recursive structure, specializing {@link Recursive} 
+  /** A compound recursive structure, specializing {@link Recursive}
    * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
    * @since 2017-03-13 */
   interface Compound<@JD T> extends Recursive<T> {
     Iterable<Recursive<T>> children();
+
     @Override default T self() {
       return null;
     }
@@ -46,7 +74,7 @@ public interface Recursive<@JD T> {
       Stream<E> $ = Stream.empty();
       for (final Recursive<E> ¢ : children())
         $ = Stream.concat(¢.descendants(), $);
-       return Stream.concat($, streamSelf());
+      return Stream.concat($, streamSelf());
     }
   }
 
@@ -57,7 +85,7 @@ public interface Recursive<@JD T> {
    * @since 2017-03-13 */
   interface Preorder<E> extends Compound<E> {
     @Override default Stream<E> descendants() {
-      Stream<E> $ = self() == null? Stream.empty(): Stream.of(self()); 
+      Stream<E> $ = self() == null ? Stream.empty() : Stream.of(self());
       for (final Recursive<E> ¢ : children())
         $ = Stream.concat($, ¢.descendants());
       return $;
