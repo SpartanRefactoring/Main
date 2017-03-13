72a3f714f83b28c9d94277e340214f5d3b016c17
orimarco <marcovitch.ori@gmail.com>
2017-03-13 21:42:23 +0200

    sync

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_Summary2.java
@@ -0,0 +1,196 @@
+package il.org.spartan.spartanizer.cmdline.tables;
+
+import java.util.*;
+import java.util.function.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.text.edits.*;
+
+import static il.org.spartan.spartanizer.ast.navigate.wizard.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.cmdline.*;
+import il.org.spartan.spartanizer.cmdline.nanos.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.research.*;
+import il.org.spartan.spartanizer.research.analyses.*;
+import il.org.spartan.spartanizer.research.util.*;
+import il.org.spartan.spartanizer.utils.*;
+import il.org.spartan.tables.*;
+import il.org.spartan.utils.*;
+
+/** Generates a table summarizing important statistics about nano patterns
+ * @author orimarco <tt>marcovitch.ori@gmail.com</tt>
+ * @since 2016-12-25 */
+public class Table_Summary2 {
+  static final AgileSpartanizer spartanizer = new AgileSpartanizer();
+  static final CompilationUnitCoverageStatistics statistics = new CompilationUnitCoverageStatistics();
+  static final SpartAnalyzer spartanalyzer = new SpartAnalyzer();
+  protected static Function<String, String> analyze = spartanalyzer::fixedPoint;
+  private static final NanoPatternsStatistics npStatistics = new NanoPatternsStatistics();
+  static final NanoPatternsOccurencesStatistics npDistributionStatistics = new NanoPatternsOccurencesStatistics();
+  static final Stack<MethodRecord> scope = new Stack<>();
+  static Table writer;
+  protected static final SortedMap<Integer, List<MethodRecord>> statementsCoverageStatistics = new TreeMap<>(Integer::compareTo);
+  static {
+    Logger.subscribe(Table_Summary2::logNanoContainingMethodInfo);
+    Logger.subscribe(npStatistics::logNPInfo);
+    Logger.subscribe(npDistributionStatistics::logNPInfo);
+  }
+
+  public static void main(final String[] args) {
+    new FileSystemASTVisitor(args) {
+      @Override protected void done(final String path) {
+        summarize(path);
+        resetAll();
+      }
+
+      public void summarize(final String path) {
+        if (writer == null)
+          initializeWriter();
+        writer//
+            .col("Project", path)//
+            .col("Commands", statementsCoverage())//
+            .col("Expressions", expressionsCoverage())//
+            .col("methodsCovered", fMethods())//
+            .col("methodsTouched", touched())//
+            .col("Iteratives", iterativesCoverage())//
+            .col("ConditionalExpressions", conditionalExpressionsCoverage())//
+            .col("ConditionalCommands", conditionalStatementsCoverage())//
+            .col("total Commands", statements())//
+            .col("total Methods", methods())//
+            .nl();
+      }
+    }.fire(new ASTVisitor(true) {
+      @Override public boolean visit(final MethodDeclaration ¢) {
+        if (excludeMethod(¢))
+          return true;
+        try {
+          final MethodRecord m = new MethodRecord(¢);
+          scope.push(m);
+          final MethodDeclaration d = findFirst.instanceOf(MethodDeclaration.class)
+              .in(ast(Wrap.Method.off(spartanalyzer.fixedPoint(Wrap.Method.on(¢ + "")))));
+          if (d != null)
+            npDistributionStatistics.logNode(d);
+          final Integer key = Integer.valueOf(measure.commands(d));
+          statementsCoverageStatistics.putIfAbsent(key, new ArrayList<>());
+          statementsCoverageStatistics.get(key).add(m);
+        } catch (final AssertionError __) {
+          ___.unused(__);
+        }
+        return true;
+      }
+
+      @Override public boolean visit(final FieldDeclaration ¢) {
+        spartanalyzer.fixedPoint(ast(¢ + ""));
+        return true;
+      }
+
+      @Override public void endVisit(final MethodDeclaration ¢) {
+        if (!excludeMethod(¢))
+          scope.pop();
+      }
+
+      @Override public boolean visit(final CompilationUnit ¢) {
+        ¢.accept(new AnnotationCleanerVisitor());
+        try {
+          statistics.logCompilationUnit(¢);
+          final String spartanzied = spartanizer.fixedPoint(¢);
+          statistics.logAfterSpartanization(into.cu(spartanzied));
+          analyze.apply(spartanzied);
+        } catch (final AssertionError | MalformedTreeException | IllegalArgumentException __) {
+          ___.unused(__);
+        }
+        return true;
+      }
+    });
+    writer.close();
+  }
+
+  static void resetAll() {
+    statementsCoverageStatistics.clear();
+    npDistributionStatistics.clear();
+    npStatistics.clear();
+    scope.clear();
+  }
+
+  static boolean excludeMethod(final MethodDeclaration ¢) {
+    return iz.constructor(¢)//
+        || step.body(¢) == null//
+        || extract.annotations(¢).stream().anyMatch(λ -> "@Test".equals(λ + ""));
+  }
+
+  private static void logNanoContainingMethodInfo(final ASTNode n, final String np) {
+    if (!containedInInstanceCreation(n))
+      scope.peek().markNP(n, np);
+  }
+
+  static void initializeWriter() {
+    writer = new Table(Table_Summary2.class);
+  }
+
+  static int statements() {
+    return statementsCoverageStatistics.keySet().stream().mapToInt(λ -> Unbox.it(λ) * statementsCoverageStatistics.get(λ).size()).sum();
+  }
+
+  private static int statementsCovered() {
+    return statementsCoverageStatistics.values().stream().flatMap(Collection::stream).mapToInt(MethodRecord::numNPStatements).sum();
+  }
+
+  private static int expressionsCovered() {
+    return statementsCoverageStatistics.values().stream().flatMap(Collection::stream).mapToInt(MethodRecord::numNPExpressions).sum();
+  }
+
+  private static int expressions() {
+    return statementsCoverageStatistics.values().stream().flatMap(Collection::stream).mapToInt(λ -> λ.numExpressions).sum();
+  }
+
+  static int methods() {
+    return (int) statementsCoverageStatistics.values().stream().mapToLong(Collection::size).sum();
+  }
+
+  static double fMethods() {
+    return getNodeCoverage(ASTNode.METHOD_DECLARATION);
+  }
+
+  private static double getNodeCoverage(final int type) {
+    return npDistributionStatistics.coverage(type);
+  }
+
+  static double iterativesCoverage() {
+    return getNodeCoverage(ASTNode.ENHANCED_FOR_STATEMENT);
+  }
+
+  static double conditionalExpressionsCoverage() {
+    return getNodeCoverage(ASTNode.CONDITIONAL_EXPRESSION);
+  }
+
+  static double conditionalStatementsCoverage() {
+    return getNodeCoverage(ASTNode.IF_STATEMENT);
+  }
+
+  static double touched() {
+    return format.perc(methodsTouched(), methods() - methodsCovered());
+  }
+
+  static double statementsCoverage() {
+    return format.perc(statementsCovered(), statements());
+  }
+
+  static double expressionsCoverage() {
+    return format.perc(expressionsCovered(), expressions());
+  }
+
+  private static int methodsTouched() {
+    return (int) statementsCoverageStatistics.values().stream().flatMap(Collection::stream).filter(λ -> λ.touched() && !λ.fullyMatched()).count();
+  }
+
+  private static int methodsCovered() {
+    return (int) statementsCoverageStatistics.values().stream().flatMap(Collection::stream).filter(MethodRecord::fullyMatched).count();
+  }
+
+  private static boolean containedInInstanceCreation(final ASTNode ¢) {
+    return yieldAncestors.untilClass(ClassInstanceCreation.class).from(¢) != null;
+  }
+}
