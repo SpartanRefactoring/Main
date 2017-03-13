db945ad8f7f9cf0bed333901d53c79f970e24107
Yossi Gil <yossi.gil@gmail.com>
2017-03-13 15:42:20 +0200

    Documentation

--- src/main/java/il/org/spartan/utils/lazy.java
+++ src/main/java/il/org/spartan/utils/lazy.java
@@ -2,12 +2,17 @@ package il.org.spartan.utils;
 
 import java.util.function.*;
 
-/** neat class for lazy initialization
+/** lazy initialization as in {@code
+    static final lazy<Collection<Thing>> things = lazy.get(() -> as.list(//
+      new Thing("one"); //
+      new Thing("two"); //
+  ));} use {@code things.get()} to obtain value; it would be computed only on
+ * first call, and cached hence after
  * @param <T>
  * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
  * @since 2017-03-10 */
 public interface lazy<T> extends Supplier<T> {
-  static <T> lazy<T> get(final Supplier<T> ¢) {
+  static <T> lazy<T> get(@JD final Supplier<T> ¢) {
     return new lazy<T>() {
       T $;
 
