f23a4e2f8da74deb8e79d776eb575ceba0986c26
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-03-15 20:27:27 +0200

    removed if else

--- src/main/java/il/org/spartan/spartanizer/tipping/ReplaceCurrentNode.java
+++ src/main/java/il/org/spartan/spartanizer/tipping/ReplaceCurrentNode.java
@@ -18,22 +18,22 @@ public abstract class ReplaceCurrentNode<N extends ASTNode> extends CarefulTippe
   @Override public final Tip tip(final N n) {
     assert prerequisite(n) : fault.dump() + "\n n = " + n + fault.done();
     final ASTNode $ = replacement(n);
-//    System.out.println("-------------------");
-    if($ == null)
-      return null;
-    else {
-      return new Tip(description(n), n, getClass()) {
-        @Override public void go(final ASTRewrite r, final TextEditGroup g) {
-//          System.out.println("++++++++++++++++");
-          r.replace(n, $, g);
-        }
-      };
-    }
-//    return $ == null ? null : new Tip(description(n), n, getClass()) {
-//      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
-//        System.out.println("++++++++++++++++");
-//        r.replace(n, $, g);
-//      }
-//    };
+////    System.out.println("-------------------");
+//    if($ == null)
+//      return null;
+//    else {
+//      return new Tip(description(n), n, getClass()) {
+//        @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+////          System.out.println("++++++++++++++++");
+//          r.replace(n, $, g);
+//        }
+//      };
+//    }
+    return $ == null ? null : new Tip(description(n), n, getClass()) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+        System.out.println("++++++++++++++++");
+        r.replace(n, $, g);
+      }
+    };
   }  
 }
