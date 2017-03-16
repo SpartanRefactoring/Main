fd6292cc502eb1e3f9349086c9d4b733e9a1aff0
Yossi Gil <yossi.gil@gmail.com>
2017-03-16 12:46:21 +0200

    More work #62

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue0062.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue0062.java
@@ -2,11 +2,13 @@ package il.org.spartan.spartanizer.issues;
 
 import static il.org.spartan.spartanizer.testing.TestsUtilsTrimmer.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** Unit test for the GitHub issue thus numbered. case of inlining into the
- * expression of an enhanced for
+import il.org.spartan.spartanizer.tippers.*;
+
+/** Unit test for the GitHub issue thus numbered.
  * @author Yossi Gil <tt>yossi.gil@gmail.com</tt>
  * @since 2017-03-16 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -14,10 +16,29 @@ import org.junit.runners.*;
 public class Issue0062 {
   @Test public void a() {
     trimmingOf("int f(int i) { for(;i<100;i=i+1) if(false) break; return i; }")//
-        .gives("int f(int i) { for(;i<100;i+=1) if(false) break; return i; }")//
-        .gives("int f(int i) { for(;i<100;i++) if(false) break; return i; }")//
-        .gives("int f(int i) { for(;i<100;++i) if(false) break; return i; }")//
-        .gives("int f(int i) { for(;i<100;++i){} return i; }")//
+        .gives("int f(int ¢){for(;¢<100;¢=¢+1)if(false)break;return ¢;}") //
+        .gives("int f(int ¢){for(;¢<100;¢+=1){}return ¢;}") //
         .stays();
   }
+
+  /** Introduced by Yossi on Thu-Mar-16-12:37:12-IST-2017 (code automatically
+   * generated in 'il.org.spartan.spartanizer.cmdline.anonymize.java') */
+  @Test public void b() {
+    trimmingOf("int a(int b) { for (; b < 100; b = b + 1) if (false) break; return b; }") //
+        .using(Assignment.class, new AssignmentToFromInfixIncludingTo()) //
+        .gives("int a(int b){for(;b<100;b+=1)if(false)break;return b;}") //
+        .using(IfStatement.class, new IfTrueOrFalse()) //
+        .gives("int a(int b){for(;b<100;b+=1){}return b;}") //
+        .stays() //
+    ;
+  }
+  @Test public void intaIntbForb100bb1IfFalseBreakReturnb() {
+    trimmingOf("int a(int b) { for (; b < 100; b = b + 1) if (false) break; return b; }") //
+  .using(Assignment.class, new AssignmentToFromInfixIncludingTo()) //
+  .gives("int a(int b){for(;b<100;b+=1)if(false)break;return b;}") //
+  .using(IfStatement.class, new IfTrueOrFalse()) //
+  .gives("int a(int b){for(;b<100;b+=1){}return b;}") //
+   .stays() //
+   ;
+ }
 }
