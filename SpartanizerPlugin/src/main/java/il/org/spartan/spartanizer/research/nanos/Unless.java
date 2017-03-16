55438ff06d9eccac42e85c16a6b0ee7ee45bbda7
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-16 15:50:38 +0200

    Format

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/Unless.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nanos/Unless.java
@@ -18,7 +18,8 @@ public final class Unless extends NanoPatternTipper<ConditionalExpression> {
   private static final long serialVersionUID = -3872855694179875392L;
   private static final List<UserDefinedTipper<ConditionalExpression>> tippers = as.list(
       patternTipper("$X1 ? $D : $X2", "unless($X1).eval(() -> $X2).defaultTo($D)", "Go fluent: Unless pattern"),
-      patternTipper("$X1  ? $X2 : $D", "unless(!$X1).eval(() -> $X2).defaultTo($D)", "Go fluent: Unless pattern"));
+      patternTipper("$X1  ? $X2 : $D", "unless(!$X1).eval(() -> $X2).defaultTo($D)", "Go fluent: Unless pattern")//
+      );
   private static final Collection<NanoPatternTipper<ConditionalExpression>> rivals = as.list(new DefaultsTo(), new SafeReference());
 
   @Override public boolean canTip(final ConditionalExpression ¢) {
