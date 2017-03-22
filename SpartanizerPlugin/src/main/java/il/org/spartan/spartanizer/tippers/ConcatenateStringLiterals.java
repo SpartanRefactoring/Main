4d247c403de3f271e982c309798bd58c6ff51bb0
Doron Mehsulam <doronmmm@hotmail.com>
2017-03-22 21:50:10 +0200

    Starting #1150, programming through Niv's user

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/ConcatenateStringLiterals.java
@@ -0,0 +1,8 @@
+package il.org.spartan.spartanizer.tippers;
+
+/**
+ * TODO Doron Mehsulam: document class 
+ * @author Doron Mehsulam <tt>doronmmm@hotmail.com</tt>
+ * @since 2017-03-22
+ */
+public class ConcatenateStringLiterals {}
