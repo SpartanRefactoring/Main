30c374258714e6d68d73ddef27d6f9507c0be585
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-22 18:11:36 +0200

    warning

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tipping/MultipleReplaceCurrentNode.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tipping/MultipleReplaceCurrentNode.java
@@ -26,6 +26,7 @@ public abstract class MultipleReplaceCurrentNode<N extends ASTNode> extends Care
 
   @Override public final Tip tip(final N n) {
     return new Tip(description(n), n, myClass()) {
+      @SuppressWarnings("boxing")
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final List<ASTNode> input = new ArrayList<>(), output = new ArrayList<>();
         MultipleReplaceCurrentNode.this.go(r, n, g, input, output);
