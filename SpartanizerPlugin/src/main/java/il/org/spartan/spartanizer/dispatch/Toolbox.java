e343a729216dcd870bc80631499410eddba61439
Snorg <shalmon.niv@gmail.com>
2017-03-22 22:51:33 +0200

    issue #1150 done

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
@@ -229,6 +229,7 @@ public class Toolbox {
             new InfixIndexOfToStringContains(), // v 2.7
             new InfixSimplifyComparisionOfAdditions(), //
             new InfixSimplifyComparisionOfSubtractions(), //
+            new ConcatenateStringLiterals(),//
             null)
         .add(MethodDeclaration.class, //
             new AnnotationSort<>(), //
