01f1d805d23b9a483ecfdd57faa96e99279ecc3d
orenafek <oren.afek@gmail.com>
2017-03-22 17:46:24 +0200

    First Implemntation of Nested.

---
+++ src/main/java/il/org/spartan/utils/Nested.java
@@ -0,0 +1,32 @@
+package il.org.spartan.utils;
+
+import java.util.stream.*;
+
+public interface Nested<@¢ T> {
+  default Stream<T> ancestors() {
+    return Stream.empty();
+  }
+
+  default T self() {
+    return null;
+  }
+
+  default Stream<T> streamSelf() {
+    return self() == null ? Stream.empty() : Stream.of(self());
+  }
+
+  interface Root<@¢ T> extends Nested<T> {
+    @Override default Stream<T> ancestors() {
+      return streamSelf();
+    }
+  }
+
+  interface Compound<@¢ T> extends Nested<T> {
+    Nested<T> parent();
+
+    @Override
+    default Stream<T> ancestors() {
+      return Stream.concat(parent().ancestors(), streamSelf());
+    }
+  }
+}
