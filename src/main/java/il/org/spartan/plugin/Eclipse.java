3458346bfae7a6fadbd7f63c548157d8b4d98461
Ori Roth <ori.rothh@gmail.com>
2017-03-22 04:51:41 +0200

    work on widget

--- src/main/java/il/org/spartan/plugin/Eclipse.java
+++ src/main/java/il/org/spartan/plugin/Eclipse.java
@@ -3,6 +3,9 @@ package il.org.spartan.plugin;
 import java.text.*;
 import java.util.*;
 
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+
 /** Eclipse common utilities.
  * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
  * @since 2017-03-21 */
@@ -23,5 +26,11 @@ public class Eclipse {
    * @return current date */
   public static String date(final String format) {
     return new SimpleDateFormat(format).format(new Date());
+  }
+
+  /** @return current mouse location */
+  public static Point mouseLocation() {
+    return Optional.ofNullable(Display.getCurrent()) //
+        .map(λ -> λ.getCursorLocation()).orElse(new Point(0, 0));
   }
 }
