477320797bb7d5ebb5edf05bd74d6be4c74288df
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-23 07:56:51 +0200

    Renaming

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragementInitializerStatement.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragementInitializerStatement.java
@@ -9,7 +9,7 @@ import il.org.spartan.spartanizer.engine.*;
 /** TODO Yossi Gil {@code Yossi.Gil@GMail.COM} please add a description
  * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since Sep 25, 2016 */
-public abstract class FragementInitializerStatement extends FragmentTipper {
+public abstract class FragementInitializerStatement extends $FragmentTipper {
   @Override public boolean prerequisite(final VariableDeclarationFragment ¢) {
     return super.prerequisite(¢) && initializer() != null;
   }
