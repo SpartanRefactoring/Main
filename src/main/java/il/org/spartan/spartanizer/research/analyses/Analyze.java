49397f89d8c40aa9fc97f67b7dc0247ff2db9425
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-23 03:51:43 +0200

    Sync

--- src/main/java/il/org/spartan/spartanizer/research/analyses/Analyze.java
+++ src/main/java/il/org/spartan/spartanizer/research/analyses/Analyze.java
@@ -36,7 +36,7 @@ public enum Analyze {
   }
   private static InteractiveSpartanizer spartanizer;
   @SuppressWarnings("rawtypes") private static final Map<String, Analyzer> analyses = new HashMap<String, Analyzer>() {
-    private static final long serialVersionUID = 1318990517359365384L;
+    static final long serialVersionUID = 1318990517359365384L;
     {
       put("AvgIndicatorMetrical", new AvgIndicatorMetricalAnalyzer());
       put("understandability", new UnderstandabilityAnalyzer());
--- src/main/java/il/org/spartan/spartanizer/tippers/FragmentTipperNoInitializer.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/FragmentTipperNoInitializer.java
@@ -8,7 +8,7 @@ import org.eclipse.jdt.core.dom.*;
 public abstract class FragmentTipperNoInitializer extends FragmentTipper {
   private static final long serialVersionUID = 3636250496963900917L;
 
-  @Override public boolean prerequisite(final VariableDeclarationFragment f) {
-    return super.prerequisite(f) && initializer() == null;
+  @Override public boolean prerequisite(final VariableDeclarationFragment ¢) {
+    return super.prerequisite(¢) && initializer() == null;
   }
 }
