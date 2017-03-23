c45475048f25a8f6d91b38c5fd886925165b3496
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-23 07:57:34 +0200

    Moving

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/InfixIndexOfToStringContains.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/InfixIndexOfToStringContains.java
@@ -9,6 +9,7 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.research.*;
 import il.org.spartan.spartanizer.tipping.*;
 
