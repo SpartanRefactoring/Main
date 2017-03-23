8c4837d14914539d168fb19d69ab95e492e7e47d
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-23 08:11:52 +0200

    renaming

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragmentInitializerIfUpdateAssignment.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragmentInitializerIfUpdateAssignment.java
@@ -23,7 +23,7 @@ import il.org.spartan.spartanizer.engine.Inliner.*;
  * }
  * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since 2015-08-07 */
-public final class FragmentInitializerIfUpdateAssignment extends $FragementAndStatement//
+public final class FragmentInitializerIfUpdateAssignment extends $FragmentAndStatement//
     implements TipperCategory.Inlining {
   private static final long serialVersionUID = 3617599837633261268L;
 
