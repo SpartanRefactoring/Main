b41e6dfe4a7d1bd8c94fc1c156c389f644151f43
Yossi Gil <yossi.gil@gmail.com>
2017-03-24 09:12:15 +0300

    Spartanize

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/AssignmentAndUpdateAssignmentToSame.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/AssignmentAndUpdateAssignmentToSame.java
@@ -33,7 +33,7 @@ import il.org.spartan.utils.*;
  * @since 2017-03-04 */
 public final class AssignmentAndUpdateAssignmentToSame extends GoToNextStatement<Assignment>//
     implements TipperCategory.Unite {
-  private static final long serialVersionUID = 6704758959472646819L;
+  private static final long serialVersionUID = 0x5D0C16C013FC1AA3L;
 
   @Override @NotNull public Example[] examples() {
     return new Example[] { //
