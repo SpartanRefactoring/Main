4a0d8c4f259f4b1b3c0273b8142bf29f6722632e
Yossi Gil <yossi.gil@gmail.com>
2017-03-24 23:02:07 +0300

    More thread safe

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_RawNanoStatistics.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/cmdline/tables/Table_RawNanoStatistics.java
@@ -52,8 +52,10 @@ public class Table_RawNanoStatistics extends NanoTable {
       }
 
       void initializeWriter() {
-        if (table == null)
-          table = new Table(Table.classToNormalizedFileName(Table_RawNanoStatistics.class) + "-" + corpus, outputFolder);
+        synchronized (table) {
+          if (table == null)
+            table = new Table(Table.classToNormalizedFileName(Table_RawNanoStatistics.class) + "-" + corpus, outputFolder);
+        }
       }
     }.fire(new ASTVisitor(true) {
       @Override public boolean visit(@NotNull final CompilationUnit $) {
