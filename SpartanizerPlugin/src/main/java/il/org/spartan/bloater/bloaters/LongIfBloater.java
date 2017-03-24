ebf554f007bcd4ef207ff0e3f5063833498c5f4f
Yuval Simon <siyuval@campus.technion.ac.il>
2017-03-24 14:48:02 +0300

    working on #1092

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/LongIfBloater.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/LongIfBloater.java
@@ -14,8 +14,21 @@ import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.zoomer.zoomin.expanders.*;
 
-/** Test case is {@link Issue0976} Issue #976 Convert: {@code if (a == b && c ==
- * d) { a = 5; } } to: {@code if (a == b) { if (c == d) { a = 5; } } }
+/** Test case is {@link Issue0976} Issue #976 
+ * Convert: 
+ * {@code 
+ *    if (a == b && c == d) {
+ *      a = 5; 
+ *    } 
+ *  } 
+ * to: 
+ * {@code 
+ *    if (a == b) { 
+ *      if (c == d) {
+ *        a = 5; 
+ *      } 
+ *    } 
+ *  }
  * @author tomerdragucki {@code tomerd@campus.technion.ac.il}
  * @since 2017-01-09 */
 public class LongIfBloater extends ReplaceCurrentNode<IfStatement>//
@@ -25,30 +38,28 @@ public class LongIfBloater extends ReplaceCurrentNode<IfStatement>//
   @Override @Nullable public ASTNode replacement(@NotNull final IfStatement ¢) {
     if (!shouldTip(¢))
       return null;
-    @Nullable final InfixExpression ie = az.infixExpression(¢.getExpression());
-    final IfStatement newThen = subject.pair(then(¢), null)
-        .toIf(!ie.hasExtendedOperands() ? ie.getRightOperand() : az.expression(getReducedIEFromIEWithExtOp(ie))),
-        $ = subject.pair(newThen, null).toIf(az.infixExpression(¢.getExpression()).getLeftOperand());
+    @Nullable final InfixExpression $ = az.infixExpression(¢.getExpression());
+    final IfStatement newThen = subject.pair(then(¢), null).toIf(!$.hasExtendedOperands() ? $.getRightOperand() : getReducedIEFromIEWithExtOp($));
     final Statement oldElse = ¢.getElseStatement();
-    if (oldElse != null) {
-      newThen.setElseStatement(oldElse);
-      $.setThenStatement(newThen);
-      $.setElseStatement(oldElse);
-    }
-    return $;
+    if (oldElse == null)
+      return subject.pair(newThen, null).toIf($.getLeftOperand());
+    newThen.setElseStatement(copy.of(oldElse));
+    return subject.pair(newThen, copy.of(oldElse)).toIf($.getLeftOperand());
   }
 
   @Override @NotNull public String description(@SuppressWarnings("unused") final IfStatement __) {
     return "Replace an if statement that contains && with two ifs";
   }
 
-  private static InfixExpression getReducedIEFromIEWithExtOp(@NotNull final InfixExpression ¢) {
+  private static Expression getReducedIEFromIEWithExtOp(@NotNull final InfixExpression ¢) {
     final InfixExpression $ = subject.pair(¢.getRightOperand(), first(extendedOperands(¢))).to(¢.getOperator());
-    subject.append($, step.extendedOperands(¢)).extendedOperands().remove(0);
+    subject.append($, step.extendedOperands(¢));
+    if(!$.extendedOperands().isEmpty())
+      $.extendedOperands().remove(0);
     return $;
   }
 
   private static boolean shouldTip(@NotNull final IfStatement ¢) {
-    return iz.infixExpression(¢.getExpression()) && iz.conditionalAnd((InfixExpression) ¢.getExpression());
+    return iz.infixExpression(¢.getExpression()) && iz.conditionalAnd(az.infixExpression(¢.getExpression()));
   }
 }
