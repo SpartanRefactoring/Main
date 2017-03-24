32552037f8c07cacd8c26b3fbb9b0262b903612d
Yossi Gil <yossi.gil@gmail.com>
2017-03-24 09:07:44 +0300

    Consolidate work on Inliner2

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/engine/Inliner2.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/engine/Inliner2.java
@@ -4,6 +4,8 @@ import static org.eclipse.jdt.core.dom.ASTNode.*;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
+import static il.org.spartan.spartanizer.ast.navigate.wizard.*;
+
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
@@ -11,6 +13,7 @@ import org.eclipse.jdt.core.dom.rewrite.*;
 import org.eclipse.text.edits.*;
 import org.jetbrains.annotations.*;
 
+import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
@@ -140,4 +143,116 @@ public final class Inliner2 {
   public interface By {
     @NotNull Inliner2 in(List<? extends ASTNode> ns);
   }
+
+/** Replace an occurrence of a {@link SimpleName} with an {@link Expression} in
+ * an array of
+ * @year 2015
+ * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
+ * @since Sep 13, 2016 */
+public interface replaceAll {
+  @NotNull static ASTRewrite go(final TextEditGroup g, @NotNull final ASTRewrite r) {
+    return new Inner().go(g, r);
+  }
+
+  @Nullable static Inner in(final ASTNode... ¢) {
+    return new Inner().in(¢);
+  }
+
+  @Nullable static Inner of(final SimpleName ¢) {
+    return new Inner().of(¢);
+  }
+
+  @NotNull static Inner with(final Expression ¢) {
+    return new Inner().with(¢);
+  }
+
+  @NotNull static Wrapper<ASTNode>[] wrap(@NotNull final ASTNode[] ns) {
+    @NotNull @SuppressWarnings("unchecked") final Wrapper<ASTNode>[] $ = new Wrapper[ns.length];
+    @NotNull final Int i = new Int();
+    as.list(ns).forEach(λ -> $[i.next()] = new Wrapper<>(λ));
+    return $;
+  }
+
+  class Inner extends Wrapper<Expression> {
+    private SimpleName name;
+    private ASTNode[] range;
+    private Expression with;
+    @Nullable private List<SimpleName> occurrences;
+
+    @NotNull ASTRewrite go(final TextEditGroup g, @NotNull final ASTRewrite $) {
+      occurrences.forEach(λ -> $.replace(λ, !iz.expression(λ) ? copy.of(with) : make.plant(with).into(λ.getParent()), g));
+      return $;
+    }
+
+    /** Computes the number of AST nodes added as a result of the replacement
+     * operation.
+     * @param es JD
+     * @return A non-negative integer, computed from the number of occurrences
+     *         of {@link #name} in the operands, and the size of the
+     *         replacement. */
+    public int addedSize() {
+      return occurrences.size() * (metrics.size(with) - 1);
+    }
+
+    public boolean canGo() {
+      if (with == null || occurrences.isEmpty())
+        return false;
+      if (iz.nodeTypeIn(with, //
+          BOOLEAN_LITERAL, //
+          CHARACTER_LITERAL, //
+          FIELD_ACCESS, //
+          LAMBDA_EXPRESSION, //
+          NULL_LITERAL, //
+          NUMBER_LITERAL, //
+          QUALIFIED_NAME, //
+          SIMPLE_NAME, //
+          STRING_LITERAL, //
+          THIS_EXPRESSION, //
+          TYPE_LITERAL))
+        return true;
+      final SimpleName occurrence = lisp.onlyOne(occurrences);
+      if (occurrence == null)
+        return false;
+      switch (Coupling.of(occurrence).withRespectTo(commonAncestor(occurrence, with))) {
+        case IFF:
+          return !iz.fragile(with);
+        case IMPLIED:
+          return !haz.sideEffects(with);
+        case INDEPENDENT:
+          return !iz.deterministic(with);
+        default:
+          return true;
+      }
+    }
+
+    boolean noHiding() {
+      noHiding();
+      return collect.definitionsOf(name).in().isEmpty();
+    }
+
+    @Nullable public Inner in(final ASTNode[] ¢) {
+      occurrences = collect.usesOf(name).in(range = ¢);
+      return null;
+    }
+
+    @Nullable public Inner of(final SimpleName ¢) {
+      occurrences = collect.usesOf(name = ¢).in(range);
+      return null;
+    }
+
+    /** Computes the total number of AST nodes in the replaced parameters
+     * @return A non-negative integer, computed from original size of the
+     *         parameters, the number of occurrences of {@link #name} in the
+     *         operands, and the size of the replacement. */
+    public int replacedSize() {
+      return metrics.size(range) + occurrences.size() * (metrics.size(get()) - 1);
+    }
+
+    @NotNull public Inner with(final Expression ¢) {
+      with = ¢;
+      return this;
+    }
+  }
+}
+
 }
