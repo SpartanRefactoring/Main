39e8eb69da1852d88cc3548dc1f0a5162935b7aa
Yuval Simon <siyuval@campus.technion.ac.il>
2017-03-25 23:25:49 +0300

    closes #1147

---
+++ src/test/java/il/org/spartan/spartanizer/issues/Issue1147.java
@@ -0,0 +1,53 @@
+package il.org.spartan.spartanizer.issues;
+
+import static il.org.spartan.spartanizer.testing.TestsUtilsTrimmer.*;
+
+import org.junit.*;
+
+import il.org.spartan.spartanizer.tippers.*;
+
+/** Test case for {@link SwitchBranchSort}
+ * @author Yuval Simon <tt>siyuval@campus.technion.ac.il</tt>
+ * @since 2017-03-25
+ */
+@SuppressWarnings("static-method")
+public class Issue1147 {
+  @Test public void t1() {
+    trimmingOf("switch (digits(round3(¢))) {"
+        + "case -1:"
+        + "case 0:"
+        + "return \"%.3f\";"
+        + "case 1:"
+        + "return \"%.2f\";"
+        + "case 2:"
+        + "return \"%.1f\";"
+        + "default:"
+        + "return \"%.0f\";"
+        + "}").stays();
+  }
+  
+  @Test public void t2() {
+    trimmingOf("switch (digits(round3(¢))) {"
+        + "case 0:"
+        + "case 1:"
+        + "return \"%.3f\";"
+        + "case -1:"
+        + "return \"%.2f\";"
+        + "case 2:"
+        + "return \"%.1f\";"
+        + "default:"
+        + "return \"%.0f\";"
+        + "}")
+    .gives("switch (digits(round3(¢))) {"
+        + "case -1:"
+        + "return \"%.2f\";"
+        + "case 0:"
+        + "case 1:"
+        + "return \"%.3f\";"
+        + "case 2:"
+        + "return \"%.1f\";"
+        + "default:"
+        + "return \"%.0f\";"
+        + "}").stays();
+  }
+}
