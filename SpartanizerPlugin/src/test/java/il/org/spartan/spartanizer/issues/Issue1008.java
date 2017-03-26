63932ed99cfe53ec680cc26c1aec26fa1348b8b7
Yossi Gil <yossi.gil@gmail.com>
2017-03-26 13:57:12 +0300

    Sync

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue1008.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue1008.java
@@ -1,12 +1,15 @@
 package il.org.spartan.spartanizer.issues;
 
 import org.jetbrains.annotations.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import il.org.spartan.spartanizer.meta.*;
 
 /** Unit tests for {@link
  * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since 2017-01-17 */
+@RunWith(Parameterized.class)
 public class Issue1008 extends MetaFixture {
   @NotNull AlphabeticallySortedSentence case1 = new AlphabeticallySortedSentence() {
     /** [[SuppressWarningsSpartan]] */
