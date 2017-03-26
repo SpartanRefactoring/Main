2ce260d2509e680990378cc83a9fa79217d6366b
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-26 15:55:30 +0300

    Remove switch tippers

--- src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
+++ src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
@@ -174,7 +174,6 @@ public class Toolbox {
         .add(SwitchStatement.class, //
             new SwitchEmpty(), //
             new MergeSwitchBranches(), //
-            new SwitchBranchSort(), //
             null)
         .add(SwitchCase.class, //
             new RemoveRedundantSwitchCases(), //
