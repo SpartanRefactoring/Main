0ee8279ca1771e8cf8abd5d3a988a380dbba16e0
dormaayn <dor.d.ma@gmail.com>
2017-03-27 23:08:24 +0300

    Renaming

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/refactoringMechanism/VariableFragmnetUninitialized.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/refactoringMechanism/VariableFragmnetUninitialized.java
@@ -7,10 +7,10 @@ import il.org.spartan.utils.*;
 /** TODO dormaayn: document class
  * @author dormaayn <tt>dor.d.ma@gmail.com</tt>
  * @since 2017-03-27 */
-public abstract class LocalVariableUninitialized extends LocalVariable {
+public abstract class VariableFragmnetUninitialized extends LocalVariable {
   private static final long serialVersionUID = 0x40D2B631F771C9F4L;
 
-  public LocalVariableUninitialized() {
+  public VariableFragmnetUninitialized() {
     andAlso(Proposition.of("Fragment must not be initialized", () -> initializer == null));
   }
 
