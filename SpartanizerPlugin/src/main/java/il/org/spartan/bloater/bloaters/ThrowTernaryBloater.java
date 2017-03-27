28e331fe098b7bf0fb2bb603244f2e21aba54ccb
Doron Mehsulam <doronmmm@hotmail.com>
2017-03-27 11:10:16 +0300

    Some todos #709

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/ThrowTernaryBloater.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/ThrowTernaryBloater.java
@@ -4,30 +4,23 @@ import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
 
-/** Same as ReturnTernaryExpander just for "throw" {@link Issue #998}
+/** Same as ReturnTernaryExpander just for "throw" @link {Issue0998}
  * @author Doron Meshulam {@code doronmmm@hotmail.com}
  * @since 2016-12-26 */
 public class ThrowTernaryBloater extends ReplaceCurrentNode<ThrowStatement>//
     implements TipperCategory.Bloater {
   private static final long serialVersionUID = 0x6F38EBD99BF2A49EL;
 
-  private static ASTNode innerThrowReplacement(final Expression x,  final Statement s) {
-     final ConditionalExpression ¢;
-    if (!(x instanceof ParenthesizedExpression))
-      ¢ = az.conditionalExpression(x);
-    else {
-      // TODO Doron Meshulam: Use extract.core --yg
-       final Expression unpar = expression(az.parenthesizedExpression(x));
-      if (!(unpar instanceof ConditionalExpression))
-        return null;
-      ¢ = az.conditionalExpression(unpar);
-      if (¢ == null)
-        return null;
-    }
+  private static ASTNode innerThrowReplacement(final Expression x, final Statement s) {
+    final ConditionalExpression ¢ = az.conditionalExpression(extract.core(x));
+    if (¢ == null)
+      return null;
+
     final IfStatement $ = s.getAST().newIfStatement();
     $.setExpression(copy.of(expression(¢)));
     final ThrowStatement then = ¢.getAST().newThrowStatement();
@@ -39,17 +32,17 @@ public class ThrowTernaryBloater extends ReplaceCurrentNode<ThrowStatement>//
     return $;
   }
 
-  private static ASTNode replaceReturn( final Statement ¢) {
-     final ThrowStatement $ = az.throwStatement(¢);
+  private static ASTNode replaceReturn(final Statement ¢) {
+    final ThrowStatement $ = az.throwStatement(¢);
     return $ == null || !(expression($) instanceof ConditionalExpression) && !(expression($) instanceof ParenthesizedExpression) ? null
         : innerThrowReplacement(expression($), ¢);
   }
 
-  @Override  public ASTNode replacement( final ThrowStatement ¢) {
+  @Override public ASTNode replacement(final ThrowStatement ¢) {
     return replaceReturn(¢);
   }
 
-  @Override  public String description(@SuppressWarnings("unused") final ThrowStatement __) {
+  @Override public String description(@SuppressWarnings("unused") final ThrowStatement __) {
     return "expanding a ternary operator to a full if-else statement";
   }
 }
