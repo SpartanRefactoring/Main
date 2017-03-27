fbac9b2c752efe31e996e6da2701bb7b4bdaa653
Ori Roth <ori.rothh@gmail.com>
2017-03-28 02:28:23 +0300

    work on view

--- src/main/java/il/org/spartan/plugin/SpartanView.java
+++ src/main/java/il/org/spartan/plugin/SpartanView.java
@@ -16,17 +16,22 @@ import org.eclipse.ui.part.*;
 
 public class SpartanView extends ViewPart {
   private static final String VIEW_ID = "il.org.spartan.spartanizer.view";
+  private static final int BUTTON_SIZE = 100;
   TableViewer viewer;
-  Function<ImageData, ImageData> scaler = λ -> λ.scaledTo(100, 100);
+  Table table;
+  static Function<ImageData, ImageData> scaler = λ -> λ.scaledTo(BUTTON_SIZE, BUTTON_SIZE);
   final Map<SpartanViewSection, ToolTip> tooltips = new HashMap<>();
   final Map<ToolTip, Rectangle> bounds = new HashMap<>();
   static final SpartanViewSection[] sections = { //
       section("Spartanize1", ICON, mouseListener(λ -> System.out.println("!"), λ -> System.out.println("@"), λ -> System.out.println("#"))), //
-      section("Spartanize2", ICON, mouseListener(λ -> {/**/}, λ -> {/**/}, λ -> {/**/})) //
+      section("Spartanize2", LOGO, mouseListener(λ -> {/**/}, λ -> {/**/}, λ -> {/**/})), //
+      section("Spartanize3", DELTA, mouseListener(λ -> {/**/}, λ -> {/**/}, λ -> {/**/})), //
+      section("Spartanize4", CATEGORY, mouseListener(λ -> {/**/}, λ -> {/**/}, λ -> {/**/})), //
   };
 
   @Override public void createPartControl(Composite parent) {
-    viewer = new TableViewer(parent);
+    viewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
+    table = viewer.getTable();
     viewer.setContentProvider(ArrayContentProvider.getInstance());
     viewer.setLabelProvider(new LabelProvider() {
       @Override public String getText(@SuppressWarnings("unused") Object element) {
@@ -34,13 +39,14 @@ public class SpartanView extends ViewPart {
       }
 
       @Override public Image getImage(Object element) {
-        return Dialogs.image(((SpartanViewSection) element).image, VIEW_ID + ((SpartanViewSection) element).tooltip, scaler);
+        return ((SpartanViewSection) element).image;
       }
     });
     viewer.setInput(sections);
     getSite().setSelectionProvider(viewer);
-    viewer.getTable().addMouseListener(new MouseListener() {
+    table.addMouseListener(new MouseListener() {
       @Override public void mouseUp(MouseEvent e) {
+        viewer.setSelection(null);
         Optional.ofNullable(getSVS(e)).ifPresent(λ -> λ.ml.mouseUp(e));
       }
 
@@ -54,11 +60,11 @@ public class SpartanView extends ViewPart {
 
       SpartanViewSection getSVS(MouseEvent e) {
         return Optional.of(e).map(λ -> new Point(λ.x, λ.y)) //
-            .map(λ -> viewer.getTable().getItem(λ)).map(λ -> λ.getData()) //
+            .map(λ -> table.getItem(λ)).map(λ -> λ.getData()) //
             .filter(λ -> λ instanceof SpartanViewSection).map(SpartanViewSection.class::cast).orElse(null);
       }
     });
-    viewer.getTable().addFocusListener(new FocusListener() {
+    table.addFocusListener(new FocusListener() {
       @Override public void focusLost(@SuppressWarnings("unused") FocusEvent __) {
         tooltips.values().forEach(λ -> λ.setVisible(false));
       }
@@ -67,12 +73,12 @@ public class SpartanView extends ViewPart {
         //
       }
     });
-    viewer.getTable().addListener(SWT.MouseHover, new Listener() {
+    table.addListener(SWT.MouseHover, new Listener() {
       @Override public void handleEvent(Event e) {
         final Rectangle r = e.getBounds();
         if (r == null)
           return;
-        TableItem ti = viewer.getTable().getItem(new Point(e.x, e.y));
+        TableItem ti = table.getItem(new Point(e.x, e.y));
         if (ti == null)
           return;
         SpartanViewSection svs = Optional.of(ti) //
@@ -87,8 +93,8 @@ public class SpartanView extends ViewPart {
           tt.setAutoHide(true);
           tooltips.put(svs, tt);
         }
-        final Rectangle tp = viewer.getTable().getBounds();
-        final Point tl = viewer.getTable().toDisplay(tp.x + r.x, tp.y + r.y);
+        final Rectangle tp = table.getBounds();
+        final Point tl = table.toDisplay(tp.x + r.x, tp.y + r.y);
         final Rectangle tr = new Rectangle(tl.x, tl.y, r.width, r.height);
         final ToolTip tt = tooltips.get(svs);
         bounds.put(tt, tr);
@@ -98,8 +104,8 @@ public class SpartanView extends ViewPart {
       }
     });
     final Listener killTooltips = e -> tooltips.values().forEach(λ -> λ.setVisible(false));
-    viewer.getTable().addListener(SWT.MouseMove, killTooltips);
-    viewer.getTable().addListener(SWT.MouseWheel, killTooltips);
+    table.addListener(SWT.MouseMove, killTooltips);
+    table.addListener(SWT.MouseWheel, killTooltips);
   }
 
   @Override public void setFocus() {
@@ -112,7 +118,7 @@ public class SpartanView extends ViewPart {
 
   protected static class SpartanViewSection {
     public final String tooltip;
-    public final String image;
+    public final Image image;
     public final String id;
     public final MouseListener ml;
     private final int idi;
@@ -120,7 +126,7 @@ public class SpartanView extends ViewPart {
 
     public SpartanViewSection(String tooltip, String image, MouseListener ml) {
       this.tooltip = tooltip;
-      this.image = image;
+      this.image = Dialogs.image(image, VIEW_ID + tooltip, scaler);
       idi = idGen++;
       id = idi + "";
       this.ml = ml;
