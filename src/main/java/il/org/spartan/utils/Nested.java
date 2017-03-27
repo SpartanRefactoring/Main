970a3ae7b199868cd924879347075a0742df3dd6
dormaayn <dor.d.ma@gmail.com>
2017-03-27 14:24:08 +0300

    Added @Autors

--- src/main/java/il/org/spartan/utils/Nested.java
+++ src/main/java/il/org/spartan/utils/Nested.java
@@ -3,6 +3,10 @@ package il.org.spartan.utils;
 import java.util.*;
 import java.util.stream.*;
 
+/** @author Dor Ma'ayan <tt>dor.d.ma@gmail.com</tt>
+ * @author Ori Roth
+ * @author Oren Afek
+ * @since 2017-03-27 */
 public interface Nested<@¢ T> extends Bolt<T> {
   interface Root<@¢ T> extends Nested<T>, Bolt.Atomic<T> {
     //
