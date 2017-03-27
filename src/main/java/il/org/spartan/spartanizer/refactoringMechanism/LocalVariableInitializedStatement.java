a46295fb4c1f2f5e39696be5f70ae1a7848da99d
dormaayn <dor.d.ma@gmail.com>
2017-03-27 22:43:40 +0300

    New Organization of the new tools

---
+++ src/main/java/il/org/spartan/spartanizer/refactoringMechanism/LocalVariableInitializedStatement.java
@@ -0,0 +1,22 @@
+package il.org.spartan.spartanizer.refactoringMechanism;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
+
+/**
+ * TODO dormaayn: document class 
+ * @author dormaayn <tt>dor.d.ma@gmail.com</tt>
+ * @since 2017-03-27
+ */
+public abstract class LocalVariableInitializedStatement extends LocalVariableInitialized {
+  private static final long serialVersionUID = 0x1B70489B1D1340L;
+
+  @Override public final Tip tip(final VariableDeclarationFragment f, final ExclusionManager exclude) {
+    final Tip $ = super.tip(f, exclude);
+    if ($ != null && exclude != null)
+      exclude.exclude(f.getParent());
+    return $;
+  }
+}
