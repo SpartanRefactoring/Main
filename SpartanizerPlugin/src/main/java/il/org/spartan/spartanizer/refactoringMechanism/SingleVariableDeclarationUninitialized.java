7a719ef0f71f69cf9b775f44db8486c1ddbf96de
dormaayn <dor.d.ma@gmail.com>
2017-03-28 00:02:21 +0300

    More Work

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/refactoringMechanism/SingleVariableDeclarationUninitialized.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/refactoringMechanism/SingleVariableDeclarationUninitialized.java
@@ -2,24 +2,20 @@ package il.org.spartan.spartanizer.refactoringMechanism;
 
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.utils.*;
 
-/**
- * TODO dormaayn: document class 
+/** TODO dormaayn: document class
  * @author dormaayn <tt>dor.d.ma@gmail.com</tt>
- * @since 2017-03-27
- */
-public abstract class SingleVariableDeclarationNoInitiallizer extends AbstractPattern<SingleVariableDeclaration> {
+ * @since 2017-03-27 */
+public abstract class SingleVariableDeclarationUninitialized extends AbstractPattern<SingleVariableDeclaration> {
   private static final long serialVersionUID = -6714605477414039462L;
   protected Expression initializer;
   protected SimpleName name;
   protected Type type;
 
-
-  SingleVariableDeclarationNoInitiallizer() {
+  SingleVariableDeclarationUninitialized() {
     andAlso(new Proposition.Singleton("Illegal Definition", () -> {
-      if(object().getInitializer()!=null)
+      if (object().getInitializer() != null)
         return false;
       name = object().getName();
       type = object().getType();
