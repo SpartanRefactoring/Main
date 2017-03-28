359ba0f99eb0c6f5265bbf1f45879989c454dac4
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-03-28 17:30:46 +0300

    adding some examples to tippers

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/IfDegenerateElse.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/IfDegenerateElse.java
@@ -1,5 +1,7 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static il.org.spartan.utils.Example.*;
+
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
 import org.eclipse.jdt.core.dom.*;
@@ -9,6 +11,7 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.nominal.*;
 import il.org.spartan.spartanizer.tipping.*;
+import il.org.spartan.utils.*;
 
 /** convert {@code if (x) return b; else {} } into {@code
  * if (x)
@@ -20,6 +23,14 @@ public final class IfDegenerateElse extends ReplaceCurrentNode<IfStatement>//
     implements TipperCategory.SyntacticBaggage {
   private static final long serialVersionUID = 0x6A05F681B3F27CB0L;
 
+  @Override public Example[] examples() {
+    return new Example[] { //
+        convert("if (x) return b; else {}") //
+            .to("if (x) return b;"), //
+        Example.ignores("if (x) return b;"), //
+    };
+  }
+  
   static boolean degenerateElse(final IfStatement ¢) {
     return elze(¢) != null && iz.vacuousElse(¢);
   }
