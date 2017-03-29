d046b36df0ba9dd5de244df1fc836ecd5c4e8a7e
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-29 18:20:49 +0300

    Important renaming

--- src/main/java/il/org/spartan/spartanizer/patterns/LocalVariableUninitialized.java
+++ src/main/java/il/org/spartan/spartanizer/patterns/LocalVariableUninitialized.java
@@ -7,10 +7,10 @@ import il.org.spartan.utils.*;
 /** TODO dormaayn: document class
  * @author dormaayn <tt>dor.d.ma@gmail.com</tt>
  * @since 2017-03-27 */
-public abstract class VariableFragmnetUninitialized extends LocalVariable {
+public abstract class LocalVariableUninitialized extends LocalVariable {
   private static final long serialVersionUID = 0x40D2B631F771C9F4L;
 
-  public VariableFragmnetUninitialized() {
+  public LocalVariableUninitialized() {
     andAlso(Proposition.of("Fragment must not be initialized", () -> initializer == null));
   }
 
