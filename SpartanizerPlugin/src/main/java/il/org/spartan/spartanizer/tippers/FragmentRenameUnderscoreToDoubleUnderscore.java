587e6769ba74847f2f31e8ee5a5915b864f5bf40
Ori Roth <ori.rothh@gmail.com>
2017-03-29 23:16:42 +0300

    examples + descriptions

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragmentRenameUnderscoreToDoubleUnderscore.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/FragmentRenameUnderscoreToDoubleUnderscore.java
@@ -5,6 +5,9 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.engine.nominal.*;
 import il.org.spartan.spartanizer.tipping.*;
+import il.org.spartan.utils.*;
+
+import static il.org.spartan.utils.Example.*;
 
 /** Replaces name of variables named "_" into "__"
  * @author Ori Roth
@@ -19,8 +22,31 @@ public final class FragmentRenameUnderscoreToDoubleUnderscore<N extends Variable
     return "Use double underscore instead of " + trivia.gist(¢);
   }
 
+  @Override public String description() {
+    return "Use \"__\" instead of \"_\"";
+  }
+
   @Override protected boolean change(final N ¢) {
     return "_".equals(¢.getName() + "");
+  }
+
+  /** [[SuppressWarningsSpartan]] */
+  @Override public Example[] examples() {
+    return new Example[] {
+        convert("" //
+            + "void f(int _) {\n" //
+            + "}")
+                .to("" //
+                    + "void f(int __) {\n" //
+                    + "}"), //
+        convert("" //
+            + "void f() {\n" //
+            + "  int _ = 1;\n" + "}")
+                .to("" //
+                    + "void f() {\n" //
+                    + "  int __ = 1;\n" //
+                    + "}"), //
+    };
   }
 
   @Override protected SimpleName replacement(final N ¢) {
