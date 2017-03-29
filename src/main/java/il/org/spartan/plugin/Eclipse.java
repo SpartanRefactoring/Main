14a85f3271c8f9428a9eabbfcb3ebf9d5b77beac
Ori Roth <ori.rothh@gmail.com>
2017-03-29 15:42:21 +0300

    quick fix option to disable tipper

--- src/main/java/il/org/spartan/plugin/Eclipse.java
+++ src/main/java/il/org/spartan/plugin/Eclipse.java
@@ -1,17 +1,25 @@
 package il.org.spartan.plugin;
 
+import java.lang.reflect.*;
 import java.text.*;
 import java.util.*;
 import java.util.function.*;
 
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.*;
+import org.eclipse.jface.dialogs.*;
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
+
+import il.org.spartan.utils.*;
 
 /** Eclipse common utilities.
  * @author Ori Roth {@code ori.rothh@gmail.com}
  * @since 2017-03-21 */
 public class Eclipse {
+  private static final boolean REFRESH_OPENS_DIALOG = false;
   /** Height of default tooltips. */
   public static final int TOOLTIP_HEIGHT = 25;
 
@@ -58,5 +66,41 @@ public class Eclipse {
         mouseDoubleClick.accept(¢);
       }
     };
+  }
+
+  /** Refreshes project, while applying new configuration.
+   * @param p JD
+   * @throws CoreException
+   * @throws InvocationTargetException
+   * @throws InterruptedException */
+  public static void refreshProject(final IProject p) throws CoreException, InvocationTargetException, InterruptedException {
+    if (p != null && p.isOpen() && p.getNature(Nature.NATURE_ID) != null)
+      if (!REFRESH_OPENS_DIALOG)
+        new Job("Refreshing " + p.getName()) {
+          @Override protected IStatus run(final IProgressMonitor m) {
+            try {
+              p.build(IncrementalProjectBuilder.FULL_BUILD, m);
+              return Status.OK_STATUS;
+            } catch (final CoreException ¢) {
+              monitor.log(¢);
+              return Status.CANCEL_STATUS;
+            }
+          }
+        }.schedule();
+      else {
+        final ProgressMonitorDialog d = Dialogs.progress(true);
+        d.run(true, true, m -> {
+          SpartanizationHandler.runAsynchronouslyInUIThread(() -> {
+            final Shell s = d.getShell();
+            if (s != null)
+              s.setText("Refreshing project");
+          });
+          try {
+            p.build(IncrementalProjectBuilder.FULL_BUILD, m);
+          } catch (final CoreException ¢) {
+            monitor.log(¢);
+          }
+        });
+      }
   }
 }
