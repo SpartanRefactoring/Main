b1640756eeff07e15a72cabacdd579c62871a77c
orenafek <oren.afek@gmail.com>
2017-03-30 13:18:04 +0300

    Successfully integrated MetaTester into eclipse.

---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/metatester/SimpleTest.java
@@ -0,0 +1,27 @@
+package il.org.spartan.spartanizer.research.metatester;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+/**
+ * @author Oren Afek
+ * @since 3/27/2017
+ */
+
+@RunWith(MetaTester.class)
+public class SimpleTest {
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void testF(){
+        System.out.println("");
+        assertEquals(1,1);
+        assertEquals(1,2);
+        System.out.println("");
+        assertTrue(true);
+       
+    }
+
+}
---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/metatester/generated/SimpleTest_CustomTest.java
@@ -0,0 +1,23 @@
+package il.org.spartan.spartanizer.research.metatester.generated;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("static-method")
+public class SimpleTest_CustomTest { 
+
+	@Test
+	public void testF_20() {
+	        assertEquals(1,1);
+	}
+
+	@Test
+	public void testF_21() {
+	        assertEquals(1,2);
+	}
+
+	@Test
+	public void testF_23() {
+	        assertTrue(true);
+	}
+}
