40f6e9ffdc52df1584552e760c1f0d5c6adfbab5
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-30 18:57:50 +0300

    renaming

--- src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseFoo.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseFoo.java
@@ -1,5 +1,7 @@
 package il.org.spartan.spartanizer.tippers;
 
+import static il.org.spartan.utils.Example.*;
+
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.rewrite.*;
 import org.eclipse.text.edits.*;
@@ -8,21 +10,16 @@ import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.patterns.*;
 import il.org.spartan.utils.*;
 
 /** @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since 2015-09-05 */
-public final class IfFooElseFoo extends AbstractPattern<IfStatement> implements TipperCategory.CommnonFactoring {
+public final class IfFooElseFoo extends IfAbstractPattern implements TipperCategory.CommnonFactoring {
   private static final long serialVersionUID = -1173304247743605864L;
-  private Statement then;
 
   public IfFooElseFoo() {
-    andAlso(Proposition.of("Then and else are identical",
-        () -> wizard.same(//
-            then = current().getThenStatement(), //
-            current().getElseStatement()
-        )));
+    andAlso(Proposition.of("Then and else are identical", //
+        () -> wizard.same(then, elze)));
   }
 
   @Override protected ASTRewrite go(ASTRewrite r, TextEditGroup g) {
@@ -34,5 +31,11 @@ public final class IfFooElseFoo extends AbstractPattern<IfStatement> implements 
 
   @Override public String description(@SuppressWarnings("unused") IfStatement __) {
     return "Eliminate 'if' with two identical branches";
+  }
+
+  @Override public Example[] examples() {
+    return new Example[] { //
+        convert("if(f()==g())h();else h();")//
+            .to("f();g();h();") };
   }
 }
