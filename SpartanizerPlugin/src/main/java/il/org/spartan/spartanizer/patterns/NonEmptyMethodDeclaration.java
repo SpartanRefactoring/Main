0c69b62bc4a722f9bf841b920fe57f7092a4ad27
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-03-30 16:56:38 +0300

    working on pattern for tipper MethodDeclarationSingleParameter #1178

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/patterns/NonEmptyMethodDeclaration.java
@@ -0,0 +1,36 @@
+package il.org.spartan.spartanizer.patterns;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.java.*;
+import il.org.spartan.utils.*;
+
+/**
+ * A nonempty method declaration
+ * @author Raviv Rachmiel <tt>raviv.rachmiel@gmail.com</tt>
+ * @since 2017-03-30
+ */
+public abstract class NonEmptyMethodDeclaration extends AbstractPattern<MethodDeclaration>{
+
+  private static final long serialVersionUID = 5190682195211446352L;
+  protected Javadoc jdoc;
+  protected SimpleName name;
+  
+  public NonEmptyMethodDeclaration() {
+    andAlso(new Proposition.Singleton("Applicable only on non empty methods", () -> {
+      if (!haz.anyStatements(object()))
+        return false;
+      name = object().getName();
+      jdoc = object().getJavadoc();
+      return true;
+    }));
+  }
+
+  protected Javadoc jdoc() {
+    return jdoc;
+  }
+
+  protected SimpleName name() {
+    return name;
+  }
+}
