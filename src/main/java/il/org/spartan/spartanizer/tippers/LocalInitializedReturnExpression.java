0d135c1971a6a7eaf9550718f664a1f23e3318e2
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-31 21:46:22 +0300

    fix bugs

--- src/main/java/il/org/spartan/spartanizer/tippers/LocalInitializedReturnExpression.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/LocalInitializedReturnExpression.java
@@ -8,42 +8,41 @@ import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
-import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.engine.Inliner.*;
 import il.org.spartan.spartanizer.patterns.*;
+import il.org.spartan.utils.*;
 
-/** convert {@code int a = 3;return a;} into {@code return a;}
+/** See {@link #examples()}
  * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
  * @since 2015-08-07 */
-public final class LocalInitializedReturnExpression extends LocalVariableInitializedStatement  //
+public final class LocalInitializedReturnExpression extends LocalVariableInitializedStatement //
     implements TipperCategory.Inlining {
-  public LocalInitializedReturnExpression() {
-    andAlso("Not used in subsequent initializers",
-        () -> !usedInSubsequentInitializers() 
-        ) ;// 
+  private static final long serialVersionUID = 6685864331590860235L;
+  private ReturnStatement returnStatement;
+  private Expression returnValue;
+
+  public LocalInitializedReturnExpression() {//
+    andAlso("Variable not used in subsequent initializers", //
+        () -> !usedInSubsequentInitializers());//
+    andAlso("Next statement is return", //
+        () -> iz.not.null¢(returnStatement = az.returnStatement(nextStatement)));//
+    andAlso("Next statement returns a value return", //
+        () -> iz.not.null¢(returnValue = returnStatement.getExpression()));//
+    andAlso("Value return is local variable", //
+        () -> wizard.eq(name, returnValue)//
+    );
   }
 
-  private static final long serialVersionUID = 0xECFC7713ABB0D4AL;
+  @Override public Example[] examples() {
+    return new Example[] { Example.convert("int a = 3; return a;").to("return 3;") };
+  }
 
   @Override public String description(final VariableDeclarationFragment ¢) {
     return "Eliminate local " + ¢.getName() + " and inline its value into the expression of the subsequent return statement";
   }
 
   @Override protected ASTRewrite go(final ASTRewrite $, final TextEditGroup g) {
-    final ReturnStatement s = az.returnStatement(nextStatement);
-    if (s == null)
-      return null;
-    final Expression newReturnValue = s.getExpression();
-    if (newReturnValue == null)
-      return null;
-    final InlinerWithValue i = new Inliner(name, $, g).byValue(initializer);
-    if (wizard.eq(name, newReturnValue) || !i.canSafelyInlineinto(newReturnValue)
-        || i.replacedSize(newReturnValue) - eliminationSaving() - metrics.size(newReturnValue) > 0)
-      return null;
-    $.replace(s.getExpression(), newReturnValue, g);
-    i.inlineInto(newReturnValue);
-    il.org.spartan.spartanizer.ast.factory.remove.deadFragment(current, $, g);
+    $.replace(returnValue, copy.of(initializer), g);
+    remove.deadFragment(current, $, g);
     return $;
   }
-
 }
