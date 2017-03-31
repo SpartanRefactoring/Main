01c56bfe37e4aa6dc26d176d2b3b9ff3027cf336
Yossi Gil <yogi@cs.technion.ac.il>
2017-03-31 22:30:40 +0300

    f=15; e=5; skip=49

---
+++ src/main/java/il/org/spartan/spartanizer/tippers/LocalUninitializedDead.java
@@ -0,0 +1,41 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.utils.Example.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.engine.nominal.*;
+import il.org.spartan.spartanizer.java.namespace.*;
+import il.org.spartan.spartanizer.patterns.*;
+import il.org.spartan.utils.*;
+
+/** See {@link #examples()}
+ * @author Yossi Gil {@code Yossi.Gil@GMail.COM}
+ * @since 2015-08-07 */
+public final class LocalUninitializedDead extends LocalVariableUninitialized implements TipperCategory.Deadcode {
+  private static final long serialVersionUID = 0x14812B0904DFB002L;
+
+  public LocalUninitializedDead() {
+    andAlso("Local variable is not used anywhere", //
+        () -> collect.usesOf(name).in(scope.of(name)).isEmpty());
+  }
+
+  @Override public String description(final VariableDeclarationFragment ¢) {
+    return "Remove unused local variable " + trivia.gist(¢.getName());
+  }
+
+  @Override public Example[] examples() {
+    return new Example[] { //
+        convert("int b; a = 3; f(b); f(a,b);a = f(a,b); b= f(a,b);}")//
+            .to("a = 3; f(b); f(a,b);a = f(a,b); b= f(a,b);") };
+  }
+
+  @Override protected ASTRewrite go(final ASTRewrite r, final TextEditGroup g) {
+    il.org.spartan.spartanizer.ast.factory.remove.deadFragment(current(), r, g);
+    return r;
+  }
+}
