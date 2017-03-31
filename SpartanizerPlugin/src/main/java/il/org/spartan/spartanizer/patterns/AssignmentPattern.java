6e4a76d0ce8370b4914e652587676f05520852f7
dormaayn <dor.d.ma@gmail.com>
2017-03-31 09:01:18 +0300

    Assignment Pattern

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/patterns/AssignmentPattern.java
@@ -0,0 +1,24 @@
+package il.org.spartan.spartanizer.patterns;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.Assignment.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+/** TODO dormaayn: document class
+ * @author dormaayn <tt>dor.d.ma@gmail.com</tt>
+ * @since 2017-03-31 */
+public abstract class AssignmentPattern extends AbstractPattern<Assignment> {
+  private static final long serialVersionUID = 7758028221095317214L;
+  protected Expression left, right;
+  protected Operator operator;
+
+  public AssignmentPattern() {
+    andAlso("Must bre legal assignment", () -> {
+      left = current.getLeftHandSide();
+      right = current.getLeftHandSide();
+      operator = current.getOperator();
+      return left != null && right != null && operator != null;
+    });
+  }
+}
