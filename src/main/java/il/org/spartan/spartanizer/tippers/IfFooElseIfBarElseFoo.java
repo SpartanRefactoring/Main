1e4fb21dbd4e0776661881cfd072b365644584ac
Niv Shalmon <shalmon.niv@gmail.com>
2017-04-01 11:24:36 +0300

    fix implementation of #1129

--- src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseIfBarElseFoo.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseIfBarElseFoo.java
@@ -11,12 +11,13 @@ import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.spartanizer.issues.*;
 import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.utils.*;
 
 /** Merges if and else blocks when they are the same and there is an else if
- * clause.
+ * clause. Tests in {@link Issue1129}
  * @author Niv Shalmon <tt>shalmon.niv@gmail.com</tt>
  * @since 2017-03-26 */
 public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
@@ -24,8 +25,11 @@ public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
   private static final long serialVersionUID = -8897742439908596829L;
 
   @Override public Example[] examples() {
-    return new Example[] { convert("if(a) f(); else if(b) g(); else f();")//
-        .to("if(a || !b) f(); else if(b) g();"), ignores("if(a) f(); else if (x()) g(); else f();") };
+    return new Example[] { //
+        convert("if(a) f(); else if(b) g(); else f();")//
+            .to("if(a || !b) f(); else if(b) g();"), //
+        ignores("if(a) f(); else if (x()) g(); else f();")//
+    };
   }
 
   @Override public ASTNode replacement(final IfStatement s) {
@@ -35,17 +39,20 @@ public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
     s2.setElseStatement(null);
     $.setElseStatement(s2);
     final IfStatement p = az.ifStatement(s.getParent());
-    if (p == null || s.getLocationInParent() == IfStatement.ELSE_STATEMENT_PROPERTY)
+    if (p == null || s.getLocationInParent().equals(IfStatement.ELSE_STATEMENT_PROPERTY))
       return $;
     final Block bl = subject.statement($).toBlock();
-    boolean b;
-    try {
-      p.setThenStatement(bl);
-      b = iz.blockEssential(extract.statements(bl).get(0));
-    } finally {
-      p.setThenStatement(s);
+    IfStatement originalParent = p, newParent = copy.of(originalParent);
+    newParent.setThenStatement(bl);
+    /* copies the tree as long as only IfStatements are the parents, so
+     * iz.blockEssential can tell us whether the block is essential or not. */
+    while (originalParent.getLocationInParent().equals(IfStatement.THEN_STATEMENT_PROPERTY)) {
+      Statement child = newParent;
+      originalParent = az.ifStatement(originalParent.getParent());
+      newParent = copy.of(originalParent);
+      newParent.setThenStatement(child);
     }
-    return !b ? $ : subject.statement($).toBlock();
+    return !iz.blockEssential(extract.statements(bl).get(0)) ? $ : subject.statement($).toBlock();
   }
 
   @Override protected boolean prerequisite(final IfStatement ¢) {
