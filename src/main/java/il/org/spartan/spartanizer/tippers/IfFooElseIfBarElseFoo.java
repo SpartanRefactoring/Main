72ee29a9725471946bbd90f8c950c0bbbda21c10
Niv Shalmon <shalmon.niv@gmail.com>
2017-04-01 12:22:30 +0300

    convert IfFooElseIfBarElseFoo to abstractPattern

--- src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseIfBarElseFoo.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/IfFooElseIfBarElseFoo.java
@@ -6,6 +6,8 @@ import static org.eclipse.jdt.core.dom.InfixExpression.Operator.*;
 import static org.eclipse.jdt.core.dom.PrefixExpression.Operator.*;
 
 import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
@@ -13,17 +15,23 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.issues.*;
 import il.org.spartan.spartanizer.java.*;
-import il.org.spartan.spartanizer.tipping.*;
+import il.org.spartan.spartanizer.patterns.*;
 import il.org.spartan.utils.*;
 
 /** Merges if and else blocks when they are the same and there is an else if
  * clause. Tests in {@link Issue1129}
  * @author Niv Shalmon <tt>shalmon.niv@gmail.com</tt>
  * @since 2017-03-26 */
-public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
+public class IfFooElseIfBarElseFoo extends IfElseIfAbstractPattern //
     implements TipperCategory.Inlining {
   private static final long serialVersionUID = -8897742439908596829L;
 
+  public IfFooElseIfBarElseFoo() {
+    andAlso("if and final else must be the same",() -> wizard.eq(then, elzeElze));
+    andAlso("else if expression must have no side effects",() ->//
+      sideEffects.free(elzeIfCondition));
+  }
+  
   @Override public Example[] examples() {
     return new Example[] { //
         convert("if(a) f(); else if(b) g(); else f();")//
@@ -32,15 +40,21 @@ public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
     };
   }
 
-  @Override public ASTNode replacement(final IfStatement s) {
-    final IfStatement s2 = copy.of(az.ifStatement(s.getElseStatement())), $ = copy.of(s);
-    $.setExpression(operands(s.getExpression(), operand(s2.getExpression()).to(NOT))//
+  @Override public String description(@SuppressWarnings("unused") final IfStatement __) {
+    return "Merges if and else blocks when they are the same and there is an else if clause.";
+  }
+
+  @Override protected ASTRewrite go(ASTRewrite r, TextEditGroup g) {
+    final IfStatement s2 = copy.of(az.ifStatement(elze)), $ = copy.of(current);
+    $.setExpression(operands(condition, operand(elzeIfCondition).to(NOT))//
         .to(CONDITIONAL_OR));
     s2.setElseStatement(null);
     $.setElseStatement(s2);
-    final IfStatement p = az.ifStatement(s.getParent());
-    if (p == null || s.getLocationInParent().equals(IfStatement.ELSE_STATEMENT_PROPERTY))
-      return $;
+    final IfStatement p = az.ifStatement(current.getParent());
+    if (p == null || current.getLocationInParent().equals(IfStatement.ELSE_STATEMENT_PROPERTY)){
+      r.replace(current, $, g);
+      return r;
+    }
     final Block bl = subject.statement($).toBlock();
     IfStatement originalParent = p, newParent = copy.of(originalParent);
     newParent.setThenStatement(bl);
@@ -53,16 +67,8 @@ public class IfFooElseIfBarElseFoo extends ReplaceCurrentNode<IfStatement> //
       newParent = copy.of(originalParent);
       newParent.setThenStatement(child);
     }
-    return !iz.blockEssential(extract.statements(bl).get(0)) ? $ : subject.statement($).toBlock();
-  }
-
-  @Override protected boolean prerequisite(final IfStatement ¢) {
-    final IfStatement $ = az.ifStatement(¢.getElseStatement());
-    return $ != null && wizard.eq(¢.getThenStatement(), $.getElseStatement()) //
-        && sideEffects.free($.getExpression());
-  }
-
-  @Override public String description(@SuppressWarnings("unused") final IfStatement __) {
-    return "Merges if and else blocks when they are the same and there is an else if clause.";
+    r.replace(current, !iz.blockEssential(extract.statements(bl).get(0)) ? $ //
+        : subject.statement($).toBlock(), g);
+    return r;
   }
 }
