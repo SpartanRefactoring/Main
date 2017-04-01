902bd3b6ff1b51f1c8595c92b12e462a4e8b5b61
Niv Shalmon <shalmon.niv@gmail.com>
2017-04-01 12:02:07 +0300

    move IfAbstractPattern to Patterns package

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/patterns/IfAbstractPattern.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/patterns/IfAbstractPattern.java
@@ -1,4 +1,4 @@
-package il.org.spartan.spartanizer.tippers;
+package il.org.spartan.spartanizer.patterns;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
@@ -8,15 +8,15 @@ import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.patterns.*;
+import il.org.spartan.spartanizer.tippers.*;
 import il.org.spartan.utils.*;
 
 /** @author Yossi Gil {@code  Yossi.Gil@GMail.COM}
  * @since 2015-08-01 */
 public abstract class IfAbstractPattern extends AbstractPattern<IfStatement> {
   private static final long serialVersionUID = 1L;
-  @Property Statement then, elze;
-  @Property Expression condition;
+  @Property protected Statement then, elze;
+  @Property protected Expression condition;
   @Property protected List<Statement> subsequentStatements;
 
   public IfAbstractPattern() {
