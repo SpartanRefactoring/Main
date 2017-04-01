30093f6634475d8532bd74174eb717392450a139
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-01 18:59:39 +0300

    mr

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/ast/navigate/compute.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/ast/navigate/compute.java
@@ -1,9 +1,9 @@
 package il.org.spartan.spartanizer.ast.navigate;
 
-import static il.org.spartan.Utils.*;
-import static org.eclipse.jdt.core.dom.PrefixExpression.Operator.*;
+import static java.util.stream.Collectors.*;
 
 import java.util.*;
+import java.util.stream.*;
 
 import org.eclipse.jdt.core.dom.*;
 
@@ -12,20 +12,17 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.engine.nominal.*;
 import il.org.spartan.utils.*;
 
-/**
- * TODO Yossi Gil: document class 
+/** TODO Yossi Gil: document class
  * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
- * @since 2017-04-01
- */
+ * @since 2017-04-01 */
 public enum compute {
   ;
-
   public static List<String> usedNames(final Expression x) {
-    return new ExpressionMapReducer<List<String>>() {
+    return new ASTMapReducer<List<String>>() {
       @Override public List<String> reduce() {
         return new ArrayList<>();
       }
-  
+
       @Override public List<String> reduce(final List<String> ss1, final List<String> ss2) {
         if (ss1 == null && ss2 == null)
           return new ArrayList<>();
@@ -36,24 +33,37 @@ public enum compute {
         ss1.addAll(ss2);
         return ss1;
       }
-  
+
       @Override protected List<String> map(final SimpleName ¢) {
         final String $ = ¢.getIdentifier();
         return guessName.of($) != guessName.METHOD_OR_VARIABLE ? reduce() : as.list($);
       }
-  
+
       @Override protected List<String> map(@SuppressWarnings("unused") final ThisExpression ¢) {
         return reduce();
       }
     }.map(x);
   }
 
-  public static List<ASTNode> updateSpots(final Expression x) {
-    final List<ASTNode> $ = new ExpressionMapReducer<List<ASTNode>>() {
+  public static List<ASTNode> updateSpots(final ASTNode... ns) {
+    return Stream.of(ns).map(compute::updateSpots).flatMap(List<ASTNode>::stream).collect(toList());
+  }
+
+  public static List<ASTNode> updateSpots(final ASTNode x) {
+    final List<ASTNode> $ = new ASTMapReducer<List<ASTNode>>() {
       @Override public List<ASTNode> reduce() {
         return new LinkedList<>();
       }
-  
+
+      public List<ASTNode> reduce(final ASTNode n, final List<ASTNode> ns) {
+        if (n == null)
+          return ns;
+        if (ns == null)
+          return as.list(n);
+        ns.add(0,n);
+        return ns;
+      }
+
       @Override public List<ASTNode> reduce(final List<ASTNode> l1, final List<ASTNode> l2) {
         if (l1 == null)
           return l2;
@@ -62,29 +72,17 @@ public enum compute {
         l1.addAll(l2);
         return l1;
       }
-  
+
       @Override protected List<ASTNode> map(final Assignment ¢) {
-        return reduce(list(¢), super.map(¢));
+        return reduce((¢), super.map(¢));
       }
-  
+
       @Override protected List<ASTNode> map(final PostfixExpression ¢) {
-        return reduce(Collections.singletonList(step.expression(¢)), super.map(¢));
+        return reduce((¢.getOperand()), super.map(¢));
       }
-  
+
       @Override protected List<ASTNode> map(final PrefixExpression ¢) {
-        return reduce(!updating(¢) ? reduce() : atomic(¢.getOperand()), super.map(¢));
-      }
-  
-      List<ASTNode> atomic(final Expression ¢) {
-        return Collections.singletonList(¢);
-      }
-  
-      List<ASTNode> list(final ASTNode ¢) {
-        return new ArrayList<>(Collections.singletonList(¢));
-      }
-  
-      boolean updating(final PrefixExpression ¢) {
-        return in(¢.getOperator(), INCREMENT, DECREMENT);
+        return reduce(!iz.updating(¢) ? reduce() : as.list(¢.getOperand()), super.map(¢));
       }
     }.map(x);
     return $ != null ? $ : empty.list();
---
+++ SpartanizerPlugin/src/test/java/il/org/spartan/zoomer/zoomin/expanders/computeTest.java
@@ -0,0 +1,24 @@
+package il.org.spartan.zoomer.zoomin.expanders;
+import static il.org.spartan.azzert.*;
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.meta.*;
+
+/** Unit tests for {@link compute}
+ * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
+ * @since 2017-04-01 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@Ignore("Yossi Gil")
+@SuppressWarnings("static-method")
+public class computeTest extends MetaFixture {
+  @Test public void test1a() {
+    compute.updateSpots(into.s("return a *=2"));
+    azzert.that(
+        compute.updateSpots(into.s("return a *=2")).size(),
+        is(1));
+  }
+}
