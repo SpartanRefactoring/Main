daf3def3f466e8b06679b12fcca51ec035851f3a
YuvalSimon <yuvaltechnion@gmail.com>
2017-04-02 23:56:06 +0300

    fixing zoomer tests

--- SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/OutlineTernaryMethodInvocation.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/bloater/bloaters/OutlineTernaryMethodInvocation.java
@@ -2,16 +2,15 @@ package il.org.spartan.bloater.bloaters;
 
 import static il.org.spartan.spartanizer.ast.navigate.step.*;
 
+import java.util.*;
+
 import org.eclipse.jdt.core.dom.*;
-import org.eclipse.jdt.core.dom.rewrite.*;
-import org.eclipse.text.edits.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.java.*;
-import il.org.spartan.spartanizer.patterns.*;
-import il.org.spartan.utils.*;
+import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.zoomer.zoomin.expanders.*;
 
 /** convert {@code
@@ -22,44 +21,36 @@ import il.org.spartan.zoomer.zoomin.expanders.*;
  * } Test case is {@link Issue1091}
  * @author YuvalSimon {@code yuvaltechnion@gmail.com}
  * @since 2017-01-18 */
-public class OutlineTernaryMethodInvocation extends MethodInvocationAbstarctPattern//
+public class OutlineTernaryMethodInvocation extends ReplaceCurrentNode<MethodInvocation>//
     implements TipperCategory.Bloater {
-  private static final long serialVersionUID = 0x54464757E1B1C1A1L;
+  private static final long serialVersionUID = -2446197092801176661L;
 
-  public OutlineTernaryMethodInvocation() {
-    andAlso("Parnt is not a Lambda Expression", () -> {
-      return !iz.lambdaExpression(current.getParent());
-    });
-    andAlso("There is at least one argument argument", () -> {
-      return !arguments.isEmpty();
-    });
-  }
-
-  @Override public Examples examples() {
-    return null;
-  }
-
-  @Override public String description(@SuppressWarnings("unused") final MethodInvocation ¢) {
-    return null;
-  }
-
-  @Override protected ASTRewrite go(final ASTRewrite r, final TextEditGroup g) {
-    for (int i = 0; i < arguments.size(); ++i) {
+  @Override public ASTNode replacement(final MethodInvocation n) {
+    if (n == null || iz.lambdaExpression(n.getParent()))
+      return null;
+    final List<Expression> l = arguments(n);
+    if (l.isEmpty())
+      return null;
+    // TODO Yuval Simon: move into loop --yg
+    for (int i = 0; i < l.size(); ++i) {
       final ConditionalExpression $;
-      if (($ = az.conditionalExpression(arguments.get(i))) != null) {
+      if (($ = az.conditionalExpression(l.get(i))) != null) {
         if (iz.nullLiteral(then($)) || iz.nullLiteral(elze($)))
           return null;
-        final MethodInvocation whenTrue = copy.of(current), whenFalse = copy.of(current);
+        final MethodInvocation whenTrue = copy.of(n), whenFalse = copy.of(n);
         arguments(whenTrue).remove(i);
         arguments(whenTrue).add(i, copy.of(then($)));
         arguments(whenFalse).remove(i);
         arguments(whenFalse).add(i, copy.of(elze($)));
-        r.replace(current, make.parethesized(subject.pair(whenTrue, whenFalse).toCondition(expression($))), g);
-        return r;
+        return make.parethesized(subject.pair(whenTrue, whenFalse).toCondition(expression($)));
       }
-      if (haz.sideEffects(arguments.get(i)))
+      if (haz.sideEffects(l.get(i)))
         break;
     }
     return null;
+  }
+
+  @Override public String description(@SuppressWarnings("unused") final MethodInvocation __) {
+    return "";
   }
 }
