4ae5318a178d765e7d28f05fa7762f41d10cc28d
oran1248 <oran.gilboa1@gmail.com>
2017-04-02 19:53:21 +0300

    fix #1190

---
+++ src/test/java/il/org/spartan/spartanizer/issues/Issue1190.java
@@ -0,0 +1,42 @@
+package il.org.spartan.spartanizer.issues;
+
+import java.io.*;
+import java.util.function.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.spartanizer.cmdline.*;
+import il.org.spartan.spartanizer.dispatch.*;
+import il.org.spartan.utils.*;
+
+/** Batch testing - run the spartinizer on itself with no errors
+ * @author oran1248 <tt>oran.gilboa1@gmail.com</tt>
+ * @since 2017-04-01 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Issue1190 {
+  Trimmer trimmer;
+
+  @Before public void setUp() {
+    trimmer = new Trimmer(Toolbox.defaultInstance());
+    trimmer.setExceptionListener(new Consumer<Exception>() {
+      @Override public void accept(Exception ¢) {
+        ¢.printStackTrace();
+        assert false;
+      }
+    });
+  }
+  @Test(timeout=30000) public void runTheSpartinizerOnItself() {
+    new ASTInFilesVisitor(new String[] { "-i", ".", "." }) {
+      @Override protected void visit(java.io.File ¢) {
+        try {
+          trimmer.fixed(FileUtils.read(¢));
+        } catch (IOException e) {
+          e.printStackTrace();
+          assert false;
+        }
+      }
+    }.fire(new ASTVisitor() {/**/});
+  }
+}
