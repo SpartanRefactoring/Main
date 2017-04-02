b3e7111caeb499b3cc2a392df09e52f7bdd98e04
Niv Shalmon <shalmon.niv@gmail.com>
2017-04-02 08:03:20 +0300

    restore TipperTest to final version

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/testing/TipperTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/testing/TipperTest.java
@@ -1,5 +1,7 @@
 package il.org.spartan.spartanizer.testing;
 
+import org.eclipse.jdt.core.dom.*;
+
 import il.org.spartan.spartanizer.issues.*;
 import il.org.spartan.spartanizer.tipping.*;
 
@@ -8,31 +10,41 @@ import il.org.spartan.spartanizer.tipping.*;
  * class's trimmingOf. Usage example in {@link Issue1146}
  * @author Niv Shalmon <tt>shalmon.niv@gmail.com</tt>
  * @since 2017-04-01 */
-public abstract class TipperTest {
+public abstract class TipperTest<N extends ASTNode> {
   /** A method used to set the tipper to be tested.
    * @return a new instance of the tipper to be tested */
-  public abstract Tipper<?> tipper();
+  public abstract Tipper<N> tipper();
 
-  public TipperTrimmingOperand trimmingOf(final String from) {
-    return new TipperTrimmingOperand(from, tipper());
+  /** A method used to set the class the tipper works on.
+   * @return the class object of the ASTNode the tipper tips on */
+  public abstract Class<N> tipsOn();
+
+  public TipperTrimmingOperand<N> trimmingOf(final String from) {
+    return new TipperTrimmingOperand<>(from, tipper(), tipsOn());
   }
 
   // an inner class used to wrap usingTipper into gives and stays
-  public class TipperTrimmingOperand extends TrimmingOperand {
-    private final Tipper<?> tipper;
+  public class TipperTrimmingOperand<M extends ASTNode> extends TrimmingOperand {
+    private final Tipper<M> tipper;
+    private final Class<M> tipsOn;
 
-    public TipperTrimmingOperand(final String inner, final Tipper<?> tipper) {
+    public TipperTrimmingOperand(final String inner, final Tipper<M> tipper, final Class<M> tipsOn) {
       super(inner);
       this.tipper = tipper;
-      usingTipper(tipper);
+      this.tipsOn = tipsOn;
+      using(tipsOn,tipper);
+    }
+    
+    private TipperTrimmingOperand(final TrimmingOperand o, final Tipper<M> tipper, final Class<M> tipsOn){
+      this(o.get(),tipper,tipsOn);
     }
 
-    @Override public TipperTrimmingOperand gives(final String $) {
-      return new TipperTrimmingOperand(super.gives($).get(), tipper);
+    @Override public TipperTrimmingOperand<M> gives(final String $) {
+      return new TipperTrimmingOperand<>(super.gives($), tipper, tipsOn);
     }
 
-    @Override public TipperTrimmingOperand givesEither(final String... options) {
-      return new TipperTrimmingOperand(super.givesEither(options).get(), tipper);
+    @Override public TipperTrimmingOperand<M> givesEither(final String... options) {
+      return new TipperTrimmingOperand<>(super.givesEither(options), tipper, tipsOn);
     }
   }
 }
