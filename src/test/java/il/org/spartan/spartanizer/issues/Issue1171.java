9ce5de37bf85b4fe094c24cf86b484fb0bb4e862
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-03 22:10:29 +0300

    #1171: started work

--- src/test/java/il/org/spartan/spartanizer/issues/Issue1171.java
+++ src/test/java/il/org/spartan/spartanizer/issues/Issue1171.java
@@ -4,25 +4,24 @@ import static il.org.spartan.spartanizer.testing.TestsUtilsTrimmer.*;
 
 import org.junit.*;
 
-/** TODO Ori Roth: document class
- * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
- * @since 2017-03-26 */
+/** Unit test for GitHub issue thus numbered
+ * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
+ * @since 2017-04-03 */
 @SuppressWarnings("static-method")
 public class Issue1171 {
-
   @Test public void inlineArrayInitialization2() {
-    trimmingOf("public double[] solve(){ " //
-        + " final SimpleRegression regress=new SimpleRegression(true); " //
-        + " for(double[] dxx : points) " //
-        + " regress.addData(d[0], d[1]); " //
-        + " final double[] $={ regress.getSlope(), regress.getIntercept()}; " //
+    trimmingOf("public double[] h(){ " //
+        + " final S r=new S(true); " //
+        + " for(double[] dxx : ps) " //
+        + " r.a(d[0], d[1]); " //
+        + " final double[] $={ r.getSlope(), r.g()}; " //
         + " return $; " //
         + "}")
-            .gives("public double[] solve(){ " //
-                + " final SimpleRegression regress=new SimpleRegression(true); " //
-                + " for(double[] dxx : points) " //
-                + " regress.addData(d[0], d[1]); " //
-                + " return new double[] { regress.getSlope(), regress.getIntercept()}; " //
+            .gives("public double[] h(){ " //
+                + " final S r=new S(true); " //
+                + " for(double[] dxx : ps) " //
+                + " r.a(d[0], d[1]); " //
+                + " return new double[] { r.getSlope(), r.g()}; " //
                 + "}");
   }
 }
