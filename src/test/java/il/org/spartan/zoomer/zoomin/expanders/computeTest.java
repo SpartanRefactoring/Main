cff39e5a1fb9be6d4ba73ac8dd051170d33293fe
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-03 07:22:03 +0300

    sync

--- src/test/java/il/org/spartan/zoomer/zoomin/expanders/computeTest.java
+++ src/test/java/il/org/spartan/zoomer/zoomin/expanders/computeTest.java
@@ -2,6 +2,9 @@ package il.org.spartan.zoomer.zoomin.expanders;
 
 import static il.org.spartan.azzert.*;
 
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 import org.junit.runners.*;
 
@@ -14,12 +17,17 @@ import il.org.spartan.spartanizer.meta.*;
  * @author Yossi Gil <tt>yogi@cs.technion.ac.il</tt>
  * @since 2017-04-01 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Ignore("Yossi Gil")
 @SuppressWarnings("static-method")
 public class computeTest extends MetaFixture {
-  @Test public void test1a() {
+  @Test public void updatedSpots() {
     compute.updateSpots(into.s("return a *=2"));
     azzert.that(compute.updateSpots(into.s("return a *=2")).size(), is(1));
     azzert.that(compute.updateSpots(into.s("return a =2")).size(), is(1));
+    final List<ASTNode> updateSpots = compute.updateSpots(into.s("return local +=2"));
+    
+    azzert.that(updateSpots.size() ,is(1));
+   
+    azzert.that(lisp.onlyOne(updateSpots) + "", is("local"));
+    assert ! updateSpots.stream().noneMatch(λ -> wizard.eq(λ, lisp.onlyOne(updateSpots).getAST().newSimpleName("local")));
   }
 }
