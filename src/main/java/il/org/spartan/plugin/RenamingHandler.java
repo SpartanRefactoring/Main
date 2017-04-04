4f2b6fb4d0894668ec29601985bf862b5188ea4f
dormaayn <dor.d.ma@gmail.com>
2017-04-04 18:42:43 +0300

    Added a new shortcut

---
+++ src/main/java/il/org/spartan/plugin/RenamingHandler.java
@@ -0,0 +1,22 @@
+package il.org.spartan.plugin;
+
+import org.eclipse.core.commands.*;
+
+import il.org.spartan.bloater.*;
+import il.org.spartan.bloater.collateral.*;
+
+/** Handler for the Renamig thesis of Dor. Uses {@link BloatApplicator} as an
+ * {@link Applicator} and {@link Augmenter} as an {@link Application}.
+ * @author Dor Ma'ayan
+ * @since Apr 4, 2017 */
+public class RenamingHandler extends AbstractHandler {
+  @Override public Object execute(@SuppressWarnings("unused") final ExecutionEvent __) {
+    applicator().go();
+    return null;
+  }
+
+  public static GUIBatchLaconizer applicator() {
+    System.out.println("hi im here !!!!");
+    return null;
+  }
+}
