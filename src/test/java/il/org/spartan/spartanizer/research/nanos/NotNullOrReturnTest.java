bb80e4ad1ab35dc3034ec502e3752d276b12347e
orimarco <marcovitch.ori@gmail.com>
2017-04-06 13:26:03 +0300

    unspartanized

--- src/test/java/il/org/spartan/spartanizer/research/nanos/NotNullOrReturnTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/NotNullOrReturnTest.java
@@ -5,14 +5,14 @@ import static il.org.spartan.spartanizer.testing.TestsUtilsTrimmer.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-/** Tests {@link NotNullOrReturn}
+/** Tests {@link NotNullAssumed}
  * @author orimarco {@code marcovitch.ori@gmail.com}
  * @since 2017-01-08 */
 @SuppressWarnings("static-method")
 public class NotNullOrReturnTest {
   @Test public void a() {
     trimmingOf("statement(); if(x == null) return; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .gives("statement(); azzert.notNull(x); use(); use();")//
         .gives("statement();assert x!=null;use();use();") //
         .stays();
@@ -20,13 +20,13 @@ public class NotNullOrReturnTest {
 
   @Test public void sanity() {
     trimmingOf("statement(); azzert.notNull(x); use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .stays();
   }
 
   @Test public void a2() {
     trimmingOf("statement(); if(x == null || y == null) return; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .gives("statement(); azzert.notNull(x,y); use(); use();")//
         .gives("statement();assert y!=null:x;use();use();") //
         .stays();
@@ -34,7 +34,7 @@ public class NotNullOrReturnTest {
 
   @Test public void b() {
     trimmingOf("statement(); if(x == null) return null; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .gives("statement(); azzert.notNull(x); use(); use();")//
         .gives("statement();assert x!=null;use();use();") //
         .stays();
@@ -42,7 +42,7 @@ public class NotNullOrReturnTest {
 
   @Test public void b2() {
     trimmingOf("statement(); if(x == null || y == null) return null; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .gives("statement(); azzert.notNull(x,y); use(); use();")//
         .gives("statement();assert y!=null:x;use();use();") //
         .stays();
@@ -50,19 +50,19 @@ public class NotNullOrReturnTest {
 
   @Test public void respect() {
     trimmingOf("void m(){if(x == null) return; use(); use();}")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .stays();
   }
 
   @Test public void respect2() {
     trimmingOf("void m(){use(); if(x == null) return false; use(); use();}")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .stays();
   }
 
   @Test public void respect3() {
     trimmingOf("void m(){use(); if(x == null) return; use(); use();}")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .gives("void m(){use();azzert.notNull(x);use();use();}") //
         .gives("void m(){use();assert x!=null;use();use();}") //
         .stays();
@@ -70,7 +70,7 @@ public class NotNullOrReturnTest {
 
   @Test public void respect4() {
     trimmingOf("statement(); if(x == null) return false; use(); use();")//
-        .using(IfStatement.class, new NotNullOrReturn())//
+        .using(IfStatement.class, new NotNullAssumed())//
         .stays();
   }
 }
