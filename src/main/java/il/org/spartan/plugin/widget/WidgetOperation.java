0241f2b8aa86e2cd441579da72943e21646c2c1f
Ori Roth <ori.rothh@gmail.com>
2017-04-06 16:43:37 +0300

    start working on widget see #1223

---
+++ src/main/java/il/org/spartan/plugin/widget/WidgetOperation.java
@@ -0,0 +1,59 @@
+package il.org.spartan.plugin.widget;
+
+import java.io.*;
+import java.util.function.*;
+
+import org.eclipse.swt.graphics.*;
+
+import il.org.spartan.plugin.*;
+import il.org.spartan.spartanizer.issues.*;
+
+/** Operation, image, description etc. of a widget button.
+ * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
+ * @since 2017-04-06 [[SuppressWarningsSpartan]] */
+@SuppressWarnings("unused")
+public abstract class WidgetOperation implements Serializable {
+  private static final long serialVersionUID = 0x5DEE331D4A6A17CEL;
+
+  /** Operation on mouse down. */
+  public void onMouseDown(final WidgetContext context) {
+    // DO noting
+  }
+
+  /** Operation on mouse up. A simple single click operation should be
+   * implemented here. */
+  public void onMouseUp(final WidgetContext context) {
+    // DO noting
+  }
+
+  /** Operation on mouse hold. Will be called once per
+   * {@link SpartanWidgetHandler#OPERATION_HOLD_INTERVAL} ms, unless last
+   * holdoperation did not finished. */
+  public void onMouseHold(final WidgetContext context) {
+    // DO noting
+  }
+
+  /** Operation on mouse double click. */
+  public void onDoubleClick() {
+    // Do nothing
+  }
+
+  /** @return URL of image of this operation. */
+  public abstract String imageURL();
+
+  /** @return short human readable description of this operation. */
+  public abstract String description();
+
+  /** @return scaled SWT image of this operation.
+   * @see #scale() */
+  public Image image() {
+    final String $ = imageURL();
+    return Dialogs.image($, $, scale());
+  }
+
+  /** Scaling the image of the operation, does nothing by default.
+   * @return scaler for SWT image of this operation. */
+  @SuppressWarnings("static-method") protected Function<ImageData, ImageData> scale() {
+    return λ -> λ;
+  }
+}
