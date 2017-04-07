2ae3fbd17a185fcb3909aaf7b4909ef6cfc60453
Yuval Simon <siyuval@campus.technion.ac.il>
2017-04-07 04:30:37 +0300

    #1222

---
+++ src/main/java/il/org/spartan/spartanizer/tippers/Names.java
@@ -0,0 +1,14 @@
+package il.org.spartan.spartanizer.tippers;
+
+import java.util.function.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+/**
+ * TODO Yuval Simon: document class 
+ * @author Yuval Simon <tt>siyuval@campus.technion.ac.il</tt>
+ * @since 2017-04-07
+ */
+public final class Names {
+  public static Function<ASTNode,String> methodReturnName = λ -> "$";
+}
