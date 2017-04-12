f3b9853a603c71de8922a4fdf85726c6fed41103
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-12 13:59:06 +0300

    #1248

--- src/main/java/il/org/spartan/spartanizer/tippers/MethodDeclarationRenameReturnToDollar.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/MethodDeclarationRenameReturnToDollar.java
@@ -35,16 +35,13 @@ public final class MethodDeclarationRenameReturnToDollar extends EagerTipper<Met
 
   @Override public Tip tip(final MethodDeclaration d) {
     final Type t = d.getReturnType2();
-    if (t == null)
-      return null;
     switch (t + "") {
+      case "null":
       case "boolean":
       case "void":
       case "Void":
       case "java.lang.Void":
         return null;
-      default:
-        break;
     }
     final String name = Names.methodReturnName.apply(t, d);
     if (name == null || scope.hasInScope(d.getBody(), name))
