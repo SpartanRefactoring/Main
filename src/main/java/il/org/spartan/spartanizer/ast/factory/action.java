54b787a294d8992422a10ab4f8d0d3c61fa33efb
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-12 23:10:47 +0300

    Sync

--- src/main/java/il/org/spartan/spartanizer/ast/factory/action.java
+++ src/main/java/il/org/spartan/spartanizer/ast/factory/action.java
@@ -20,7 +20,6 @@ import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tippers.*;
-import il.org.spartan.utils.*;
 import il.org.spartan.utils.fluent.*;
 
 /** A number of utility functions common to all tippers.
