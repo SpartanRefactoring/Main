a29f54a01d2789e09ef344f00bfb4a0976c1b63f
orenafek <oren.afek@gmail.com>
2017-04-13 18:48:37 +0300

    working on Prefix Metatester.

--- src/test/java/il/org/spartan/spartanizer/research/metatester/SimpleTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/metatester/SimpleTest.java
@@ -7,14 +7,17 @@ import org.junit.runner.*;
 
 /** @author Oren Afek
  * @since 3/27/2017 */
-@Ignore
-//@RunWith(MetaTester.class)
+@RunWith(MetaTester.class)
 public class SimpleTest {
   @Test @SuppressWarnings("static-method") public void testF() {
-    assertEquals(1, 2);
+    assertEquals(1, 1);
     System.out.println("");
     assertEquals(1, 1);
     System.out.println("");
     assert true;
+  }
+
+  @Test @SuppressWarnings("static-method") public void testG() {
+    assertTrue(true);
   }
 }
