7beeb0968e45c7587c14a1f02e34ba9a4081e630
orenafek <oren.afek@gmail.com>
2017-04-13 19:53:10 +0300

    MetaTester is now doing prefixes. PropositionTest passes!!

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/metatester/ASTTestClassGenerator.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/metatester/ASTTestClassGenerator.java
@@ -1,33 +1,49 @@
 package il.org.spartan.spartanizer.research.metatester;
 
+import static il.org.spartan.spartanizer.research.metatester.FileUtils.*;
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
 import org.eclipse.jdt.core.dom.*;
-
-import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.java.*;
+import il.org.spartan.spartanizer.utils.*;
 import il.org.spartan.utils.*;
 
 /** TODO orenafek: document class
  * @author orenafek
  * @since 2017-04-12 */
 @UnderConstruction("OrenAfek -- 12/04/2017")
-@SuppressWarnings("all")
 public class ASTTestClassGenerator implements TestClassGenerator {
+  class Test {
+    public ASTNode source;
+    public List<MemberValuePair> values;
+
+    public Test(ASTNode source, List<MemberValuePair> values) {
+      this.source = source;
+      this.values = values;
+    }
+  }
+
   private ASTNode root;
 
   enum SourceLineType {
     TEST, OTHER;
   }
 
-  Set<ASTNode> sourceLines;
+  Set<Test> sourceLines;
+  private final Class<?> testClass;
+  private final String sourcePath;
+  public final String packageName;
+  private final Int testNoGenerator = Int.valueOf(0);
 
-  public ASTTestClassGenerator() {
+  public ASTTestClassGenerator(final Class<?> testClass) {
+    this.testClass = testClass;
+    sourcePath = makePath(System.getProperty("user.dir"), "src", "test", "java", packageName("\\\\", testClass));
+    packageName = packageName("\\.", testClass);
     sourceLines = new HashSet<>();
   }
 
@@ -46,40 +62,27 @@ public class ASTTestClassGenerator implements TestClassGenerator {
     });
   }
 
-  private void addToMap(ASTNode ¢) {
-    sourceLines.add(¢);
+  void addToMap(Test t) {
+    sourceLines.add(t);
   }
 
-  private void removeOriginalTestsFromTree(ASTNode root) {
+  private void removeOriginalTestsFromTree() {
     sourceLines.stream() //
-        .peek(λ -> System.out.println("Node = " + λ)) //
+        .map(x -> x.source) //
         .forEach(λ -> az.abstractTypeDeclaration(λ.getParent()).bodyDeclarations().remove(λ));
-  }
-
-  private void fillMap(ASTNode root) {
-    root.accept(new DispatchingVisitor() {
-      @Override protected <N extends ASTNode> boolean go(N __) {
-        return false;
-      }
-    });
-    step.statements(root).stream().forEach(λ -> {
-      if (!sourceLines.contains(λ))
-        addToMap(λ);
-    });
   }
 
   @Override public Class<?> generate(String testClassName, final File originalSourceFile) {
     root = makeAST(originalSourceFile);
     modifyClass();
-    testClassSkelaton(allTestMethods().stream().map(λ -> tests(prefixes(λ))).collect(Collectors.toList()));
-    
-    return Object.class;
+    String $ = testClassSkelaton(allTestMethods().stream().map(λ -> tests(λ, prefixes(λ))).collect(Collectors.toList()));
+    return loadClass(testClassName, format.code($), testClass, sourcePath);
   }
 
-  private List<String> prefixes(MethodDeclaration d) {
+  private static List<String> prefixes(Test t) {
     List<String> $ = new ArrayList<>();
     StringBuilder prefix = new StringBuilder();
-    step.statements(d).stream().map(λ -> {
+    step.statements((MethodDeclaration) t.source).stream().map(λ -> {
       prefix.append(λ + "\n");
       return prefix + "";
     }).forEach($::add);
@@ -87,7 +90,7 @@ public class ASTTestClassGenerator implements TestClassGenerator {
   }
 
   private String testClassSkelaton(List<List<String>> tests) {
-    removeOriginalTestsFromTree(root);
+    removeOriginalTestsFromTree();
     StringBuilder $ = new StringBuilder();
     $.append(root + "");
     $.replace($.lastIndexOf("}"), $.length(), "");
@@ -96,43 +99,49 @@ public class ASTTestClassGenerator implements TestClassGenerator {
     return $ + "";
   }
 
-  private List<String> tests(List<String> prefixes) {
-    final Int $ = Int.valueOf(0);
-    return prefixes.stream().map(λ -> String.format("@Test public void test%d(){\n%s}", $.next(), λ)).collect(Collectors.toList());
+  private static <T> String removeBraces(List<T> l) {
+    StringBuilder $ = new StringBuilder();
+    l.stream().forEach(e -> $.append(e + ", "));
+    return $.substring(0, $.length() - 2);
   }
 
-  private List<MethodDeclaration> allTestMethods() {
-    List<MethodDeclaration> $ = new ArrayList<>();
+  @SuppressWarnings("boxing") private List<String> tests(Test t, List<String> prefixes) {
+    String annotaionString = "@Test" + (t.values != null ? "(" + removeBraces(t.values) + ")" : "");
+    return prefixes.stream().map(λ -> String.format("%s public void test%d(){\n%s}", annotaionString, testNoGenerator.next(), λ))
+        .collect(Collectors.toList());
+  }
+
+  private List<Test> allTestMethods() {
+    List<Test> $ = new ArrayList<>();
     root.accept(new ASTVisitor() {
       @Override public boolean visit(MethodDeclaration node) {
-        boolean result = extract.annotations(node).stream().anyMatch(λ -> haz.name(λ, "Test"));
-        if (result) {
-          addToMap(node);
-          // step.statements(node).stream().forEach(λ ->
-          // addToMap(SourceLineType.TEST, λ));
-          $.add(node);
+        Annotation a = extract.annotations(node).stream().filter(λ -> haz.name(λ, "Test")).findAny().orElse(null);
+        if (a != null) {
+          List<MemberValuePair> l = a instanceof NormalAnnotation ? step.values(((NormalAnnotation) a)) : null;
+          Test t = new Test(node, l);
+          addToMap(t);
+          $.add(t);
         }
-        return result;
+        return a != null;
       }
     });
     return $;
   }
 
-  private ASTNode makeAST(final File originalSourceFile) {
+  private static ASTNode makeAST(final File originalSourceFile) {
     return wizard.ast(readAll(originalSourceFile));
   }
 
-  private String readAll(File f) {
+  private static String readAll(File f) {
     final StringBuilder $ = new StringBuilder();
-    try {
-      final BufferedReader linesStream = new BufferedReader(new FileReader(f));
+    try (final BufferedReader linesStream = new BufferedReader(new FileReader(f))) {
       String line = linesStream.readLine();
-      for (int ¢ = 1; line != null; ++¢) {
+      for (; line != null;) {
         $.append(line).append("\n");
         line = linesStream.readLine();
       }
       return $ + "";
-    } catch (final IOException ignore) {/**/}
+    } catch (@SuppressWarnings("unused") final IOException ignore) {/**/}
     return "";
   }
 }
