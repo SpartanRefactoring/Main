c86879f9093115a701101b519ddaf373e9e41e7e
orenafek <oren.afek@gmail.com>
2017-04-13 18:52:11 +0300

    refactoring.

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/metatester/SimpleTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/metatester/SimpleTest.java
@@ -3,9 +3,11 @@ package il.org.spartan.spartanizer.research.metatester;
 import static org.junit.Assert.*;
 
 import org.junit.*;
+import org.junit.runner.*;
 
 /** @author Oren Afek
  * @since 3/27/2017 */
+@Ignore
 @RunWith(MetaTester.class)
 public class SimpleTest {
   @Test @SuppressWarnings("static-method") public void testF() {
