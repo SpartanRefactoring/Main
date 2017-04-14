7a3968a09ba7003f2110a4bd725b17bba2d5e254
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-14 09:52:37 +0300

    sync

--- SpartanizerPlugin/src/test/java/il/org/spartan/utils/fluent/TestFluent_null¢.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/utils/fluent/TestFluent_null¢.java
@@ -8,21 +8,61 @@ import il.org.spartan.*;
 import il.org.spartan.iteration.closures.*;
 
 public class TestFluent_null¢ {
-    static String a = "Hello";
-    String b;
-    final Function<String, Integer> f = String::length;
- 
-    @Test public void t() {
-      final Integer i1 = null¢.guardingly(String::length).on(a);
-      azzert.that(i1,is(5));
-      final Integer i2 = null¢.guardingly(String::length).on(b);
-      azzert.that(i1,is(5));
-      azzert.isNull(i2);
-      final Integer i3 = null¢.guardingly(f).on(b);
-      azzert.that(i1,is(5));
-      azzert.isNull(i2);
-      azzert.isNull(i3);
-      
-    }
+  static String helloString = "Hello";
 
- }
+  @Test public void t() {
+    final Integer i1 = null¢.guardingly(String::length).on(helloString);
+    azzert.that(i1, is(5));
+    final Integer i2 = null¢.guardingly(String::length).on(nullString);
+    azzert.that(i1, is(5));
+    azzert.isNull(i2);
+    final Integer i3 = null¢.guardingly(f).on(nullString);
+    azzert.that(i1, is(5));
+    azzert.isNull(i2);
+    azzert.isNull(i3);
+    
+    null¢.guardingly(State::getName).on(null);
+
+    californiaCustomer.getAddress().getState().getName();
+    nullAddressCustomer.getAddress().getState();
+    nullStateCustomer.getAddress().getState();
+    nullNameCustomer.getAddress().getState();
+  }
+
+  String nullString;
+  final Customer californiaCustomer = () -> () -> new State() {
+    @Override public String getName() {
+      return "California";
+    }
+  };
+  final Function<String, Integer> f = String::length;
+  final Customer nullAddressCustomer = new Customer() {
+    @Override public Address getAddress() {
+      return null;
+    }
+  };
+  final Customer nullNameCustomer = () -> () -> new State() {};
+  final Customer nullStateCustomer = new Customer() {
+    @Override public Address getAddress() {
+      return new Address() {
+        @Override public State getState() {
+          return null;
+        }
+      };
+    }
+  };
+  final Customer nullStateCustomer1 = new Customer() {
+    @Override public Address getAddress() {
+      return new Address() {
+        @Override public State getState() {
+          return null;
+        }
+      };
+    }
+  };
+
+  //@formatter:off
+  interface Address { State getState(); }
+  interface Customer { Address getAddress(); }
+  interface State {default String getName() { return null; } }
+}
