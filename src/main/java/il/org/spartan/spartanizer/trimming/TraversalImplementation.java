43e87d14bceda61ce99de869b59c5cbbefe7f1a9
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-15 06:59:06 +0300

    cleanup

--- src/main/java/il/org/spartan/spartanizer/trimming/TraversalImplementation.java
+++ src/main/java/il/org/spartan/spartanizer/trimming/TraversalImplementation.java
@@ -8,7 +8,6 @@ import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.rewrite.*;
 
-import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
