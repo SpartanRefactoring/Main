ea4ef6a9a5283e41b4a58c86c78ef6c337dd9c9f
Ori Roth <ori.rothh@gmail.com>
2017-04-16 16:22:05 +0300

    nano a/an

---
+++ SpartanizerPlugin/src/main/java/nano/a/singleton.java
@@ -0,0 +1,18 @@
+package nano.a;
+
+import java.util.*;
+
+/** Singleton collections.
+ * @author Ori Roth
+ * @since 2017-04-16 */
+public interface singleton {
+  /** Singleton list. */
+  static <T> List<T> list(T ¢) {
+    return Collections.singletonList(¢);
+  }
+
+  /** Singleton array. */
+  static <T> Object[] array(T ¢) {
+    return new Object[] { ¢ };
+  }
+}
