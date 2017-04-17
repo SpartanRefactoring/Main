d7a35fedb72720efff9360957d53597d1274a8ae
orimarco <marcovitch.ori@gmail.com>
2017-04-17 20:37:16 +0300

    renaming

--- src/main/java/il/org/spartan/spartanizer/research/nanos/EvaluateUnlessDefaultsTo.java
+++ src/main/java/il/org/spartan/spartanizer/research/nanos/EvaluateUnlessDefaultsTo.java
@@ -14,7 +14,7 @@ import il.org.spartan.spartanizer.research.nanos.common.*;
 /** X false ? Y : null
  * @author Ori Marcovitch
  * @since Dec 13, 2016 */
-public final class Unless extends NanoPatternTipper<ConditionalExpression> {
+public final class EvaluateUnlessDefaultsTo extends NanoPatternTipper<ConditionalExpression> {
   private static final long serialVersionUID = -0x35BF25BC0DF0BA40L;
   private static final List<UserDefinedTipper<ConditionalExpression>> tippers = as.list(
       patternTipper("$X1 ? $D : $X2", "unless($X1).eval(() -> $X2).defaultTo($D)", "Go fluent: Unless pattern"),
@@ -32,17 +32,5 @@ public final class Unless extends NanoPatternTipper<ConditionalExpression> {
 
   @Override public String description() {
     return "Evaluate an expression unless some condition is satisfied";
-  }
-
-  @Override public String example() {
-    return firstPattern(tippers);
-  }
-
-  @Override public String symbolycReplacement() {
-    return firstReplacement(tippers);
-  }
-
-  @Override public Category category() {
-    return Category.Safety;
   }
 }
