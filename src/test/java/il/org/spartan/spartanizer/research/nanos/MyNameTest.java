1f35bcc6bea038eb74d937cf131891e708ad1feb
orimarco <marcovitch.ori@gmail.com>
2017-04-17 21:50:45 +0300

    MN

--- src/test/java/il/org/spartan/spartanizer/research/nanos/MyNameTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/nanos/MyNameTest.java
@@ -5,35 +5,35 @@ import static il.org.spartan.spartanizer.testing.TestsUtilsSpartanizer.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-/** Tests {@link Reduction}
+/** Tests {@link MyName}
  * @author orimarco {@code marcovitch.ori@gmail.com}
  * @since 2017-02-01 */
 @SuppressWarnings("static-method")
-public class ReductionTest {
+public class MyNameTest {
   @Test public void a() {
     trimminKof("void foo(){ foo(a);}")//
-        .using(new Reduction(), MethodInvocation.class)//
+        .using(new MyName(), MethodInvocation.class)//
         .gives("void foo(){reduce¢(a);}")//
         .stays();
   }
 
   @Test public void b() {
     trimminKof("void foo(){ return foo(a) + foo(a,b);}")//
-        .using(new Reduction(), MethodInvocation.class)//
+        .using(new MyName(), MethodInvocation.class)//
         .gives("void foo(){ return reduce¢(a) + reduce¢(a,b);}")//
         .stays();
   }
 
   @Test public void c() {
     trimminKof("void foo(){ return foo(a) + foo(a,b) == 7 ? foo() : bar();}")//
-        .using(new Reduction(), MethodInvocation.class)//
+        .using(new MyName(), MethodInvocation.class)//
         .gives("void foo(){ return reduce¢(a) + reduce¢(a,b) == 7 ? foo() : bar();}")//
         .stays();
   }
 
   @Test public void d() {
     trimminKof("void foo(int a){ return foo(a) + foo(a,b) == 7 ? foo() : barar();}")//
-        .using(new Reduction(), MethodInvocation.class)//
+        .using(new MyName(), MethodInvocation.class)//
         .gives("void foo(int a){ return foo(a) + reduce¢(a,b) == 7 ? reduce¢() : barar();}")//
         .gives("void foo(int a){ return foo(a) + reduce¢(a,b) != 7 ? barar() : reduce¢();}")//
         .stays();
