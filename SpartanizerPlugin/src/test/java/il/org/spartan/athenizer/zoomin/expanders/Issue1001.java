96c0edf690c16143798fd8cf17207bdf5fc4b9a0
Yuval Simon <siyuval@campus.technion.ac.il>
2017-04-21 15:26:27 +0300

    closes #1214 closes #973

--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1001.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1001.java
@@ -10,7 +10,6 @@ import il.org.spartan.spartanizer.meta.*;
 /** Unit test for {@link AssignmentOperatorBloater}.
  * @author Ori Roth {@code ori.rothh@gmail.com}
  * @since 2016-12-25 [[SuppressWarningsSpartan]] */
-@Ignore // TODO Ori ROth
 @SuppressWarnings("static-method")
 public class Issue1001 {
   @Test public void basic() {
@@ -31,18 +30,22 @@ public class Issue1001 {
             + "  a = 0;\n" //
             + "  b = 0;\n" //
             + "  x(a = a + (b += 1));\n" //
-            + "}", "f2")
+            + "}", "f2");
+  }
+  
+  @Test public void inclusion2() {
+    bloatingOf(Issue1001Aux.instance())
         .givesWithBinding("" //
-            + "void f2() {\n" //
+            + "void f22() {\n" //
             + "  int a;\n" //
             + "  int b;\n" //
             + "  a = 0;\n" //
             + "  b = 0;\n" //
             + "  x(a = a + (b = b + 1));\n" //
-            + "}", "f2");
+            + "}", "f22");
   }
 
-  @Test public void inclusion2() {
+  @Test public void inclusion3() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f3() {\n" //
         + "  int a;\n" //
@@ -66,15 +69,19 @@ public class Issue1001 {
             + "  a = 0;\n" //
             + "  b = 0;\n" //
             + "  x(a = a % (b |= 1));\n" //
-            + "}", "f4")
+            + "}", "f4");
+  }
+  
+  @Test public void operators2() {
+    bloatingOf(Issue1001Aux.instance())
         .givesWithBinding("" //
-            + "void f4() {\n" //
+            + "void f44() {\n" //
             + "  int a;\n" //
             + "  int b;\n" //
             + "  a = 0;\n" //
             + "  b = 0;\n" //
             + "  x(a = a % (b = b | 1));\n" //
-            + "}", "f4");
+            + "}", "f44");
   }
 
   /** [[SuppressWarningsSpartan]] */
@@ -84,7 +91,10 @@ public class Issue1001 {
       return new Issue1001Aux();
     }
 
-    void f1() { /* Empty */
+    void f1() {
+      int a;
+      a = 0;
+      a += 1;
     }
 
     void f2() {
@@ -94,11 +104,21 @@ public class Issue1001 {
       b = 0;
       x(a += b += 1);
     }
+    
+    void f22() {
+      int a;
+      int b;
+      a = 0;
+      b = 0;
+      x(a = a + (b += 1));
+    }
 
     void f3() {
       int a;
+      int b;
       a = 0;
-      x(a += 1);
+      b = 0;
+      x(a += (b = 1));
     }
 
     void f4() {
@@ -108,13 +128,13 @@ public class Issue1001 {
       b = 0;
       x(a %= b |= 1);
     }
-
-    void f5() {
+    
+    void f44() {
       int a;
-      double b;
+      int b;
       a = 0;
       b = 0;
-      x(a += b += 1);
+      x(a = a % (b |= 1));
     }
 
     void x(final int y) {
