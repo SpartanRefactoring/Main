6f7ca1bceb00c517c8f093208203b2807fe2c741
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-21 18:04:06 +0300

    Fix broken build

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/traversal/Tippers.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/traversal/Tippers.java
@@ -11,19 +11,9 @@ import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.utils.*;
 import nano.ly.*;
 
-/** This interface {@link Tippers} contain Tippers functionality for extracting
- * tip from {@link ASTNode}, finding Tipper's serialVersionUID, getting Tipper's
- * name, and more. {@link Tipper}.<br>
- * The inner interface {@link cache} contains few tipper mapping tables:
- * <ul>
- * <li>serialVersionUID -> Tipper</li>
- * <li>Tipper's class -> Tipper</li>
- * <li>Tipper's class -> Tipper's description</li>
- * <li>Tipper's class -> Tipper's examples</li>
- * </ul>
- * NOTE: the tables are built lazily
- * @author oran1248
- * @since 2017-04-21 */
+/** TODO Ori Roth document
+ * @author Yossi Gil
+ * @since 2017-04-12 */
 public interface Tippers {
   interface cache {
     Map<String, Class<? extends Tipper<?>>> serivalVersionUIDToTipper = anonymous.ly(() -> {
@@ -32,7 +22,7 @@ public interface Tippers {
         $.put(find(¢) + "", ¢.myClass());
       return $;
     });
-    Map<Class<? extends Tipper<?>>, Tipper<?>> TipperObjectByClassCache = anonymous.ly(() -> {
+    Map<Class<? extends Tipper<?>>, Tipper<?>> tipperClassToTipperInstance = anonymous.ly(() -> {
       final Map<Class<? extends Tipper<?>>, Tipper<?>> $ = new HashMap<>();
       for (final Tipper<? extends ASTNode> ¢ : Configurations.all().getAllTippers())
         $.put(¢.myClass(), ¢);
@@ -50,41 +40,27 @@ public interface Tippers {
         $.put(¢.myClass(), ¢.examples());
       return $;
     });
-    Map<String, String> TipperIDNameTranslationTable = anonymous.ly(() -> {
+    Map<String, String> idToNameOriWhatsThisFindAGoodName = anonymous.ly(() -> {
       final Map<String, String> $ = new HashMap<>();
-      for (final Tipper<? extends ASTNode> ¢ : Configurations.allClone().getAllTippers())
-        $.put(find(¢) + "", ¢.tipperName());
+      for (final Tipper<? extends ASTNode> ¢ : Configurations.all().getAllTippers())
+        $.put(Tippers.find(¢) + "", ¢.tipperName());
       return $;
     });
   }
 
-<<<<<<< HEAD
-  Map<String, String> TipperIDNameTranslationTable = anonymous.ly(() -> {
-    final Map<String, String> $ = new HashMap<>();
-    for (final Tipper<? extends ASTNode> ¢ : Configurations.all().getAllTippers())
-      $.put(find(¢) + "", ¢.tipperName());
-    return $;
-  });
-
-=======
-  /** Returns the tip tipper {@code t} can apply to the given node {@code n}. */
->>>>>>> branch 'master' of git@github.com:SpartanRefactoring/Spartanizer.git
   static Tip extractTip(final Tipper<? extends ASTNode> t, final ASTNode n) {
     @SuppressWarnings("unchecked") final Tipper<ASTNode> $ = (Tipper<ASTNode>) t;
     return $.tip(n);
   }
 
-  /** Returns the serialVersionUID field's value for the given tipper. */
   static long find(final Tipper<? extends ASTNode> ¢) {
     return ObjectStreamClass.lookup(¢.getClass()).getSerialVersionUID();
   }
 
-  /** Returns the first tipper that can apply to node {@code n}. */
   @SafeVarargs static <N extends ASTNode> Tipper<N> findTipper(final N n, final Tipper<N>... ts) {
     return Stream.of(ts).filter(λ -> λ.check(n)).findFirst().orElse(null);
   }
 
-  /** Returns the given tipper's name. */
   static <T extends Tipper<? extends ASTNode>> String name(final T ¢) {
     return ¢.getClass().getSimpleName();
   }
