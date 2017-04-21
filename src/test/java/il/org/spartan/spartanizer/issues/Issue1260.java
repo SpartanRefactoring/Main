792be3f3f6b5cf4ec602a25e576fdaaabb2e553c
oran1248 <oran.gilboa1@gmail.com>
2017-04-21 20:27:01 +0300

    #1260 using azzert.that instead of assertEquals

--- src/test/java/il/org/spartan/spartanizer/issues/Issue1260.java
+++ src/test/java/il/org/spartan/spartanizer/issues/Issue1260.java
@@ -1,13 +1,12 @@
 package il.org.spartan.spartanizer.issues;
 
-import static org.junit.Assert.*;
-
 import java.util.*;
 import java.util.stream.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.*;
 
+import il.org.spartan.*;
 import il.org.spartan.utils.*;
 
 /** Tests for reducers
@@ -17,32 +16,32 @@ import il.org.spartan.utils.*;
 public class Issue1260 {
   @Test public void list() {
     ListMergingReducer<Integer> r = new ListMergingReducer<>();
-    assertThat(IntStream.range(1, 31).boxed().collect(Collectors.toList()), is(r.reduce(IntStream.range(1, 11).boxed().collect(Collectors.toList()),
+    azzert.that(IntStream.range(1, 31).boxed().collect(Collectors.toList()), is(r.reduce(IntStream.range(1, 11).boxed().collect(Collectors.toList()),
         IntStream.range(11, 21).boxed().collect(Collectors.toList()), IntStream.range(21, 31).boxed().collect(Collectors.toList()))));
-    assertEquals(new LinkedList<Integer>(), r.reduce());
+    azzert.that(new LinkedList<Integer>(), is(r.reduce()));
     List<Integer> l = new LinkedList<>();
-    assertEquals(l, r.reduce(l));
+    azzert.that(l, is(r.reduce(l)));
   }
 
   @Test @SuppressWarnings("boxing") public void integer() {
     IntegerAdditionReducer r = new IntegerAdditionReducer();
-    assertEquals(Box.box(0), r.reduce());
-    assertEquals(Box.box(1), r.reduce(1));
-    assertEquals(Box.box(3), r.reduce(1, 2));
-    assertEquals(Box.box(6), r.reduce(1, 2, 3));
+    azzert.that(Box.box(0), is(r.reduce()));
+    azzert.that(Box.box(1), is(r.reduce(1)));
+    azzert.that(Box.box(3), is(r.reduce(1, 2)));
+    azzert.that(Box.box(6), is(r.reduce(1, 2, 3)));
   }
 
   @Test @SuppressWarnings("boxing") public void firstNotNull() {
     FirstNotNullReducer<Integer> r = new FirstNotNullReducer<>();
-    assertEquals(null, r.reduce());
-    assertEquals(Box.box(1), r.reduce(1, null, 2, null));
-    assertEquals(Box.box(2), r.reduce(null, null, 2, 3));
+    azzert.that(null, is(r.reduce()));
+    azzert.that(Box.box(1), is(r.reduce(1, null, 2, null)));
+    azzert.that(Box.box(2), is(r.reduce(null, null, 2, 3)));
   }
 
   @Test @SuppressWarnings("boxing") public void booleanOr() {
     BooleanOrReducer r = new BooleanOrReducer();
-    assertEquals(Box.box(false), r.reduce());
-    assertEquals(Box.box(false), r.reduce(null, null));
-    assertEquals(Box.box(true), r.reduce(null, false, true, false));
+    azzert.that(Box.box(false), is(r.reduce()));
+    azzert.that(Box.box(false), is(r.reduce(null, null)));
+    azzert.that(Box.box(true), is(r.reduce(null, false, true, false)));
   }
 }
