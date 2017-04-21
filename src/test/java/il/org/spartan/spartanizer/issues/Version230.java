bd0cadc0ae949b552ae5830cd0eda2fa63ea3546
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-21 17:04:17 +0300

    work on #1271

--- src/test/java/il/org/spartan/spartanizer/issues/Version230.java
+++ src/test/java/il/org/spartan/spartanizer/issues/Version230.java
@@ -87,73 +87,7 @@ public final class Version230 {
         .gives("@SuppressWarnings(\"something\")void m(){}");
   }
 
-  @Test public void assignmentAssignmentChain1() {
-    trimminKof("c=a=13;b=13;")//
-        .gives("b=c=a=13;");
-  }
-
-  @Test public void assignmentAssignmentChain2() {
-    trimminKof("a=13;b=c=13;")//
-        .gives("b=c=a=13;");
-  }
-
-  @Test public void assignmentAssignmentChain3() {
-    trimminKof("a=b=13;c=d=13;")//
-        .gives("c=d=a=b=13;");
-  }
-
-  @Test public void assignmentAssignmentChain4() {
-    trimminKof("a1=a2=a3=a4=13;b1=b2=b3=b4=b5=13;")//
-        .gives("b1=b2=b3=b4=b5=a1=a2=a3=a4=13;");
-  }
-
-  @Test public void assignmentAssignmentChain5() {
-    trimminKof("a1=(a2=(a3=(a4=13)));b1=b2=b3=((((b4=(b5=13)))));")//
-        .gives("a1=(a2=(a3=(a4=13)));b1=b2=b3=(((b4=(b5=13))));") //
-        .gives("a1=(a2=(a3=(a4=13)));b1=b2=b3=((b4=(b5=13)));") //
-        .gives("a1=(a2=(a3=(a4=13)));b1=b2=b3=(b4=(b5=13));") //
-        .stays()//
-    ;
-  }
-
-  @Test public void assignmentAssignmentNew() {
-    trimminKof("a=new B();b=new B();")//
-        .stays();
-  }
-
-  @Test public void assignmentAssignmentNewArray() {
-    trimminKof("a=new A[3];b=new A[3];")//
-        .stays();
-  }
-
-  @Test public void assignmentAssignmentNull() {
-    trimminKof("c=a=null;b=null;")//
-        .stays();
-  }
-
-  @Test public void assignmentAssignmentSideEffect() {
-    trimminKof("a=f();b=f();")//
-        .stays();
-  }
-
-  @Test public void assignmentAssignmentVanilla() {
-    trimminKof("a=13;b=13;")//
-        .gives("b=a=13;");
-  }
-
-  @Test public void assignmentAssignmentVanilla0() {
-    trimminKof("a=0;b=0;")//
-        .gives("b=a=0;");
-  }
-
-  @Test public void assignmentAssignmentVanillaScopeIncludes() {
-    included("a=3;b=3;", Assignment.class).in(new AssignmentAndAssignmentOfSameValue());
-  }
-
-  @Test public void assignmentAssignmentVanillaScopeIncludesNull() {
-    included("a=null;b=null;", Assignment.class).notIn(new AssignmentAndAssignmentOfSameValue());
-  }
-
+  
   @Test public void assignmentReturn0() {
     trimminKof("a=3;return a;")//
         .gives("return a=3;");
