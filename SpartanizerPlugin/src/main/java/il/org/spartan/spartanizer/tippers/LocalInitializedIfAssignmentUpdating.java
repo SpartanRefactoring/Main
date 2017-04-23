10915b828fdefafeb8665233171f61f9396d9a8d
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-23 14:26:14 +0300

    sync

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/LocalInitializedIfAssignmentUpdating.java
@@ -0,0 +1,40 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static org.eclipse.jdt.core.dom.Assignment.Operator.*;
+
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.java.*;
+import il.org.spartan.utils.*;
+
+/** TODO Yossi Gil: document class
+ * @author Yossi Gil
+ * @since 2017-04-23 */
+public class LocalInitializedIfAssignmentUpdating extends LocalInitializedIfAssignmentPattern {
+  public LocalInitializedIfAssignmentUpdating() {
+    andAlso("Assignment must be updating", () -> operator != ASSIGN);
+    andAlso("Initializer has no side effects", () -> sideEffects.free(initializer));
+//    andAlso("Initializer is deterministic ", () -> sideEffects.deterministic(initializer));
+//    andAlso("Condition does not use initializer", () -> compute.usedNames(condition).contains(name + ""));
+//    andAlso("From does not use initializer", () -> compute.usedNames(from).contains(name + ""));
+  }
+
+  private static final long serialVersionUID = -4268240777527142541L;
+
+  @Override public Examples examples() {
+    return convert("int a = 2;if (b) a += 3;").to("int a = y ? 2 + 3 : 2");
+  }
+
+  @Override protected ASTRewrite go(ASTRewrite $, TextEditGroup g) {
+    remove.statement(nextIf, $, g);
+    $.replace(initializer,
+        subject.pair(//
+            subject.pair(initializer, from).to(op.assign2infix(operator)),//
+            initializer //
+        ).toCondition(condition), g);
+    return $;
+  }
+}
