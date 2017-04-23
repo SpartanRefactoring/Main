d7bf398f086d416d0587fd2dc6390f3a23ca91fa
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-23 14:40:29 +0300

    Sync

--- src/main/java/il/org/spartan/spartanizer/ast/navigate/visibility.java
+++ src/main/java/il/org/spartan/spartanizer/ast/navigate/visibility.java
@@ -8,14 +8,6 @@ import org.eclipse.jdt.core.dom.*;
 public interface visibility {
   static int of(final BodyDeclaration d) {
     final int m = d.getModifiers();
-    if (Modifier.isPublic(m))
-      return 4;
-    if (Modifier.isProtected(m))
-      return 3;
-    if (Modifier.isDefault(m))
-      return 2;
-    if (Modifier.isPrivate(m))
-      return 1;
-    return 0;
+    return Modifier.isPublic(m) ? 4 : Modifier.isProtected(m) ? 3 : Modifier.isPrivate(m) ? 1 : 2;
   }
 }
