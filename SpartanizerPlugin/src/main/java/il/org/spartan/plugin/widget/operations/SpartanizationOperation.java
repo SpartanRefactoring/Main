14a09ff5e030cfad23070ca3ebc38db330022780
tomerdragucki <tomerd@campus.technion.ac.il>
2017-04-24 20:29:24 +0300

    start learning widget programming #1228

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/widget/operations/SpartanizationOperation.java
@@ -0,0 +1,18 @@
+package il.org.spartan.plugin.widget.operations;
+
+import il.org.spartan.plugin.widget.*;
+
+/** spartanizes code
+ * @author tomerdragucki
+ * @since 2017-04-24 */
+public class SpartanizationOperation extends WidgetOperation {
+  private static final long serialVersionUID = -4656510109908909403L;
+
+  @Override public String imageURL() {
+    return null;
+  }
+
+  @Override public String description() {
+    return "Spartanize current selection";
+  }
+}
