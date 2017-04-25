181e0a66c0ebb18f25b089b46980b8c2c2536636
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-25 21:18:24 +0300

    zilch

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue0116.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/issues/Issue0116.java
@@ -10,7 +10,7 @@ import il.org.spartan.spartanizer.tipping.*;
 /** Unit tests for {@link InfixConcatenationEmptyStringLeft}
  * @author Niv Shalmon
  * @since 2016 */
-@SuppressWarnings({ "static-method", "javadoc" })
+@SuppressWarnings({ "javadoc" })
 public final class Issue0116 extends TipperTest<InfixExpression> {
   @Override public Tipper<InfixExpression> tipper() {
     return new InfixConcatenationEmptyStringLeft();
@@ -41,6 +41,7 @@ public final class Issue0116 extends TipperTest<InfixExpression> {
 
   @Test public void issue116_04() {
     trimmingOf("String s = \"\" + x.foo();")//
+        .gives("String s=x.foo()+\"\";") //
         .gives("x.foo();")//
         .stays();
   }
