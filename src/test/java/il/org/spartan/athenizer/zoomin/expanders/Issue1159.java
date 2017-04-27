1cae549934969b9ce5a15d1405acd5f5792f790e
Yuval Simon <siyuval@campus.technion.ac.il>
2017-04-27 21:56:31 +0300

    #1159

--- src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1159.java
+++ src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1159.java
@@ -10,7 +10,6 @@ import il.org.spartan.athenizer.bloaters.*;
 /** test case for {@link SwitchAddDefault}
  * @author Yuval Simon <tt>siyuval@campus.technion.ac.il</tt>
  * @since 2017-03-31 */
-//@Ignore("Please fix --yg")
 @SuppressWarnings("static-method")
 public class Issue1159 {
   @Test public void t1() {
