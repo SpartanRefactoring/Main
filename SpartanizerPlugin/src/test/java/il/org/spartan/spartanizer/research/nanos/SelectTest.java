fb85336315f8dda0ef2be21dfd4c92ea3ca3f981
Ori Roth <ori.rothh@gmail.com>
2017-04-27 15:29:45 +0300

    cleanup

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/SelectTest.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/research/nanos/SelectTest.java
@@ -5,7 +5,6 @@ import static il.org.spartan.spartanizer.testing.TestsUtilsSpartanizer.*;
 import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
 
-import il.org.spartan.spartanizer.research.analyses.*;
 import il.org.spartan.spartanizer.research.nanos.deprecated.*;
 
 /** TODO Ori Marcovitch please add a description
