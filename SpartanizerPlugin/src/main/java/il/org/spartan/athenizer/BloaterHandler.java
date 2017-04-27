00d9b91320fbc20454d3fe3581d9e22197e2899e
Yuval Simon <siyuval@campus.technion.ac.il>
2017-04-27 19:11:18 +0300

    starting #1287

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/BloaterHandler.java
@@ -0,0 +1,13 @@
+package il.org.spartan.athenizer;
+
+import org.eclipse.core.commands.*;
+
+/** TODO Yuval Simon: document class 
+ * 
+ * @author Yuval Simon
+ * @since 2017-04-27 */
+public class BloaterHandler extends AbstractHandler {
+  @Override public Object execute(@SuppressWarnings("unused") ExecutionEvent __) {
+    return null;
+  }
+}
