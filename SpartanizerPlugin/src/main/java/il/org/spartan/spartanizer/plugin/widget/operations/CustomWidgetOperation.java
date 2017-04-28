c525e3b6bada7a230a8b8e78e87bbcebd19ffd56
Yuval Simon <siyuval@campus.technion.ac.il>
2017-04-28 21:25:15 +0300

    closes #1227

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/plugin/widget/operations/CustomWidgetOperation.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/plugin/widget/operations/CustomWidgetOperation.java
@@ -8,9 +8,10 @@ import il.org.spartan.spartanizer.plugin.widget.*;
  * @since 2017-04-28 */
 public class CustomWidgetOperation extends WidgetOperation {
   private static final long serialVersionUID = -8598821174972679263L;
-
+  private static final List<String> commands = new ArrayList<>();
+  
   @Override public String imageURL() {
-    return null;
+    return "platform:/plugin/org.eclipse.wb.doc.user/html/userinterface/images/wizard.gif";
   }
 
   @Override public String description() {
@@ -22,9 +23,19 @@ public class CustomWidgetOperation extends WidgetOperation {
     getCommands().forEach(λ -> CmdOperation.go(λ));
   }
   
-  // stab
-  @SuppressWarnings("unused")
+  /** configuration should be Map<?,String>. 
+   * configuration.values() should be Collection<String> that includes
+   * the commands the user configured. The keys associated with the values does not matter. 
+   */
+  @Override public boolean register(final Map<?, ?> configuration) {
+    if(!configuration.values().stream().allMatch(λ -> λ instanceof String))
+      return false;
+    commands.clear();
+    configuration.values().forEach(λ -> commands.add((String) λ));
+    return true;
+  }
+  
   private static List<String> getCommands() {
-    return new ArrayList<String>();
+    return commands;
   }
 }
