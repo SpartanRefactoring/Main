e5f7c055b73a7d576ad74d94ade5cdc7661790f3
tomerdragucki <tomerd@campus.technion.ac.il>
2017-04-29 15:49:52 +0300

    spartanize

--- SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/traversal/Issue1285.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/spartanizer/traversal/Issue1285.java
@@ -9,10 +9,10 @@ import org.junit.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /** Test that all tippers in {@link Configurations} doesn't have a
- * serialVersionUID equals to 1L (default) and that they're all different-
- * Issue #1285
+ * serialVersionUID equals to 1L (default) and that they're all different- Issue
+ * #1285
  * @author tomerdragucki
- * @since 2017-04-29 [[SuppressWarningsSpartan]] */
+ * @since 2017-04-29 */
 @SuppressWarnings("static-method")
 public class Issue1285 {
   @Test public void serialVersionUIDNotDefault() {
@@ -20,13 +20,12 @@ public class Issue1285 {
       Assert.assertNotEquals(1L, ObjectStreamClass.lookup(¢.getClass()).getSerialVersionUID());
   }
 
-  @SuppressWarnings("boxing") @Test public void allSerialUIDsAreDifferent() {
+  @Test @SuppressWarnings("boxing") public void allSerialUIDsAreDifferent() {
     HashSet<Long> serialUIDs = new HashSet<>();
     Set<Class<?>> distinctTippersClasses = new HashSet<>();
     for (Tipper<? extends ASTNode> ¢ : Configurations.all().getAllTippers())
       distinctTippersClasses.add(¢.getClass());
-    for (Class<?> ¢ : distinctTippersClasses) {
+    for (Class<?> ¢ : distinctTippersClasses)
       assert serialUIDs.add(ObjectStreamClass.lookup(¢).getSerialVersionUID());
-    }
   }
 }
