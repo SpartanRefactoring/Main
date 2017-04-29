a74da55de8c381a60e35a8720e594f9946d24bae
RoeyMaor <maor.roey@gmail.com>
2017-04-29 18:43:28 +0300

    eclipse + renamings

---
+++ resources/il/org/spartan/Leonidas/plugin/tippers/leonidas/RemoveCurlyBracesFromIfStatement.java
@@ -0,0 +1,45 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import com.intellij.psi.PsiIfStatement;
+import il.org.spartan.Leonidas.plugin.leonidas.Leonidas;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.GenericPsiElementStub.booleanExpression;
+import static il.org.spartan.Leonidas.plugin.leonidas.GenericPsiElementStub.statement;
+import static il.org.spartan.Leonidas.plugin.leonidas.The.the;
+
+/**
+ * Remove redundant curly braces
+ * author Oren Afek, Shron Kuninin, Michal Cohen
+ * since 06/01/17
+ */
+public class RemoveCurlyBracesFromIfStatement implements LeonidasTipperDefinition {
+
+    @Override
+    public void constraints() {
+        the(booleanExpression(0)).isNot(()-> !booleanExpression(2));
+        the(statement(1)).is(()->{
+            if(booleanExpression(3)){
+                statement(5);
+            }
+        });
+    }
+
+    @Override
+    @Leonidas(PsiIfStatement.class)
+    public Template matcher() {
+        return new Template(() -> {
+            if (booleanExpression(0))
+				statement(1);
+        });
+    }
+
+    @Override
+    public Template replacer() {
+        return new Template(() -> {
+            if (booleanExpression(0))
+                statement(1);
+        });
+    }
+
+
+}
