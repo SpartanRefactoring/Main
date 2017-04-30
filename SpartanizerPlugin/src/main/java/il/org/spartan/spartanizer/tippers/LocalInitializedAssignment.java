c4e530df4ac43a00898317f0b4f41aaeee5a9911
dormaayn <dor.d.ma@gmail.com>
2017-04-30 12:42:35 +0300

    Progress #1162

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/LocalInitializedAssignment.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tippers/LocalInitializedAssignment.java
@@ -21,7 +21,7 @@ import il.org.spartan.utils.*;
  * @since 2015-08-07 */
 public final class LocalInitializedAssignment extends $FragmentAndStatement//
     implements TipperCategory.Inlining {
-  private static final long serialVersionUID = -2866629311596528031L;
+  private static final long serialVersionUID = -0x27C8502BBC6F019FL;
 
   @Override public String description(final VariableDeclarationFragment ¢) {
     return "Consolidate declaration of " + Trivia.gist(¢.getName()) + " with its subsequent initialization";
