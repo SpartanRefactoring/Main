3525fe4c8b972eee4eb73673b92a14152c75b26c
Ori Roth <ori.rothh@gmail.com>
2017-05-01 00:56:08 +0300

    reverting

--- Y/src/test/java/il/org/spartan/spartanizer/annotation/NestedENV.java
+++
@@ -1,8 +0,0 @@
-package il.org.spartan.spartanizer.annotation;
-
-/** @TODO real documentation here.
- * @author Alex Kopakzon
- * @since 2016 */
-public @interface NestedENV {
-  String[] value();
-}
