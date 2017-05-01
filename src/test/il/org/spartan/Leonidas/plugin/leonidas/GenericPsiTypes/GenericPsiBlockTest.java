cd1878ad0a3c43bd3758b084c7707f5d6ced209a
Anna Belozovsky <anechka2005@gmail.com>
2017-05-01 12:33:22 +0300

    Added tests for GenericPsiBlock #183

--- src/test/il/org/spartan/Leonidas/plugin/leonidas/GenericPsiTypes/GenericPsiBlockTest.java
+++ src/test/il/org/spartan/Leonidas/plugin/leonidas/GenericPsiTypes/GenericPsiBlockTest.java
@@ -1,21 +1,50 @@
 package il.org.spartan.Leonidas.plugin.leonidas.GenericPsiTypes;
 
+import com.intellij.psi.PsiElement;
 import il.org.spartan.Leonidas.PsiTypeHelper;
+import il.org.spartan.Leonidas.plugin.leonidas.KeyDescriptionParameters;
 
 /**
- * Created by  on 4/29/2017.
+ * @author Anna Belozovsky
+ * @since 01/05/2017
  */
 public class GenericPsiBlockTest extends PsiTypeHelper {
-    public void testGeneralizes() throws Exception {
+    PsiElement psiElement;
+    GenericPsiBlock genericPsiBlock;
 
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        psiElement = createTestStatementFromString("int x;");
+        genericPsiBlock = new GenericPsiBlock(psiElement);
+    }
+
+    public void testGeneralizes() throws Exception {
+        PsiElement e = createTestBlockStatementFromString("{int x=0;}");
+        assert genericPsiBlock.generalizes(e);
+        e = createTestCodeBlockFromString("{int x=0;}");
+        assert genericPsiBlock.generalizes(e);
+        e = createTestStatementFromString("int x=0;");
+        assert genericPsiBlock.generalizes(e);
+        e = createTestExpression("x++");
+        assert !genericPsiBlock.generalizes(e);
     }
 
     public void testToString() throws Exception {
-
+        genericPsiBlock.putUserData(KeyDescriptionParameters.ID, 100);
+        assert genericPsiBlock.toString().equals("GenericBlock100");
     }
 
     public void testCopy() throws Exception {
-
+        GenericPsiBlock copyGenericPsiBlock = genericPsiBlock.copy();
+        assert copyGenericPsiBlock.getInner() != psiElement;
+        assert copyGenericPsiBlock.getInner().toString().equals(psiElement.toString());
+        assert copyGenericPsiBlock != genericPsiBlock;
+        assert copyGenericPsiBlock.toString().equals(genericPsiBlock.toString());
     }
 
+    public void testGetProject() {
+        assert genericPsiBlock.getProject().equals(psiElement.getProject());
+    }
 }
