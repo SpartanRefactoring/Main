e42ce0bfbc8af3273f9122a261695637f7c85a03
Michal Cohen <cohenmelany1@gmail.com>
2017-05-01 07:04:22 +0300

    ifEmptyThen1
    Signed-off-by: melanyc <cohenmelany1@gmail.com>

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/IfEmptyThen.java
@@ -0,0 +1,46 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import com.intellij.psi.PsiIfStatement;
+import il.org.spartan.Leonidas.plugin.leonidas.Leonidas;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.GenericPsiElementStub.booleanExpression;
+import static il.org.spartan.Leonidas.plugin.leonidas.GenericPsiElementStub.statement;
+
+/**
+ * Replace if(b); else{s;} with if(!b){s;}
+ * IfEmptyThen
+ *
+ * @author melanyc
+ * @since 30-04-2017
+ */
+public class IfEmptyThen implements LeonidasTipperDefinition {
+
+    /**
+     * Write here additional constraints on the matcher tree.
+     * The constraint are of the form:
+     * the(<generic element>(<id>)).{is/isNot}(() - > <template>)[.ofType(Psi class)];
+     */
+    @Override
+    public void constraints() {
+    }
+
+    @Override
+    @Leonidas(PsiIfStatement.class)
+    public void matcher() {
+        new Template(() -> {
+            if (booleanExpression(0))
+                ;
+            else
+                statement(1);
+        });
+    }
+
+    @Override
+    @Leonidas(PsiIfStatement.class)
+    public void replacer() {
+        new Template(() -> {
+            if (!(booleanExpression(0)))
+                statement(1);
+        });
+    }
+}
