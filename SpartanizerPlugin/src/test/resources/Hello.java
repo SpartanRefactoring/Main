246c31a394c29966efc826b6b1de4f13a2699c3a
Ori Roth <ori.rothh@gmail.com>
2017-05-02 13:58:51 +0300

    hello commented out

--- SpartanizerPlugin/src/test/resources/Hello.java
+++ SpartanizerPlugin/src/test/resources/Hello.java
@@ -5,23 +5,23 @@
  * @since Oct 23, 2016
  */
 public final class Hello {
-    public static void main(final String[] args) {
-        System.out.println("Hello, World\n");
-    }
-
-    /** [[SuppressWarningsSpartan]] */
-    public static foo(int i1, int i2) {
-        boolean cmp;
-        cmp = false;
-        if (!(int1 < int2 + 1)) {
-            cmp = false;
-        } else {
-            cmp = true;
-        }
-        return cmp;
-    }
-
-    public static foo(int int1, int int2) {
-        return int1 <= int2;
-    }
+//    public static void main(final String[] args) {
+//        System.out.println("Hello, World\n");
+//    }
+//
+//    /** [[SuppressWarningsSpartan]] */
+//    public static foo(int i1, int i2) {
+//        boolean cmp;
+//        cmp = false;
+//        if (!(int1 < int2 + 1)) {
+//            cmp = false;
+//        } else {
+//            cmp = true;
+//        }
+//        return cmp;
+//    }
+//
+//    public static foo(int int1, int int2) {
+//        return int1 <= int2;
+//    }
 }
