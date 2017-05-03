c702000aeb9ec90f4f6336b022a780da747f0ce6
amirsagiv83 <amirsagiv15@gmail.com>
2017-05-03 19:14:05 +0300

    Added tests for GenericStatement. #185

--- src/test/il/org/spartan/Leonidas/plugin/leonidas/GenericPsiTypes/GenericPsiStatementTest.java
+++ src/test/il/org/spartan/Leonidas/plugin/leonidas/GenericPsiTypes/GenericPsiStatementTest.java
@@ -1,21 +1,34 @@
 package il.org.spartan.Leonidas.plugin.leonidas.GenericPsiTypes;
 
-import junit.framework.TestCase;
+import com.intellij.psi.PsiElement;
+import il.org.spartan.Leonidas.PsiTypeHelper;
+import il.org.spartan.Leonidas.plugin.leonidas.Encapsulator;
+import il.org.spartan.Leonidas.plugin.leonidas.GenericStatement;
+
 
 /**
- * Created by melanyc on 4/29/2017.
+ * @author Amir Sagiv
+ * @since 03/05/2017
  */
-public class GenericPsiStatementTest extends TestCase {
-    public void testToString() throws Exception {
+public class GenericPsiStatementTest extends PsiTypeHelper {
 
+    PsiElement psiElement;
+    GenericStatement genericStatement;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        psiElement = createTestStatementFromString("int x;");
+        genericStatement = new GenericStatement(psiElement);
     }
+
 
     public void testGeneralizes() throws Exception {
-
+        assert genericStatement.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x + 1;")));
+        assert !genericStatement.generalizes(Encapsulator.buildTreeFromPsi(createTestExpressionFromString("x+1")));
+        assert !genericStatement.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("{int x; int y;}")));
+        assert genericStatement.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("for(;;){}")));
     }
 
-    public void testCopy() throws Exception {
-
-    }
 
 }
