126b56aac1f3ce95a2c6ed6bd884a48249436c19
Niv Shalmon <shalmon.niv@gmail.com>
2017-05-03 10:35:31 +0300

    setup widget context #1225 #1292

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/plugin/widget/WidgetContext.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/plugin/widget/WidgetContext.java
@@ -8,6 +8,7 @@ import il.org.spartan.spartanizer.plugin.*;
 /** Widget button activation context. Useful context information to be used by
  * {@link WidgetOperation}. Also used to pass data between operations' phases.
  * @author Ori Roth <tt>ori.rothh@gmail.com</tt>
+ * @author Niv Shalmon
  * @since 2017-04-06 */
 public class WidgetContext {
   public IProject project;
@@ -15,4 +16,18 @@ public class WidgetContext {
   public Selection currentSelecetion;
   public Selection currentCompilationUnit;
   public Selection allCompilationUnits;
+  
+  /**
+   * @return a WidgetContext representing the current context of the workspace
+   */
+  public static WidgetContext generateContext(){
+    WidgetContext $ = new WidgetContext();
+    $.project = Selection.Util.project();
+    $.javaProject = Selection.Util.getJavaProject();
+    $.currentSelecetion = Selection.Util.current();
+    $.currentCompilationUnit = Selection.Util.getCurrentCompilationUnit();
+    $.allCompilationUnits = Selection.Util.getAllCompilationUnits();
+    return $;
+  }
+  
 }
