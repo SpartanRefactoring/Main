6dea6ab4ae7d645db1ed9a541311029dfec3360f
RoeiRaz <m.roei.raz@gmail.com>
2017-05-04 19:23:26 +0300

    copied gradle-project structure from https://github.com/diffplug/goomph-eclipse-plugin

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/tipping/Structural.java
+++
@@ -1,7 +0,0 @@
-package il.org.spartan.spartanizer.tipping;
-
-/** Auxiliary __: non public intentionally */
-@FunctionalInterface
-interface Structural extends TipperCategory {
-  /* Empty intentionally */
-}
