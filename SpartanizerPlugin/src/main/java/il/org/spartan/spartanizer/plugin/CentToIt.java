9138b39e6e77de6df13666882833840d9c4a0906
dormaayn <dor.d.ma@gmail.com>
2017-05-05 11:19:07 +0300

    Centification Shortcut: almost done #1039

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/plugin/CentToIt.java
@@ -0,0 +1,17 @@
+package il.org.spartan.spartanizer.plugin;
+
+import org.eclipse.core.commands.*;
+
+import il.org.spartan.plugin.preferences.revision.*;
+
+/** Handler for the shortcut of changing the spartanizar prefrence from cent to
+ * it.
+ * @author dormaayn
+ * @since 2017-05-05 */
+public class CentToIt extends AbstractHandler {
+  @Override public Object execute(ExecutionEvent ¢) {
+    System.out.println("Strted Press");
+    PreferencesPage.changeCentToIt();
+    return (new TopMenuHandlers()).execute(¢);
+  }
+}
