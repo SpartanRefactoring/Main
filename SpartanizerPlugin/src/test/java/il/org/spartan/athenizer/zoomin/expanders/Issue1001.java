7472a56f2f1829e4f7c70d66331c49439fba0be7
Yuval Simon <siyuval@campus.technion.ac.il>
2017-05-06 19:43:30 +0300

    remove duplicate class + re-commit issue1001

--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1001.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1001.java
@@ -11,7 +11,6 @@ import il.org.spartan.spartanizer.meta.*;
  * @author Ori Roth {@code ori.rothh@gmail.com}
  * @since 2016-12-25 [[SuppressWarningsSpartan]] */
 @SuppressWarnings("static-method")
-@Ignore("TODO Assignee of #1001")
 public class Issue1001 {
   @Test public void basic() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
@@ -21,6 +20,7 @@ public class Issue1001 {
         + "  a = a + 1;\n" //
         + "}", "f1");
   }
+
   @Test public void inclusion() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f2() {\n" //
@@ -31,6 +31,7 @@ public class Issue1001 {
         + "  x(a = a + (b += 1));\n" //
         + "}", "f2");
   }
+
   @Test public void inclusion2() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f22() {\n" //
@@ -41,6 +42,7 @@ public class Issue1001 {
         + "  x(a = a + (b = b + 1));\n" //
         + "}", "f22");
   }
+
   @Test public void inclusion3() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f3() {\n" //
@@ -51,9 +53,11 @@ public class Issue1001 {
         + "  x(a = a + (b = 1));\n" //
         + "}", "f3");
   }
+
   @Test public void nonMatchingPrimitives() {
     bloatingOf(Issue1001Aux.instance()).staysWithBinding();
   }
+
   @Test public void operators() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f4() {\n" //
@@ -64,6 +68,7 @@ public class Issue1001 {
         + "  x(a = a % (b |= 1));\n" //
         + "}", "f4");
   }
+  
   @Test public void operators2() {
     bloatingOf(Issue1001Aux.instance()).givesWithBinding("" //
         + "void f44() {\n" //
@@ -74,16 +79,20 @@ public class Issue1001 {
         + "  x(a = a % (b = b | 1));\n" //
         + "}", "f44");
   }
-
+  
   /** [[SuppressWarningsSpartan]] */
   @SuppressWarnings({ "unused", "TooBroadScope" })
   public static class Issue1001Aux extends MetaFixture {
     public static Issue1001Aux instance() {
       return new Issue1001Aux();
     }
+
     void f1() {
-      /**/
+      int a;
+      a = 0;
+      a += 1;
     }
+
     void f2() {
       int a;
       int b;
@@ -91,6 +100,7 @@ public class Issue1001 {
       b = 0;
       x(a += b += 1);
     }
+
     void f22() {
       int a;
       int b;
@@ -98,11 +108,15 @@ public class Issue1001 {
       b = 0;
       x(a = a + (b += 1));
     }
+
     void f3() {
       int a;
+      int b;
       a = 0;
-      x(a += 1);
+      b = 0;
+      x(a += (b = 1));
     }
+
     void f4() {
       int a;
       int b;
@@ -110,6 +124,7 @@ public class Issue1001 {
       b = 0;
       x(a %= b |= 1);
     }
+
     void f44() {
       int a;
       int b;
@@ -117,6 +132,7 @@ public class Issue1001 {
       b = 0;
       x(a = a % (b |= 1));
     }
+
     void x(final int y) {
       //
     }
