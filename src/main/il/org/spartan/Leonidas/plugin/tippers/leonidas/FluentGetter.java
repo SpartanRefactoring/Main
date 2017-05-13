ef398c23b75d4d7cfc477af9edcd694851b33556
Sharon <sharonkl@campus.technion.ac.il>
2017-05-13 20:55:53 +0300

    Start implementing the FluentGetter tipper
    This tipper will refactor all getter methods to be more fluent,
    meaning they will return "this" to allow method call concatenation.

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/FluentGetter.java
@@ -0,0 +1,49 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import com.intellij.psi.PsiMethod;
+import il.org.spartan.Leonidas.plugin.leonidas.Leonidas;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.anyBlock;
+
+/**
+ * @author Sharon
+ * @since 13.5.17
+ */
+public class FluentGetter implements LeonidasTipperDefinition {
+    @Override
+    public void constraints() {
+
+    }
+
+    @Override
+    @Leonidas(PsiMethod.class)
+    public void matcher() {
+        new MatcherMethod() {
+            @Override
+            public void method0() {
+                anyBlock(1);
+            }
+        };
+    }
+
+    @Override
+    @Leonidas(PsiMethod.class)
+    public void replacer() {
+        new ReplacerMethod() {
+            @Override
+            public Object method0() {
+                anyBlock(1);
+
+                return this;
+            }
+        };
+    }
+
+    private abstract class MatcherMethod {
+        public abstract void method0();
+    }
+
+    private abstract class ReplacerMethod {
+        public abstract Object method0();
+    }
+}
