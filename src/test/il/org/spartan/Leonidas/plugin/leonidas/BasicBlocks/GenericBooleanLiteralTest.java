317d7091a3312b0ad857d92795c0b5b39517da8d
Sharon <sharonkl@campus.technion.ac.il>
2017-05-14 09:45:42 +0300

    Add negative tests for BooleanLiteral building block

--- src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/GenericBooleanLiteralTest.java
+++ src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/GenericBooleanLiteralTest.java
@@ -19,4 +19,20 @@ public class GenericBooleanLiteralTest extends PsiTypeHelper {
         assert booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("true")));
         assert booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("false")));
     }
+
+    public void testDoesNotGeneralizeStatements() {
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x++;")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x = y + z;")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("m();")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("return 's';")));
+    }
+
+    public void testDoesNotGeneralizeOtherExpressions() {
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x + y")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x++")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("'a'")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("2")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("null")));
+        assert !booleanLiteral.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("a()")));
+    }
 }
