0ab474264970005399ea8c444b37efc0701e6bd5
Yuval Simon <siyuval@campus.technion.ac.il>
2017-05-15 00:29:50 +0300

    closes #1337

--- SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/bloaters/$MatchBloatersAndTippers.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/bloaters/$MatchBloatersAndTippers.java
@@ -3,6 +3,7 @@ package il.org.spartan.athenizer.bloaters;
 import java.util.*;
 import java.util.stream.*;
 
+import il.org.spartan.athenizer.*;
 import il.org.spartan.spartanizer.tippers.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.spartanizer.traversal.*;
@@ -14,6 +15,7 @@ public class $MatchBloatersAndTippers {
   @SuppressWarnings("rawtypes")
   static final Map<Class<? extends Tipper>, Class<? extends Tipper>> matching = new HashMap<>();
   
+  /** Complete match */
   static {
     matching.put(AssignmentAndAssignmentBloater.class, AssignmentAndAssignmentOfSameValue.class);
     matching.put(AssignmentOperatorBloater.class, AssignmentToFromInfixIncludingTo.class);
@@ -31,43 +33,49 @@ public class $MatchBloatersAndTippers {
     matching.put(MethodInvocationTernaryBloater.class, IfExpressionStatementElseSimilarExpressionStatement.class);
     matching.put(ExtractExpressionFromReturn.class, LocalInitializedReturnExpression.class);
     matching.put(DeclarationWithInitializerBloater.class, LocalInitializedUpdateAssignment.class);
+    matching.put(CasesSplit.class, MergeSwitchBranches.class);
+    matching.put(ToStringExpander.class, MethodInvocationToStringToEmptyStringAddition.class);
   }
   
-  // close match
+  /** Close match */
   static {
     matching.put(ParenthesesBloater.class, ParenthesizedRemoveExtraParenthesis.class);
     matching.put(AssignmentTernaryBloater.class, IfAssignToFooElseAssignToFoo.class);
     matching.put(ClassInstanceIntoVariable.class, LocalInitializedUnusedRemove.class);
     matching.put(TernaryPushup.class, TernaryPushdown.class);
     matching.put(OutlineTernaryMethodInvocation.class, TernaryPushdown.class); 
+    matching.put(StatementExtractParameters.class, LocalInitializedStatementTerminatingScope.class);
   }
   
-  // expanders that should be checked
+  /** Expanders that should be checked */
   static {
     matching.put(PostFixToInfixExpander.class, PlusAssignToPostfix.class);
     matching.put(PrefixToInfix.class, PlusAssignToPostfix.class);
   }
   
-  // non match
+  /** Non match */
   static {
     matching.put(BooleanExpressionBloater.class, null);
-    matching.put(CasesSplit.class, null);
     matching.put(IfElseToSwitch.class, null);
     matching.put(LongIfBloater.class, null);
     matching.put(MethodDeclarationNameExpander.class, null);
-    matching.put(StatementExtractParameters.class, null);
-    matching.put(SwitchMissingDefaultAdd.class, null);   
-    matching.put(ToStringExpander.class, null);
+    matching.put(SwitchMissingDefaultAdd.class, null);
     matching.put(VariableDeclarationStatementExpand.class, null);
   }
   
-  /**
-   * Prints non matched tippers (only of tippers in Configurations(toolbox))
-   */
+  /** Prints non matched tippers (only of tippers in Configurations(toolbox)) */
   public static void printNonMatchedTippers() {
     List< Class<?> > $ = new ArrayList<>();
     $.addAll(Configurations.all.get().getAllTippers().stream().map(Object::getClass).collect(Collectors.toList()));
     $.removeAll(matching.values());
+    $.forEach(x->System.out.println(x.getName()));
+  }
+  
+  /** Prints bloaters that are not in "matching" map (only of bloaters in InflaterProvider) */
+  public static void printBloatersNotInMatching() {
+    List< Class<?> > $ = new ArrayList<>();
+    $.addAll(InflaterProvider.freshCopyOfAllExpanders().getAllTippers().stream().map(Object::getClass).collect(Collectors.toList()));
+    $.removeAll(matching.keySet());
     $.forEach(x->System.out.println(x.getName()));
   }
 }
