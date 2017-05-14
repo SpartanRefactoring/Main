f01f9ed10ee44419acd4bc0086ce3274f7af7189
tomerdragucki <tomerd@campus.technion.ac.il>
2017-05-14 22:56:40 +0300

    close #980

--- SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/bloaters/BooleanExpressionBloater.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/athenizer/bloaters/BooleanExpressionBloater.java
@@ -1,6 +1,6 @@
 package il.org.spartan.athenizer.bloaters;
 
-import static il.org.spartan.spartanizer.ast.navigate.step.*;
+import static org.eclipse.jdt.core.dom.ASTNode.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.InfixExpression.*;
@@ -9,6 +9,7 @@ import org.eclipse.text.edits.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.java.namespace.*;
 import il.org.spartan.spartanizer.tipping.*;
@@ -21,42 +22,41 @@ import il.org.spartan.spartanizer.tipping.*;
  *  a && b
  * }
  * @author Dor Ma'ayan {@code dor.d.ma@gmail.com}
+ * @author tomerdragucki
  * @since 2017-01-13 */
 public class BooleanExpressionBloater extends CarefulTipper<InfixExpression>//
     implements TipperCategory.Bloater {
   private static final long serialVersionUID = 0x6593D58F0DEFC96AL;
 
   @Override protected boolean prerequisite(final InfixExpression ¢) {
-    return ¢.getOperator() == Operator.CONDITIONAL_AND || ¢.getOperator() == Operator.AND || ¢.getOperator() == Operator.OR
-        || ¢.getOperator() == Operator.CONDITIONAL_OR;
+    return (¢.getOperator() == Operator.CONDITIONAL_AND || ¢.getOperator() == Operator.AND || ¢.getOperator() == Operator.OR
+        || ¢.getOperator() == Operator.CONDITIONAL_OR) && (isComplicated(¢.getLeftOperand()) || isComplicated(¢.getRightOperand()));
   }
-  /** [[SuppressWarningsSpartan]] */
   @Override public Tip tip(final InfixExpression ¢) {
-    SingleVariableDeclaration d1 = getSeperate(¢.getLeftOperand());
-    SingleVariableDeclaration d2 = getSeperate(¢.getRightOperand());
-    InfixExpression i = subject.pair(d1.getName(), d2.getName()).to(¢.getOperator());
-    System.out.println(d1);
-    System.out.println(d2);
-    System.out.println(i);
+    VariableDeclarationStatement $ = getSeperate(¢.getLeftOperand()), v2 = getSeperate(¢.getRightOperand());
+    InfixExpression i = subject.pair(step.fragments($).get(0).getName(), step.fragments(v2).get(0).getName()).to(¢.getOperator());
     return new Tip(description(¢), getClass(), ¢) {
       @Override public void go(final ASTRewrite r, final TextEditGroup g) {
         final ListRewrite l = r.getListRewrite(yieldAncestors.untilContainingBlock().from(¢), Block.STATEMENTS_PROPERTY);
-        l.insertAfter(d1, parent(¢), g);
-        l.insertAfter(d2, d1, g);
-        l.insertAfter(i, d2, g);
-        l.remove(¢, g);
+        l.insertBefore($, yieldAncestors.untilClass(Statement.class).from(¢), g);
+        l.insertAfter(v2, $, g);
+        r.replace(¢, i, g);
       }
     };
   }
-  private static SingleVariableDeclaration getSeperate(final Expression x) {
-    final SingleVariableDeclaration $ = x.getAST().newSingleVariableDeclaration();
-    $.setInitializer(copy.of(x));
+  private static VariableDeclarationStatement getSeperate(final Expression x) {
+    final VariableDeclarationFragment f = x.getAST().newVariableDeclarationFragment();
+    f.setInitializer(copy.of(x));
     final PrimitiveType t = x.getAST().newPrimitiveType(PrimitiveType.BOOLEAN);
+    f.setName(make.from(x).identifier(scope.newName(x, t)));
+    final VariableDeclarationStatement $ = x.getAST().newVariableDeclarationStatement(f);
     $.setType(t);
-    $.setName(make.from(x).identifier(scope.newName(x, t)));
     return $;
   }
   @Override public String description(@SuppressWarnings("unused") final InfixExpression __) {
     return "Expand boolean expression";
+  }
+  private static boolean isComplicated(final Expression ¢) {
+    return iz.nodeTypeIn(¢, METHOD_INVOCATION, INFIX_EXPRESSION, ASSIGNMENT, LAMBDA_EXPRESSION);
   }
 }
--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue0980.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue0980.java
@@ -6,38 +6,44 @@ import org.junit.*;
 
 /** Test Class for Expand boolean expressions
  * @author Dor Ma'ayan {@code dor.d.ma@gmail.com}
+ * @author tomerdragucki
  * @since 2017-01-13 */
-@Ignore
 @SuppressWarnings("static-method")
 public class Issue0980 {
+  @Test public void test00() {
+    bloatingOf("return a && b;")//
+        .stays();
+  }
   @Test public void test0() {
-    bloatingOf("{return x && y();}")//
-        .gives("boolean a = x; boolean b = y(); return a && b;");
+    bloatingOf("return x() && y();")//
+        .gives("boolean a = x(); boolean b = y(); return a && b;");
   }
   @Test public void test1() {
-    bloatingOf("return true && y;")//
+    bloatingOf("return true && y();")//
         .gives("boolean a = true; boolean b = y(); return a&&b;");
   }
   @Test public void test2() {
-    bloatingOf("boolean t =  x && y();")//
-        .gives("boolean a = x; boolean b = y(); boolean t = a&&b;");
+    bloatingOf("boolean t = x && y();")//
+        .gives("boolean t; t = x && y();")//
+        .gives("boolean t; boolean a = x; boolean b = y(); t = a&&b;");
   }
   @Test public void test3() {
     bloatingOf("return x || y();")//
         .gives("boolean a = x; boolean b = y(); return a || b;");
   }
   @Test public void test4() {
-    bloatingOf("return true || y;")//
+    bloatingOf("return true || y();")//
         .gives("boolean a = true; boolean b = y(); return a || b;");
   }
   @Test public void test5() {
     bloatingOf("boolean t =  x || y();")//
-        .gives("boolean a = x; boolean b = y(); boolean t = a || b;");
+        .gives("boolean t; t =  x || y();")//
+        .gives("boolean t; boolean a = x; boolean b = y(); t = a || b;");
   }
   @Test public void test6() {
     bloatingOf("return x && y() || z;")//
-        .gives("boolean a = x && y();return a && z;")//
-        .gives("boolean a = x;a = a && y();return a && z;")//
-        .stays();
+        .gives("boolean a = x && y(); boolean b = z; return a || b;")//
+        .gives("boolean a; a = x && y(); boolean b = z; return a || b;")
+        .gives("boolean a; boolean a2 = x;boolean b2 = y(); a = a2 && b2; boolean b = z; return a || b;");
   }
 }
