357881d75e336ab17aea2b2cff309e6ff0406ddc
dormaayn <dor.d.ma@gmail.com>
2017-05-18 09:55:08 +0300

    Dor thesis progress #1352

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/NominalTables.java
@@ -0,0 +1,13 @@
+package il.org.spartan.spartanizer.java.namespace.tables;
+
+import java.util.*;
+
+import il.org.spartan.tables.*;
+
+/** @author Dor Ma'ayan
+ * @since 2017-05-18 */
+public class NominalTables {
+  static Table table;
+  static String tableName;
+  static Map<String,Integer> namePrevelance;
+}
