c5736c8012ae19cbbcff780da8abb6fbcefef80e
dormaayn <dor.d.ma@gmail.com>
2017-05-20 20:52:12 +0300

    Test Pass #1340

--- SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1340.java
+++ SpartanizerPlugin/src/test/java/il/org/spartan/athenizer/zoomin/expanders/Issue1340.java
@@ -6,7 +6,6 @@ import static il.org.spartan.spartanizer.testing.TestUtilsBloating.*;
 /** Bug in {@link MultiplicationToCast} test case
  * @author Yuval Simon
  * @since 2017-05-12 */
-@Ignore
 @SuppressWarnings("static-method")
 public class Issue1340 {
   @Test public void t1() {
