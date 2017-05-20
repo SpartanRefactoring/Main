bde726644e9e484704ae563302a9e2238e8934a7
Yossi Gil <yogi@cs.technion.ac.il>
2017-05-20 22:48:49 +0300

    Sort members

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/ast/safety/property.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/ast/safety/property.java
@@ -7,19 +7,10 @@ import org.eclipse.jdt.core.dom.*;
  * @since 2016-12-27 */
 public enum property {
   DUMMY_ENUM_INSTANCE_INTRODUCING_SINGLETON_WITH_STATIC_METHODS;
-  @FunctionalInterface
-  public interface Attached {
-    void to(ASTNode n);
-  }
-
-  @FunctionalInterface
-  public interface Obtainer<N> {
-    N from(ASTNode n);
-  }
-
   public static Attached attach(final Object o) {
     return λ -> λ.setProperty(key(o.getClass()), o);
   }
+
   /** Get property from node.
    * @param n JD
    * @param key property name
@@ -28,15 +19,13 @@ public enum property {
   public static <T> T get(final ASTNode n, final String key) {
     return n == null ? null : (T) n.getProperty(key);
   }
+
   /** Checks node has a property.
    * @param n JD
    * @param key property name
    * @return whether node contains the key property */
   public static boolean has(final ASTNode n, final String key) {
     return n != null && n.properties().keySet().contains(key);
-  }
-  static <N> String key(final Class<N> ¢) {
-    return ¢.getCanonicalName();
   }
   @SuppressWarnings("unchecked") public static <N> Obtainer<N> obtain(final Class<N> c) {
     return λ -> (N) λ.getProperty(key(c));
@@ -63,5 +52,16 @@ public enum property {
   public static void unset(final ASTNode n, final String key) {
     if (n != null)
       n.setProperty(key, null);
+  }
+  static <N> String key(final Class<N> ¢) {
+    return ¢.getCanonicalName();
+  }
+  @FunctionalInterface
+  public interface Attached {
+    void to(ASTNode n);
+  }
+  @FunctionalInterface
+  public interface Obtainer<N> {
+    N from(ASTNode n);
   }
 }
