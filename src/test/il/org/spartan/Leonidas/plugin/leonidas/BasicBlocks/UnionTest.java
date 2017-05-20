c2d26302875d05f8a1f9ce83c54035b55400f44f
Sharon <sharonkl@campus.technion.ac.il>
2017-05-20 12:28:11 +0300

    Add more union tests
    To make sure a union doesn't accept un wanted blocks.

--- src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/UnionTest.java
+++ src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/UnionTest.java
@@ -13,12 +13,40 @@ public class UnionTest extends PsiTypeHelper {
         return (PsiMethodCallExpression) createTestExpression(s);
     }
 
-    public void testUnionOfMethodAndStatement() {
+    public void testUnionOfStatementAcceptsStatements() {
         PsiElement element = methodCallExpression("union(0, statement(1))");
         Union union = new Union(element);
 
         assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("int x;")));
         assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x++;")));
         assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("System.out.println();")));
+    }
+
+    public void testUnionOfStatementDoesNotAcceptNonStatements() {
+        PsiElement element = methodCallExpression("union(0, statement())");
+        Union union = new Union(element);
+
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x++"))));
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("1 + 2"))));
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestClassFromString("class XX {}"))));
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void a()"))));
+    }
+
+    public void testUnionOfStatementAndMethodAcceptsStatementsAndMethods() {
+        PsiElement element = methodCallExpression("union(0, statement(1), method(2))");
+        Union union = new Union(element);
+
+        assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("int x;")));
+        assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("int a() {return 0;}")));
+        assert union.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public int b(double x) {return x - 1;}")));
+    }
+
+    public void testUnionOfStatementAndMethodDoesNotAcceptNonStatementsAndMethods() {
+        PsiElement element = methodCallExpression("union(0, statement(1), method(2))");
+        Union union = new Union(element);
+
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x++"))));
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("1 + 2"))));
+        assertFalse(union.generalizes(Encapsulator.buildTreeFromPsi(createTestClassFromString("class XX {}"))));
     }
 }
