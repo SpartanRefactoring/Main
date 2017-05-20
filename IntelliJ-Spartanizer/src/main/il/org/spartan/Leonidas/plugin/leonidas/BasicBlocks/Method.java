c221a80a1069fd04b91740fbd61a195137e97381
RoeiRaz <m.roei.raz@gmail.com>
2017-05-20 16:46:30 +0300

    added Intellij-Spartanizer sources

---
+++ IntelliJ-Spartanizer/src/main/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/Method.java
@@ -0,0 +1,34 @@
+package il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks;
+
+import il.org.spartan.Leonidas.auxilary_layer.iz;
+
+/**
+ * @author Sharon
+ * @since 13.5.17
+ */
+public class Method extends GenericMethodCallBasedBlock {
+    public static final String TEMPLATE = "method";
+
+    public Method(Encapsulator e) {
+        super(e, TEMPLATE);
+    }
+
+    public Method() {
+        super(TEMPLATE);
+    }
+
+    @Override
+    public boolean generalizes(Encapsulator e) {
+        return iz.method(e.getInner());
+    }
+
+    @Override
+    protected boolean goUpwards(Encapsulator prev, Encapsulator next) {
+        return next != null && iz.method(next.getInner());
+    }
+
+    @Override
+    public GenericEncapsulator create(Encapsulator e) {
+        return new Method(e);
+    }
+}
