8eb5d83f15c03468fa5582d8b31dc35d3459a96c
Yossi Gil <yogi@cs.technion.ac.il>
2017-05-20 23:38:11 +0300

    Strategies idea

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nominal/Namer.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/research/nominal/Namer.java
@@ -1,15 +1,60 @@
 package il.org.spartan.spartanizer.research.nominal;
 
-/** Explore ways to give meaningful names to identifiers 
- * according using different strategies.
- * 
+import java.util.function.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.spartanizer.ast.safety.*;
+
+/** Explore ways to give meaningful names to identifiers according using
+ * different strategies.
  * @author Dor Ma'ayan
  * @since 2017-05-20 */
 public class Namer {
-  public enum Strategies{
-    
+  /** The semantics of function {@link #apply(Type)} here is this:
+   * <ol>
+   * <li>If the argument is {@code null} then the result is {@code null}</li>
+   * <li>If the argument is not {@code null} then the result is {@code null} if
+   * the argument <em>can not<em/> be simplified by this strategy</li>
+   * <li>A non-{@code null} is returned, then the this instance can offer a
+   * simplification of this the result <em>can not<em/> be simplified by this
+   * strategy</li>
+   * </ol>
+   * @author Yossi Gil
+   * @since 2017-05-20 */
+  public interface Simplifier extends Function<Type, Simplification> {
+    @Override default Simplification apply(@SuppressWarnings("unused") Type __) {
+      return null;
+    }
   }
-  //functionArgument(MethodDeclaration d,int argNum){
-    
-  //}
+
+  /** This is the actual simplification.
+   * @author Yossi Gil
+   * @since 2017-05-20 */
+  public interface Simplification extends UnaryOperator<String>, Supplier<Type> {
+    /** This is how to change the result of further simplifications */
+    @Override default String apply(String ¢) {
+      return ¢;
+    }
+    /** This is the result of this simplification, guaranteed to be
+     * non-{@code null} and having fewer nodes than the input. */
+    @Override default Type get() {
+      return null;
+    }
+  }
+
+  static final String PLURALS = "s";
+  public static Simplifier array = new Simplifier() {
+    @Override public Simplification apply(Type t) {
+      return new Simplification() {
+        @Override public String apply(String ¢) {
+          return ¢ + PLURALS;
+        }
+        @Override public Type get() {
+          // TODO: Dor Maayan - this could be int[][][]; add as many s as needed.
+          return !t.isArrayType() ? null : az.arrayType(t).getElementType();
+        }
+      };
+    }
+  };
 }
