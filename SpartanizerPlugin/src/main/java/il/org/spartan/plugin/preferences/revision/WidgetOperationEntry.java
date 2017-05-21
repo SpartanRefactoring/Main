8bf4ac7f8a1e589ecf6429125a9af0501252c0a5
Niv Shalmon <shalmon.niv@gmail.com>
2017-05-21 20:08:59 +0300

    Created WidgetOperationEntry with @ravivos #1321

---
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/WidgetOperationEntry.java
@@ -0,0 +1,45 @@
+package il.org.spartan.plugin.preferences.revision;
+
+import java.io.*;
+import java.util.*;
+
+/** A widget operation with it's configuration 
+ * 
+ * @author Niv Shalmon
+ * @author Raviv Rachmiel
+ * @since 2017-05-21 */
+public class WidgetOperationEntry implements Serializable {
+  
+  private static final long serialVersionUID = -0x295E9D34CC34A530L;
+  public final long widgetSUID;
+  public final Map<?,?> configuration;
+  private String name;
+  private boolean isEnabled;
+  
+  public WidgetOperationEntry(long widgetSUID, Map<?, ?> configuration, String name) {
+    this.widgetSUID = widgetSUID;
+    this.configuration = configuration;
+    this.name = name;
+  }
+  
+  public void enable(){
+    isEnabled = true;
+  }
+  
+  public void disable(){
+    isEnabled = false;
+  }
+  
+  public boolean isEnabled(){
+    return isEnabled;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+}
