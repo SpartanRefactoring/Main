f7304b228d371e33b4ab9fa839c156144ba8658b
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-05-21 04:31:07 +0300

    nicer GUI #1349

--- SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/OperationListEditor.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/OperationListEditor.java
@@ -22,10 +22,30 @@ import fluent.ly.*;
  * @since 2017-05-10 */
 public class OperationListEditor extends ListEditor {
   static final String DELIMETER = ",";
-  final List<Map.Entry<String, Object>> elements_list;
+  List<Map.Entry<String, Object>> elements_list;
   Button configureButton;
   Button ableButton;
-
+  public OperationListEditor(final String name, final String labelText, final Composite parent) {
+    super(name, labelText, parent);
+    final Composite buttonBox = new Composite(parent, SWT.NULL);
+    final GridLayout layout = new GridLayout();
+    layout.marginWidth = 0;
+    buttonBox.setLayout(layout);
+    buttonBox.addDisposeListener(λ -> {
+      configureButton = null;
+      ableButton = null;
+    });
+    buttonBox.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+    ableButton = new Button(buttonBox, SWT.PUSH);
+    ableButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+    ableButton.setText("Enable/Disable operations");
+    ableButton.setEnabled(false);
+    ableButton.setVisible(true);
+    configureButton = new Button(buttonBox, SWT.PUSH);
+    configureButton.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+    configureButton.setText("Configure operation");
+    configureButton.setEnabled(false);
+  }
   OperationListEditor(final String name, final String labelText, final Composite parent, final List<Map.Entry<String, Object>> elements,
       final Consumer<Object> onConfigure, final Function<Object, Boolean> isAble, final Consumer<Object> onAble) {
     super(name, labelText, parent);
@@ -105,6 +125,71 @@ public class OperationListEditor extends ListEditor {
         //
       }
     });
+  }
+  public OperationListEditor lazyConstruct(final Composite parent, final List<Map.Entry<String, Object>> elements,
+      final Consumer<Object> onConfigure, final Function<Object, Boolean> isAble, final Consumer<Object> onAble) {
+    elements_list = as.list(elements);
+    
+    
+    ableButton.addSelectionListener(new SelectionListener() {
+      @Override public void widgetSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        onSelection();
+      }
+      @Override public void widgetDefaultSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        onSelection();
+      }
+      @SuppressWarnings("synthetic-access") void onSelection() {
+        final int i = getList().getSelectionIndex();
+        if (i < 0)
+          return;
+        onAble.accept(elements_list.get(i).getValue());
+        if (isAble.apply(elements_list.get(i).getValue()).booleanValue()) {
+          ableButton.setText("Disable operation");
+          configureButton.setEnabled(true);
+        } else {
+          ableButton.setText("Enable operation");
+          configureButton.setEnabled(false);
+        }
+      }
+    });
+    
+    configureButton.setVisible(true);
+    configureButton.addSelectionListener(new SelectionListener() {
+      @Override public void widgetSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        onSelection();
+      }
+      @Override public void widgetDefaultSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        onSelection();
+      }
+      @SuppressWarnings("synthetic-access") void onSelection() {
+        final int i = getList().getSelectionIndex();
+        if (i >= 0)
+          onConfigure.accept(elements_list.get(i).getValue()); // perform the on
+                                                               // configure on
+                                                               // widget op
+      }
+    });
+    parent.addDisposeListener(λ -> {
+      configureButton = null;
+      ableButton = null;
+    });
+    getList().addSelectionListener(new SelectionListener() {
+      @Override @SuppressWarnings("synthetic-access") public void widgetSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        final int i = getList().getSelectionIndex();
+        if (i >= 0)
+          if (isAble.apply(elements_list.get(i).getValue()).booleanValue()) {
+            ableButton.setText("Disable operations");
+            configureButton.setEnabled(true);
+          } else {
+            ableButton.setText("Enable operations");
+            configureButton.setEnabled(false);
+          }
+      }
+      @Override public void widgetDefaultSelected(@SuppressWarnings("unused") final SelectionEvent __) {
+        //
+      }
+    });
+    return this;
   }
   @Override protected void doFillIntoGrid(final Composite parent, final int numColumns) {
     super.doFillIntoGrid(parent, numColumns);
