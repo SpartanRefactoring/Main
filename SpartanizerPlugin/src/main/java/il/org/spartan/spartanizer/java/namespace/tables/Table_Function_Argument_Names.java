62ed179ecc2ec895109405d29693845c3a2bbd21
dormaayn <dor.d.ma@gmail.com>
2017-05-24 00:22:37 +0300

    Dor Thesis: more work

--- SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/Table_Function_Argument_Names.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/Table_Function_Argument_Names.java
@@ -7,6 +7,7 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.cmdline.*;
 import il.org.spartan.spartanizer.engine.nominal.*;
+import il.org.spartan.spartanizer.utils.tdd.*;
 import il.org.spartan.tables.*;
 
 /** Generates a table of funtion arguments
@@ -33,8 +34,12 @@ public class Table_Function_Argument_Names extends NominalTables {
         initializeWriter();
         namePrevelance.entrySet().stream().sorted(Map.Entry.<String, Integer> comparingByValue().reversed())
             .forEachOrdered(λ -> table.col("Name", λ.getKey().split("--")[0]).col("Type", λ.getKey().split("--")[1])
-                .col("Suggestion", abbreviate.it(λ.getKey().split("--")[1]))
-                .col("Strategy", λ.getKey().split("--")[0].toLowerCase().equals(λ.getKey().split("--")[1].toLowerCase()) ? "Type" : "Unkown")
+                .col("#Args", λ.getKey().split("--")[2]).col("RetType", λ.getKey().split("--")[3]).col("Throws", λ.getKey().split("--")[4])
+                .col("Ifs", λ.getKey().split("--")[5]).col("Loops", λ.getKey().split("--")[6])
+                // .col("Suggestion", abbreviate.it(λ.getKey().split("--")[1]))
+                // .col("Strategy",
+                // λ.getKey().split("--")[0].toLowerCase().equals(λ.getKey().split("--")[1].toLowerCase())
+                // ? "Type" : "Unkown")
                 .col("Prev", λ.getValue()).nl());
       }
       void initializeWriter() {
@@ -47,8 +52,11 @@ public class Table_Function_Argument_Names extends NominalTables {
           @Override @SuppressWarnings({ "boxing", "unchecked" }) public boolean visit(final MethodDeclaration x) {
             x.parameters().stream().forEach(p -> {
               final String n1 = az.singleVariableDeclaration(az.astNode(p)).getName() + "",
-                  n2 = az.singleVariableDeclaration(az.astNode(p)).getType() + "";
-              namePrevelance.put(n1 + "--" + n2, !namePrevelance.containsKey(n1 + "--" + n2) ? 1 : namePrevelance.get(n1 + "--" + n2) + 1);
+                  n2 = az.singleVariableDeclaration(az.astNode(p)).getType() + "", n3 = x.parameters().size() + "", n4 = x.getReturnType2() + "",
+                  n5 = x.thrownExceptionTypes().isEmpty() + "", n6 = (enumerate.ifStatements(x) != 0) + "", n7 = (enumerate.loops(x) != 0) + "";
+              namePrevelance.put(n1 + "--" + n2 + "--" + n3 + "--" + n4 + "--" + n5 + "--" + n6 + "--" + n7,
+                  !namePrevelance.containsKey(n1 + "--" + n2 + "--" + n3 + "--" + n4 + "--" + n5 + "--" + n6 + "--" + n7) ? 1
+                      : namePrevelance.get(n1 + "--" + n2 + "--" + n3 + "--" + n4 + "--" + n5 + "--" + n6 + "--" + n7) + 1);
             });
             return true;
           }
