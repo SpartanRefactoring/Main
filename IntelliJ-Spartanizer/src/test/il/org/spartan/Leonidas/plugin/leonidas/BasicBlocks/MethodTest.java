15477be509d9deac521116e2c229275d2d6b90bf
Sharon <sharonkl@campus.technion.ac.il>
2017-05-23 21:50:44 +0300

    Change method basic block definition in leonidas files
    Instead of defining the method basic block using a method (such as
    method(0)), the method basic block is now defined as a method inside
    leonidas files:
    
    void method0() {
    
    }
    
    defines a method basic block with id 0.

--- IntelliJ-Spartanizer/src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/MethodTest.java
+++ IntelliJ-Spartanizer/src/test/il/org/spartan/Leonidas/plugin/leonidas/BasicBlocks/MethodTest.java
@@ -2,6 +2,8 @@ package il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks;
 
 import il.org.spartan.Leonidas.PsiTypeHelper;
 
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.Encapsulator.buildTreeFromPsi;
+
 /**
  * @author Sharon
  * @since 13.5.17
@@ -15,33 +17,47 @@ public class MethodTest extends PsiTypeHelper {
         method = new Method();
     }
 
+    public void testConformsMethods() {
+        assertTrue(method.conforms(createTestMethodFromString("void method0(){}")));
+        assertTrue(method.conforms(createTestMethodFromString("void method192(){}")));
+        assertTrue(method.conforms(createTestMethodFromString("public static int method192(){return 0;}")));
+        assertFalse(method.conforms(createTestMethodFromString("void iDoNotStartWithMethod(){}")));
+        assertFalse(method.conforms(createTestMethodFromString("void _method321(){}")));
+    }
+
+    public void testCorrectlyExtractId() {
+        assertEquals(method.extractId(createTestMethodFromString("void method0(){}")), new Integer(0));
+        assertEquals(method.extractId(createTestMethodFromString("void method22(){}")), new Integer(22));
+        assertEquals(method.extractId(createTestMethodFromString("public static int method55(){return 0;}")), new Integer(55));
+    }
+
     public void testGeneralizesMethods() {
-        assertTrue(method.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m() {}"))));
-        assertTrue(method.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m(int i) {}"))));
-        assertTrue(method.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m(String s) {}"))));
-        assertTrue(method.generalizes(Encapsulator.buildTreeFromPsi(createTestMethodFromString("public int m() {return 123}"))));
+        assertTrue(method.generalizes(buildTreeFromPsi(createTestMethodFromString("public void m() {}"))));
+        assertTrue(method.generalizes(buildTreeFromPsi(createTestMethodFromString("public void m(int i) {}"))));
+        assertTrue(method.generalizes(buildTreeFromPsi(createTestMethodFromString("public void m(String s) {}"))));
+        assertTrue(method.generalizes(buildTreeFromPsi(createTestMethodFromString("public int m() {return 123}"))));
     }
 
     public void testDoesNotGeneralizeStatements() {
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x++;"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("x = y + z;"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("m();"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestStatementFromString("return 's';"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestStatementFromString("x++;"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestStatementFromString("x = y + z;"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestStatementFromString("m();"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestStatementFromString("return 's';"))));
     }
 
     public void testDoesNotGeneralizeExpressions() {
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x + y"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("true"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("x++"))));
-        assertFalse(method.generalizes(Encapsulator.buildTreeFromPsi(createTestExpression("a()"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestExpression("x + y"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestExpression("true"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestExpression("x++"))));
+        assertFalse(method.generalizes(buildTreeFromPsi(createTestExpression("a()"))));
     }
 
 
     public void testGoUpwards() {
-        Encapsulator e1 = Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m() {}"));
-        Encapsulator e2 = Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m(int i) {}"));
-        Encapsulator e3 = Encapsulator.buildTreeFromPsi(createTestMethodFromString("public void m(String s) {}"));
-        Encapsulator e4 = Encapsulator.buildTreeFromPsi(createTestMethodFromString("public int m() {return 123}"));
+        Encapsulator e1 = buildTreeFromPsi(createTestMethodFromString("public void m() {}"));
+        Encapsulator e2 = buildTreeFromPsi(createTestMethodFromString("public void m(int i) {}"));
+        Encapsulator e3 = buildTreeFromPsi(createTestMethodFromString("public void m(String s) {}"));
+        Encapsulator e4 = buildTreeFromPsi(createTestMethodFromString("public int m() {return 123}"));
         assertFalse(method.goUpwards(e1, e1.getParent()));
         assertFalse(method.goUpwards(e2, e2.getParent()));
         assertFalse(method.goUpwards(e3, e3.getParent()));
@@ -49,6 +65,6 @@ public class MethodTest extends PsiTypeHelper {
     }
 
     public void testCreate() {
-        assertEquals(method.create(Encapsulator.buildTreeFromPsi(createTestMethodCallExpression("method", "0"))).getText(), "method(0)");
+        assertEquals(method.create(buildTreeFromPsi(createTestMethodCallExpression("method", "0"))).getText(), "method(0)");
     }
 }
