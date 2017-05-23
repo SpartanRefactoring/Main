d7b8d8c537234fec3a4d37b878781f78c3582a88
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-05-23 20:14:41 +0300

    working on encoding/decoding #1321 with @nivShalmon

--- SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/WidgetPreferences.java
+++ SpartanizerPlugin/src/main/java/il/org/spartan/plugin/preferences/revision/WidgetPreferences.java
@@ -16,6 +16,7 @@ import il.org.spartan.spartanizer.plugin.widget.operations.*;
 public enum WidgetPreferences {
   ;
   
+  @SuppressWarnings("unused")
   private static final WidgetOperation[] defaultOrder = { new GitPullOperation()//
       , new GitPushOperation()//
       , new GitCommitOperation()//
@@ -35,11 +36,27 @@ public enum WidgetPreferences {
     return store().getInt(PreferencesResources.WIDGET_SIZE);
   }
   public static void storeEntries(final List<WidgetOperationEntry> es){
-    //TODO: serialize and store here
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    try {
+      new ObjectOutputStream(out).writeObject(es);
+    } catch (final IOException x) {
+      note.bug(x);
+    }
+    store().setValue(PreferencesResources.WIDGET_OPERATION_CONFIGURATION, Base64.getEncoder().encodeToString(out.toByteArray()));
   }
+  @SuppressWarnings("unchecked")
   public static List<WidgetOperationEntry> readEntries(){
-    //TODO: read and deserialize here
-    return null;
+    String forString = store().getString(PreferencesResources.WIDGET_OPERATION_CONFIGURATION);
+    byte[] theOutBarr = Base64.getDecoder().decode(forString);
+    final ByteArrayInputStream in = new ByteArrayInputStream(Base64.getDecoder().decode(Base64.getEncoder().encode(theOutBarr)));
+    
+    List<WidgetOperationEntry> res = null;
+    try {
+      res = (List<WidgetOperationEntry>)new ObjectInputStream(in).readObject();
+    } catch (@SuppressWarnings("unused") final IOException | ClassNotFoundException x) {
+      note.bug();
+    }
+    return res;
   }
   public static void setDefaults() {
     store().setDefault(PreferencesResources.WIDGET_SIZE,defaultWidgetSize);
