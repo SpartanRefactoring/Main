b4230e5ebc4591460ae556675d5d346e2ef12dbc
RoeyMaor <maor.roey@gmail.com>
2017-05-24 07:59:44 +0300

    remove from if examples

--- IntelliJ-Spartanizer/src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/RemoveCurlyBracesFromIfStatement.java
+++ IntelliJ-Spartanizer/src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/RemoveCurlyBracesFromIfStatement.java
@@ -1,5 +1,8 @@
 package il.org.spartan.Leonidas.plugin.tippers.leonidas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.booleanExpression;
 import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.statement;
 
@@ -30,5 +33,12 @@ public class RemoveCurlyBracesFromIfStatement implements LeonidasTipperDefinitio
             if (booleanExpression(0))
                 statement(1);
         });
+    }
+
+    @Override
+    public Map<String,String> getExamples(){
+        Map<String,String> examples = new HashMap<>();
+        examples.put("int x=5; Object a,b; if(a.hashCode()!=x){x = b.hashCode();}","int x=5; Object a,b; if(a.hashCode()!=x) x = b.hashCode();");
+        return examples;
     }
 }
