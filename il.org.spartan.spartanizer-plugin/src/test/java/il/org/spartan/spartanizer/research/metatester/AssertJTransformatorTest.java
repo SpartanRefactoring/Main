e1d812809154ecc73c6f37febebcdf4255ab12f3
orenafek <oren.afek@gmail.com>
2017-05-29 17:49:43 +0300

    New Tipper done :) #1429

--- il.org.spartan.spartanizer-plugin/src/test/java/il/org/spartan/spartanizer/research/metatester/AssertJTransformatorTest.java
+++ il.org.spartan.spartanizer-plugin/src/test/java/il/org/spartan/spartanizer/research/metatester/AssertJTransformatorTest.java
@@ -1,21 +1,31 @@
 package il.org.spartan.spartanizer.research.metatester;
 
-import static il.org.spartan.spartanizer.research.metatester.AssertJTransformator.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.*;
+import static il.org.spartan.spartanizer.research.metatester.TestTransformator.transformIfPossible;
+import static org.junit.Assert.assertEquals;
 
-/** @author Oren Afek
- * @since 5/19/2017. */
+/**
+ * @author Oren Afek
+ * @since 5/19/2017.
+ */
 @SuppressWarnings("static-method")
 public class AssertJTransformatorTest {
-  @Test public void testTransformations() {
-    assertEquals("assertThat(listy).isEmpty();", transformIfPossible("assertEquals(0,listy.size());"));
-    assertEquals("assertThat(listy).hasSize(9);", transformIfPossible("assertEquals(9,listy.size());"));
-    assertEquals("assertThat(1 + 1).isEqualTo(2);", transformIfPossible("assertEquals(2,1 + 1);"));
-    assertEquals("assertThat(listy).isNull();", transformIfPossible("assertNull(listy);"));
-    assertEquals("assertThat(listy).isNotNull();", transformIfPossible("assertNotNull(listy);"));
-    assertEquals("assertThat(1 >= 1).isTrue();", transformIfPossible("assertTrue(1 >= 1);"));
-    assertEquals("assertThat(0 < 0).isFalse();", transformIfPossible("assertFalse(0 < 0);"));
-  }
+
+    @Test
+    public void testBasicAssertTransformations() {
+        assertEquals("assertThat(0 < 0).isTrue();", transformIfPossible("assert 0 < 0;"));
+        assertEquals("assertThat(0 != 0).isFalse();", transformIfPossible("assert !(0 != 0);"));
+    }
+
+    @Test
+    public void testTransformations() {
+        assertEquals("assertThat(listy).isEmpty();", transformIfPossible("assertEquals(0,listy.size());"));
+        assertEquals("assertThat(listy).hasSize(9);", transformIfPossible("assertEquals(9,listy.size());"));
+        assertEquals("assertThat(1 + 1).isEqualTo(2);", transformIfPossible("assertEquals(2,1 + 1);"));
+        assertEquals("assertThat(listy).isNull();", transformIfPossible("assertNull(listy);"));
+        assertEquals("assertThat(listy).isNotNull();", transformIfPossible("assertNotNull(listy);"));
+        assertEquals("assertThat(1 >= 1).isTrue();", transformIfPossible("assertTrue(1 >= 1);"));
+        assertEquals("assertThat(0 < 0).isFalse();", transformIfPossible("assertFalse(0 < 0);"));
+    }
 }
