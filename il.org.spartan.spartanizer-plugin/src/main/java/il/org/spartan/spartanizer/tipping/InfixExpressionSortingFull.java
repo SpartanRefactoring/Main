7b929f06d31d642d0a796cf5fb5553358d9afc39
Niv Shalmon <shalmon.niv@gmail.com>
2017-06-12 14:01:43 +0300

    #1449

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/tipping/InfixExpressionSortingFull.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/tipping/InfixExpressionSortingFull.java
@@ -11,6 +11,7 @@ import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
 
 public abstract class InfixExpressionSortingFull extends InfixExpressionSorting {
   private static final long serialVersionUID = 0x16A058F976BA7D8EL;
@@ -19,7 +20,7 @@ public abstract class InfixExpressionSortingFull extends InfixExpressionSorting 
     if (!suitable(¢))
       return false;
     final List<Expression> $ = extract.allOperands(¢);
-    return !misc.mixedLiteralKind($) && sort($);
+    return !misc.mixedLiteralKind($) && iz.incrementDecrementFree($) && sort($);
   }
   @Override public Expression replacement(final InfixExpression ¢) {
     final List<Expression> $ = extract.allOperands(¢);
