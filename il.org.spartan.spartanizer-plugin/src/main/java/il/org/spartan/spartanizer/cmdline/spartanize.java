9d6c88dab814eea934ed6606c50362e19f280a20
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-06-26 13:56:26 +0300

    sync

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/spartanize.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/spartanize.java
@@ -11,12 +11,8 @@ import il.org.spartan.external.*;
  * @author Matteo Orru'
  * @since 2017-06-25 */
 public class spartanize {
-  
   @External(alias = "i", value = "input folder") @SuppressWarnings("CanBeFinal") protected static String inputFolder = system.isWindows() ? "" : ".";
   @External(alias = "o", value = "output folder") @SuppressWarnings("CanBeFinal") protected static String outputFolder = "/tmp";
-  
-  public static void main(final String[] args) {
-    
-    
-  }
+
+  public static void main(final String[] args) {}
 }
