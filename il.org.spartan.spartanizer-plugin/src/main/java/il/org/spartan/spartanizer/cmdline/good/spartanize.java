ceb34906b08a0cfda29be9fda589670f69257449
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-06-27 18:05:49 +0300

    use BatchSpartanizer

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/good/spartanize.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/good/spartanize.java
@@ -1,7 +1,9 @@
-package il.org.spartan.spartanizer.cmdline;
+package il.org.spartan.spartanizer.cmdline.good;
 
 import fluent.ly.*;
 import il.org.spartan.external.*;
+import il.org.spartan.spartanizer.research.analyses.*;
+import il.org.spartan.spartanizer.cmdline.good.*;
 
 /** This is a command line program, which can be thought of as a spartan
  * compiler. For each {@code .java} file it find, it encounters, it creates a
@@ -14,5 +16,17 @@ public class spartanize {
   @External(alias = "i", value = "input folder") @SuppressWarnings("CanBeFinal") protected static String inputFolder = system.isWindows() ? "" : ".";
   @External(alias = "o", value = "output folder") @SuppressWarnings("CanBeFinal") protected static String outputFolder = "/tmp";
 
-  public static void main(final String[] args) {}
+  private static final Nanonizer nanonizer = new Nanonizer();
+  private static final InteractiveSpartanizer is = new InteractiveSpartanizer();
+  
+  public static void main(final String[] args) {
+    run(args);
+  }
+
+  private static void run(final String[] args) {
+    if (args.length != 0)
+      as.list(args).forEach(λ -> new BatchSpartanizer(λ).fire());
+    else
+      new BatchSpartanizer(".", "current-working-directory").fire();
+  }
 }
