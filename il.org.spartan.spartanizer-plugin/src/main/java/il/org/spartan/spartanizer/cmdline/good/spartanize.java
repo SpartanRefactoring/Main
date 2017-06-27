0fa00f68fd94399fbb6a9b0823073c17d39d9ca1
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-06-27 20:38:02 +0300

    merge

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/good/spartanize.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/good/spartanize.java
@@ -1,5 +1,7 @@
 package il.org.spartan.spartanizer.cmdline.good;
 
+import org.junit.*;
+
 import fluent.ly.*;
 import il.org.spartan.external.*;
 
@@ -7,25 +9,22 @@ import il.org.spartan.external.*;
  * compiler. For each {@code .java} file it find, it encounters, it creates a
  * corresponding {@code .javas} file, which is the spartanized version of the
  * original. The {@code .javas} will be in the same folder as the {@code .java}
- * file, and would be overwritten each time this program is done.
+ * file, and would be overwritten each time this program is run.
  * @author Matteo Orru'
  * @since 2017-06-25 */
 public class spartanize {
-  @External(alias = "i", value = "input folder") @SuppressWarnings("CanBeFinal") protected static String inputDir = system.isWindows() ? "" : ".";
-  @External(alias = "o", value = "output folder") @SuppressWarnings("CanBeFinal") protected static String outputDir = "/tmp";
-  @External(alias = "t", value = "sp_type") @SuppressWarnings("CanBeFinal") protected static String spartanization_type = "batch";
+  @External(alias = "i", value = "input folder") @SuppressWarnings("CanBeFinal") protected static String inputFolder = system.isWindows() ? "" : ".";
+  @External(alias = "o", value = "output folder") @SuppressWarnings("CanBeFinal") protected static String outputFolder = "/tmp";
+  protected static final String[] defaultArguments = as.array("..");
 
-  private static final InteractiveSpartanizer is = new InteractiveSpartanizer();
-  
-  public static void main(final String[] args) {
-    if(spartanization_type.equals("batch"))
-      batchSpartanize(args);
+  public static void main(final String[] args) throws SecurityException, IllegalArgumentException {
+    visit(args.length != 0 ? args : defaultArguments);
   }
-
-  private static void batchSpartanize(final String[] args) {
-    if (args.length != 0)
-      as.list(args).forEach(λ -> new BatchSpartanizer(λ).fire());
-    else
-      new BatchSpartanizer(".", "current-working-directory").fire();
+  public static void visit(final String... arguments) {
+    for (final String ¢ : External.Introspector.extract(arguments, system.myCallerFullClassName()))
+      matteo(¢);
+  }
+  private static void matteo(String ¢) {
+    forget.it(¢);
   }
 }
