90930a43f0170661e09deb13cf15e006a9baa965
Yossi Gil <yogi@cs.technion.ac.il>
2017-06-27 20:03:15 +0300

    missi

---
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/ast/safety/MethodProperty.java
@@ -0,0 +1,37 @@
+package il.org.spartan.spartanizer.ast.safety;
+
+import static org.eclipse.jdt.core.dom.ASTNode.*;
+
+import static il.org.spartan.spartanizer.ast.navigate.step.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import fluent.ly.*;
+
+/** TODO Yossi Gil: document class
+ * @author Yossi Gil
+ * @since 2017-06-27 */
+public interface MethodProperty {
+  static boolean callingOtherMethods(final Stream<ASTNode> ¢) {
+    return ¢.noneMatch(MethodProperty::callingOtherMethods);
+  }
+  static boolean callingOtherMethods(final Block ¢) {
+    return callingOtherMethods(statements(¢));
+  }
+  static boolean callingOtherMethods(final List<Statement> statements) {
+    return callingOtherMethods(statements.stream().map(x -> (ASTNode) x));
+  }
+  static boolean callingOtherMethods(final MethodDeclaration ¢) {
+    return callingOtherMethods(body(¢));
+  }
+  static boolean callingOtherMethods(final ASTNode ¢) {
+    return iz.nodeTypeIn(¢, ARRAY_CREATION, METHOD_INVOCATION, CLASS_INSTANCE_CREATION, CONSTRUCTOR_INVOCATION, ANONYMOUS_CLASS_DECLARATION,
+        SUPER_CONSTRUCTOR_INVOCATION, SUPER_METHOD_INVOCATION, LAMBDA_EXPRESSION);
+  }
+  static boolean letItBeIn(final List<Statement> ¢) {
+    return ¢.size() == 2 && the.firstOf(¢) instanceof VariableDeclarationStatement;
+  }
+}
