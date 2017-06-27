63fd9f3d92e86e1a1975c0fa6c2e1676078c4f75
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-06-27 19:22:22 +0300

    spartanize

---
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/cmdline/good/spartanize.java
@@ -0,0 +1,31 @@
+package il.org.spartan.spartanizer.cmdline.good;
+
+import fluent.ly.*;
+import il.org.spartan.external.*;
+
+/** This is a command line program, which can be thought of as a spartan
+ * compiler. For each {@code .java} file it find, it encounters, it creates a
+ * corresponding {@code .javas} file, which is the spartanized version of the
+ * original. The {@code .javas} will be in the same folder as the {@code .java}
+ * file, and would be overwritten each time this program is done.
+ * @author Matteo Orru'
+ * @since 2017-06-25 */
+public class spartanize {
+  @External(alias = "i", value = "input folder") @SuppressWarnings("CanBeFinal") protected static String inputDir = system.isWindows() ? "" : ".";
+  @External(alias = "o", value = "output folder") @SuppressWarnings("CanBeFinal") protected static String outputDir = "/tmp";
+  @External(alias = "t", value = "sp_type") @SuppressWarnings("CanBeFinal") protected static String spartanization_type = "batch";
+
+  private static final InteractiveSpartanizer is = new InteractiveSpartanizer();
+  
+  public static void main(final String[] args) {
+    if(spartanization_type.equals("batch"))
+      batchSpartanize(args);
+  }
+
+  private static void batchSpartanize(final String[] args) {
+    if (args.length != 0)
+      as.list(args).forEach(λ -> new BatchSpartanizer(λ).fire());
+    else
+      new BatchSpartanizer(".", "current-working-directory").fire();
+  }
+}
