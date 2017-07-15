f8d4477032ffae3a973caa428185661ba1b50ed3
Yossi Gil <yogi@cs.technion.ac.il>
2017-07-15 15:57:17 +0300

    architecture

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/ast/navigate/step.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/ast/navigate/step.java
@@ -766,15 +766,4 @@ public enum step {
   @SuppressWarnings("unchecked") public static List<MemberValuePair> values(final NormalAnnotation ¢) {
     return ¢ == null ? null : ¢.values();
   }
-  static MethodInvocation getMethodInvocation(final CompilationUnit u, final int lineNumber, final MethodInvocation i) {
-    final Wrapper<MethodInvocation> $ = new Wrapper<>();
-    u.accept(new ASTVisitor(true) {
-      @Override public boolean visit(final MethodInvocation ¢) {
-        if (u.getLineNumber(¢.getStartPosition()) == lineNumber)
-          $.set(¢);
-        return super.visit(¢);
-      }
-    });
-    return $.get() == null ? i : $.get();
-  }
 }
