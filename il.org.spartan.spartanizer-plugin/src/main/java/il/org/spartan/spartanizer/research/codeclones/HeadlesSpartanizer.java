58097af7f1026cd6612af04999d45066ffed894c
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-07-17 17:28:42 +0300

    sync

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/research/codeclones/HeadlesSpartanizer.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/research/codeclones/HeadlesSpartanizer.java
@@ -1,6 +1,5 @@
 package il.org.spartan.spartanizer.research.codeclones;
 
-import java.awt.*;
 import java.io.*;
 
 import org.eclipse.jdt.core.dom.*;
@@ -21,6 +20,7 @@ public class HeadlesSpartanizer extends GrandVisitor {
   
   public static void main(final String[] args){
     hs = new HeadlesSpartanizer(args);
+    System.err.println("hs.current.location:\t" + hs.current.location);
     hs.go(hs.current.locations.get(0));
   }
   
@@ -29,7 +29,6 @@ public class HeadlesSpartanizer extends GrandVisitor {
   }
   
   public HeadlesSpartanizer() {
-    super();
   }
 
   public final TextualTraversals traversals = new TextualTraversals();
@@ -54,16 +53,15 @@ public class HeadlesSpartanizer extends GrandVisitor {
   
   public final void go(final String dirPath) {
     setUp();
-    (new GrandVisitor(new String[] { dirPath }) {
+    (new GrandVisitor(new String[] {dirPath}) {
       @Override public void visitFile(final File f) {
+        current.fileName = f.getName();
         System.err.println(f.getAbsolutePath());
         if (!spartanize(f))
           return;
         String beforeChange;
         try {
           beforeChange = FileUtils.read(f);
-          //System.err.println("location:\t" + current.location);
-          //System.err.println("size:\t" + current.locations.size());
           analyze(beforeChange, perform(beforeChange));
         } catch (final IOException ¢) {
           note.io(¢);
@@ -73,7 +71,9 @@ public class HeadlesSpartanizer extends GrandVisitor {
       protected void analyze(@SuppressWarnings("unused") final String before, final String after) {
         try {
           System.err.println(current.location);
-          FileUtils.writeToFile(current.location, after);
+          current.location = "/tmp/";
+          System.err.println(current.location + current.fileName);
+          FileUtils.writeToFile(current.location + current.fileName , after);
         } catch (final FileNotFoundException ¢) {
           note.io(¢);
         }
