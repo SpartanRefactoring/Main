b64c8ecbbc3251e328d586bd7f51710106ad97df
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-08-13 12:03:10 +0300

    sync

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/plugin/TextualTraversals.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/plugin/TextualTraversals.java
@@ -18,33 +18,14 @@ public class TextualTraversals {
   public final Traversal traversal = new TraversalImplementation()//
       .push(new TraversalTapper() {
         @Override public void begin() {
-          //System.out.println("Entering file ...");
+          System.out.println("Entering file ...");
           TraversalTapper.super.begin();
         }
         @Override public void end() {
-          //System.out.println("... Exiting File");
+          System.out.println("... Exiting File");
           TraversalTapper.super.end();
         }
-      })//
-      .push(new TraversalTapper() {
-        @Override public void begin() {
-          TraversalTapper.super.begin();
-          // table = new Table("tippers2", "/tmp");
-        }
-        @Override public void noTipper()      {/**/}
-        @Override public void setNode()       {/**/}
-        @Override public void tipperAccepts() {/**/}
-        @Override public void tipperRejects() {/**/}
-        @Override public void tipperTip()     {
-          // table.col("","");
-        }
-        @Override public void tipPrune()      {/**/}
-        @Override public void tipRewrite()    {/**/}        
-        @Override public void end() {
-          TraversalTapper.super.end();
-        }
-      })
-      ;
+      });
 
   public String fixed(final String from) {
     int n = 0;
