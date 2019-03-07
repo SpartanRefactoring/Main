2ecf6578d4294e8f38bcc64173917f039ebc7a29
dormaayn <dor.d.ma@gmail.com>
2019-03-07 18:44:13 +0200

    Sync

--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/SmellsDetector.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/SmellsDetector.java
@@ -12,15 +12,21 @@ import il.org.spartan.spartanizer.cmdline.*;
 import il.org.spartan.tables.*;
 import il.org.spartan.utils.*;
 
-/** Filter projects which does not contain any Junit test (Dor Msc.)
+/** Detect the following test smells (Dor Msc.):
+ * 'Mystery Guest': A test that uses external resources (e.g., file containing test data)
+ * 'Resource Optimism': A test that makes optimistic assumptions about the state/existence of external resources
+ * ‘Eager Test’: A test method exercising more methods of the tested object
+ * ‘Assertion Roulette’: A test that contains several assertions with no explanation
+ * ‘Indirect Testing’: A test that interacts with the object under test indirectly via another object
+ *  ‘Sensitive Equality’: A test using the ‘toString’ method directly in assert statements
  * @author Dor Ma'ayan
- * @since 2019-01-10 */
-public class AllTestClasses extends TestTables {
+ * @since 2019-02-16 */
+public class SmellsDetector extends TestTables {
   @SuppressWarnings({ "boxing", "unused" }) public static void main(final String[] args) throws Exception, UnsupportedEncodingException {
     final HashMap<String, List<String>> map = new HashMap<>();
     map.put("TestClassName", new ArrayList<>());
     map.put("TestClass", new ArrayList<>());
-    final String initialPath = "/Users/Dor/Desktop/TestingTables/TestClassesDataBase/";
+    final HashMap<String, List<String>> mapMethods = new HashMap<>();
     new GrandVisitor(args) {
       {
         listen(new Tapper() {
@@ -33,6 +39,7 @@ public class AllTestClasses extends TestTables {
       void reset() {
         map.put("TestClassName", new ArrayList<>());
         map.put("TestClass", new ArrayList<>());
+        mapMethods.clear();
       }
       protected void done(final String path) {
         summarize(path);
@@ -43,20 +50,13 @@ public class AllTestClasses extends TestTables {
         if (!map.get("TestClassName").isEmpty()) {
           for (int i = 0; i < map.get("TestClassName").size(); i++) {
             String className = map.get("TestClassName").get(i);
-            String classCode = map.get("TestClass").get(i);
-            table.col("Project", path).col("TestClassName", className) //
-                // .col("TestClass", map.get("TestClass").get(i)) //
-                .nl();
-            try {
-              File file = new File(initialPath + path + "/" + className + ".txt");
-              file.getParentFile().mkdirs();
-              FileWriter writer = new FileWriter(file, false);
-              PrintWriter output = new PrintWriter(writer);
-              output.print(classCode);
-              output.close();
-            } catch (Exception x) {
-              // TODO Auto-generated catch block
-              x.printStackTrace();
+            if (mapMethods.containsKey(className) && !mapMethods.get(className).isEmpty()) {
+              for (int j = 0; j < mapMethods.get(className).size(); j++) {
+                String methodName = mapMethods.get(className).get(j);
+                table.col("Project", path).col("TestClassName", className) //
+                    .col("MethodName", methodName)//
+                    .nl();
+              }
             }
           }
         }
@@ -67,20 +67,31 @@ public class AllTestClasses extends TestTables {
       }
     }.visitAll(new ASTVisitor(true) {
       @Override public boolean visit(final CompilationUnit ¢) {
-        System.out.println(¢.getTypeRoot().toString());
         ¢.accept(new ASTVisitor() {
           @Override public boolean visit(final TypeDeclaration x) {
             if (isJunit4(¢) || isJunit5(¢)) {
               List<String> a = map.get("TestClassName");
-              if (¢.getJavaElement() != null && ¢.getJavaElement().getPath() != null)
-                System.out.println("Hi");
-              // System.out.println(¢.getJavaElement().getPath().toString());
               a.add(extract.name(x));
               map.put("TestClassName", a);
               List<String> b = map.get("TestClass");
               b.add(x.toString());
               map.put("TestClass", b);
             }
+            x.accept(new ASTVisitor(true) {
+              @Override public boolean visit(final MethodDeclaration m) {
+                List<String> annotations = extract.annotations(m).stream().map(a -> a.getTypeName().getFullyQualifiedName())
+                    .collect(Collectors.toList());
+                if (annotations.contains("Test")
+                    || (iz.typeDeclaration(m.getParent()) && az.typeDeclaration(m.getParent()).getSuperclassType() != null
+                        && az.typeDeclaration(m.getParent()).getSuperclassType().toString().equals("TestCase"))) {
+                  if (!mapMethods.containsKey(extract.name(x))) {
+                    mapMethods.put(extract.name(x), new ArrayList<>());
+                  }
+                  mapMethods.get(extract.name(x)).add(extract.name(m));
+                }
+                return true;
+              }
+            });
             return true;
           }
         });
--- il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/TestingmMiner.java
+++ il.org.spartan.spartanizer-plugin/src/main/java/il/org/spartan/spartanizer/java/namespace/tables/TestingmMiner.java
@@ -12,71 +12,19 @@ import il.org.spartan.spartanizer.cmdline.*;
 import il.org.spartan.tables.*;
 import il.org.spartan.utils.*;
 
-/** Test Mining for Msc.thesis
+/** Filter projects which does not contain any Junit test (Dor Msc.)
  * @author Dor Ma'ayan
- * @since 2019-01-08 */
-public class TestingmMiner extends NominalTables {
-  static boolean isJunitAnnotation(List<String> annotations) {
-    String[] anno = { "After", "AfterClass", "Before", "BeforeClass", "BeforeEach", "BeforeAll", "AfterEeash", "AfterAll" };
-    List<String> annoList = Arrays.asList(anno);
-    for (String s : annotations) {
-      if (annoList.contains(s))
-        return true;
-    }
-    return false;
-  }
-  static boolean isIgnoredTest(List<String> annotations) {
-    String[] anno = { "Ignore" };
-    List<String> annoList = Arrays.asList(anno);
-    for (String s : annotations) {
-      if (annoList.contains(s))
-        return true;
-    }
-    return false;
-  }
-  static boolean isJunit4(CompilationUnit c) {
-    List<ImportDeclaration> imports = extract.imports(c);
-    for (ImportDeclaration i : imports) {
-      if (i.getName().getFullyQualifiedName().contains("org.junit") && !isJunit5(c)) {
-        return true;
-      }
-    }
-    return false;
-  }
-  static boolean isJunit5(CompilationUnit c) {
-    List<ImportDeclaration> imports = extract.imports(c);
-    for (ImportDeclaration i : imports) {
-      if (i.getName().getFullyQualifiedName().contains("org.junit.jupiter")) {
-        return true;
-      }
-    }
-    return false;
-  }
-  @SuppressWarnings({ "boxing", "unused" }) public static void main(final String[] args) throws Exception, UnsupportedEncodingException {
-    final HashMap<String, Integer> map = new HashMap<>();
-    map.put("#Tests", 0);
-    map.put("#Junit4", 0);
-    map.put("#Junit5", 0);
-    map.put("#BeforeAfterAnnotations", 0);
-    map.put("#IgnoredTests", 0);
-    map.put("#JavaAsserts", 0);
-    map.put("#JunitAsserts", 0);
-    map.put("#HamcrestAsserts", 0);
-    map.put("#IfStatements", 0);
-    map.put("#ReturnStatements", 0);
-    map.put("#0-Asseerts", 0);
-    map.put("#1-Asseerts", 0);
-    map.put("#2-Asseerts", 0);
-    map.put("#3-Asseerts", 0);
-    map.put("#4-Asseerts", 0);
-    map.put("#5-Asseerts", 0);
-    map.put("#6+-Asseerts", 0);
-    map.put("#Files", 0);
-    map.put("UsingMockito?", 0);
-    map.put("#TestLoops", 0);
-    map.put("#TryCatch", 0);
-    map.put("#LinearTests", 0);
-    PrintWriter writer = new PrintWriter("/Users/Dor/Desktop/TestingTables/raw.txt", "UTF-8");
+ * @since 2019-03-07 */
+public class TestingmMiner extends TestTables {
+  @SuppressWarnings({ "unused" }) public static void main(final String[] args) throws Exception, UnsupportedEncodingException {
+    final Set<String> testMethods = new HashSet<>();
+    // Maps from method names to counters
+    final HashMap<String, Integer> assertionMapCounter = new HashMap<>();
+    final HashMap<String, Integer> ifElseMapCounter = new HashMap<>();
+    final HashMap<String, Integer> loopMapCounter = new HashMap<>();
+    final HashMap<String, Integer> tryCatchMapCounter = new HashMap<>();
+    final HashMap<String, Integer> printMapCounter = new HashMap<>();
+    final HashMap<String, Integer> throwsExceptionMapCounter = new HashMap<>();
     new GrandVisitor(args) {
       {
         listen(new Tapper() {
@@ -87,28 +35,10 @@ public class TestingmMiner extends NominalTables {
       }
 
       void reset() {
-        map.put("#Tests", 0);
-        map.put("#Junit4", 0);
-        map.put("#Junit5", 0);
-        map.put("#BeforeAfterAnnotations", 0);
-        map.put("#IgnoredTests", 0);
-        map.put("#JavaAsserts", 0);
-        map.put("#JunitAsserts", 0);
-        map.put("#IfStatements", 0);
-        map.put("#ReturnStatements", 0);
-        map.put("#0-Asseerts", 0);
-        map.put("#1-Asseerts", 0);
-        map.put("#2-Asseerts", 0);
-        map.put("#3-Asseerts", 0);
-        map.put("#4-Asseerts", 0);
-        map.put("#5-Asseerts", 0);
-        map.put("#6+-Asseerts", 0);
-        map.put("#Files", 0);
-        map.put("UsingMockito?", 0);
-        map.put("#HamcrestAsserts", 0);
-        map.put("#TestLoops", 0);
-        map.put("#TryCatch", 0);
-        map.put("#LinearTests", 0);
+        testMethods.clear();
+        assertionMapCounter.clear();
+        ifElseMapCounter.clear();
+        tryCatchMapCounter.clear();
       }
       protected void done(final String path) {
         summarize(path);
@@ -116,13 +46,9 @@ public class TestingmMiner extends NominalTables {
       }
       public void summarize(final String path) {
         initializeWriter();
-        if (map.get("#Tests") != 0) {
-          table.col("Project", path).col("#Files", map.get("#Files")).col("#Tests", map.get("#Tests")).col("#Junit4Classes", map.get("#Junit4"))
-              .col("#Junit5Classes", map.get("#Junit5")).col("#0-Asseerts", map.get("#0-Asseerts")).col("#1-Asseerts", map.get("#1-Asseerts"))
-              .col("#2-Asseerts", map.get("#2-Asseerts")).col("#3-Asseerts", map.get("#3-Asseerts")).col("#4-Asseerts", map.get("#4-Asseerts"))
-              .col("#5-Asseerts", map.get("#5-Asseerts")).col("#6+-Asseerts", map.get("#6+-Asseerts")).col("#BeforeAfterAnnotations", map.get("#BeforeAfterAnnotations")).nl();
-          writer.write("~~~~~~~~~~~~~~~~~~~ Random Samplings from " + path + "~~~~~~~~~~~~~~~~~~~");
-          writer.write("\n \n \n \n \n \n");
+        for (String method : testMethods) {
+          table.col("Project", path).col("MethodName", method).col("#Assrtions", assertionMapCounter.get(method))
+              .col("#Conditions", ifElseMapCounter.get(method)).col("#TryCatch", tryCatchMapCounter.get(method)).nl();
         }
       }
       void initializeWriter() {
@@ -131,97 +57,45 @@ public class TestingmMiner extends NominalTables {
       }
     }.visitAll(new ASTVisitor(true) {
       @Override public boolean visit(final CompilationUnit ¢) {
-        map.put("#Files", map.get("#Files") + 1);
-        List<ImportDeclaration> imports = extract.imports(¢);
-        for (ImportDeclaration i : imports) {
-          if (i.getName().getFullyQualifiedName().equals("org.mockito.Mockito")) {
-            if (map.get("UsingMockito?") == 0)
-              map.put("UsingMockito?", 1);
-          }
-        }
-        if (isJunit4(¢))
-          map.put("#Junit4", map.get("#Junit4") + 1);
-        if (isJunit5(¢))
-          map.put("#Junit5", map.get("#Junit5") + 1);
-        // Random Sample of test classes
-        Random rand = new Random();
-        if (rand.nextInt(50) == 25 && (isJunit4(¢) || isJunit5(¢))) {
-          writer.write("~~~~~~~Test Classs~~~~~~~\n \n");
-          writer.write(¢.toString());
-          writer.write("\n \n \n \n");
-        }
         ¢.accept(new ASTVisitor() {
-          @Override public boolean visit(final MethodDeclaration x) {
-            if (x != null) {
-              List<String> annotations = extract.annotations(x).stream().map(a -> a.getTypeName().getFullyQualifiedName())
-                  .collect(Collectors.toList());
-              if (isJunitAnnotation(annotations))
-                map.put("#BeforeAfterAnnotations", map.get("#BeforeAfterAnnotations") + 1);
-              if (isIgnoredTest(annotations))
-                map.put("#IgnoredTests", map.get("#IgnoredTests") + 1);
-              if (annotations.contains("Test") || (iz.typeDeclaration(x.getParent()) && az.typeDeclaration(x.getParent()).getSuperclassType() != null
-                  && az.typeDeclaration(x.getParent()).getSuperclassType().toString().equals("TestCase"))) {
-                final Int counter = new Int(); // asseerts counter
-                final Int irregulars = new Int(); // asseerts counter
-                map.put("#Tests", map.get("#Tests") + 1);
-                x.accept(new ASTVisitor() {
-                  /** handle regular assert */
-                  @Override public boolean visit(final AssertStatement x) {
-                    map.put("#JavaAsserts", map.get("#JavaAsserts") + 1);
-                    counter.step();
-                    return true;
+          @Override public boolean visit(final TypeDeclaration x) {
+            if (isJunit4(¢) || isJunit5(¢)) {
+              x.accept(new ASTVisitor(true) {
+                @SuppressWarnings("boxing") @Override public boolean visit(final MethodDeclaration m) {
+                  List<String> annotations = extract.annotations(m).stream().map(a -> a.getTypeName().getFullyQualifiedName())
+                      .collect(Collectors.toList());
+                  if (annotations.contains("Test")) {
+                    testMethods.add(extract.name(x) + "." + extract.name(m));
                   }
-                  /** handle JunitAssert */
-                  @Override public boolean visit(final ExpressionStatement x) {
-                    if (iz.junitAssert(x)) {
-                      map.put("#JunitAsserts", map.get("#JunitAsserts") + 1);
-                      counter.step();
+                  // Define per test method counters
+                  final Int assertionCounter = new Int();
+                  final Int ifElseCounter = new Int();
+                  final Int tryCatchCounter = new Int();
+                  // Traverse over the test method AST
+                  m.accept(new ASTVisitor() {
+                    @Override public boolean visit(final ExpressionStatement a) {
+                      if (iz.junitAssert(a) || iz.hamcrestAssert(a)) {
+                        assertionCounter.step();
+                      }
+                      return true;
                     }
-                    if (iz.hamcrestAssert(x)) {
-                      map.put("#HamcrestAsserts", map.get("#HamcrestAsserts") + 1);
+                    @Override public boolean visit(final IfStatement a) {
+                      ifElseCounter.step();
+                      return true;
                     }
-                    return true;
-                  }
-                  /** handle Returns */
-                  @Override public boolean visit(final ReturnStatement x) {
-                    map.put("#ReturnStatements", map.get("#ReturnStatements") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                  /** handle Returns */
-                  @Override public boolean visit(final IfStatement x) {
-                    map.put("#IfStatements", map.get("#IfStatements") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                  @Override public boolean visit(final ForStatement x) {
-                    map.put("#TestLoops", map.get("#TestLoops") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                  @Override public boolean visit(final WhileStatement x) {
-                    map.put("#TestLoops", map.get("#TestLoops") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                  @Override public boolean visit(final EnhancedForStatement x) {
-                    map.put("#TestLoops", map.get("#TestLoops") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                  @Override public boolean visit(final TryStatement x) {
-                    map.put("#TryCatch", map.get("#TryCatch") + 1);
-                    irregulars.step();
-                    return true;
-                  }
-                });
-                if (irregulars.get() == 0)
-                  map.put("#LinearTests", map.get("#LinearTests") + 1);
-                if (counter.inner >= 0 && counter.inner < 6)
-                  map.put("#" + counter.inner + "-Asseerts", map.get("#" + counter.inner + "-Asseerts") + 1);
-                else if (counter.inner > 5)
-                  map.put("#6+-Asseerts", map.get("#6+-Asseerts") + 1);
-              }
+                    @Override public boolean visit(final TryStatement a) {
+                      tryCatchCounter.step();
+                      return true;
+                    }
+                  });
+                  // Then, put the local counters in the global map before analyzing the next
+                  // method
+                  assertionMapCounter.put(extract.name(x) + "." + extract.name(m), assertionCounter.get());
+                  ifElseMapCounter.put(extract.name(x) + "." + extract.name(m), ifElseCounter.get());
+                  tryCatchMapCounter.put(extract.name(x) + "." + extract.name(m), tryCatchCounter.get());
+                  return true;
+                }
+              });
             }
             return true;
           }
@@ -230,7 +104,6 @@ public class TestingmMiner extends NominalTables {
       }
     });
     table.close();
-    writer.close();
     System.err.println(table.description());
   }
 }
